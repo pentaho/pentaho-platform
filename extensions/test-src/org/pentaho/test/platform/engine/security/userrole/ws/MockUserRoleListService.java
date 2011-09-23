9e1ccf74ab61771e6b5be107e888a4377776e39c
Will Gorman <wgorman@pentaho.com>
2011-09-23 01:24:10 +0000

    [BISERVER-6509] transition web into extensions

---
+++ extensions/test-src/org/pentaho/test/platform/engine/security/userrole/ws/MockUserRoleListService.java
@@ -0,0 +1,65 @@
+package org.pentaho.test.platform.engine.security.userrole.ws;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.pentaho.platform.api.engine.IUserRoleListService;
+
+public class MockUserRoleListService implements IUserRoleListService {
+
+  public List<String> getAllRoles() {
+    List<String> allAuths = new ArrayList<String>(7);
+    allAuths.add("dev"); //$NON-NLS-1$
+    allAuths.add("Admin"); //$NON-NLS-1$
+    allAuths.add("devmgr"); //$NON-NLS-1$
+    allAuths.add("ceo"); //$NON-NLS-1$
+    allAuths.add("cto"); //$NON-NLS-1$
+    allAuths.add("Authenticated"); //$NON-NLS-1$
+    allAuths.add("is"); //$NON-NLS-1$
+    return allAuths;
+  }
+
+  public List<String> getAllUsers() {
+    List<String> allUsers = new ArrayList<String>(4);
+    allUsers.add("pat"); //$NON-NLS-1$
+    allUsers.add("tiffany"); //$NON-NLS-1$
+    allUsers.add("joe"); //$NON-NLS-1$
+    allUsers.add("suzy"); //$NON-NLS-1$
+    return allUsers;
+  }
+
+  public List<String> getUsersInRole(String role) {
+    if (role.equals("dev")) { //$NON-NLS-1$
+      return Arrays.asList(new String[] { "pat", "tiffany" }); //$NON-NLS-1$ //$NON-NLS-2$
+    } else if (role.equals("Admin")) { //$NON-NLS-1$
+      return Arrays.asList(new String[] { "joe" });//$NON-NLS-1$
+    } else if (role.equals("devmgr")) { //$NON-NLS-1$
+      return Arrays.asList(new String[] { "tiffany" });//$NON-NLS-1$
+    } else if (role.equals("ceo")) { //$NON-NLS-1$
+      return Arrays.asList(new String[] { "joe" });//$NON-NLS-1$
+    } else if (role.equals("cto")) { //$NON-NLS-1$
+      return Arrays.asList(new String[] { "suzy" });//$NON-NLS-1$
+    } else if (role.equals("is")) { //$NON-NLS-1$
+      return Arrays.asList(new String[] { "suzy" });//$NON-NLS-1$
+    }
+    return Collections.emptyList();
+  }
+
+  public List<String> getRolesForUser(String username) {
+    if (username.equals("pat")) { //$NON-NLS-1$
+      return Arrays.asList(new String[] { "dev", "Authenticated" });//$NON-NLS-1$ //$NON-NLS-2$
+    } else if (username.equals("tiffany")) {//$NON-NLS-1$
+      return Arrays.asList(new String[] { "dev", "devmgr", "Authenticated" });//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    } else if (username.equals("joe")) {//$NON-NLS-1$
+      return Arrays.asList(new String[] { "Admin", "ceo", "Authenticated" });//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    } else if (username.equals("suzy")) {//$NON-NLS-1$
+      return Arrays.asList(new String[] { "cto", "is", "Authenticated" });//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    }
+    return Collections.emptyList();
+
+  }
+
+}
