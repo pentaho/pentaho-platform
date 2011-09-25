cb320561f6cb6a39948e4d09e222a2fb67cf3e23
Michael D'Aamour <mdamour@pentaho.com>
2011-09-25 01:03:18 +0000

    More unit test updates/fixes

--- core/test-src/org/pentaho/platform/engine/services/MockSolutionRepository.java
+++ core/test-src/org/pentaho/platform/engine/services/MockSolutionRepository.java
@@ -1,20 +1,13 @@
 package org.pentaho.platform.engine.services;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
 import org.dom4j.Node;
-import org.pentaho.commons.connection.IPentahoStreamSource;
 import org.pentaho.platform.api.engine.IActionSequence;
 import org.pentaho.platform.api.engine.IActionSequenceResource;
 import org.pentaho.platform.api.engine.IPentahoSession;
