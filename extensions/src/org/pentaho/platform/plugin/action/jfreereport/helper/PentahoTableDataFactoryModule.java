700af4f5acd8c8d78c1379f34dc22cdc558defd5
David Kincade <dkincade@pentaho.com>
2011-12-05 19:19:26 +0000

    A bugfix in the reporting engine (PRD-3646) now required that all DataFactories used by the reporting engine be properly registered. This caused the old xaction reports to fail. This update now registers the PentahoTableDataFactory properly with the reporting engine

---
+++ extensions/src/org/pentaho/platform/plugin/action/jfreereport/helper/PentahoTableDataFactoryModule.java
@@ -0,0 +1,48 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * Copyright 2011 Pentaho Corporation. All rights reserved.
+ */
+package org.pentaho.platform.plugin.action.jfreereport.helper;
+
+import org.pentaho.reporting.engine.classic.core.metadata.ElementMetaDataParser;
+import org.pentaho.reporting.libraries.base.boot.AbstractModule;
+import org.pentaho.reporting.libraries.base.boot.ModuleInitializeException;
+import org.pentaho.reporting.libraries.base.boot.SubSystem;
+
+/**
+ * Defines the Module for PentahoTableDataFactory
+ *
+ * @author <a href="mailto:dkincade@pentaho.com">David M. Kincade</a>
+ */
+public class PentahoTableDataFactoryModule extends AbstractModule {
+  public PentahoTableDataFactoryModule() throws ModuleInitializeException {
+    loadModuleInfo();
+  }
+
+  /**
+   * Initializes the module. Use this method to perform all initial setup operations.
+   * This method is called only once in a modules lifetime. If the initializing cannot
+   * be completed, throw a ModuleInitializeException to indicate the error. The module
+   * will not be available to the system.
+   *
+   * @param subSystem the subSystem.
+   * @throws ModuleInitializeException if an error occurred while initializing the module.
+   */
+  public void initialize(final SubSystem subSystem) throws ModuleInitializeException {
+    ElementMetaDataParser
+        .initializeOptionalDataFactoryMetaData
+            ("org/pentaho/platform/plugin/action/jfreereport/helper/meta-datafactory.xml"); //$NON-NLS-1$
+  }
+}
