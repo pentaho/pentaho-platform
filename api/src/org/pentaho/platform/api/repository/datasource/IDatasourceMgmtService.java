d5667667f81d83ffd3079a37f00f96789df2a2f9
Ramaiz Mansoor <rmansoor@pentaho.com>
2012-02-23 16:31:19 +0000

    BISERVER-6956 - Adding new methods to support ids for datasource

--- api/src/org/pentaho/platform/api/repository/datasource/IDatasourceMgmtService.java
+++ api/src/org/pentaho/platform/api/repository/datasource/IDatasourceMgmtService.java
@@ -24,25 +24,89 @@ import java.util.List;
 import org.pentaho.database.model.IDatabaseConnection;
 import org.pentaho.platform.api.engine.IPentahoInitializer;
 
+/**
+ * Service to manage JDBC datasources in the platform.
+ * is a {@link IDatabaseConnection}.
+ *
+ * 
+ */
+
 public interface IDatasourceMgmtService extends IPentahoInitializer {
   
   public static final String IDATASOURCEMGMTSERVICE = "IDatasourceMgmtService"; //$NON-NLS-1$
-  
-  public void createDatasource(IDatabaseConnection databaseConnection) throws DuplicateDatasourceException, DatasourceMgmtServiceException;
 
+  /**
+   * Creates a JDBC datasource in a given repository and return an id 
+   * @param databaseConnection
+   * @return id
+   * @throws DuplicateDatasourceException
+   * @throws DatasourceMgmtServiceException
+   */
+  public String createDatasource(IDatabaseConnection databaseConnection) throws DuplicateDatasourceException, DatasourceMgmtServiceException;
+
+  /**
+   * Permanently deletes a JDBC datasource from a repository by name 
+   * @param name
+   * @throws NonExistingDatasourceException
+   * @throws DatasourceMgmtServiceException
+   */
   public void deleteDatasourceByName(String name) throws NonExistingDatasourceException, DatasourceMgmtServiceException;
 
-  //public void deleteDatasourceById(Serializable id) throws NonExistingDatasourceException, DatasourceMgmtServiceException;
+  /**
+   * Permanently deletes a JDBC datasource from a repository by id
+   * @param id
+   * @throws NonExistingDatasourceException
+   * @throws DatasourceMgmtServiceException
+   */
+  public void deleteDatasourceById(String id) throws NonExistingDatasourceException, DatasourceMgmtServiceException;
 
+  /**
+   * Retrieves a JDBC datasource form the repository by name
+   * @param name
+   * @return IDatabaseConnection
+   * @throws DatasourceMgmtServiceException
+   */
   public IDatabaseConnection getDatasourceByName(String name) throws DatasourceMgmtServiceException;
 
-  //public IDatabaseConnection getDatasourceById(Serializable id) throws DatasourceMgmtServiceException;
+  /**
+   * Retrieves a JDBC datasource form the repository by id
+   * @param id
+   * @return IDatabaseConnection
+   * @throws DatasourceMgmtServiceException
+   */
+  public IDatabaseConnection getDatasourceById(String id) throws DatasourceMgmtServiceException;
 
+  /**
+   * Retrieves all JDBC datasources from the repository
+   * @return databaseConnection List
+   * @throws DatasourceMgmtServiceException
+   */
   public List<IDatabaseConnection> getDatasources() throws DatasourceMgmtServiceException;
   
-  //public List<Serializable> getDatasourceIds() throws DatasourceMgmtServiceException;
+  /**
+   * Retrieves all JDBC datasource ids from the repository
+   * @return list of ids
+   * @throws DatasourceMgmtServiceException
+   */
+  public List<String> getDatasourceIds() throws DatasourceMgmtServiceException;
 
-  public void updateDatasourceByName(String name, IDatabaseConnection databaseConnection) throws NonExistingDatasourceException, DatasourceMgmtServiceException;
+  /**
+   * Updates a given JDBC datasource by name 
+   * @param name
+   * @param databaseConnection
+   * @return id
+   * @throws NonExistingDatasourceException
+   * @throws DatasourceMgmtServiceException
+   */
+  public String updateDatasourceByName(String name, IDatabaseConnection databaseConnection) throws NonExistingDatasourceException, DatasourceMgmtServiceException;
 
-  //public void updateDatasourceById(Serializable id, IDatabaseConnection databaseConnection) throws NonExistingDatasourceException, DatasourceMgmtServiceException;
+  /**
+   * Updates a given JDBC datasource by id 
+   * @param id
+   * @param databaseConnection
+   * @return id 
+   * @throws NonExistingDatasourceException
+   * @throws DatasourceMgmtServiceException
+   */
+  public String updateDatasourceById(String id, IDatabaseConnection databaseConnection) throws NonExistingDatasourceException, DatasourceMgmtServiceException;
 }
