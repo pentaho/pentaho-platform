4af61d9c21fa142739224f1eee9d5d90c1e110b5
Ezequiel Cuellar <ecuellar@pentaho.com>
2012-03-15 20:14:45 +0000

    Fix for BISERVER-4503 As an admin user, I want to configure the email component.

--- user-console/source/org/pentaho/mantle/client/admin/EmailAdminPanelController.java
+++ user-console/source/org/pentaho/mantle/client/admin/EmailAdminPanelController.java
@@ -20,19 +20,89 @@
 
 package org.pentaho.mantle.client.admin;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.json.client.JSONBoolean;
+import com.google.gwt.json.client.JSONNumber;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class EmailAdminPanelController extends EmailAdminPanel implements ISysAdminPanel {
 
 	public EmailAdminPanelController() {
 		super();
+		saveButton.addClickHandler(new SaveButtonChangeListener());
 	}
 
 	// -- Remote Calls.
 
+	private void getEmailConfig(String params) {
+		String serviceUrl = GWT.getHostPageBaseURL() + "api/emailconfig/setEmailConfig" + params;
+		RequestBuilder executableTypesRequestBuilder = new RequestBuilder(RequestBuilder.PUT, serviceUrl);
+		try {
+			executableTypesRequestBuilder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+
+				}
+			});
+		} catch (RequestException e) {
+		}
+	}
+
+	private void getEmailConfig() {
+		String serviceUrl = GWT.getHostPageBaseURL() + "api/emailconfig/getEmailConfig";
+		RequestBuilder executableTypesRequestBuilder = new RequestBuilder(RequestBuilder.GET, serviceUrl);
+		try {
+			executableTypesRequestBuilder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					JSONObject emailData = (JSONObject) JSONParser.parseLenient(response.getText());
+					JSONBoolean authenticate = (JSONBoolean) emailData.get("authenticate");
+					JSONBoolean debug = (JSONBoolean) emailData.get("debug");
+					JSONString defaultFrom = (JSONString) emailData.get("defaultFrom");
+					JSONString smtpHost = (JSONString) emailData.get("smtpHost");
+					JSONNumber smtpPort = (JSONNumber) emailData.get("smtpPort");
+					JSONString smtpProtocol = (JSONString) emailData.get("smtpProtocol");
+					JSONString userId = (JSONString) emailData.get("userId");
+					JSONString password = (JSONString) emailData.get("password");
+					JSONBoolean useSsl = (JSONBoolean) emailData.get("useSsl");
+					JSONBoolean useStartTls = (JSONBoolean) emailData.get("useStartTls");
+
+					authenticationCheckBox.setValue(authenticate.booleanValue());
+					smtpHostTextBox.setValue(smtpHost.stringValue());
+					portTextBox.setValue(smtpPort.toString());
+					useStartTLSCheckBox.setValue(useStartTls.booleanValue());
+					useSSLCheckBox.setValue(useSsl.booleanValue());
+					fromAddressTextBox.setValue(defaultFrom.stringValue());
+					userNameTextBox.setValue(userId.stringValue());
+					passwordTextBox.setValue(password.stringValue());
+					debuggingCheckBox.setValue(debug.booleanValue());
+
+					String protocol = smtpProtocol.stringValue();
+					protocolsListBox.setSelectedIndex(protocol.equalsIgnoreCase("smtp") ? 0 : 1);
+				}
+			});
+		} catch (RequestException e) {
+		}
+	}
+
 	// -- ISysAdminPanel implementation.
 
 	public void activate() {
+		getEmailConfig();
 	}
 
 	public String getId() {
@@ -45,4 +115,32 @@ public class EmailAdminPanelController extends EmailAdminPanel implements ISysAd
 
 	// -- Event Listeners.
 
+	class SaveButtonChangeListener implements ClickHandler {
+		public void onClick(ClickEvent event) {
+
+			StringBuffer params = new StringBuffer();
+			params.append("?authenticate=");
+			params.append(authenticationCheckBox.getValue());
+			params.append("&debug=");
+			params.append(debuggingCheckBox.getValue());
+			params.append("&defaultFrom=");
+			params.append(fromAddressTextBox.getValue());
+			params.append("&smtpHost=");
+			params.append(smtpHostTextBox.getValue());
+			params.append("&smtpPort=");
+			params.append(portTextBox.getValue());
+			params.append("&smtpProtocol=");
+			params.append(protocolsListBox.getValue(protocolsListBox.getSelectedIndex()).toLowerCase());
+			params.append("&userId=");
+			params.append(userNameTextBox.getValue());
+			params.append("&password=");
+			params.append(passwordTextBox.getValue());
+			params.append("&useSsl=");
+			params.append(useSSLCheckBox.getValue());
+			params.append("&useStartTls=");
+			params.append(useStartTLSCheckBox.getValue());
+
+			getEmailConfig(params.toString());
+		}
+	}
 }
