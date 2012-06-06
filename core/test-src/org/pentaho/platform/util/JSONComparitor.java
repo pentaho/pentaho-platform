ec961507c9b26e5b0ebe4426eac3192a6b57c5d7
Tim Kafalas <tkafalas@pentaho.com>
2012-06-06 19:52:28 +0000

    [BISERVER-7270] JSON fix for Sugar. - Add JSONComparitor and JSONComparitorTest classes. - Change versions of Jettison from 1.3.1 to 1.2 - Change version of Xstream to 1.4.2 - Dashboard jscript changes to support new JSON format - MQL Editor Junit to use JSONComparator to test JSON format.

---
+++ core/test-src/org/pentaho/platform/util/JSONComparitor.java
@@ -0,0 +1,153 @@
+package org.pentaho.platform.util;
+
+import java.util.Iterator;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ * Static methods to test two JSON strings, object or arrays for equivalence.
+ * 
+ * @author tkafalas
+ */
+public class JSONComparitor {
+
+	// Boolean, JSONArray, JSONObject, Number, String, or the JSONObject.NULL
+	// object but treat numbers and boolean as strings for comparison purposes.
+	public enum KeyType {
+		OBJECT, ARRAY, STRING, NULL
+	}
+	
+	/**
+	 * Compare two JSON strings for equivalence.
+	 * @param json1 					First JSON string
+	 * @param json2 					Second JSON string
+	 * @param compareKey			root key to start compare, if null compare the entire string.
+	 * @return								true if equivalent, false if different.
+	 * @throws JSONException	
+	 */
+	public static boolean jsonEqual(String json1, String json2, String compareKey)
+			throws JSONException {
+		return jsonEqual(new JSONObject(json1), new JSONObject(json2), compareKey);
+	}
+
+	/**
+	 * Compare two JSON objects for equivalence.
+	 * @param json1 					First JSON object
+	 * @param json2 					Second JSON object
+	 * @param compareKey			root key to start compare, if null compare the entire string.
+	 * @return								true if equivalent, false if different.
+	 * @throws JSONException	
+	 */
+	public static boolean jsonEqual(JSONObject jso1, JSONObject jso2,
+			String compareKey) throws JSONException {
+		if (jso1 == null && jso2 == null)
+			return true;
+		if (jso1 == null || jso2 == null || jso1.length() != jso2.length())
+			return false;
+
+		if (compareKey != null) {
+			return compareKey(jso1, jso2, compareKey);
+		} else {
+			Iterator<String> i = jso1.keys();
+			while (i.hasNext()) {
+				String key = i.next();
+				if (!compareKey(jso1, jso2, key))
+					return false;
+			}
+			return true;
+		}
+	}
+
+	/**
+	 * Compare two JSONArrays for equivalence.
+	 * @param jsan1 					First JSONArray
+	 * @param jsan2 					Second JSONArray
+	 * @param item						Index of array to compare, null compare entire array
+	 * @return								true if equivalent, false if different.
+	 * @throws JSONException	
+	 */
+	public static boolean jsonEqual(JSONArray jsa1, JSONArray jsa2, Integer item)
+			throws JSONException {
+		if (jsa1 == null && jsa2 == null)
+			return true;
+		if (jsa1 == null || jsa2 == null || jsa1.length() != jsa2.length())
+			return false;
+
+		if (item != null) {
+			return compareArray(jsa1, jsa2, item);
+		} else {
+			for (int i = 0; i < jsa1.length(); i++) {
+				//System.out.println("Array item " + i);
+				if (!compareArray(jsa1, jsa2, i))
+					return false;
+			}
+			return true;
+		}
+	}
+	
+	private static boolean compareKey(JSONObject jso1, JSONObject jso2, String key)
+			throws JSONException {
+		KeyType keyType = getKeyType(jso1, key);
+		if (jso2.has(key)) {
+			if (keyType == getKeyType(jso2, key))
+				if (keyType == KeyType.OBJECT) {
+					return jsonEqual(jso1.getJSONObject(key), jso2.getJSONObject(key),
+							null);
+				} else {
+					if (keyType == KeyType.ARRAY) {
+						return jsonEqual(jso1.getJSONArray(key), jso2.getJSONArray(key),
+								null);
+					} else {
+						return jso1.getString(key).equals(jso2.getString(key));
+					}
+				}
+		}
+		return false;
+	}
+
+	private static boolean compareArray(JSONArray jsa1, JSONArray jsa2, int i)
+			throws JSONException {
+		KeyType keyType = getKeyType(jsa1, i);
+		if (keyType == getKeyType(jsa2, i))
+			if (keyType == KeyType.OBJECT) {
+				return jsonEqual(jsa1.getJSONObject(i), jsa2.getJSONObject(i), null);
+			} else {
+				if (keyType == KeyType.ARRAY) {
+					return jsonEqual(jsa1.getJSONArray(i), jsa2.getJSONArray(i), null);
+				} else {
+					return jsa1.getString(i).equals(jsa2.getString(i));
+				}
+			}
+		return false;
+	}
+
+	private static KeyType getKeyType(JSONObject jso, String key) {
+		try {
+			Object o = jso.get(key);
+			if (o instanceof JSONObject)
+				return KeyType.OBJECT;
+			if (o instanceof JSONArray)
+				return KeyType.ARRAY;
+			return KeyType.STRING;
+
+		} catch (Exception ex) {
+			return KeyType.NULL;
+		}
+	}
+
+	private static KeyType getKeyType(JSONArray jso, int item) {
+		try {
+			Object o = jso.get(item);
+			if (o instanceof JSONObject)
+				return KeyType.OBJECT;
+			if (o instanceof JSONArray)
+				return KeyType.ARRAY;
+			return KeyType.STRING;
+
+		} catch (Exception ex) {
+			return KeyType.NULL;
+		}
+	}
+}
---
+++ core/test-src/org/pentaho/platform/util/JSONComparitorTest.java
@@ -0,0 +1,57 @@
+package org.pentaho.platform.util;
+
+import junit.framework.TestCase;
+
+import org.codehaus.jettison.json.JSONException;
+import org.junit.Test;
+
+public class JSONComparitorTest extends TestCase {
+	@Test
+	public static void testJSONComparator() {
+		// A JSON string that could be returned by ModelSerializer
+		String json1 = "{\"MQLQuery\":{\"cols\":[{\"Column\":{\"id\":\"mycolumn\"}}],\"conds\":[{\"condition\":{\"@operator\":\"=\",\"@value\":\"myvalue1\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}]}}";
+		// Same as json1 except this string has the order of some entries switched.
+		String json2 = "{\"MQLQuery\":{\"cols\":[{\"Column\":{\"id\":\"mycolumn\"}}],\"conds\":[{\"condition\":{\"@value\":\"myvalue1\",\"@operator\":\"=\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}]}}";
+		// Same as json1 except cols array is removed
+		String json3 = "{\"MQLQuery\":{\"cols\":{\"Column\":{\"id\":\"mycolumn\"}},\"conds\":[{\"condition\":{\"@operator\":\"=\",\"@value\":\"myvalue1\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}]}}";
+		// Change element value
+		String json4 = "{\"MQLQuery\":{\"cols\":[{\"Column\":{\"id\":\"mycolumnx\"}}],\"conds\":[{\"condition\":{\"@operator\":\"=\",\"@value\":\"myvalue1\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}]}}";
+		// Reverse cols/conditions
+		String json5 = "{\"MQLQuery\":{\"conds\":[{\"condition\":{\"@operator\":\"=\",\"@value\":\"myvalue1\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}],\"cols\":[{\"Column\":{\"id\":\"mycolumn\"}}]}}";
+		// Array order of conditions
+		String json6 = "{\"MQLQuery\":{\"cols\":[{\"Column\":{\"id\":\"mycolumn\"}}],\"conds\":[{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}},{\"condition\":{\"@operator\":\"=\",\"@value\":\"myvalue1\"}}]}}";
+		// Change label name
+		String json7 = "{\"MQLQuery\":{\"cols\":[{\"Columnx\":{\"id\":\"mycolumn\"}}],\"conds\":[{\"condition\":{\"@operator\":\"=\",\"@value\":\"myvalue1\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}]}}";
+		// omit @Operator Element
+		String json8 = "{\"MQLQuery\":{\"cols\":[{\"Column\":{\"id\":\"mycolumn\"}}],\"conds\":[{\"condition\":{\"@value\":\"myvalue1\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}]}}";
+		// Malformed JSON
+		String json9 = "{\"MQLQuery\":{\"cols\":[{\"Column\":{\"id\":\"mycolumn\"}}],\"conds\":[{\"condition\":{\"@operator\":\"=\",\"@value\":\"myvalue1\"}},{\"condition\":{\"@operator\":\"in\",\"@value\":\"myvalue2\"}}]}";
+
+		try {
+			assertTrue("Equals Test", JSONComparitor.jsonEqual(json1, json1, null));
+			assertTrue("Change Order", JSONComparitor.jsonEqual(json1, json2, null));
+			assertFalse("Remove Cols Array",
+					JSONComparitor.jsonEqual(json1, json3, null));
+			assertFalse("Remove Cols Array (reversed compare order)",
+					JSONComparitor.jsonEqual(json1, json3, null));
+			assertFalse("Change Element Value",
+					JSONComparitor.jsonEqual(json1, json4, null));
+			assertTrue("Change order Cols/Conditions",
+					JSONComparitor.jsonEqual(json1, json5, null));
+			assertFalse("Change array order",
+					JSONComparitor.jsonEqual(json1, json6, null));
+			assertFalse("Change element name",
+					JSONComparitor.jsonEqual(json1, json7, null));
+			assertFalse("Omit one element",
+					JSONComparitor.jsonEqual(json1, json8, null));
+			try {
+						JSONComparitor.jsonEqual(json1, json9, null);
+						fail("malformed JSON did not cause exception");
+			} catch (JSONException ex) {
+						//should catch here
+			}
+		} catch (Exception ex) {
+			fail("Unanticipated Exception " + ex);
+		}
+	}
+}
