b0227547516a0a36d8b7e020ed478bf228c64aa9
Tyler Band <tband@pentaho.com>
2012-07-09 19:53:57 +0000

    Work in progress to handle numeric messages

--- extensions/src/org/pentaho/platform/plugin/services/importer/PlatformImportException.java
+++ extensions/src/org/pentaho/platform/plugin/services/importer/PlatformImportException.java
@@ -3,13 +3,34 @@ package org.pentaho.platform.plugin.services.importer;
 /**
  * User: nbaker
  * Date: 5/29/12
+ * modifed: tband
+ * Date: 7/9/12
+ * 1 - Publish failed
+ * 2 - General Error -
+ * 6 - Data source problem
+ * 7 - XMLA Catalog Exists
+ * 8 - XMLA Schema Exists
+ * 
+ * 
  */
 public class PlatformImportException extends Exception {
+  int errorStatusCode = 1;
+
   public PlatformImportException(String s) {
     super(s);
   }
 
-  public PlatformImportException(String s, Throwable t){
+  public PlatformImportException(String s, int statusCode) {
+    super(s);
+    this.errorStatusCode = statusCode;
+  }
+
+  public PlatformImportException(String s, Throwable t) {
     super(s, t);
+  }
+
+  public PlatformImportException(String s, int statusCode, Throwable t) {
+    super(s, t);
+    this.errorStatusCode = statusCode;
   }
 }
