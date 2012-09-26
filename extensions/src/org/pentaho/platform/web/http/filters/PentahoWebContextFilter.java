decbc52068ed14d06622042a8f4e79c1de4bc1c1
Marc Batchelor <mbatchelor@pentaho.com>
2012-09-26 18:34:31 +0000

    BISERVER-7613, BISERVER-7614, BISERVER-7615 - Various exploits of the PentahoWebContextFilter were possible thanks to code added in BISERVER-6060. This closes those holes.

--- extensions/src/org/pentaho/platform/web/http/filters/PentahoWebContextFilter.java
+++ extensions/src/org/pentaho/platform/web/http/filters/PentahoWebContextFilter.java
@@ -17,20 +17,30 @@
 */
 package org.pentaho.platform.web.http.filters;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang.StringUtils;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.Encoder;
 import org.pentaho.platform.api.engine.IPentahoRequestContext;
 import org.pentaho.platform.api.engine.IPluginManager;
 import org.pentaho.platform.engine.core.system.PentahoRequestContextHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.util.messages.LocaleHelper;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.List;
-import java.util.Locale;
 
 /**
  * If the request is searching for a webcontext.js, it writes out the content of the webcontext.js
@@ -42,13 +52,17 @@ public class PentahoWebContextFilter implements Filter {
   public static final String WEB_CONTEXT_JS = "webcontext.js";//$NON-NLS-1$
   static final String FILTER_APPLIED = "__pentaho_web_context_filter_applied"; //$NON-NLS-1$
   static final String initialComment = "/** webcontext.js is created by a PentahoWebContextFilter. This filter searches for an " +//$NON-NLS-1$
-  "incoming URI having \"webcontext.js\" in it. If it finds that, it write CONTEXT_PATH and FULLY_QUALIFIED_SERVER_URL" +//$NON-NLS-1$
+  "incoming URI having \"webcontext.js\" in it. If it finds that, it write CONTEXT_PATH and FULLY_QUALIFIED_SERVER_URL" + //$NON-NLS-1$
   " and it values from the servlet request to this js **/ \n\n\n" ;//$NON-NLS-1$
+  static final byte[] initialCommentBytes = initialComment.getBytes();
   String fullyQualifiedUrl = null;
   private static final String JS= ".js";
   private static final String CSS = ".css";
   private static final String CONTEXT = "context";
   private static final String GLOBAL = "global";
+  //Changed to not do so much work for every request
+  private static final ThreadLocal<byte[]> THREAD_LOCAL_CONTEXT_PATH = new ThreadLocal<byte[]>();
+  
   
   public void destroy() {
     // TODO Auto-generated method stub
@@ -60,20 +74,27 @@ public class PentahoWebContextFilter implements Filter {
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-      String requestStr = httpRequest.getRequestURI();
+    String requestStr = httpRequest.getRequestURI();
+    
     if(requestStr != null && requestStr.endsWith(WEB_CONTEXT_JS)
         && httpRequest.getAttribute(FILTER_APPLIED) == null) {
       httpRequest.setAttribute(FILTER_APPLIED, Boolean.TRUE);
       try {
         response.setContentType("text/javascript"); //$NON-NLS-1$
         OutputStream out = response.getOutputStream();
-        // split out a fully qualified url, guaranteed to have a trailing slash
-        IPentahoRequestContext requestContext = PentahoRequestContextHolder.getRequestContext();
-        out.write(initialComment.getBytes());
-        String webContext = "var CONTEXT_PATH = '" + requestContext.getContextPath() + "';\n\n";//$NON-NLS-1$ //$NON-NLS-2$
-        out.write(webContext.getBytes());
+        out.write(initialCommentBytes);
+        byte[] contextPathBytes = THREAD_LOCAL_CONTEXT_PATH.get();
+        if (contextPathBytes == null) {
+          // split out a fully qualified url, guaranteed to have a trailing slash
+          IPentahoRequestContext requestContext = PentahoRequestContextHolder.getRequestContext();
+          String contextPath = requestContext.getContextPath();
+          String webContext = "var CONTEXT_PATH = '" + contextPath + "';\n\n";//$NON-NLS-1$ //$NON-NLS-2$
+          contextPathBytes = webContext.getBytes();
+          THREAD_LOCAL_CONTEXT_PATH.set(contextPathBytes);
+        }
+        
+        out.write(contextPathBytes);
         out.write(fullyQualifiedUrl.getBytes());
-
         // Compute the effective locale and set it in the global scope. Also provide it as a module if the RequireJs
         // system is available.
         Locale effectiveLocale = LocaleHelper.getLocale();
@@ -115,16 +136,35 @@ public class PentahoWebContextFilter implements Filter {
   private void printResourcesForContext(String contextName,  OutputStream out, HttpServletRequest request, boolean printCssOnly) throws IOException {
 
     IPluginManager pluginManager = PentahoSystem.get(IPluginManager.class);
+    Encoder encoder = ESAPI.encoder();
 
-    String reqStr = ((HttpServletRequest) request).getQueryString();
-    if(reqStr != null){
-      reqStr = "?"+reqStr;
-    } else {
-      reqStr = "";
+    HttpServletRequest req = ((HttpServletRequest) request); 
+    String reqStr = "";
+    Map paramMap = req.getParameterMap();
+
+    // Fix for BISERVER-7613, BISERVER-7614, BISERVER-7615
+    // Make sure that parameters in the URL are encoded for Javascript safety since they'll be
+    // added to Javascript fragments that get executed.
+    if (paramMap.size() > 0) {
+      StringBuilder sb = new StringBuilder();
+      Map.Entry<String, String[]> me = null;
+      char sep = '?'; // first separator is '?'
+      Iterator<Map.Entry<String, String[]>> it = paramMap.entrySet().iterator();
+      int i;
+      while (it.hasNext()) {
+        me = it.next();
+        for (i=0; i<me.getValue().length; i++) {
+          sb.append(sep).append(encoder.encodeForJavaScript(me.getKey().toString())).append("=").append(encoder.encodeForJavaScript(me.getValue()[i]));
+        }
+        if (sep == '?') {
+          sep = '&'; // change the separator
+        }
+      }
+      reqStr = sb.toString(); // get the request string.
     }
-
+    
     List<String> externalResources = pluginManager.getExternalResourcesForContext(contextName);
-    out.write(("<!-- Injecting web resources defined in by plugins as external-resources for: "+contextName+"-->\n").getBytes());
+    out.write(("<!-- Injecting web resources defined in by plugins as external-resources for: "+encoder.encodeForHTML(contextName)+"-->\n").getBytes());
     if(externalResources != null){
 
       for(String res : externalResources){
@@ -151,9 +191,8 @@ public class PentahoWebContextFilter implements Filter {
     if (!fullyQualifiedServerURL.endsWith("/")) { //$NON-NLS-1$
       fullyQualifiedServerURL += "/"; //$NON-NLS-1$
     }
-
     fullyQualifiedUrl = "var FULL_QUALIFIED_URL = '" + fullyQualifiedServerURL + "';\n\n"; //$NON-NLS-1$ //$NON-NLS-2$
-
+   
   }
 
 
