d7db3e44aa828ac9c970ef495de04a84dd96725f
Tyler Band <tband@pentaho.com>
2012-11-28 20:46:20 +0000

    Working on PDI Report Publish using this Jersey service - found that the import did not use the InputStream (added convert method).

--- extensions/src/org/pentaho/platform/web/http/api/resources/RepositoryImportResource.java
+++ extensions/src/org/pentaho/platform/web/http/api/resources/RepositoryImportResource.java
@@ -22,8 +22,11 @@
 package org.pentaho.platform.web.http.api.resources;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.zip.ZipInputStream;
@@ -49,96 +52,146 @@ import com.sun.jersey.multipart.FormDataParam;
 @Path("/repo/files/import")
 public class RepositoryImportResource {
 
-  private IUnifiedRepository repository;
+	private IUnifiedRepository repository;
 
-  public RepositoryImportResource() {
-    repository = PentahoSystem.get(IUnifiedRepository.class);
-  }
+	public RepositoryImportResource() {
+		repository = PentahoSystem.get(IUnifiedRepository.class);
+	}
 
-  /**
-   * @param uploadDir: JCR Directory to which the zip structure or single file will be uploaded to.
-   * @param fileIS:    Input stream for the file.
-   * @param fileInfo:  Info about he file (
-   * @return http ok response of everything went well... some other error otherwise
-   *         <p/>
-   *         This REST method takes multi-part form data and imports it to a JCR repository.
-   */
-  @POST
-  @Consumes(MediaType.MULTIPART_FORM_DATA)
-  @Produces(MediaType.TEXT_HTML)
-  public Response doPostImport(@FormDataParam("importDir") String uploadDir,
-                               @FormDataParam("fileUpload") InputStream fileIS,
-                               @FormDataParam("fileUpload") FormDataContentDisposition fileInfo) {
-    Map<String, org.pentaho.platform.plugin.services.importexport.Converter> converters = new HashMap<String, org.pentaho.platform.plugin.services.importexport.Converter>();
-    org.pentaho.platform.plugin.services.importexport.StreamConverter streamConverter = new org.pentaho.platform.plugin.services.importexport.StreamConverter();
-    converters.put("prpt", streamConverter); //$NON-NLS-1$
-    converters.put("mondrian.xml", streamConverter); //$NON-NLS-1$
-    converters.put("kjb", streamConverter); //$NON-NLS-1$
-    converters.put("ktr", streamConverter); //$NON-NLS-1$
-    converters.put("report", streamConverter); //$NON-NLS-1$
-    converters.put("rptdesign", streamConverter); //$NON-NLS-1$
-    converters.put("svg", streamConverter); //$NON-NLS-1$
-    converters.put("url", streamConverter); //$NON-NLS-1$
-    converters.put("xaction", streamConverter); //$NON-NLS-1$
-    converters.put("xanalyzer", streamConverter); //$NON-NLS-1$
-    converters.put("xcdf", streamConverter); //$NON-NLS-1$
-    converters.put("xdash", streamConverter); //$NON-NLS-1$
-    converters.put("xreportspec", streamConverter); //$NON-NLS-1$
-    converters.put("waqr.xaction", streamConverter); //$NON-NLS-1$
-    converters.put("xwaqr", streamConverter);//$NON-NLS-1$
-    converters.put("gif", streamConverter); //$NON-NLS-1$
-    converters.put("css", streamConverter); //$NON-NLS-1$
-    converters.put("html", streamConverter); //$NON-NLS-1$
-    converters.put("htm", streamConverter); //$NON-NLS-1$
-    converters.put("jpg", streamConverter); //$NON-NLS-1$
-    converters.put("jpeg", streamConverter); //$NON-NLS-1$
-    converters.put("js", streamConverter); //$NON-NLS-1$
-    converters.put("cfg.xml", streamConverter); //$NON-NLS-1$
-    converters.put("jrxml", streamConverter); //$NON-NLS-1$
-    converters.put("png", streamConverter); //$NON-NLS-1$
-    converters.put("properties", streamConverter); //$NON-NLS-1$
-    converters.put("sql", streamConverter); //$NON-NLS-1$
-    converters.put("xmi", streamConverter); //$NON-NLS-1$
-    converters.put("xml", streamConverter); //$NON-NLS-1$
-    converters.put("cda", streamConverter); //$NON-NLS-1$
+	/**
+	 * @param uploadDir
+	 *            : JCR Directory to which the zip structure or single file will
+	 *            be uploaded to.
+	 * @param fileIS
+	 *            : Input stream for the file.
+	 * @param fileInfo
+	 *            : Info about he file (
+	 * @return http ok response of everything went well... some other error
+	 *         otherwise
+	 *         <p/>
+	 *         This REST method takes multi-part form data and imports it to a
+	 *         JCR repository.
+	 */
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.TEXT_HTML)
+	public Response doPostImport(@FormDataParam("importDir") String uploadDir,
+			@FormDataParam("fileUpload") InputStream fileIS,
+			@FormDataParam("fileUpload") FormDataContentDisposition fileInfo) {
+		Map<String, org.pentaho.platform.plugin.services.importexport.Converter> converters = new HashMap<String, org.pentaho.platform.plugin.services.importexport.Converter>();
+		org.pentaho.platform.plugin.services.importexport.StreamConverter streamConverter = new org.pentaho.platform.plugin.services.importexport.StreamConverter();
+		converters.put("prpt", streamConverter); //$NON-NLS-1$
+		converters.put("mondrian.xml", streamConverter); //$NON-NLS-1$
+		converters.put("kjb", streamConverter); //$NON-NLS-1$
+		converters.put("ktr", streamConverter); //$NON-NLS-1$
+		converters.put("report", streamConverter); //$NON-NLS-1$
+		converters.put("rptdesign", streamConverter); //$NON-NLS-1$
+		converters.put("svg", streamConverter); //$NON-NLS-1$
+		converters.put("url", streamConverter); //$NON-NLS-1$
+		converters.put("xaction", streamConverter); //$NON-NLS-1$
+		converters.put("xanalyzer", streamConverter); //$NON-NLS-1$
+		converters.put("xcdf", streamConverter); //$NON-NLS-1$
+		converters.put("xdash", streamConverter); //$NON-NLS-1$
+		converters.put("xreportspec", streamConverter); //$NON-NLS-1$
+		converters.put("waqr.xaction", streamConverter); //$NON-NLS-1$
+		converters.put("xwaqr", streamConverter);//$NON-NLS-1$
+		converters.put("gif", streamConverter); //$NON-NLS-1$
+		converters.put("css", streamConverter); //$NON-NLS-1$
+		converters.put("html", streamConverter); //$NON-NLS-1$
+		converters.put("htm", streamConverter); //$NON-NLS-1$
+		converters.put("jpg", streamConverter); //$NON-NLS-1$
+		converters.put("jpeg", streamConverter); //$NON-NLS-1$
+		converters.put("js", streamConverter); //$NON-NLS-1$
+		converters.put("cfg.xml", streamConverter); //$NON-NLS-1$
+		converters.put("jrxml", streamConverter); //$NON-NLS-1$
+		converters.put("png", streamConverter); //$NON-NLS-1$
+		converters.put("properties", streamConverter); //$NON-NLS-1$
+		converters.put("sql", streamConverter); //$NON-NLS-1$
+		converters.put("xmi", streamConverter); //$NON-NLS-1$
+		converters.put("xml", streamConverter); //$NON-NLS-1$
+		converters.put("cda", streamConverter); //$NON-NLS-1$
 
-    try {
-      validateAccess();	
-      final org.pentaho.platform.plugin.services.importexport.ImportProcessor importProcessor = new org.pentaho.platform.plugin.services.importexport.SimpleImportProcessor(uploadDir, null);
-      // TODO - create a SolutionRepositoryImportHandler which delegates to these three automatically
-      importProcessor.addImportHandler(new org.pentaho.platform.plugin.services.importexport.MondrianImportHandler(repository));
-      importProcessor.addImportHandler(new org.pentaho.platform.plugin.services.importexport.MetadataImportHandler(repository));
-      importProcessor.addImportHandler(new org.pentaho.platform.plugin.services.importexport.DefaultImportHandler(repository));
-      if (fileInfo.getFileName().toLowerCase().endsWith(".zip")) {
-        importProcessor.setImportSource(new org.pentaho.platform.plugin.services.importexport.legacy.ZipSolutionRepositoryImportSource(new ZipInputStream(fileIS), "UTF-8"));
-      } else {
-        final File outFile = File.createTempFile("import", null);
-        outFile.deleteOnExit();
-        importProcessor.setImportSource(new org.pentaho.platform.plugin.services.importexport.legacy.FileSolutionRepositoryImportSource(outFile, fileInfo.getFileName(), "UTF-8"));
-      }
-      importProcessor.performImport();
-      
-      // Flush the Mondrian cache to show imported datasources. 
-      IMondrianCatalogService mondrianCatalogService = PentahoSystem.get(IMondrianCatalogService.class, "IMondrianCatalogService", PentahoSessionHolder.getSession());
-      mondrianCatalogService.reInit(PentahoSessionHolder.getSession());
-    } catch (org.pentaho.platform.plugin.services.importexport.ImportException e) {
-      return Response.serverError().entity(e.toString()).build();
-    } catch (org.pentaho.platform.plugin.services.importexport.InitializationException e) {
-      return Response.serverError().entity(e.toString()).build();
-    } catch (IOException e) {
-      return Response.serverError().entity(e.toString()).build();
-    } catch (PentahoAccessControlException e) {
-      return Response.serverError().entity(e.toString()).build();
-    }
+		try {
+			validateAccess();
+			final org.pentaho.platform.plugin.services.importexport.ImportProcessor importProcessor = new org.pentaho.platform.plugin.services.importexport.SimpleImportProcessor(
+					uploadDir, null);
+			// TODO - create a SolutionRepositoryImportHandler which delegates
+			// to these three automatically
+			importProcessor
+					.addImportHandler(new org.pentaho.platform.plugin.services.importexport.MondrianImportHandler(
+							repository));
+			importProcessor
+					.addImportHandler(new org.pentaho.platform.plugin.services.importexport.MetadataImportHandler(
+							repository));
+			importProcessor
+					.addImportHandler(new org.pentaho.platform.plugin.services.importexport.DefaultImportHandler(
+							repository));
+			if (fileInfo.getFileName().toLowerCase().endsWith(".zip")) {
+				importProcessor
+						.setImportSource(new org.pentaho.platform.plugin.services.importexport.legacy.ZipSolutionRepositoryImportSource(
+								new ZipInputStream(fileIS), "UTF-8"));
+			} else {
+				final File outFile = File.createTempFile("import", null);
+				convertInputStreamToFile(outFile, fileIS);
+				outFile.deleteOnExit();
+				importProcessor
+						.setImportSource(new org.pentaho.platform.plugin.services.importexport.legacy.FileSolutionRepositoryImportSource(
+								outFile, fileInfo.getFileName(), "UTF-8"));
+			}
+			importProcessor.performImport();
 
-    return Response.ok(Messages.getInstance().getString("FileResource.IMPORT_SUCCESS")).build();
-  }
-  
-  private void validateAccess() throws PentahoAccessControlException {
-	  IAuthorizationPolicy policy = PentahoSystem.get(IAuthorizationPolicy.class);
-	  boolean isAdmin = policy.isAllowed(IAuthorizationPolicy.READ_REPOSITORY_CONTENT_ACTION) && policy.isAllowed(IAuthorizationPolicy.CREATE_REPOSITORY_CONTENT_ACTION) && policy.isAllowed(IAuthorizationPolicy.ADMINISTER_SECURITY_ACTION); 
-	  if(!isAdmin) {
-		  throw new PentahoAccessControlException("Access Denied");
-	  }
-  }
+			// Flush the Mondrian cache to show imported datasources.
+			IMondrianCatalogService mondrianCatalogService = PentahoSystem.get(
+					IMondrianCatalogService.class, "IMondrianCatalogService",
+					PentahoSessionHolder.getSession());
+			mondrianCatalogService.reInit(PentahoSessionHolder.getSession());
+		} catch (org.pentaho.platform.plugin.services.importexport.ImportException e) {
+			return Response.serverError().entity(e.toString()).build();
+		} catch (org.pentaho.platform.plugin.services.importexport.InitializationException e) {
+			return Response.serverError().entity(e.toString()).build();
+		} catch (IOException e) {
+			return Response.serverError().entity(e.toString()).build();
+		} catch (PentahoAccessControlException e) {
+			return Response.serverError().entity(e.toString()).build();
+		}
+
+		return Response
+				.ok(Messages.getInstance().getString(
+						"FileResource.IMPORT_SUCCESS")).build();
+	}
+
+	/**
+	 * Take the InputStream and convert to Output Stream
+	 * @param outFile
+	 * @param fileIS
+	 * @throws IOException
+	 */
+	private void convertInputStreamToFile(File outFile, InputStream fileIS)
+			throws IOException {		
+		OutputStream out = new FileOutputStream(outFile);
+
+		int read = 0;
+		byte[] bytes = new byte[1024];
+
+		while ((read = fileIS.read(bytes)) != -1) {
+			out.write(bytes, 0, read);
+		}
+		fileIS.close();
+		out.flush();
+		out.close();	
+	}
+
+	private void validateAccess() throws PentahoAccessControlException {
+		IAuthorizationPolicy policy = PentahoSystem
+				.get(IAuthorizationPolicy.class);
+		boolean isAdmin = policy
+				.isAllowed(IAuthorizationPolicy.READ_REPOSITORY_CONTENT_ACTION)
+				&& policy
+						.isAllowed(IAuthorizationPolicy.CREATE_REPOSITORY_CONTENT_ACTION)
+				&& policy
+						.isAllowed(IAuthorizationPolicy.ADMINISTER_SECURITY_ACTION);
+		if (!isAdmin) {
+			throw new PentahoAccessControlException("Access Denied");
+		}
+	}
 }
