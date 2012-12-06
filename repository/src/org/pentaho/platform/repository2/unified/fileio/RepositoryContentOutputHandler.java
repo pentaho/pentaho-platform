d3aeb30306a89c1806e00fa92d5b94bc50cf3cad
Tim Kafalas <tkafalas@pentaho.com>
2012-12-06 21:51:34 +0000

    BISERVER-7780 Fix jFreeReport xaction ERROR_0017 due to bad repository path.

--- repository/src/org/pentaho/platform/repository2/unified/fileio/RepositoryContentOutputHandler.java
+++ repository/src/org/pentaho/platform/repository2/unified/fileio/RepositoryContentOutputHandler.java
@@ -1,7 +1,9 @@
 package org.pentaho.platform.repository2.unified.fileio;
 
 import org.apache.commons.io.FilenameUtils;
+import org.pentaho.platform.api.mt.ITenantedPrincipleNameResolver;
 import org.pentaho.platform.api.repository.IContentItem;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.services.outputhandler.BaseOutputHandler;
 import org.pentaho.platform.repository2.ClientRepositoryPaths;
 import org.pentaho.platform.util.web.MimeHelper;
@@ -11,7 +13,8 @@ public class RepositoryContentOutputHandler extends BaseOutputHandler {
   public IContentItem getFileOutputContentItem() {
     String filePath = getSolutionPath();
     if (filePath.startsWith("~/") || filePath.startsWith("~\\") || filePath.equals("~")) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-      filePath = ClientRepositoryPaths.getUserHomeFolderPath(getSession().getName()) + "/"; //$NON-NLS-1$
+    	ITenantedPrincipleNameResolver tenantedUserNameUtils = PentahoSystem.get(ITenantedPrincipleNameResolver.class, "tenantedUserNameUtils", getSession());
+      filePath = ClientRepositoryPaths.getUserHomeFolderPath(tenantedUserNameUtils.getPrincipleName(getSession().getName())) + "/"; //$NON-NLS-1$
       filePath = filePath + (getSolutionPath().length() > 1 ? getSolutionPath().substring(2) : getSolutionPath().substring(1));
     }
     IContentItem contentItem = null;
