2b286817b2d49ca5a7e59ccda9b04128257d1467
Ramaiz Mansoor <rmansoor@pentaho.com>
2012-12-07 15:30:06 +0000

    Removing duplicate converters

--- extensions/src/org/pentaho/platform/web/http/api/resources/RepositoryImportResource.java
+++ extensions/src/org/pentaho/platform/web/http/api/resources/RepositoryImportResource.java
@@ -78,39 +78,6 @@ public class RepositoryImportResource {
 	public Response doPostImport(@FormDataParam("importDir") String uploadDir,
 			@FormDataParam("fileUpload") InputStream fileIS,
 			@FormDataParam("fileUpload") FormDataContentDisposition fileInfo) {
-		Map<String, org.pentaho.platform.plugin.services.importexport.Converter> converters = new HashMap<String, org.pentaho.platform.plugin.services.importexport.Converter>();
-		org.pentaho.platform.plugin.services.importexport.StreamConverter streamConverter = new org.pentaho.platform.plugin.services.importexport.StreamConverter();
-		converters.put("prpt", streamConverter); //$NON-NLS-1$
-		converters.put("mondrian.xml", streamConverter); //$NON-NLS-1$
-		converters.put("kjb", streamConverter); //$NON-NLS-1$
-		converters.put("ktr", streamConverter); //$NON-NLS-1$
-		converters.put("report", streamConverter); //$NON-NLS-1$
-		converters.put("rptdesign", streamConverter); //$NON-NLS-1$
-		converters.put("svg", streamConverter); //$NON-NLS-1$
-		converters.put("url", streamConverter); //$NON-NLS-1$
-		converters.put("xaction", streamConverter); //$NON-NLS-1$
-		converters.put("xanalyzer", streamConverter); //$NON-NLS-1$
-		converters.put("xcdf", streamConverter); //$NON-NLS-1$
-		converters.put("xdash", streamConverter); //$NON-NLS-1$
-		converters.put("xreportspec", streamConverter); //$NON-NLS-1$
-		converters.put("waqr.xaction", streamConverter); //$NON-NLS-1$
-		converters.put("xwaqr", streamConverter);//$NON-NLS-1$
-		converters.put("gif", streamConverter); //$NON-NLS-1$
-		converters.put("css", streamConverter); //$NON-NLS-1$
-		converters.put("html", streamConverter); //$NON-NLS-1$
-		converters.put("htm", streamConverter); //$NON-NLS-1$
-		converters.put("jpg", streamConverter); //$NON-NLS-1$
-		converters.put("jpeg", streamConverter); //$NON-NLS-1$
-		converters.put("js", streamConverter); //$NON-NLS-1$
-		converters.put("cfg.xml", streamConverter); //$NON-NLS-1$
-		converters.put("jrxml", streamConverter); //$NON-NLS-1$
-		converters.put("png", streamConverter); //$NON-NLS-1$
-		converters.put("properties", streamConverter); //$NON-NLS-1$
-		converters.put("sql", streamConverter); //$NON-NLS-1$
-		converters.put("xmi", streamConverter); //$NON-NLS-1$
-		converters.put("xml", streamConverter); //$NON-NLS-1$
-		converters.put("cda", streamConverter); //$NON-NLS-1$
-
 		try {
 			validateAccess();
 			final org.pentaho.platform.plugin.services.importexport.ImportProcessor importProcessor = new org.pentaho.platform.plugin.services.importexport.SimpleImportProcessor(
