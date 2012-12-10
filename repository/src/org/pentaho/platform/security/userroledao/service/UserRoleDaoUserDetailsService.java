d8f4824e47f336086b039aeeb6a978ef585695a8
Peter Minutillo <pminutillo@pentaho.com>
2012-12-10 17:20:37 +0000

    BISERVER-7796 Subsequent http requests fail after valid basic authentication credentials provided - added third option of obtaining default tenant

--- repository/src/org/pentaho/platform/security/userroledao/service/UserRoleDaoUserDetailsService.java
+++ repository/src/org/pentaho/platform/security/userroledao/service/UserRoleDaoUserDetailsService.java
@@ -1,14 +1,14 @@
 /*
- * This program is free software; you can redistribute it and/or modify it under the 
- * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software 
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
  * Foundation.
  *
- * You should have received a copy of the GNU Lesser General Public License along with this 
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html 
- * or from the Free Software Foundation, Inc., 
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
@@ -31,6 +31,7 @@ import org.pentaho.platform.api.mt.ITenantedPrincipleNameResolver;
 import org.pentaho.platform.core.mt.Tenant;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.engine.core.system.TenantUtils;
 import org.pentaho.platform.security.userroledao.messages.Messages;
 import org.springframework.dao.DataAccessException;
 import org.springframework.security.GrantedAuthority;
@@ -43,159 +44,163 @@ import org.springframework.util.Assert;
 
 /**
  * A <code>UserDetailsService</code> that delegates to an {@link IUserRoleDao} to load users by username.
- * 
+ *
  * @author mlowery
  */
 public class UserRoleDaoUserDetailsService implements UserDetailsService {
 
-  // ~ Static fields/initializers ====================================================================================== 
+    // ~ Static fields/initializers ======================================================================================
 
-  // ~ Instance fields =================================================================================================
+    // ~ Instance fields =================================================================================================
 
-  private String rolePrefix = "ROLE_"; //$NON-NLS-1$
+    private String rolePrefix = "ROLE_"; //$NON-NLS-1$
 
-  private IUserRoleDao userRoleDao;
-  
-  /**
-   * A default role which will be assigned to all authenticated users if set
-   */
-  private GrantedAuthority defaultRole;
-  
-  private String defaultRoleString;
-  
-  ITenantedPrincipleNameResolver tenantedUserNameUtils;
-  
-  ITenantedPrincipleNameResolver tenantedRoleNameUtils;
+    private IUserRoleDao userRoleDao;
 
-  // ~ Constructors ====================================================================================================
-  public UserRoleDaoUserDetailsService(final ITenantedPrincipleNameResolver tenantedUserNameUtils, final ITenantedPrincipleNameResolver tenantedRoleNameUtils) {
-    this.tenantedRoleNameUtils = tenantedRoleNameUtils;
-    this.tenantedUserNameUtils = tenantedUserNameUtils;
-  }
+    /**
+     * A default role which will be assigned to all authenticated users if set
+     */
+    private GrantedAuthority defaultRole;
 
-  // ~ Methods =========================================================================================================
+    private String defaultRoleString;
 
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
-    final boolean ACCOUNT_NON_EXPIRED = true;
-    final boolean CREDS_NON_EXPIRED = true;
-    final boolean ACCOUNT_NON_LOCKED = true;
+    ITenantedPrincipleNameResolver tenantedUserNameUtils;
 
-    IPentahoUser user;
-    try {
-      if(userRoleDao == null) {
-        userRoleDao = PentahoSystem.get(IUserRoleDao.class, "userRoleDaoProxy", PentahoSessionHolder.getSession());
-      }
-      user = userRoleDao.getUser(null, username);
-    } catch (UncategorizedUserRoleDaoException e) {
-      throw new UserRoleDaoUserDetailsServiceException(Messages.getInstance()
-          .getString("UserRoleDaoUserDetailsService.ERROR_0003_DATA_ACCESS_EXCEPTION"), e); //$NON-NLS-1$
+    ITenantedPrincipleNameResolver tenantedRoleNameUtils;
+
+    // ~ Constructors ====================================================================================================
+    public UserRoleDaoUserDetailsService(final ITenantedPrincipleNameResolver tenantedUserNameUtils, final ITenantedPrincipleNameResolver tenantedRoleNameUtils) {
+        this.tenantedRoleNameUtils = tenantedRoleNameUtils;
+        this.tenantedUserNameUtils = tenantedUserNameUtils;
     }
 
-    if (user == null) {
-      throw new UsernameNotFoundException(Messages.getInstance().getString("UserRoleDaoUserDetailsService.ERROR_0001_USER_NOT_FOUND")); //$NON-NLS-1$
-    }
-    // convert IPentahoUser to a UserDetails instance
-    List<IPentahoRole> userRoles = userRoleDao.getUserRoles(null, username);
-    int authsSize = userRoles != null ? userRoles.size() : 0;
-    GrantedAuthority[] auths = new GrantedAuthority[authsSize];
-    int i = 0;
-    for (IPentahoRole role : userRoles) {
-      auths[i++] = new GrantedAuthorityImpl(tenantedRoleNameUtils.getPrincipleId(role.getTenant(), role.getName()));
-    }
+    // ~ Methods =========================================================================================================
 
-    List<GrantedAuthority> dbAuths = new ArrayList<GrantedAuthority>(Arrays.asList(auths));
-    addCustomAuthorities(tenantedUserNameUtils.getPrincipleId(user.getTenant(), user.getUsername()), dbAuths);
-    
-    // Get the tenant information from the username first
-    ITenant tenant = tenantedUserNameUtils.getTenant(username);
-    
-    // If the tenant information is not present then get the tenant information from session
-    if(tenant == null || tenant.getId() == null) {
-      IPentahoSession session = PentahoSessionHolder.getSession();
-      String tenantId = (String) session.getAttribute(IPentahoSession.TENANT_ID_KEY);
-      if(tenantId != null) {
-        tenant = new Tenant(tenantId, true);        
-      }
-    }
-    
-    // reconstruct the default role with tenant
-    defaultRole = new GrantedAuthorityImpl(tenantedRoleNameUtils.getPrincipleId(tenant, defaultRoleString));
-    
-    if (defaultRole != null && !dbAuths.contains(defaultRole)) {
-      dbAuths.add(defaultRole);
-    }
-    
-    if (dbAuths.size() == 0) {
-      throw new UsernameNotFoundException(Messages.getInstance()
-          .getString("UserRoleDaoUserDetailsService.ERROR_0002_NO_AUTHORITIES")); //$NON-NLS-1$
-    }
-    
-    GrantedAuthority[] arrayAuths = dbAuths.toArray(new GrantedAuthority[dbAuths.size()]);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
+        final boolean ACCOUNT_NON_EXPIRED = true;
+        final boolean CREDS_NON_EXPIRED = true;
+        final boolean ACCOUNT_NON_LOCKED = true;
 
-    return new User(tenantedUserNameUtils.getPrincipleId(user.getTenant(), user.getUsername()), user.getPassword(), user.isEnabled(), ACCOUNT_NON_EXPIRED, CREDS_NON_EXPIRED,
-        ACCOUNT_NON_LOCKED, arrayAuths);
-  }
+        IPentahoUser user;
+        try {
+            if(userRoleDao == null) {
+                userRoleDao = PentahoSystem.get(IUserRoleDao.class, "userRoleDaoProxy", PentahoSessionHolder.getSession());
+            }
+            user = userRoleDao.getUser(null, username);
+        } catch (UncategorizedUserRoleDaoException e) {
+            throw new UserRoleDaoUserDetailsServiceException(Messages.getInstance()
+                    .getString("UserRoleDaoUserDetailsService.ERROR_0003_DATA_ACCESS_EXCEPTION"), e); //$NON-NLS-1$
+        }
 
-  /**
-   * Allows subclasses to add their own granted authorities to the list to be returned in the
-   * <code>User</code>.
-   *
-   * @param username the username, for use by finder methods
-   * @param authorities the current granted authorities, as populated from the <code>authoritiesByUsername</code>
-   *        mapping
-   */
-  protected void addCustomAuthorities(String username, List authorities) {
-  }
+        if (user == null) {
+            throw new UsernameNotFoundException(Messages.getInstance().getString("UserRoleDaoUserDetailsService.ERROR_0001_USER_NOT_FOUND")); //$NON-NLS-1$
+        }
+        // convert IPentahoUser to a UserDetails instance
+        List<IPentahoRole> userRoles = userRoleDao.getUserRoles(null, username);
+        int authsSize = userRoles != null ? userRoles.size() : 0;
+        GrantedAuthority[] auths = new GrantedAuthority[authsSize];
+        int i = 0;
+        for (IPentahoRole role : userRoles) {
+            auths[i++] = new GrantedAuthorityImpl(tenantedRoleNameUtils.getPrincipleId(role.getTenant(), role.getName()));
+        }
 
-  /**
-   * Allows a default role prefix to be specified. If this is set to a non-empty value, then it is
-   * automatically prepended to any roles read in from the db. This may for example be used to add the
-   * <code>ROLE_</code> prefix expected to exist in role names (by default) by some other Spring Security framework
-   * classes, in the case that the prefix is not already present in the db.
-   *
-   * @param rolePrefix the new prefix
-   */
-  public void setRolePrefix(String rolePrefix) {
-    if (rolePrefix == null) {
-      this.rolePrefix = ""; //$NON-NLS-1$
-    } else {
-      this.rolePrefix = rolePrefix;
-    }
-  }
+        List<GrantedAuthority> dbAuths = new ArrayList<GrantedAuthority>(Arrays.asList(auths));
+        addCustomAuthorities(tenantedUserNameUtils.getPrincipleId(user.getTenant(), user.getUsername()), dbAuths);
 
-  public String getRolePrefix() {
-    return rolePrefix;
-  }
+        // Get the tenant information from the username first
+        ITenant tenant = tenantedUserNameUtils.getTenant(username);
 
-  /**
-   * A data access exception specific to a <code>IUserRoleDao</code>-based <code>UserDetailsService</code>.
-   */
-  protected class UserRoleDaoUserDetailsServiceException extends DataAccessException {
+        // If the tenant information is not present then get the tenant information from session
+        if(tenant == null || tenant.getId() == null) {
+            IPentahoSession session = PentahoSessionHolder.getSession();
+            String tenantId = (String) session.getAttribute(IPentahoSession.TENANT_ID_KEY);
+            if(tenantId != null) {
+                tenant = new Tenant(tenantId, true);
+            }
+        }
 
-    private static final long serialVersionUID = -3598806635515478946L;
+        // add third option of getting default tenant
+        tenant = new Tenant(TenantUtils.getDefaultTenant(), true);
 
-    public UserRoleDaoUserDetailsServiceException(String msg) {
-      super(msg);
+        // reconstruct the default role with tenant
+
+        defaultRole = new GrantedAuthorityImpl(tenantedRoleNameUtils.getPrincipleId(tenant, defaultRoleString));
+
+        if (defaultRole != null && !dbAuths.contains(defaultRole)) {
+            dbAuths.add(defaultRole);
+        }
+
+        if (dbAuths.size() == 0) {
+            throw new UsernameNotFoundException(Messages.getInstance()
+                    .getString("UserRoleDaoUserDetailsService.ERROR_0002_NO_AUTHORITIES")); //$NON-NLS-1$
+        }
+
+        GrantedAuthority[] arrayAuths = dbAuths.toArray(new GrantedAuthority[dbAuths.size()]);
+
+        return new User(tenantedUserNameUtils.getPrincipleId(user.getTenant(), user.getUsername()), user.getPassword(), user.isEnabled(), ACCOUNT_NON_EXPIRED, CREDS_NON_EXPIRED,
+                ACCOUNT_NON_LOCKED, arrayAuths);
     }
 
-    public UserRoleDaoUserDetailsServiceException(String msg, Throwable cause) {
-      super(msg, cause);
+    /**
+     * Allows subclasses to add their own granted authorities to the list to be returned in the
+     * <code>User</code>.
+     *
+     * @param username the username, for use by finder methods
+     * @param authorities the current granted authorities, as populated from the <code>authoritiesByUsername</code>
+     *        mapping
+     */
+    protected void addCustomAuthorities(String username, List authorities) {
     }
 
-  }
+    /**
+     * Allows a default role prefix to be specified. If this is set to a non-empty value, then it is
+     * automatically prepended to any roles read in from the db. This may for example be used to add the
+     * <code>ROLE_</code> prefix expected to exist in role names (by default) by some other Spring Security framework
+     * classes, in the case that the prefix is not already present in the db.
+     *
+     * @param rolePrefix the new prefix
+     */
+    public void setRolePrefix(String rolePrefix) {
+        if (rolePrefix == null) {
+            this.rolePrefix = ""; //$NON-NLS-1$
+        } else {
+            this.rolePrefix = rolePrefix;
+        }
+    }
 
-  public void setUserRoleDao(IUserRoleDao userRoleDao) {
-    this.userRoleDao = userRoleDao;
-  }
+    public String getRolePrefix() {
+        return rolePrefix;
+    }
 
-  /**
-   * The default role which will be assigned to all users.
-   *
-   * @param defaultRole the role name, including any desired prefix.
-   */
-  public void setDefaultRole(String defaultRole) {
-      Assert.notNull(defaultRole);
-      this.defaultRoleString = defaultRole;
-      this.defaultRole = new GrantedAuthorityImpl(defaultRole);
-  }
+    /**
+     * A data access exception specific to a <code>IUserRoleDao</code>-based <code>UserDetailsService</code>.
+     */
+    protected class UserRoleDaoUserDetailsServiceException extends DataAccessException {
+
+        private static final long serialVersionUID = -3598806635515478946L;
+
+        public UserRoleDaoUserDetailsServiceException(String msg) {
+            super(msg);
+        }
+
+        public UserRoleDaoUserDetailsServiceException(String msg, Throwable cause) {
+            super(msg, cause);
+        }
+
+    }
+
+    public void setUserRoleDao(IUserRoleDao userRoleDao) {
+        this.userRoleDao = userRoleDao;
+    }
+
+    /**
+     * The default role which will be assigned to all users.
+     *
+     * @param defaultRole the role name, including any desired prefix.
+     */
+    public void setDefaultRole(String defaultRole) {
+        Assert.notNull(defaultRole);
+        this.defaultRoleString = defaultRole;
+        this.defaultRole = new GrantedAuthorityImpl(defaultRole);
+    }
 }
