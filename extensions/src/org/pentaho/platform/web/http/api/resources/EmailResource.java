272aa2d41928ec973cfd4f76a2d94962a73aae96
mdamour1976 <mdamour1976@gmail.com>
2013-01-30 10:32:47 -0500

    Import cleanup

--- extensions/src/org/pentaho/platform/web/http/api/resources/EmailResource.java
+++ extensions/src/org/pentaho/platform/web/http/api/resources/EmailResource.java
@@ -28,8 +28,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.plugin.services.email.EmailConfiguration;
 import org.pentaho.platform.plugin.services.email.EmailService;
 
