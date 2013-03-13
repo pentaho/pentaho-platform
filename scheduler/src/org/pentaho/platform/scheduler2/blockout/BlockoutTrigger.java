86c5373e13f68033f09ee0e6cea8ee0d0eb58f9f
Willaim E. Seyler <wseyler@pentaho.com>
2013-03-13 11:37:42 -0400

    BISERVER-8101: Added implementation for blockout manager added test refactored the triggers

---
+++ scheduler/src/org/pentaho/platform/scheduler2/blockout/BlockoutTrigger.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2007 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * BI Platform.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ *
+ * @created Mar 12, 2013 
+ * @author wseyler
+ */
+
+
+package org.pentaho.platform.scheduler2.blockout;
+
+import java.util.Date;
+
+import org.quartz.SimpleTrigger;
+
+/**
+ * @author wseyler
+ *
+ */
+public class BlockoutTrigger extends SimpleTrigger {
+
+  private static final long serialVersionUID = 5382112382745363351L;
+  private long blockDuration;
+  
+  /**
+   * @param name
+   * @param group
+   * @param jobName
+   * @param jobGroup
+   * @param startTime
+   * @param endTime
+   * @param repeatCount
+   * @param repeatInterval
+   * @param blockDuration
+   */
+  public BlockoutTrigger(String name, String group, String jobName, String jobGroup, Date startTime, Date endTime,
+      int repeatCount, long repeatInterval, long blockDuration) {
+    super(name, group, jobName, jobGroup, startTime, endTime, repeatCount, repeatInterval);
+    this.blockDuration = blockDuration;
+  }
+
+  public long getBlockDuration() {
+    return blockDuration;
+  }
+
+  public void setBlockDuration(long blockDuration) {
+    this.blockDuration = blockDuration;
+  }
+}
