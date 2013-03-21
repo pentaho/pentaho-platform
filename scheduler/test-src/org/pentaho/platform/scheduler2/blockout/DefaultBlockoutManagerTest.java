855c8b2597886311e9dbfffc32513d571f68780b
Willaim E. Seyler <wseyler@pentaho.com>
2013-03-21 11:04:48 -0400

    Updated test to include UpdateBlockout test.

--- scheduler/test-src/org/pentaho/platform/scheduler2/blockout/DefaultBlockoutManagerTest.java
+++ scheduler/test-src/org/pentaho/platform/scheduler2/blockout/DefaultBlockoutManagerTest.java
@@ -158,7 +158,18 @@ public class DefaultBlockoutManagerTest {
    */
   @Test
   public void testUpdateBlockout() {
-    fail("Not yet implemented"); //$NON-NLS-1$
+    long INITIAL_BLOCK_DURATION = 50000l;
+    long UPDATED_BLOCK_DURATION = 1000000l;
+    SimpleBlockoutTrigger trigger = new SimpleBlockoutTrigger("updateBlockout", new Date(), null, -1, 1000000, INITIAL_BLOCK_DURATION); //$NON-NLS-1$
+    try {
+      blockOutManager.addBlockout(trigger);
+      SimpleBlockoutTrigger updatedTrigger = new SimpleBlockoutTrigger("updateBlockout", new Date(), null, -1, 1000000, UPDATED_BLOCK_DURATION); //$NON-NLS-1$
+      blockOutManager.updateBlockout(updatedTrigger.getName(), updatedTrigger);
+      SimpleBlockoutTrigger retrievedTrigger = (SimpleBlockoutTrigger) blockOutManager.getBlockout(updatedTrigger.getName());
+      assertEquals(UPDATED_BLOCK_DURATION, retrievedTrigger.getBlockDuration());
+    } catch (SchedulerException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   /**
