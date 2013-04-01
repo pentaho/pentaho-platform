26909bfe1f8359dc12d89734aed88ee327c4e871
Peter Minutillo <pminutillo@pentaho.com>
2013-04-01 07:41:36 -0400

    Squashed commit for LDAP changes BISERVER-8668 - Insufficient permissions for creating folders for LDAP users. Added roleMapper interface and class. Added to DefaultRoleUserDetailsServiceDecorator since it is already adding the default role Authenticated. Still need to find the right place to add the admin role
    BISERVER-8223 - Fixing LDAP on server
    
    BISERVER-8688 - Add default namespace to fix XPath query after XML DTD change
    
    BISERVER-8690 Create IRoleMapper impl for JDBC and LDAP

---
+++ api/src/org/pentaho/platform/api/engine/security/IRoleMapper.java
@@ -0,0 +1,42 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2013 Pentaho Corporation.  All rights reserved.
+ *
+ */
+package org.pentaho.platform.api.engine.security;
+
+/**
+ * Map ldap roles to pentaho security roles using map defined
+ * in spring configuration (applicationContext-spring-security-ldap.xml)
+ */
+public interface IRoleMapper {
+
+  /**
+   * Take a string name of third party role and return mapped pentaho security role
+   * @param ldapRole
+   * @return pentaho security role
+   */
+  public String toPentahoRole(String thirdPartyRole);
+
+  /**
+   * Take a pentaho security role and return mapped third party role
+   * @param pentahoRole
+   * @return ldap role
+   */
+  public String fromPentahoRole(String pentahoRole);
+
+  
+
+}
