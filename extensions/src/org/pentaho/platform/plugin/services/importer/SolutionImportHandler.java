94838055da7a4462007f4c57415685ba063823f3
Nick Baker <nbaker@pentaho.com>
2013-04-12 11:14:47 -0400

    Revert: [BIServer 6509] rewrite import/export to use latest pen: spring (6acf570)

--- extensions/src/org/pentaho/platform/plugin/services/importer/SolutionImportHandler.java
+++ extensions/src/org/pentaho/platform/plugin/services/importer/SolutionImportHandler.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -37,8 +36,6 @@ import org.apache.commons.logging.LogFactory;
 import org.pentaho.metadata.repository.DomainAlreadyExistsException;
 import org.pentaho.metadata.repository.DomainIdNullException;
 import org.pentaho.metadata.repository.DomainStorageException;
-import org.pentaho.platform.api.engine.IContentInfo;
-import org.pentaho.platform.api.engine.IPluginManager;
 import org.pentaho.platform.api.repository2.unified.RepositoryFile;
 import org.pentaho.platform.api.repository2.unified.RepositoryFileAcl;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
@@ -51,267 +48,225 @@ import org.pentaho.platform.repository2.unified.exportManifest.ExportManifestEnt
 
 public class SolutionImportHandler implements IPlatformImportHandler {
 
-  private static final Log log = LogFactory.getLog(SolutionImportHandler.class);
+	private static final Log log = LogFactory.getLog(SolutionImportHandler.class);
+	private IPlatformImportMimeResolver mimeResolver;
+	private List<String> blackList;
+	private List<String> whiteList;
+	private ExportManifest manifest;
 
-  private IPlatformImportMimeResolver mimeResolver;
+	public SolutionImportHandler(IPlatformImportMimeResolver mimeResolver) {
+		this.mimeResolver = mimeResolver;
+	}
 
-  private List<String> hideOnImport;
+	public void importFile(IPlatformImportBundle bundle) throws PlatformImportException, DomainIdNullException, DomainAlreadyExistsException, DomainStorageException, IOException {
 
-  private List<String> whiteList;
+		RepositoryFileImportBundle importBundle = (RepositoryFileImportBundle) bundle;
+		ZipInputStream zipImportStream = new ZipInputStream(bundle.getInputStream());
+		SolutionRepositoryImportSource importSource = new SolutionRepositoryImportSource(zipImportStream);
+		LocaleFilesProcessor localeFilesProcessor = new LocaleFilesProcessor();
 
-  private List<IContentInfo> contentInfoList;
+		IPlatformImporter importer = PentahoSystem.get(IPlatformImporter.class);
+		for (IRepositoryFileBundle file : importSource.getFiles()) {
+			String fileName = file.getFile().getName();
+			RepositoryFileImportBundle.Builder bundleBuilder = new RepositoryFileImportBundle.Builder();
+			String repositoryFilePath = RepositoryFilenameUtils.concat(PentahoPlatformImporter.computeBundlePath(file.getPath()), fileName);
 
-  private ExportManifest manifest;
+			// Validate against importing system related artifacts.
+			if (isSystemPath(repositoryFilePath)) {
+				log.trace("Skipping [" + repositoryFilePath + "], it is in admin / system folders");
+				continue;
+			}
 
+			InputStream bundleInputStream = null;
+			if (file.getFile().isFolder()) {
+				bundleBuilder.mime("text/directory");
+				bundleBuilder.file(file.getFile());
+				fileName = repositoryFilePath;
+				repositoryFilePath = importBundle.getPath();
+			} else {
+				byte[] bytes = IOUtils.toByteArray(file.getInputStream());
+				bundleInputStream = new ByteArrayInputStream(bytes);
+				// If is locale file store it for later processing.
+				if (localeFilesProcessor.isLocaleFile(file, importBundle.getPath(), bytes)) {
+				  log.trace("Skipping [" + repositoryFilePath + "], it is a locale property file");
+					continue;
+				}
+				bundleBuilder.input(bundleInputStream);
+				bundleBuilder.mime(mimeResolver.resolveMimeForFileName(fileName));
+				String filePath = (file.getPath().equals("/") || file.getPath().equals("\\")) ? "" : file.getPath();
+				repositoryFilePath = RepositoryFilenameUtils.concat(importBundle.getPath(), filePath);
+			}
 
-  public SolutionImportHandler(IPlatformImportMimeResolver mimeResolver, List<IContentInfo> aContentInfoList) {
-    this.mimeResolver = mimeResolver;
-    this.contentInfoList = aContentInfoList;
-  }
+			bundleBuilder.name(fileName);
+			bundleBuilder.path(repositoryFilePath);
+			
+	    String sourcePath;
+	    if (file.getPath().startsWith("/")) {
+	      sourcePath = RepositoryFilenameUtils.concat(file.getPath().substring(1), fileName);
+	    } else {
+	      if (file.getFile().isFolder()) {
+	          sourcePath = fileName;
+	        } else {
+	          sourcePath = RepositoryFilenameUtils.concat(file.getPath(), fileName);
+	      }
+	    }
 
-  public void importFile(IPlatformImportBundle bundle) throws PlatformImportException, DomainIdNullException,
-      DomainAlreadyExistsException, DomainStorageException, IOException {
+			bundleBuilder.charSet(bundle.getCharset());
+			bundleBuilder.overwriteFile(bundle.overwriteInRepository());
+			bundleBuilder.hidden(isFileHidden(bundle, sourcePath));
+			bundleBuilder.applyAclSettings(bundle.isApplyAclSettings());
+			bundleBuilder.retainOwnership(bundle.isRetainOwnership());
+			bundleBuilder.overwriteAclSettings(bundle.isOverwriteAclSettings());
+			bundleBuilder.acl(processAclForFile(bundle, sourcePath));
+			IPlatformImportBundle platformImportBundle = bundleBuilder.build();
+			importer.importFile(platformImportBundle);
 
-    RepositoryFileImportBundle importBundle = (RepositoryFileImportBundle) bundle;
-    ZipInputStream zipImportStream = new ZipInputStream(bundle.getInputStream());
-    SolutionRepositoryImportSource importSource = new SolutionRepositoryImportSource(zipImportStream);
-    LocaleFilesProcessor localeFilesProcessor = new LocaleFilesProcessor();
+			if (bundleInputStream != null) {
+				bundleInputStream.close();
+				bundleInputStream = null;
+			}
+		}
+		// Process locale files.
+		localeFilesProcessor.processLocaleFiles(importer);
+	}
 
-    IPlatformImporter importer = PentahoSystem.get(IPlatformImporter.class);
-    for (IRepositoryFileBundle file : importSource.getFiles()) {
-      String fileName = file.getFile().getName();
-      RepositoryFileImportBundle.Builder bundleBuilder = new RepositoryFileImportBundle.Builder();
-      String repositoryFilePath = RepositoryFilenameUtils.concat(
-          PentahoPlatformImporter.computeBundlePath(file.getPath()), fileName);
+	private RepositoryFileAcl processAclForFile(IPlatformImportBundle bundle, String filePath) {
+		// If we are not overwriting ACL's or owners then make sure a null gets in the bundle.
+		// If we are writing ACL's we'll have to check later in RepositoryFileImportHandler whether to overwrite
+		// based on the isOverwriteAcl setting and whether we are creating or updating the RepositoryFile.
+		RepositoryFileAcl acl = null;
+		if (bundle.isApplyAclSettings() || !bundle.isRetainOwnership()) {
+			try {
+				if (manifest != null) {
+					ExportManifestEntity entity = manifest.getExportManifestEntity(filePath);
+					if (entity != null) {
+						acl = entity.getRepositoryFileAcl();
+					}
+				}
+			} catch (Exception e) {
+				log.trace(e);
+			}
+		}
+		return acl;
+	}
+	
+	/**
+	 * Determines if the file or folder should be hidden.  If there is a manifest entry for
+	 * the file, and we are not ignoring the manifest, then set the hidden flag based on the
+	 * manifest.  Otherwise use the blacklist to determine if it is hidden.
+	 * 
+	 * @param bundle
+	 * @param filePath
+	 * @return true if file/folder should be hidden, false otherwise
+	 */
+	private boolean isFileHidden(IPlatformImportBundle bundle, String filePath) {
+	   if ((bundle.isApplyAclSettings() || !bundle.isRetainOwnership()) 
+	       && manifest !=null && manifest.getExportManifestEntity(filePath) != null ) {
+	         return manifest.getExportManifestEntity(filePath).getRepositoryFile().isHidden();
+	     }
+     return isBlackListed(bundle.getName());
+	}
 
-      // Validate against importing system related artifacts.
-      if (isSystemPath(repositoryFilePath)) {
-        log.trace("Skipping [" + repositoryFilePath + "], it is in admin / system folders");
-        continue;
-      }
+	private boolean isSystemPath(final String bundlePath) {
+		final String[] split = StringUtils.split(bundlePath, RepositoryFile.SEPARATOR);
+		return isSystemDir(split, 0) || isSystemDir(split, 1);
+	}
 
-      InputStream bundleInputStream = null;
-      if (file.getFile().isFolder()) {
-        bundleBuilder.mime("text/directory");
-        bundleBuilder.file(file.getFile());
-        fileName = repositoryFilePath;
-        repositoryFilePath = importBundle.getPath();
-      } else {
-        byte[] bytes = IOUtils.toByteArray(file.getInputStream());
-        bundleInputStream = new ByteArrayInputStream(bytes);
-        // If is locale file store it for later processing.
-        if (localeFilesProcessor.isLocaleFile(file, importBundle.getPath(), bytes)) {
-          log.trace("Skipping [" + repositoryFilePath + "], it is a locale property file");
-          continue;
-        }
-        bundleBuilder.input(bundleInputStream);
-        bundleBuilder.mime(mimeResolver.resolveMimeForFileName(fileName));
-        String filePath = (file.getPath().equals("/") || file.getPath().equals("\\")) ? "" : file.getPath();
-        repositoryFilePath = RepositoryFilenameUtils.concat(importBundle.getPath(), filePath);
-      }
+	private boolean isSystemDir(final String[] split, final int index) {
+		return (split != null && index < split.length && (StringUtils.equals(split[index], "system") || StringUtils.equals(split[index], "admin")));
+	}
 
-      bundleBuilder.name(fileName);
-      bundleBuilder.path(repositoryFilePath);
+	private boolean isBlackListed(String fileName) {
+		boolean isBlackListed = false;
+		for (String extension : blackList) {
+			if (fileName.endsWith(extension)) {
+				isBlackListed = true;
+				break;
+			}
+		}
+		return isBlackListed;
+	}
 
-      String sourcePath;
-      if (file.getPath().startsWith("/")) {
-        sourcePath = RepositoryFilenameUtils.concat(file.getPath().substring(1), fileName);
-      } else {
-        if (file.getFile().isFolder()) {
-          sourcePath = fileName;
-        } else {
-          sourcePath = RepositoryFilenameUtils.concat(file.getPath(), fileName);
-        }
-      }
+	public void setBlackList(List blackList) {
+		this.blackList = blackList;
+	}
 
-      bundleBuilder.charSet(bundle.getCharset());
-      bundleBuilder.overwriteFile(bundle.overwriteInRepository());
-      bundleBuilder.hidden(isFileHidden(bundle, sourcePath));
-      bundleBuilder.applyAclSettings(bundle.isApplyAclSettings());
-      bundleBuilder.retainOwnership(bundle.isRetainOwnership());
-      bundleBuilder.overwriteAclSettings(bundle.isOverwriteAclSettings());
-      bundleBuilder.acl(processAclForFile(bundle, sourcePath));
-      IPlatformImportBundle platformImportBundle = bundleBuilder.build();
-      importer.importFile(platformImportBundle);
+	public void setWhiteList(List whiteList) {
+		this.whiteList = whiteList;
+	}
 
-      if (bundleInputStream != null) {
-        bundleInputStream.close();
-        bundleInputStream = null;
-      }
-    }
-    // Process locale files.
-    initContentInfo();
-    localeFilesProcessor.processLocaleFiles(importer);
-  }
+	class SolutionRepositoryImportSource {
+		private ZipInputStream zipInputStream;
+		private List<IRepositoryFileBundle> files;
 
-  private void initContentInfo() {
-    if (whiteList == null) {
-      whiteList = new ArrayList();
-    }
-    for (IContentInfo type : contentInfoList) {
-      if (!whiteList.contains(type.getExtension())) {
-        whiteList.add(type.getExtension());
-      }
-    }
-  }
+		public SolutionRepositoryImportSource(final ZipInputStream zipInputStream) {
+			this.zipInputStream = zipInputStream;
+			this.files = new ArrayList<IRepositoryFileBundle>();
+			initialize();
+		}
 
-  private RepositoryFileAcl processAclForFile(IPlatformImportBundle bundle, String filePath) {
-    // If we are not overwriting ACL's or owners then make sure a null gets in the bundle.
-    // If we are writing ACL's we'll have to check later in RepositoryFileImportHandler whether to overwrite
-    // based on the isOverwriteAcl setting and whether we are creating or updating the RepositoryFile.
-    RepositoryFileAcl acl = null;
-    if (bundle.isApplyAclSettings() || !bundle.isRetainOwnership()) {
-      try {
-        if (manifest != null) {
-          ExportManifestEntity entity = manifest.getExportManifestEntity(filePath);
-          if (entity != null) {
-            acl = entity.getRepositoryFileAcl();
-          }
-        }
-      } catch (Exception e) {
-        log.trace(e);
-      }
-    }
-    return acl;
-  }
+		protected void initialize() {
+			try {
+				ZipEntry entry = zipInputStream.getNextEntry();
+				while (entry != null) {
+					final String entryName = RepositoryFilenameUtils.separatorsToRepository(entry.getName());
+					File tempFile = null;
+					boolean isDir = entry.isDirectory();
+					if (!isDir) {
+						if (!isWhiteListed(entryName)) {
+							zipInputStream.closeEntry();
+							entry = zipInputStream.getNextEntry();
+							continue;
+						}
+						tempFile = File.createTempFile("zip", null);
+						tempFile.deleteOnExit();
+						FileOutputStream fos = new FileOutputStream(tempFile);
+						IOUtils.copy(zipInputStream, fos);
+						fos.close();
+					}
+					File file = new File(entryName);
+					RepositoryFile repoFile = new RepositoryFile.Builder(file.getName()).folder(isDir).hidden(false).build();
+					String parentDir = new File(entryName).getParent() == null ? RepositoryFile.SEPARATOR : new File(entryName).getParent() + RepositoryFile.SEPARATOR;
+					IRepositoryFileBundle repoFileBundle = new RepositoryFileBundle(repoFile, null, parentDir, tempFile, "UTF-8", null);
 
-  /**
-   * Determines if the file or folder should be hidden.  If there is a manifest entry for
-   * the file, and we are not ignoring the manifest, then set the hidden flag based on the
-   * manifest.  Otherwise use the blacklist to determine if it is hidden.
-   * 
-   * @param bundle
-   * @param filePath
-   * @return true if file/folder should be hidden, false otherwise
-   */
-  private boolean isFileHidden(IPlatformImportBundle bundle, String filePath) {
-    if ((bundle.isApplyAclSettings() || !bundle.isRetainOwnership()) && manifest != null
-        && manifest.getExportManifestEntity(filePath) != null) {
-      return manifest.getExportManifestEntity(filePath).getRepositoryFile().isHidden();
-    }
-    return hideOnImportExtensions(bundle.getName());
-  }
+					if (file.getName().equals("exportManifest.xml")) {
+						initializeAclManifest(repoFileBundle);
+					} else {
+						files.add(repoFileBundle);
+					}
+					zipInputStream.closeEntry();
+					entry = zipInputStream.getNextEntry();
+				}
+				zipInputStream.close();
+			} catch (IOException exception) {
+				final String errorMessage = Messages.getInstance().getErrorString("", exception.getLocalizedMessage());
+				log.trace(errorMessage);
+			}
+		}
 
-  private boolean isSystemPath(final String bundlePath) {
-    final String[] split = StringUtils.split(bundlePath, RepositoryFile.SEPARATOR);
-    return isSystemDir(split, 0) || isSystemDir(split, 1);
-  }
+		private void initializeAclManifest(IRepositoryFileBundle file) {
+			try {
+				byte[] bytes = IOUtils.toByteArray(file.getInputStream());
+				ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+				manifest = ExportManifest.fromXml(in);
+			} catch (Exception e) {
+				log.trace(e);
+			}
+		}
 
-  private boolean isSystemDir(final String[] split, final int index) {
-    return (split != null && index < split.length && (StringUtils.equals(split[index], "system") || StringUtils.equals(
-        split[index], "admin")));
-  }
+		private boolean isWhiteListed(String fileName) {
+			boolean isWhiteListed = false;
+			for (String extension : whiteList) {
+				if (fileName.endsWith(extension)) {
+					isWhiteListed = true;
+					break;
+				}
+			}
+			return isWhiteListed;
+		}
 
-  /**
-   * hide file on import
-   * @param fileName
-   * @return
-   */
-  private boolean hideOnImportExtensions(String fileName) {
-    boolean hideExtension = false;
-    for (String extension : hideOnImport) {
-      if (fileName.endsWith(extension)) {
-        hideExtension = true;
-        break;
-      }
-    }
-    return hideExtension;
-  }
-
-  public void setHideOnImport(List hideonimport) {
-    this.hideOnImport = hideonimport;
-  }
-
-  public void setWhiteList(List whiteList) {
-    this.whiteList = whiteList;
-  }
-
-  class SolutionRepositoryImportSource {
-    private static final String EXPORT_MANIFEST_XML = "exportManifest.xml";
-
-    private ZipInputStream zipInputStream;
-
-    private List<IRepositoryFileBundle> files;
-
-    public SolutionRepositoryImportSource(final ZipInputStream zipInputStream) {
-      this.zipInputStream = zipInputStream;
-      this.files = new ArrayList<IRepositoryFileBundle>();
-      initialize();
-    }
-
-    protected void initialize() {
-      try {
-        ZipEntry entry = zipInputStream.getNextEntry();
-        while (entry != null) {
-          final String entryName = RepositoryFilenameUtils.separatorsToRepository(entry.getName());
-          File tempFile = null;
-          boolean isDir = entry.isDirectory();
-          if (!isDir) {
-            if (!canExtensionBeImported(entryName)) {
-              zipInputStream.closeEntry();
-              entry = zipInputStream.getNextEntry();
-              continue;
-            }
-            tempFile = File.createTempFile("zip", null);
-            tempFile.deleteOnExit();
-            FileOutputStream fos = new FileOutputStream(tempFile);
-            IOUtils.copy(zipInputStream, fos);
-            fos.close();
-          }
-          File file = new File(entryName);
-          RepositoryFile repoFile = new RepositoryFile.Builder(file.getName()).folder(isDir).hidden(false).build();
-          String parentDir = new File(entryName).getParent() == null ? RepositoryFile.SEPARATOR : new File(entryName)
-              .getParent() + RepositoryFile.SEPARATOR;
-          IRepositoryFileBundle repoFileBundle = new RepositoryFileBundle(repoFile, null, parentDir, tempFile, "UTF-8",
-              null);
-
-          if (file.getName().equals(EXPORT_MANIFEST_XML)) {
-            initializeAclManifest(repoFileBundle);
-          } else {
-            files.add(repoFileBundle);
-          }
-          zipInputStream.closeEntry();
-          entry = zipInputStream.getNextEntry();
-        }
-        zipInputStream.close();
-      } catch (IOException exception) {
-        final String errorMessage = Messages.getInstance().getErrorString("", exception.getLocalizedMessage());
-        log.trace(errorMessage);
-      }
-    }
-
-    private void initializeAclManifest(IRepositoryFileBundle file) {
-      try {
-        byte[] bytes = IOUtils.toByteArray(file.getInputStream());
-        ByteArrayInputStream in = new ByteArrayInputStream(bytes);
-        manifest = ExportManifest.fromXml(in);
-      } catch (Exception e) {
-        log.trace(e);
-      }
-    }
-
-    private boolean canExtensionBeImported(String fileName) {
-      boolean isWhiteListed = false;
-      for (String extension : whiteList) {
-        if (fileName.endsWith(extension)) {
-          isWhiteListed = true;
-          break;
-        }
-      }
-      return isWhiteListed;
-    }
-
-    public List<IRepositoryFileBundle> getFiles() {
-      return this.files;
-    }
-  }
-
-  public List<String> getHideOnImport() {
-    return hideOnImport;
-  }
-
-  public List<String> getWhiteList() {
-    return whiteList;
-  }
-
+		public List<IRepositoryFileBundle> getFiles() {
+			return this.files;
+		}
+	}
 }
