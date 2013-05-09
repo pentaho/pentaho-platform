2e2664263ad31614ec13e247c5de10811e3d706a
mdamour1976 <mdamour1976@gmail.com>
2013-05-09 11:51:34 -0400

    BISERVER-8759 Fixing edit mode enable/disable

--- user-console/source/org/pentaho/mantle/client/events/SolutionBrowserSelectEvent.java
+++ user-console/source/org/pentaho/mantle/client/events/SolutionBrowserSelectEvent.java
@@ -23,7 +23,7 @@ import org.pentaho.mantle.client.solutionbrowser.filelist.FileItem;
 import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.user.client.ui.Widget;
 
-public class SolutionBrowserSelectEvent extends GwtEvent<SolutionBrowserSelectEventHandler> {
+public class SolutionBrowserSelectEvent extends GwtEvent<SolutionBrowserSelectEventHandler> implements ISolutionBrowserEvent {
 
   public static Type<SolutionBrowserSelectEventHandler> TYPE = new Type<SolutionBrowserSelectEventHandler>();
   
