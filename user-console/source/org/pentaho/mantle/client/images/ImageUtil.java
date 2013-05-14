959db621d1040ad4f9e7c3e2ba582d987e490dad
Rob Fellows <rfellows@pentaho.com>
2013-05-13 23:02:47 -0400

    BISERVER-9050 - Made the close icon on closeable tabs themeable.

--- user-console/source/org/pentaho/mantle/client/images/ImageUtil.java
+++ user-console/source/org/pentaho/mantle/client/images/ImageUtil.java
@@ -1,29 +1,16 @@
 package org.pentaho.mantle.client.images;
 
-import com.google.gwt.user.client.ui.Image;
-
 /**
  * User: RFellows
  * Date: 5/8/13
  */
-public class ImageUtil {
+public class ImageUtil extends org.pentaho.gwt.widgets.client.utils.ImageUtil {
 
-  public static final String BLANK_IMAGE_PATH = "mantle/images/spacer.gif";
+  private static final String BLANK_IMAGE_PATH = "mantle/images/spacer.gif";
 
-  /**
-   * Returns a GWT Image with the src value set to a blank image and applies the provided css style to the element
-   * to allow for a background image to be used instead.
-   * @param cssStyleName
-   * @return
-   */
-  public static Image getThemeableImage(String... cssStyleName) {
-    Image image = new Image(BLANK_IMAGE_PATH);
-
-    for(String style : cssStyleName) {
-      image.addStyleName(style);
-    }
-
-    return image;
+  @Override
+  public String getBlankImagePath() {
+    return BLANK_IMAGE_PATH;
   }
 
 }
