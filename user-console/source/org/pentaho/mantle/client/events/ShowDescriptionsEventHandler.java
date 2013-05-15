d5c285573263d97eb2012c950d3852d441735881
diogofscmariano <diogo.mariano@webdetails.pt>
2013-05-15 17:30:30 +0100

    BISERVER-8738 - Included remaining UX team feedback

---
+++ user-console/source/org/pentaho/mantle/client/events/ShowDescriptionsEventHandler.java
@@ -0,0 +1,10 @@
+package org.pentaho.mantle.client.events;
+
+import com.google.gwt.event.shared.EventHandler;
+
+/**
+ * @author Diogo Mariano
+ */
+public interface ShowDescriptionsEventHandler extends EventHandler{
+  void onEdit(ShowDescriptionsEvent event);
+}
