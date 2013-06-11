e0d1371e0b67d073c142d66cae6628687cbf0c58
Rowell Belen <rbelen@pentaho.com>
2013-06-11 11:30:40 -0400

    Adding copyright header

--- user-console/source/org/pentaho/mantle/client/dialogs/scheduling/ScheduleOutputLocationDialog.java
+++ user-console/source/org/pentaho/mantle/client/dialogs/scheduling/ScheduleOutputLocationDialog.java
@@ -1,3 +1,20 @@
+/*
+\ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2013 Pentaho Corporation.  All rights reserved.
+ *
+ */
 package org.pentaho.mantle.client.dialogs.scheduling;
 
 import com.google.gwt.event.dom.client.*;
@@ -11,9 +28,7 @@ import org.pentaho.gwt.widgets.client.utils.string.StringUtils;
 import org.pentaho.mantle.client.dialogs.SelectFolderDialog;
 import org.pentaho.mantle.client.messages.Messages;
 
-/**
- * @author Rowell Belen
- */
+
 public abstract class ScheduleOutputLocationDialog extends PromptDialogBox
 {
   private String filePath;
