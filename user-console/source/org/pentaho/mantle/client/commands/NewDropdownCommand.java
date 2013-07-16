f49e9194c61867f499887a3d19eaf711a955b0dc
Michael D'Amour <mdamour1976@gmail.com>
2013-07-16 07:28:17 -0400

    BISERVER-9662 PUC Toolbar - "Create New" Icon Consolidation

--- user-console/source/org/pentaho/mantle/client/commands/NewDropdownCommand.java
+++ user-console/source/org/pentaho/mantle/client/commands/NewDropdownCommand.java
@@ -19,7 +19,9 @@ package org.pentaho.mantle.client.commands;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.pentaho.gwt.widgets.client.dialogs.GlassPane;
 import org.pentaho.gwt.widgets.client.dialogs.MessageDialogBox;
+import org.pentaho.gwt.widgets.client.utils.FrameUtils;
 import org.pentaho.mantle.client.messages.Messages;
 import org.pentaho.mantle.client.objects.JsCreateNewConfig;
 import org.pentaho.mantle.client.objects.JsCreateNewConfigComparator;
@@ -28,20 +30,28 @@ import org.pentaho.mantle.client.solutionbrowser.SolutionBrowserPanel;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.JsonUtils;
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class NewDropdownCommand extends AbstractCommand {
 
+  private static FocusPanel pageBackground = null;
   private Widget anchorWidget;
 
   public NewDropdownCommand() {
@@ -56,9 +66,78 @@ public class NewDropdownCommand extends AbstractCommand {
   }
 
   protected void performOperation(boolean feedback) {
+    final PopupPanel popup = new PopupPanel(true, false) {
+      public void show() {
+        // show glass pane
+        super.show();
+        if (pageBackground == null) {
+          pageBackground = new FocusPanel() {
+            public void onBrowserEvent(Event event) {
+              int type = event.getTypeInt();
+              switch (type) {
+              case Event.ONKEYDOWN: {
+                if ((char) event.getKeyCode() == KeyCodes.KEY_ESCAPE) {
+                  event.stopPropagation();
+                  hide();
+                }
+                return;
+              }
+              }
+              super.onBrowserEvent(event);
+            };
+          };
+          pageBackground.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+              hide();
+              pageBackground.setVisible(false);
+              pageBackground.getElement().getStyle().setDisplay(Display.NONE);
+            }
+          });
+          RootPanel.get().add(pageBackground, 0, 0);
+        }
+        pageBackground.setSize("100%", Window.getClientHeight() + Window.getScrollTop() + "px"); //$NON-NLS-1$ //$NON-NLS-2$
+        pageBackground.setVisible(true);
+        pageBackground.getElement().getStyle().setDisplay(Display.BLOCK);
+
+        // hide <embeds>
+        // TODO: migrate to GlassPane Listener
+        FrameUtils.toggleEmbedVisibility(false);
+
+        // Notify listeners that we're showing a dialog (hide PDFs, flash).
+        GlassPane.getInstance().show();
+      }
+
+      public void hide(boolean autoClosed) {
+        super.hide(autoClosed);
+        pageBackground.setVisible(false);
+        GlassPane.getInstance().hide();
+      }
+
+      protected void onPreviewNativeEvent(final NativePreviewEvent event) {
+        // Switch on the event type
+        int type = event.getTypeInt();
+        switch (type) {
+        case Event.ONKEYDOWN: {
+          Event nativeEvent = Event.as(event.getNativeEvent());
+          if ((char) nativeEvent.getKeyCode() == KeyCodes.KEY_ESCAPE) {
+            event.cancel();
+            hide();
+          }
+          return;
+        }
+        }
+      };
+    };
+
+    if (popup.isShowing()) {
+      popup.hide();
+      return;
+    }
+
     String url = GWT.getHostPageBaseURL() + "api/plugin-manager/settings/new-toolbar-button"; //$NON-NLS-1$
     RequestBuilder rb = new RequestBuilder(RequestBuilder.GET, url);
     rb.setHeader("Content-Type", "text/plain"); //$NON-NLS-1$//$NON-NLS-2$
+    rb.setHeader("accept", "application/json"); //$NON-NLS-1$//$NON-NLS-2$
     try {
       rb.sendRequest(null, new RequestCallback() {
 
@@ -74,26 +153,31 @@ public class NewDropdownCommand extends AbstractCommand {
           if (response.getStatusCode() == 200) {
             JsArray<JsCreateNewConfig> jsarray = parseJson(JsonUtils.escapeJsonForEval(response.getText()));
             final ArrayList<JsCreateNewConfig> sorted = new ArrayList<JsCreateNewConfig>();
-            for (int i=0;i<jsarray.length();i++) {
+            for (int i = 0; i < jsarray.length(); i++) {
               sorted.add(jsarray.get(i));
             }
             Collections.sort(sorted, new JsCreateNewConfigComparator());
-            final PopupPanel popup = new PopupPanel(true, false);
+            popup.setStyleName("newToolbarDropdown");
             VerticalPanel buttonPanel = new VerticalPanel();
             popup.add(buttonPanel);
-            for (int i=0;i<sorted.size();i++) {
+            for (int i = 0; i < sorted.size(); i++) {
               final int finali = i;
               Button button = new Button(Messages.getString(sorted.get(i).getLabel()));
               button.setStyleName("pentaho-button");
+              button.getElement().addClassName("newToolbarDropdownButton");
               button.addClickHandler(new ClickHandler() {
                 public void onClick(ClickEvent event) {
-                  SolutionBrowserPanel.getInstance().getContentTabPanel().showNewURLTab(Messages.getString(sorted.get(finali).getTabName()), Messages.getString(sorted.get(finali).getTabName()), sorted.get(finali).getActionUrl(), false);
+                  SolutionBrowserPanel
+                      .getInstance()
+                      .getContentTabPanel()
+                      .showNewURLTab(Messages.getString(sorted.get(finali).getTabName()), Messages.getString(sorted.get(finali).getTabName()),
+                          sorted.get(finali).getActionUrl(), false);
                   popup.hide();
                 }
               });
               buttonPanel.add(button);
             }
-            popup.setPopupPosition(anchorWidget.getAbsoluteLeft(), anchorWidget.getAbsoluteTop()+anchorWidget.getOffsetHeight());
+            popup.setPopupPosition(anchorWidget.getAbsoluteLeft(), anchorWidget.getAbsoluteTop() + anchorWidget.getOffsetHeight());
             popup.show();
           } else {
             MessageDialogBox dialogBox = new MessageDialogBox(Messages.getString("error"), Messages.getString("error"), //$NON-NLS-1$ //$NON-NLS-2$
@@ -115,8 +199,8 @@ public class NewDropdownCommand extends AbstractCommand {
   /*-{
     var obj = eval('(' + json + ')');
     return obj.setting;
-  }-*/;  
-  
+  }-*/;
+
   public Widget getAnchorWidget() {
     return anchorWidget;
   }
