3355e368de513e1910fa2784bff9dabf21826ca8
Bryan Rosander <brosander@pentaho.com>
2013-08-09 13:58:00 -0400

    BISERVER-9672 - Changed caching implementation to look for users by their username, not principleId, added dummy implementation of UserDetails to cache users that can't be found

--- extensions/src/org/pentaho/platform/plugin/services/security/userrole/PentahoCachingUserDetailsService.java
+++ extensions/src/org/pentaho/platform/plugin/services/security/userrole/PentahoCachingUserDetailsService.java
@@ -3,13 +3,11 @@ package org.pentaho.platform.plugin.services.security.userrole;
 import org.pentaho.platform.api.mt.ITenant;
 import org.pentaho.platform.api.mt.ITenantedPrincipleNameResolver;
 import org.pentaho.platform.repository2.unified.jcr.JcrTenantUtils;
-import org.springframework.security.userdetails.UserDetailsService;
-
-
 import org.springframework.security.providers.dao.UserCache;
 import org.springframework.security.providers.dao.cache.NullUserCache;
 import org.springframework.security.userdetails.UserDetails;
-import org.springframework.util.Assert;
+import org.springframework.security.userdetails.UserDetailsService;
+import org.springframework.security.userdetails.UsernameNotFoundException;
 
 /**
  * User: nbaker
@@ -35,25 +33,45 @@ public class PentahoCachingUserDetailsService implements UserDetailsService {
   public void setUserCache(UserCache userCache) {
     this.userCache = userCache;
   }
+  
+  /**
+   * Gets the principle id from the principle name
+   * TODO: This method will not work with multi-tenancy
+   */
+  private String getPrincipleId(String principleName) {
+    ITenant tenant = JcrTenantUtils.getCurrentTenant();
+    if (tenant == null || tenant.getId() == null) {
+      tenant = JcrTenantUtils.getDefaultTenant();
+    }
+    return nameResolver.getPrincipleId(tenant, principleName);
+  }
 
   public UserDetails loadUserByUsername(String username) {
+    boolean tenanted = JcrTenantUtils.isTenantedUser(username);
+    String principleName = tenanted ? JcrTenantUtils.getPrincipalName(username, true) : username;
 
-    // we need to have a tenanted form of the username as the cache will be populated with names as such.
-    if(!JcrTenantUtils.isTenantedUser(username)){
-      ITenant tenant = JcrTenantUtils.getCurrentTenant();
-      if (tenant == null || tenant.getId() == null) {
-        tenant = JcrTenantUtils.getDefaultTenant();
-      }
-      username = nameResolver.getPrincipleId(tenant, username);
-    }
-    UserDetails user = userCache.getUserFromCache(username);
+    UserDetails user = userCache.getUserFromCache(principleName);
+
     if (user == null) {
-      user = delegate.loadUserByUsername(username);
+      String principleId = tenanted ? username : getPrincipleId(username);
+      try {
+        user = delegate.loadUserByUsername(principleId);
+      } catch (UsernameNotFoundException e) {
+        user = new NotFoundUserDetails(principleName, e);
+      }
+      
+      if (user == null) {
+        user = new NotFoundUserDetails(principleName, new UsernameNotFoundException("UserDetailsService " + delegate
+            + " returned null for username " + username + ". " + "This is an interface contract violation"));
+      }
+      
       userCache.putUserInCache(user);
     }
 
-    Assert.notNull(user, "UserDetailsService " + delegate + " returned null for username " + username + ". " +
-        "This is an interface contract violation");
+    if (user instanceof NotFoundUserDetails) {
+      UsernameNotFoundException e = ((NotFoundUserDetails) user).getOriginalException();
+      throw new UsernameNotFoundException(e.getMessage(), e);
+    }
 
     return user;
   }
