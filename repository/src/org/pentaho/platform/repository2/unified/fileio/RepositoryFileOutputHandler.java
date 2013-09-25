f6768fd5a33a38f98fa1b461854bb32f66c729a6
mdamour1976 <mdamour1976@gmail.com>
2013-09-25 11:02:58 -0400

    BISERVER-10234 XActions which have 'contentRepo' output are failing and throw exceptions during scheduler execution BISERVER-10120 A lot of samples in bidevelopers are broken

--- repository/src/org/pentaho/platform/repository2/unified/fileio/RepositoryFileOutputHandler.java
+++ repository/src/org/pentaho/platform/repository2/unified/fileio/RepositoryFileOutputHandler.java
@@ -1,3 +1,20 @@
+/*
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License, version 2 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+*
+* Copyright 2006 - 2013 Pentaho Corporation.  All rights reserved.
+*/
 package org.pentaho.platform.repository2.unified.fileio;
 
 import java.io.IOException;
@@ -8,6 +25,7 @@ import org.pentaho.platform.api.engine.IMimeTypeListener;
 import org.pentaho.platform.api.engine.IOutputHandler;
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.api.repository.IContentItem;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.util.web.MimeHelper;
 
@@ -70,7 +88,13 @@ public class RepositoryFileOutputHandler implements IOutputHandler {
         output = PentahoSystem.getOutputDestinationFromContentRef(outputName + ":" + contentName, session); //$NON-NLS-1$
       }
       if (output != null) {
+    	output.setSession(PentahoSessionHolder.getSession());
         output.setInstanceId(instanceId);
+        String filePath = "~/workspace/" + contentName; //$NON-NLS-1$
+        if (contentName.startsWith("/")) {
+        	filePath = contentName;
+        }
+        output.setSolutionPath(filePath);
         output.setMimeType(localMimeType);
         outputContentItem = output.getFileOutputContentItem();
       }
