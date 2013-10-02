8f3c85d5c91a549634daee46edfed0435184ad4a
Ramaiz Mansoor <rmansoor@pentaho.com>
2013-10-02 09:18:50 -0400

    BISERVER-10129 - Updated REST services to have admin check before processing the requests. Non admin will receive a unauthorized when they access these REST services

--- extensions/src/org/pentaho/platform/web/http/api/resources/SystemPermissionsResource.java
+++ extensions/src/org/pentaho/platform/web/http/api/resources/SystemPermissionsResource.java
@@ -18,12 +18,20 @@
 package org.pentaho.platform.web.http.api.resources;
 
 
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.pentaho.platform.api.engine.IAuthorizationPolicy;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.security.policy.rolebased.actions.AdministerSecurityAction;
+import org.pentaho.platform.security.policy.rolebased.actions.RepositoryCreateAction;
+import org.pentaho.platform.security.policy.rolebased.actions.RepositoryReadAction;
 
 @Path("/legacy/permissions")
 public class SystemPermissionsResource extends AbstractJaxRSResource {
@@ -32,9 +40,22 @@ public class SystemPermissionsResource extends AbstractJaxRSResource {
     @Produces({MediaType.APPLICATION_XML})
     public Response getLegacyPermissions()  throws Exception {
         try {
-            return Response.ok(SystemResourceUtil.getPermissions().asXML()).type(MediaType.APPLICATION_XML).build();
+            if(canAdminister()) {
+              return Response.ok(SystemResourceUtil.getPermissions().asXML()).type(MediaType.APPLICATION_XML).build();  
+            } else {
+              return Response.status(UNAUTHORIZED).build();
+            }
+            
         } catch (Throwable t) {
           throw new WebApplicationException(t);
         }
+    }
+    
+    private boolean canAdminister() {
+      IAuthorizationPolicy policy = PentahoSystem
+          .get(IAuthorizationPolicy.class);
+      return policy
+          .isAllowed(RepositoryReadAction.NAME) && policy.isAllowed(RepositoryCreateAction.NAME)
+          && (policy.isAllowed(AdministerSecurityAction.NAME));
     }
 }
