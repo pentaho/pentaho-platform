e1d46b51c4df0e6d487a5267a9ce877b360a9cca
Ramaiz Mansoor <rmansoor@pentaho.com>
2013-10-07 11:10:11 -0400

    BISERVER-10302 - Updating the start pentaho script to have different memory configurations for 32 bit vs 64 bit OS

--- assembly/package-res/biserver/start-pentaho.bat
+++ assembly/package-res/biserver/start-pentaho.bat
@@ -10,7 +10,14 @@ call set-pentaho-env.bat "%~dp0jre"
 
 cd tomcat\bin
 set CATALINA_HOME=%~dp0tomcat
-set CATALINA_OPTS=-Xmx2048m -Xmx2048m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000
+
+SET BITS=64
+for /f %%a in ('echo %PROCESSOR_ARCHITECTURE% ^| "%SystemRoot%\system32\find" /c "64"') do if not "%%a"=="1" SET BITS=32
+IF "%BITS%" == "64" (
+  set CATALINA_OPTS=-Xms1024m -Xmx2048m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000
+) ELSE (
+  set CATALINA_OPTS=-Xms256m -Xmx768m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000
+)
 
 rem Make sure we set the appropriate variable so Tomcat can start (e.g. JAVA_HOME iff. _PENTAHO_JAVA_HOME points to a JDK)
 if not exist "%_PENTAHO_JAVA_HOME%\bin\jdb.exe" goto noJdk
