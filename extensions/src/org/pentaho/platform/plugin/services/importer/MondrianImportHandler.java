6a8f367c3aab22730097fd0c9c138fed4fb65f8d
Tyler Band <tband@pentaho.com>
2013-10-11 10:20:51 -0400

    BISERVER-10299 add enableXmla flag to parameters

--- extensions/src/org/pentaho/platform/plugin/services/importer/MondrianImportHandler.java
+++ extensions/src/org/pentaho/platform/plugin/services/importer/MondrianImportHandler.java
@@ -156,7 +156,9 @@ public class MondrianImportHandler implements IPlatformImportHandler {
     if (dsName != null) {
         sb.append("DataSource=" + dsName + ";");
     }
-
+    if(!parameters.containsKey("EnableXmla")){
+      sb.append("EnableXmla=" + xmlaEnabled +";");
+    }
     sb.append("Provider=" + provider);
 
     // Build a list of the remaining properties
