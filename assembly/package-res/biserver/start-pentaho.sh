1db59234f9a7d528eb69b67db8ea0e915e256b0a
Ramaiz Mansoor <rmansoor@pentaho.com>
2013-10-18 07:39:12 -0400

    Backing out the change for BISERVER-10302 for sh script

--- assembly/package-res/biserver/start-pentaho.sh
+++ assembly/package-res/biserver/start-pentaho.sh
@@ -20,13 +20,7 @@ if [ -e "$DIR/promptuser.sh" ]; then
 fi
 if [ "$?" = 0 ]; then
   cd "$DIR/tomcat/bin"
-  
-  linuxbitness=`getconf LONG_BIT`
-  if [ ${linuxbitness} == '64' ]; then
-    export CATALINA_OPTS="-Xms1024m -Xmx2048m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
-  else
-    export CATALINA_OPTS="-Xms256m -Xmx768m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
-  fi
+  export CATALINA_OPTS="-Xmx2048m -Xmx2048m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
   JAVA_HOME=$_PENTAHO_JAVA_HOME
   sh startup.sh
 fi
