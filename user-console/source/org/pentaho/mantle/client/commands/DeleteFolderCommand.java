032479ad79e4d25c4fef14c8027ea0f9863cf4fc
e-cuellar <ecuellar@pentaho.com>
2013-11-22 10:35:32 -0500

    Fix for SP-588 Backport of BISERVER-10297 - Able to inject tags into the dialog (5.0)

--- user-console/source/org/pentaho/mantle/client/commands/DeleteFolderCommand.java
+++ user-console/source/org/pentaho/mantle/client/commands/DeleteFolderCommand.java
@@ -85,7 +85,8 @@ public class DeleteFolderCommand extends AbstractCommand {
 
     final String filesList = repositoryFile.getId();
     final String folderName = repositoryFile.getTitle() == null ? repositoryFile.getName() : repositoryFile.getTitle();
-    final HTML messageTextBox = new HTML(Messages.getString("moveToTrashQuestionFolder",folderName));
+    final HTML messageTextBox =
+        new HTML( Messages.getString( "moveToTrashQuestionFolder", escapeHtmlEntities( folderName ) ) );
     final PromptDialogBox folderDeleteWarningDialogBox = new PromptDialogBox(Messages.getString("moveToTrash"), Messages.getString("yesMoveToTrash"), Messages.getString("no"), true, true);
     folderDeleteWarningDialogBox.setContent(messageTextBox);
 
@@ -143,5 +144,9 @@ public class DeleteFolderCommand extends AbstractCommand {
 
 
   }
-
+  
+  public static String escapeHtmlEntities( String text ) {
+    return text.replace( "&", "&amp;" ).replace( "\"", "&quot;" ).replace( "'", "&apos;" ).replace( "<", "&lt;" )
+      .replace( ">", "&gt;" );
+  }  
 }
