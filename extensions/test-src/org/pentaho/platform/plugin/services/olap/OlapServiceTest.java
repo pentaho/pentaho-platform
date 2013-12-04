1f95c2c83c1dc399355d8e433dd8c7b6b19e46ec
Luc Boudreau <lucboudreau@gmail.com>
2013-12-04 10:52:20 -0500

    [ANALYZER-2197] Fixes some tests in OlapServiceTest.

--- extensions/test-src/org/pentaho/platform/plugin/services/olap/OlapServiceTest.java
+++ extensions/test-src/org/pentaho/platform/plugin/services/olap/OlapServiceTest.java
@@ -16,75 +16,82 @@ package org.pentaho.platform.plugin.services.olap;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.Properties;
 
 import junit.framework.TestCase;
 
+import org.pentaho.database.model.DatabaseConnection;
+import org.pentaho.database.model.IDatabaseConnection;
+import org.pentaho.database.service.DatabaseDialectService;
+import org.pentaho.database.service.IDatabaseDialectService;
+import org.pentaho.platform.api.engine.ICacheManager;
+import org.pentaho.platform.api.engine.IPentahoSession;
+import org.pentaho.platform.api.engine.ISolutionEngine;
+import org.pentaho.platform.api.engine.IUserRoleListService;
+import org.pentaho.platform.api.engine.IPentahoDefinableObjectFactory.Scope;
+import org.pentaho.platform.api.repository2.unified.IRepositoryFileData;
 import org.pentaho.platform.api.repository2.unified.IUnifiedRepository;
 import org.pentaho.platform.api.repository2.unified.RepositoryFile;
+import org.pentaho.platform.api.util.IPasswordService;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.core.system.StandaloneSession;
+import org.pentaho.platform.engine.core.system.SystemSettings;
+import org.pentaho.platform.engine.services.solution.SolutionEngine;
 import org.pentaho.platform.plugin.action.olap.IOlapService;
-import org.pentaho.platform.plugin.action.olap.IOlapServiceException;
 import org.pentaho.platform.plugin.action.olap.impl.OlapServiceImpl;
-import org.pentaho.test.platform.repository2.unified.MockUnifiedRepository;
+import org.pentaho.platform.plugin.services.cache.CacheManager;
+import org.pentaho.platform.repository2.ClientRepositoryPaths;
+import org.pentaho.platform.util.Base64PasswordService;
+import org.pentaho.test.platform.engine.core.MicroPlatform;
+import org.pentaho.test.platform.plugin.UserRoleMapperTest.TestUserRoleListService;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.pentaho.platform.repository2.unified.UnifiedRepositoryTestUtils.*;
 
 public class OlapServiceTest extends TestCase {
 
   private IUnifiedRepository repository;
+  private MicroPlatform booter;
 
   @Override
   protected void setUp() throws Exception {
 
     PentahoSessionHolder.setSession(new StandaloneSession( "admin" ) );
 
-    repository = new MockUnifiedRepository( new MockUserProvider() );
+    repository = mock( IUnifiedRepository.class );
 
-    repository.createFolder(
-      MockUnifiedRepository.root(),
-      new RepositoryFile.Builder( "etc" ).folder( true ).build(),
-      "initialization" );
-
-    RepositoryFile mondrianFolder =
-      repository.createFolder(
-        repository.getFile( "/etc" ).getId(),
-        new RepositoryFile.Builder( "mondrian" ).folder( true ).build(),
-        "initialization" );
-    RepositoryFile olapFolder =
-      repository.createFolder(
-        repository.getFile( "/etc" ).getId(),
-        new RepositoryFile.Builder( "olap-servers" ).folder( true ).build(),
-        "initialization" );
-
-    // Make sure the repository is setup correctly
-    assertNotNull( mondrianFolder );
-    assertNotNull( mondrianFolder.getId() );
-    assertNotNull( olapFolder );
-    assertNotNull( olapFolder.getId() );
-
-    final Map<String, Serializable> fileMetadata =
-      repository.getFileMetadata(mondrianFolder.getId() );
-    assertNotNull( fileMetadata );
-    assertEquals( 0, fileMetadata.size() );
+    booter = new MicroPlatform( "test-src/solution" );
+    booter.define( IPasswordService.class, Base64PasswordService.class, Scope.GLOBAL );
+    booter.define( IDatabaseConnection.class, DatabaseConnection.class, Scope.GLOBAL );
+    booter.define( IDatabaseDialectService.class, DatabaseDialectService.class, Scope.GLOBAL );
+    booter.define( ICacheManager.class, CacheManager.class, Scope.GLOBAL );
+    booter.define( IUserRoleListService.class, TestUserRoleListService.class, Scope.GLOBAL );
+    booter.defineInstance( IUnifiedRepository.class, repository );
+    booter.define( ISolutionEngine.class, SolutionEngine.class, Scope.GLOBAL );
+    booter.define( IOlapService.class, OlapServiceImpl.class, Scope.GLOBAL );
+    booter.setSettingsProvider( new SystemSettings() );
+    booter.start();
   }
 
-  public void testDoImport() throws Exception {
+  public void testImportHosted() throws Exception {
 
-    if ( true ) {
-      // lboudreau - 2013-12-02
-      // Disabling this test until I can get it to work.
-      return;
-    }
+    // Stub /etc/mondrian
+    final String mondrianFolderPath =
+      ClientRepositoryPaths.getEtcFolderPath() + RepositoryFile.SEPARATOR + "mondrian";
+    stubGetFolder( repository, mondrianFolderPath );
+    stubGetChildren( repository, mondrianFolderPath ); // return no children
 
-    final IOlapService olapService = new OlapServiceImpl( repository );
-    try {
-      olapService.addHostedCatalog( null, null, null, true, null );
-      fail( "Null data should throw exception" );
-    } catch ( IOlapServiceException success ) {
-    }
+    final String testFolderPath = mondrianFolderPath + RepositoryFile.SEPARATOR + "test-server";
+    stubGetFileDoesNotExist( repository, testFolderPath );
+    stubCreateFolder( repository, testFolderPath );
+    final String metadataPath = testFolderPath + RepositoryFile.SEPARATOR + "metadata";
+    stubCreateFile( repository, metadataPath );
+
+    final IPentahoSession session = new StandaloneSession( "admin" );
+    final IOlapService olapService = PentahoSystem.get( IOlapService.class );
 
     final InputStream is =
       new FileInputStream(
@@ -95,19 +102,56 @@ public class OlapServiceTest extends TestCase {
       "Provider=mondrian;DataSource=SampleData",
       is,
       true,
-      new StandaloneSession( "admin" ) );
-    assertNotNull( repository.getFile( "/etc/mondrian/test-server" ) );
+      session );
+
+    verify( repository ).createFile(
+      eq( makeIdObject( testFolderPath ) ),
+      argThat( isLikeFile( makeFileObject( metadataPath ) ) ),
+      argThat( hasData(
+        pathPropertyPair( "/catalog/definition", "mondrian:/" + "test-server" ),
+        pathPropertyPair( "/catalog/datasourceInfo", "Provider=mondrian;DataSource=SampleData" )
+      ) ), anyString() );
+
+    verify( repository ).createFile( eq( makeIdObject( testFolderPath ) ),
+      argThat( isLikeFile( makeFileObject( testFolderPath + RepositoryFile.SEPARATOR + "schema.xml" ) ) ),
+      any( IRepositoryFileData.class ), anyString() );
   }
 
-  private class MockUserProvider implements MockUnifiedRepository.ICurrentUserProvider {
-    @Override
-    public String getUser() {
-      return "admin";
-    }
+  public void testImportOlap4j() throws Exception {
 
-    @Override
-    public List<String> getRoles() {
-      return Arrays.asList( "Administrator" );
-    }
+    // Stub /etc/mondrian
+    final String olapFolderPath =
+      ClientRepositoryPaths.getEtcFolderPath() + RepositoryFile.SEPARATOR + "olap-servers";
+    stubGetFolder( repository, olapFolderPath );
+    stubGetChildren( repository, olapFolderPath ); // return no children
+
+    final String testFolderPath = olapFolderPath + RepositoryFile.SEPARATOR + "test-server-2";
+    stubGetFileDoesNotExist( repository, testFolderPath );
+    stubCreateFolder( repository, testFolderPath );
+    final String metadataPath = testFolderPath + RepositoryFile.SEPARATOR + "metadata";
+    stubCreateFile( repository, metadataPath );
+
+    final IPentahoSession session = new StandaloneSession( "admin" );
+    final IOlapService olapService = PentahoSystem.get( IOlapService.class );
+
+    olapService.addOlap4jCatalog(
+      "test-server-2",
+      "class-name",
+      "url",
+      "user",
+      "password",
+      new Properties(),
+      true,
+      session );
+
+    verify( repository ).createFile(
+      eq( makeIdObject( testFolderPath ) ),
+      argThat( isLikeFile( makeFileObject( metadataPath ) ) ),
+      argThat( hasData(
+        pathPropertyPair( "/name", "test-server-2" ),
+        pathPropertyPair( "/className", "class-name" ),
+        pathPropertyPair( "/url", "url" ),
+        pathPropertyPair( "/password", "password" )
+      ) ), anyString() );
   }
 }
