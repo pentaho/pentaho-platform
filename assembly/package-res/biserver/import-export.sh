4b84a8ee066193d502447899c85cc8801707832d
Kanstantsin Karneliuk <kanstantsin_karneliuk@epam.com>
2014-02-13 13:33:06 +0300

    BISERVER-3498 - Platform shell scripts use Bourne Again Shell commands while using the Bourne Shell interpreter
    What was done:
    1) changed export of variable
    2) changed file test condition to be suported by sh
    3) commented out cd call which is unsupported by sh and seems to be
    unnecessary

--- assembly/package-res/biserver/import-export.sh
+++ assembly/package-res/biserver/import-export.sh
@@ -2,7 +2,7 @@
 DIR_REL=`dirname $0`
 cd $DIR_REL
 DIR=`pwd`
-cd -
+#cd -
 
 . "$DIR/set-pentaho-env.sh"
 setPentahoEnv
--- assembly/package-res/biserver/stop-pentaho.sh
+++ assembly/package-res/biserver/stop-pentaho.sh
@@ -8,7 +8,7 @@
 DIR_REL=`dirname $0`
 cd $DIR_REL
 DIR=`pwd`
-cd -
+#cd -
 
 . "$DIR/set-pentaho-env.sh"
 
