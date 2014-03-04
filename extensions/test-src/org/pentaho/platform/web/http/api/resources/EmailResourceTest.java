51cca1a5b8aa1d9d5ff1b99b4cfd01a8229c7a7e
Ramaiz Mansoor <rmansoor@pentaho.com>
2014-03-04 14:54:21 -0500

    BISERVER-10923 - Fixed core, extensions, scheduler unit tests

--- extensions/test-src/org/pentaho/platform/web/http/api/resources/EmailResourceTest.java
+++ extensions/test-src/org/pentaho/platform/web/http/api/resources/EmailResourceTest.java
@@ -12,19 +12,25 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * Copyright 2012 Pentaho Corporation. All rights reserved.
+ *
+ * Copyright 2006 - 2013 Pentaho Corporation.  All rights reserved.
  */
+
 package org.pentaho.platform.web.http.api.resources;
 
-import java.io.File;
-
-import javax.ws.rs.core.Response;
-
 import junit.framework.TestCase;
-
 import org.pentaho.platform.api.email.IEmailConfiguration;
+import org.pentaho.platform.api.engine.IAuthorizationPolicy;
+import org.pentaho.platform.api.mt.ITenant;
+import org.pentaho.platform.api.mt.ITenantManager;
+import org.pentaho.platform.core.mt.Tenant;
 import org.pentaho.platform.plugin.services.email.EmailConfiguration;
 import org.pentaho.platform.plugin.services.email.EmailService;
+import org.pentaho.test.platform.engine.core.MicroPlatform;
+
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.util.List;
 
 /**
  * Class Description
@@ -36,56 +42,70 @@ public class EmailResourceTest extends TestCase {
   private File defaultConfigFile = null;
   private final int OK_STATUS = Response.ok().build().getStatus();
   private final EmailConfiguration BLANK_CONFIG = new EmailConfiguration();
-
+  private MicroPlatform mp;
   @Override
   protected void setUp() throws Exception {
     // Setup the temp email config file
-    defaultConfigFile = File.createTempFile("email_config_", ".xml");
-    this.emailResource = new EmailResource(new EmailService(defaultConfigFile));
+    mp = new MicroPlatform();
+    mp.defineInstance( IAuthorizationPolicy.class, new TestAuthorizationPolicy() );
+    mp.start();
+
+    defaultConfigFile = File.createTempFile( "email_config_", ".xml" );
+    this.emailResource = new EmailResource( new EmailService( defaultConfigFile ) );
   }
 
   public void testEmailConfig() throws Exception {
-    {
-      try {
-        new EmailResource(null);
-        fail("Exception should be thrown when a null EmailService is provided");
-      } catch (IllegalArgumentException success) {
-      }
+
+    try {
+      new EmailResource( null );
+      fail( "Exception should be thrown when a null EmailService is provided" );
+    } catch ( IllegalArgumentException success ) {
+      //ignore
     }
 
-    {
-      try {
-        new EmailResource(new EmailService(new File(defaultConfigFile, "cannot.exist")));
-        fail("Exception should be thrown when an invalid EmailService is provided");
-      } catch (IllegalArgumentException success) {
-      }
+    try {
+      new EmailResource( new EmailService( new File( defaultConfigFile, "cannot.exist" ) ) );
+      fail( "Exception should be thrown when an invalid EmailService is provided" );
+    } catch ( IllegalArgumentException success ) {
+      //ignore
     }
 
-    {
-      final IEmailConfiguration emptyConfig = emailResource.getEmailConfig();
-      assertTrue(BLANK_CONFIG.equals(emptyConfig));
-    }
+    IEmailConfiguration emptyConfig = emailResource.getEmailConfig();
+    assertTrue( BLANK_CONFIG.equals( emptyConfig ) );
 
-    {
-      final Response response = emailResource.setEmailConfig(new EmailConfiguration());
-      assertEquals(OK_STATUS, response.getStatus());
-      final IEmailConfiguration emptyConfig = emailResource.getEmailConfig();
-      assertTrue(BLANK_CONFIG.equals(emptyConfig));
-    }
+    Response response = emailResource.setEmailConfig( new EmailConfiguration() );
+    assertEquals( OK_STATUS, response.getStatus() );
+    emptyConfig = emailResource.getEmailConfig();
+    assertTrue( BLANK_CONFIG.equals( emptyConfig ) );
 
-    {
-      final EmailConfiguration emailConfigOriginal = new EmailConfiguration(true, false, "test@pentaho.com", "Pentaho Scheduler", null, new Short((short) 36), "", true, "user",
-          null, false, true);
-      final Response response = emailResource.setEmailConfig(emailConfigOriginal);
-      assertEquals(OK_STATUS, response.getStatus());
+    final EmailConfiguration emailConfigOriginal =
+        new EmailConfiguration( true, false, "test@pentaho.com", "Pentaho Scheduler", null, new Short( (short) 36 ),
+            "", true, "user", null, false, true );
+    response = emailResource.setEmailConfig( emailConfigOriginal );
+    assertEquals( OK_STATUS, response.getStatus() );
 
-      final IEmailConfiguration emailConfigNew = emailResource.getEmailConfig();
-      assertTrue(emailConfigOriginal.equals(emailConfigNew));
-    }
+    final IEmailConfiguration emailConfigNew = emailResource.getEmailConfig();
+    assertTrue( emailConfigOriginal.equals( emailConfigNew ) );
 
   }
 
   public void testSendEmailTest() throws Exception {
 
+  }
+  
+  class TestAuthorizationPolicy implements IAuthorizationPolicy {
+
+    @Override
+    public boolean isAllowed( String actionName ) {
+      // TODO Auto-generated method stub
+      return true;
+    }
+
+    @Override
+    public List<String> getAllowedActions( String actionNamespace ) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+    
   }
 }
--- extensions/test-src/org/pentaho/test/platform/plugin/EmailTest.java
+++ extensions/test-src/org/pentaho/test/platform/plugin/EmailTest.java
@@ -12,26 +12,21 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright 2005 - 2009 Pentaho Corporation.  All rights reserved.
- *
+ * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
 */
+
 package org.pentaho.test.platform.plugin;
 
-import java.io.OutputStream;
-
-import org.junit.Ignore;
-import org.pentaho.platform.api.engine.IRuntimeContext;
-import org.pentaho.platform.engine.core.output.SimpleOutputHandler;
-import org.pentaho.platform.engine.core.solution.SimpleParameterProvider;
-import org.pentaho.platform.plugin.services.messages.Messages;
-import org.pentaho.test.platform.engine.core.BaseTestCase;
 import com.dumbster.smtp.SimpleSmtpServer;
+import org.junit.Ignore;
+import org.pentaho.test.platform.engine.core.BaseTestCase;
 
 @SuppressWarnings("nls")
 @Ignore
 public class EmailTest extends BaseTestCase {
 
   private SimpleSmtpServer smtpServer;
+
   public EmailTest() {
     super(SOLUTION_PATH);
   }
@@ -54,56 +49,5 @@ public class EmailTest extends BaseTestCase {
       // ignored.
     }
     super.tearDown();
-  }
-  
-//  public void testEmailLoop() throws Exception {
-//    SimpleParameterProvider parameterProvider = new SimpleParameterProvider();
-//    OutputStream outputStream = getOutputStream(SOLUTION_PATH, "text_only_email-loop", ".html"); //$NON-NLS-1$ //$NON-NLS-2$
-//    SimpleOutputHandler outputHandler = new SimpleOutputHandler(outputStream, true);
-//    IRuntimeContext context = run(
-//        SOLUTION_PATH + "/test/email/", "text_only_email-loop.xaction", parameterProvider, outputHandler); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-//    assertEquals(
-//        Messages.getInstance().getString("BaseTest.USER_RUNNING_ACTION_SEQUENCE"), IRuntimeContext.RUNTIME_STATUS_SUCCESS, context.getStatus()); //$NON-NLS-1$
-//  }
-//
-//  public void testEmailOnly() throws Exception {
-//    SimpleParameterProvider parameterProvider = new SimpleParameterProvider();
-//    OutputStream outputStream = getOutputStream(SOLUTION_PATH, "text_only_email", ".html"); //$NON-NLS-1$ //$NON-NLS-2$
-//    SimpleOutputHandler outputHandler = new SimpleOutputHandler(outputStream, true);
-//    IRuntimeContext context = run(
-//        SOLUTION_PATH + "/test/email/", "text_only_email.xaction", parameterProvider, outputHandler); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-//    assertEquals(
-//        Messages.getInstance().getString("BaseTest.USER_RUNNING_ACTION_SEQUENCE"), IRuntimeContext.RUNTIME_STATUS_SUCCESS, context.getStatus()); //$NON-NLS-1$
-//  }
-
-  /*public void testEmailLoopProp() {
-    SimpleParameterProvider parameterProvider = new SimpleParameterProvider();
-    OutputStream outputStream = getOutputStream(SOLUTION_PATH, "text_only_email-loop_property", ".html"); //$NON-NLS-1$ //$NON-NLS-2$
-    SimpleOutputHandler outputHandler = new SimpleOutputHandler(outputStream, true);
-    IRuntimeContext context = run(
-        SOLUTION_PATH + "/test/email/", "text_only_email-loop_property.xaction", parameterProvider, outputHandler); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$        
-    assertEquals(
-        Messages.getInstance().getString("BaseTest.USER_RUNNING_ACTION_SEQUENCE"), IRuntimeContext.RUNTIME_STATUS_SUCCESS, context.getStatus()); //$NON-NLS-1$
-  }*/
-
-//  public void testEmailHtml() throws Exception {
-//    SimpleParameterProvider parameterProvider = new SimpleParameterProvider();
-//    OutputStream outputStream = getOutputStream(SOLUTION_PATH, "text_html_attach_email", ".html"); //$NON-NLS-1$ //$NON-NLS-2$
-//    SimpleOutputHandler outputHandler = new SimpleOutputHandler(outputStream, true);
-//    IRuntimeContext context = run(
-//        SOLUTION_PATH + "/test/email/", "text_html_attach_email.xaction", parameterProvider, outputHandler); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-//    assertEquals(
-//        Messages.getInstance().getString("BaseTest.USER_RUNNING_ACTION_SEQUENCE"), IRuntimeContext.RUNTIME_STATUS_SUCCESS, context.getStatus()); //$NON-NLS-1$
-//  }
-
-  public static void main(String[] args) throws Exception {
-    EmailTest test = new EmailTest();
-    try {
-      //test.testEmailLoop();
-      //test.testEmailOnly();
-//      test.testEmailLoopProp();
-      //test.testEmailHtml();
-    } finally {
-    }
   }
 }
