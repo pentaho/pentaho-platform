ddc1f1ec3b7f3d032736cd2a091385079c591de6
Pedro Teixeira <pedrofvteixeira@gmail.com>
2014-04-08 14:29:09 +0100

    [BISERVER-11276] Implement Connection Pooling to respect "disabled" and "enabled" pools at the same time 	- "Enable Connection Pooling" checkbox now defaults to 'checked' (on creation) 	- implemented DinamicallyPooledOrJndiDatasourceService that delegates to a pooled / nonPooled DatasourceService depending on databaseConnection.isUsingConnectionPool() 	- implemented DinamicallyPooledDatasourceSystemListener to handle pooling / nonPooling depending on databaseConnection.isUsingConnectionPool() 	- connection pooling properties implemented: testOnBorrow, testWhileIdle, testOnReturn, defaultReadOnly, defaultAutoCommit, defaultTransactionIsolation, defaultCatalog, poolPreparedStatements, maxOpenPreparedStatements, accessToUnderlyingConnectionAllowed, timeBetweenEvictionRunsMillis, removeAbandoned, removeAbandonedTimeout, logAbandoned

---
+++ core/src/org/pentaho/platform/engine/services/connection/datasource/dbcp/DinamicallyPooledOrJndiDatasourceService.java
@@ -0,0 +1,85 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2006 - 2014 Pentaho Corporation.  All rights reserved.
+ */
+package org.pentaho.platform.engine.services.connection.datasource.dbcp;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.pentaho.database.model.IDatabaseConnection;
+import org.pentaho.platform.api.data.DBDatasourceServiceException;
+import org.pentaho.platform.api.data.IDBDatasourceService;
+import org.pentaho.platform.api.repository.datasource.DatasourceMgmtServiceException;
+import org.pentaho.platform.api.repository.datasource.IDatasourceMgmtService;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.engine.services.messages.Messages;
+
+public class DinamicallyPooledOrJndiDatasourceService extends BaseDatasourceService {
+
+  private static final Log logger = LogFactory.getLog( PooledOrJndiDatasourceService.class );
+
+  private IDBDatasourceService pooledDatasourceService;
+  private IDBDatasourceService nonPooledDatasourceService;
+
+  public DinamicallyPooledOrJndiDatasourceService() {
+  }
+  
+  @Override
+  public DataSource getDataSource( String datasource ) throws DBDatasourceServiceException {
+
+    try {
+
+      IDatasourceMgmtService datasourceMgmtSvc =
+          (IDatasourceMgmtService) PentahoSystem.get( IDatasourceMgmtService.class, PentahoSessionHolder.getSession() );
+
+      // Look in the database for the datasource
+      IDatabaseConnection databaseConnection = datasourceMgmtSvc.getDatasourceByName( datasource );
+
+      if ( databaseConnection != null ) {
+
+        return databaseConnection.isUsingConnectionPool() ? getPooledDatasourceService().getDataSource( datasource )
+            : getNonPooledDatasourceService().getDataSource( datasource );
+      }
+
+    } catch ( DatasourceMgmtServiceException daoe ) {
+      daoe.printStackTrace();
+      logger.info( Messages.getInstance().getErrorString(
+          "DinamicallyPooledOrJndiDatasourceService.DEBUG_0001_UNABLE_TO_FIND_DATASOURCE_IN_REPOSITORY",
+          daoe.getLocalizedMessage() ), daoe );
+    }
+
+    return getJndiDataSource( datasource );
+  }
+
+  public IDBDatasourceService getPooledDatasourceService() {
+    return pooledDatasourceService;
+  }
+
+  public void setPooledDatasourceService( IDBDatasourceService pooledDatasourceService ) {
+    this.pooledDatasourceService = pooledDatasourceService;
+  }
+
+  public IDBDatasourceService getNonPooledDatasourceService() {
+    return nonPooledDatasourceService;
+  }
+
+  public void setNonPooledDatasourceService( IDBDatasourceService nonPooledDatasourceService ) {
+    this.nonPooledDatasourceService = nonPooledDatasourceService;
+  }
+}
