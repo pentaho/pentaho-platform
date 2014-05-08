63694201a6b6efdb414c084a808e363b70816cd3
AliaksandrDrebenchuk <Aliaksandr_Drebenchuk@epam.com>
2014-05-08 13:56:01 +0300

    SP-1233 Japanese characters in file names are not appearing in the Browse perspective. (5.0)
    filename re-encoded

--- extensions/src/org/pentaho/platform/web/http/api/resources/RepositoryImportResource.java
+++ extensions/src/org/pentaho/platform/web/http/api/resources/RepositoryImportResource.java
@@ -21,16 +21,6 @@
  */
 package org.pentaho.platform.web.http.api.resources;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.apache.log4j.Level;
 import org.pentaho.platform.api.engine.IAuthorizationPolicy;
 import org.pentaho.platform.api.engine.PentahoAccessControlException;
@@ -48,108 +38,116 @@ import org.pentaho.platform.security.policy.rolebased.actions.PublishAction;
 import org.pentaho.platform.security.policy.rolebased.actions.RepositoryCreateAction;
 import org.pentaho.platform.security.policy.rolebased.actions.RepositoryReadAction;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 
-@Path("/repo/files/import")
+@Path( "/repo/files/import" )
 public class RepositoryImportResource {
 
-	/**
-	 * @param uploadDir
-	 *            : JCR Directory to which the zip structure or single file will
-	 *            be uploaded to.
-	 * @param fileIS
-	 *            : Input stream for the file.
-	 * @param fileInfo
-	 *            : Info about he file (
-	 * @return http ok response of everything went well... some other error
-	 *         otherwise
-	 *         <p/>
-	 *         This REST method takes multi-part form data and imports it to a
-	 *         JCR repository.
-	 *         --import --url=http://localhost:8080/pentaho --username=admin --password=password --source=file-system --type=files --charset=UTF-8 --path=/public  --file-path="C:/pentahotraining/BootCamp Labs/Pilot Project/SteelWheels.csv" --permission=true --overwrite=true --retainOwnership=true --rest=true
-	 */
-	@POST
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	@Produces(MediaType.TEXT_HTML)
-	public Response doPostImport(
-			@FormDataParam("importDir") String uploadDir,
-			@FormDataParam("fileUpload") InputStream fileIS,
-			@FormDataParam("overwriteFile") String overwriteFile,
-			@FormDataParam("overwriteAclPermissions") String overwriteAclPermissions,
-			@FormDataParam("applyAclPermissions") String applyAclPermission,
-			@FormDataParam("retainOwnership") String retainOwnership,
-			@FormDataParam("charSet") String charSet,
-			@FormDataParam("logLevel") String logLevel,
-			@FormDataParam("fileUpload") FormDataContentDisposition fileInfo) {
-	      IRepositoryImportLogger importLogger = null;
-		    ByteArrayOutputStream importLoggerStream = new ByteArrayOutputStream();
-        boolean logJobStarted = false;
-			try {
-				validateAccess();
-				
-				boolean overwriteFileFlag = ("false".equals(overwriteFile) ? false : true);
-				boolean overwriteAclSettingsFlag = ("true".equals(overwriteAclPermissions) ? true : false);
-				boolean applyAclSettingsFlag = ("true".equals(applyAclPermission) ? true : false);
-				boolean retainOwnershipFlag = ("true".equals(retainOwnership) ? true : false);
-				
-				Level level = Level.toLevel(logLevel);
-				ImportSession.getSession().setAclProperties(applyAclSettingsFlag, retainOwnershipFlag, overwriteAclSettingsFlag);
-	
-				RepositoryFileImportBundle.Builder bundleBuilder = new RepositoryFileImportBundle.Builder();
-				bundleBuilder.input(fileIS);
-				bundleBuilder.charSet(charSet == null?"UTF-8":charSet);
-				bundleBuilder.hidden(false);
-				bundleBuilder.path(uploadDir);
-				bundleBuilder.overwriteFile(overwriteFileFlag);
-				bundleBuilder.applyAclSettings(applyAclSettingsFlag);
-				bundleBuilder.overwriteAclSettings(overwriteAclSettingsFlag);
-				bundleBuilder.retainOwnership(retainOwnershipFlag);
-				bundleBuilder.name(fileInfo.getFileName());
-				IPlatformImportBundle bundle = bundleBuilder.build();
-	
-				NameBaseMimeResolver mimeResolver = PentahoSystem.get(NameBaseMimeResolver.class);
-				bundleBuilder.mime(mimeResolver.resolveMimeForFileName(fileInfo.getFileName()));
-	
-				IPlatformImporter importer = PentahoSystem.get(IPlatformImporter.class);
-				importLogger = importer.getRepositoryImportLogger();
+  private static final String DEFAULT_CHAR_SET = "UTF-8";
 
-        final String mimeType = bundle.getMimeType() != null ? bundle.getMimeType() : mimeResolver.resolveMimeForBundle(bundle);
-        if (mimeType == null) {
-          return Response.ok("INVALID_MIME_TYPE", MediaType.TEXT_HTML).build();
-        }
+  /**
+   * @param uploadDir
+   *          : JCR Directory to which the zip structure or single file will be uploaded to.
+   * @param fileIS
+   *          : Input stream for the file.
+   * @param fileInfo
+   *          : Info about he file (
+   * @return http ok response of everything went well... some other error otherwise
+   *         <p/>
+   *         This REST method takes multi-part form data and imports it to a JCR repository. --import
+   *         --url=http://localhost:8080/pentaho --username=admin --password=password --source=file-system --type=files
+   *         --charset=UTF-8 --path=/public --file-path="C:/pentahotraining/BootCamp Labs/Pilot Project/SteelWheels.csv"
+   *         --permission=true --overwrite=true --retainOwnership=true --rest=true
+   */
+  @POST
+  @Consumes( MediaType.MULTIPART_FORM_DATA )
+  @Produces( MediaType.TEXT_HTML )
+  public Response doPostImport( @FormDataParam( "importDir" ) String uploadDir,
+      @FormDataParam( "fileUpload" ) InputStream fileIS, @FormDataParam( "overwriteFile" ) String overwriteFile,
+      @FormDataParam( "overwriteAclPermissions" ) String overwriteAclPermissions,
+      @FormDataParam( "applyAclPermissions" ) String applyAclPermission,
+      @FormDataParam( "retainOwnership" ) String retainOwnership, @FormDataParam( "charSet" ) String pCharSet,
+      @FormDataParam( "logLevel" ) String logLevel, @FormDataParam( "fileUpload" ) FormDataContentDisposition fileInfo ) {
+    IRepositoryImportLogger importLogger = null;
+    ByteArrayOutputStream importLoggerStream = new ByteArrayOutputStream();
+    boolean logJobStarted = false;
+    try {
+      validateAccess();
 
-        logJobStarted = true;
-        importLogger.startJob(importLoggerStream, uploadDir, level);
-				importer.importFile(bundle);
-	
-				// Flush the Mondrian cache to show imported data-sources.
-				IMondrianCatalogService mondrianCatalogService = PentahoSystem.get(
-                                                            IMondrianCatalogService.class, "IMondrianCatalogService",
-                                                            PentahoSessionHolder.getSession());
-				mondrianCatalogService.reInit(PentahoSessionHolder.getSession());
-			} catch (PentahoAccessControlException e) {
-				return Response.serverError().entity(e.toString()).build();
-			} catch (Exception e) {
-				return Response.serverError().entity(e.toString()).build();
-			} finally {
-        if (logJobStarted == true) {
-				  importLogger.endJob();
-        }
-			}
-			return Response.ok(importLoggerStream.toString(), MediaType.TEXT_HTML).build();
-	}
+      boolean overwriteFileFlag = ( "false".equals( overwriteFile ) ? false : true );
+      boolean overwriteAclSettingsFlag = ( "true".equals( overwriteAclPermissions ) ? true : false );
+      boolean applyAclSettingsFlag = ( "true".equals( applyAclPermission ) ? true : false );
+      boolean retainOwnershipFlag = ( "true".equals( retainOwnership ) ? true : false );
 
-	private void validateAccess() throws PentahoAccessControlException {
-		IAuthorizationPolicy policy = PentahoSystem
-				.get(IAuthorizationPolicy.class);
-		boolean isAdmin = policy
-				.isAllowed(RepositoryReadAction.NAME)
-				&& policy.isAllowed(RepositoryCreateAction.NAME)
-				&& (policy.isAllowed(AdministerSecurityAction.NAME)
-         || policy.isAllowed(PublishAction.NAME));
-		if (!isAdmin) {
-			throw new PentahoAccessControlException("Access Denied");
-		}
-	}
+      Level level = Level.toLevel( logLevel );
+      ImportSession.getSession().setAclProperties( applyAclSettingsFlag, retainOwnershipFlag, overwriteAclSettingsFlag );
+
+      String charSet = pCharSet == null ? DEFAULT_CHAR_SET : pCharSet;
+      String fileName = new String( fileInfo.getFileName().getBytes(), charSet );
+
+      RepositoryFileImportBundle.Builder bundleBuilder = new RepositoryFileImportBundle.Builder();
+      bundleBuilder.input( fileIS );
+      bundleBuilder.charSet( charSet );
+      bundleBuilder.hidden( false );
+      bundleBuilder.path( uploadDir );
+      bundleBuilder.overwriteFile( overwriteFileFlag );
+      bundleBuilder.applyAclSettings( applyAclSettingsFlag );
+      bundleBuilder.overwriteAclSettings( overwriteAclSettingsFlag );
+      bundleBuilder.retainOwnership( retainOwnershipFlag );
+      bundleBuilder.name( fileName );
+      IPlatformImportBundle bundle = bundleBuilder.build();
+
+      NameBaseMimeResolver mimeResolver = PentahoSystem.get( NameBaseMimeResolver.class );
+      bundleBuilder.mime( mimeResolver.resolveMimeForFileName( fileName ) );
+
+      IPlatformImporter importer = PentahoSystem.get( IPlatformImporter.class );
+      importLogger = importer.getRepositoryImportLogger();
+
+      final String mimeType =
+          bundle.getMimeType() != null ? bundle.getMimeType() : mimeResolver.resolveMimeForBundle( bundle );
+      if ( mimeType == null ) {
+        return Response.ok( "INVALID_MIME_TYPE", MediaType.TEXT_HTML ).build();
+      }
+
+      logJobStarted = true;
+      importLogger.startJob( importLoggerStream, uploadDir, level );
+      importer.importFile( bundle );
+
+      // Flush the Mondrian cache to show imported data-sources.
+      IMondrianCatalogService mondrianCatalogService =
+          PentahoSystem.get( IMondrianCatalogService.class, "IMondrianCatalogService", PentahoSessionHolder
+              .getSession() );
+      mondrianCatalogService.reInit( PentahoSessionHolder.getSession() );
+    } catch ( PentahoAccessControlException e ) {
+      return Response.serverError().entity( e.toString() ).build();
+    } catch ( Exception e ) {
+      return Response.serverError().entity( e.toString() ).build();
+    } finally {
+      if ( logJobStarted == true ) {
+        importLogger.endJob();
+      }
+    }
+    return Response.ok( importLoggerStream.toString(), MediaType.TEXT_HTML ).build();
+  }
+
+  private void validateAccess() throws PentahoAccessControlException {
+    IAuthorizationPolicy policy = PentahoSystem.get( IAuthorizationPolicy.class );
+    boolean isAdmin =
+        policy.isAllowed( RepositoryReadAction.NAME ) && policy.isAllowed( RepositoryCreateAction.NAME )
+            && ( policy.isAllowed( AdministerSecurityAction.NAME ) || policy.isAllowed( PublishAction.NAME ) );
+    if ( !isAdmin ) {
+      throw new PentahoAccessControlException( "Access Denied" );
+    }
+  }
 }
