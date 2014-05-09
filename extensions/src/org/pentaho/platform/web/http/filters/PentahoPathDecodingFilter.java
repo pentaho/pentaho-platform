0548b07099384b7b1c9075a0f20384c17bf6d634
Nick Baker <nbaker@pentaho.com>
2014-05-09 09:52:17 -0400

    [BISERVER-11268] Backing-out the double-encoding of query params and names.

--- extensions/src/org/pentaho/platform/web/http/filters/PentahoPathDecodingFilter.java
+++ extensions/src/org/pentaho/platform/web/http/filters/PentahoPathDecodingFilter.java
@@ -11,17 +11,13 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
 
 /**
  * This filter is designed to work around a limitation of some web servers which prohibit the encoded form of / and \
  * (%2F and %5C) being in the URL. Clients can pass double-encoded forms of these encodings instead (%252F and %255C).
  * This filter will detect any occurance of these double encodings and correct them before passing on to the rest of the
  * servlet chain.
- * <p/>
+ *
  * Created by nbaker on 4/8/14.
  */
 public class PentahoPathDecodingFilter implements Filter {
@@ -41,111 +37,22 @@ public class PentahoPathDecodingFilter implements Filter {
 
   }
 
-  private static final class EncodingAwareHttpServletRequestWrapper extends HttpServletRequestWrapper {
+  private static final class EncodingAwareHttpServletRequestWrapper extends HttpServletRequestWrapper{
     private EncodingAwareHttpServletRequestWrapper( HttpServletRequest request ) {
       super( request );
     }
 
     @Override public String getPathInfo() {
       final String pathInfo = super.getPathInfo();
-      if ( StringUtils.isEmpty( pathInfo ) ) {
+      if( StringUtils.isEmpty(pathInfo)){
         return pathInfo;
       }
-      return decodeSingleEncoded( pathInfo );
+      return pathInfo.replaceAll( "%5C", "\\\\" ).replaceAll( "%2F", "/" );
     }
 
     @Override public String getRequestURI() {
       final String requestURI = super.getRequestURI();
-      return decodeDoubleEncoding( requestURI );
+      return requestURI.replaceAll( "%255C", "%5C" ).replaceAll( "%252F", "%2F" );
     }
-
-    /**
-     * Not decoded by the superclass
-     *
-     * @return
-     */
-    @Override public String getQueryString() {
-      final String queryString = super.getQueryString();
-      if ( !StringUtils.isEmpty( queryString ) ) {
-        return decodeDoubleEncoding( queryString );
-      }
-      return null;
-    }
-
-    /**
-     * Decoded by the superclass
-     *
-     * @param name
-     * @return
-     */
-    @Override public String getParameter( String name ) {
-      if ( this.getMethod().equals( "POST" ) ) {
-        // POST payload parameters need no special encoding. A decode would be destructive. Unfortunately we cannot
-        // easily know if a parameter came from the query-string or the post data. So we'll return the original value
-        // for now.
-        // TODO: handle mix of POST data and query-string parameters
-        return super.getParameter( name );
-      }
-      final String parameter = super.getParameter( name );
-      if ( !StringUtils.isEmpty( parameter ) ) {
-        return decodeSingleEncoded( parameter );
-      }
-      return null;
-    }
-
-    @Override public String[] getParameterValues( String name ) {
-      if ( this.getMethod().equals( "POST" ) ) {
-        // POST payload parameters need no special encoding. A decode would be destructive. Unfortunately we cannot
-        // easily know if a parameter came from the query-string or the post data. So we'll return the original value
-        // for now.
-        // TODO: handle mix of POST data and query-string parameters
-        return super.getParameterValues( name );
-      }
-      final String[] parameterValues = super.getParameterValues( name );
-      for ( int i = 0; i < parameterValues.length; i++ ) {
-        String parameterValue = parameterValues[ i ];
-        parameterValues[ i ] = decodeSingleEncoded( parameterValue );
-      }
-      return parameterValues;
-    }
-
-    @Override public Enumeration getParameterNames() {
-      if ( this.getMethod().equals( "POST" ) ) {
-        // POST payload parameters need no special encoding. A decode would be destructive. Unfortunately we cannot
-        // easily know if a parameter came from the query-string or the post data. So we'll return the original value
-        // for now.
-        // TODO: handle mix of POST data and query-string parameters
-        return super.getParameterNames();
-      }
-      final Enumeration parameterNames = super.getParameterNames();
-      List<String> decoded = new ArrayList<String>();
-      while ( parameterNames.hasMoreElements() ) {
-        String o = parameterNames.nextElement().toString();
-        decoded.add( decodeSingleEncoded( o ) );
-      }
-      return Collections.enumeration( decoded );
-    }
-
-    /**
-     * Decodes Doubly-Encoded forward and backslashes
-     *
-     * @param input
-     * @return
-     */
-    private static String decodeDoubleEncoding( String input ) {
-      return input.replaceAll( "%255C", "%5C" ).replaceAll( "%252F", "%2F" );
-    }
-
-
-    /**
-     * Decodes Single-Encoded forward and backslashes
-     *
-     * @param input
-     * @return
-     */
-    private static String decodeSingleEncoded( String input ) {
-      return input.replaceAll( "%5C", "\\\\" ).replaceAll( "%2F", "/" );
-    }
-
   }
 }
--- extensions/test-src/org/pentaho/platform/web/http/filters/PentahoPathDecodingFilterTest.java
+++ extensions/test-src/org/pentaho/platform/web/http/filters/PentahoPathDecodingFilterTest.java
@@ -9,11 +9,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import java.util.Collections;
-import java.util.Enumeration;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by nbaker on 4/8/14.
@@ -22,66 +18,17 @@ public class PentahoPathDecodingFilterTest {
   @Test
   public void testDoFilter() throws Exception {
     PentahoPathDecodingFilter decodingFilter = new PentahoPathDecodingFilter();
-
-    // Check PathInfo and RequestURI
-    MockHttpServletRequest request =
+    final MockHttpServletRequest request =
       new MockHttpServletRequest( "GET", "http://localhost:8080/pentaho/encoded%255Cpath" );
     request.setPathInfo( "/pentaho/encoded%5Cpath" );
-    decodingFilter.doFilter( request, new MockHttpServletResponse(), new MockFilterChain() {
+    decodingFilter.doFilter( request, new MockHttpServletResponse(), new MockFilterChain(){
       @Override public void doFilter( ServletRequest request, ServletResponse response ) {
         //pathInfo is to be fully decoded
-        assertEquals( "/pentaho/encoded\\path", ( (HttpServletRequest) request ).getPathInfo() );
+        assertEquals("/pentaho/encoded\\path", ((HttpServletRequest) request).getPathInfo());
         //requestURI is not decoded
-        assertEquals( "http://localhost:8080/pentaho/encoded%5Cpath",
-          ( (HttpServletRequest) request ).getRequestURI() );
+        assertEquals("http://localhost:8080/pentaho/encoded%5Cpath", ((HttpServletRequest) request).getRequestURI());
       }
-    } );
-
-    // Test query-string and parameter values with GET request
-    request =
-      new MockHttpServletRequest( "GET",
-        "http://localhost:8080/pentaho/encoded%255Cpath?val1=%255Chome&val2=%252Fpublic" );
-    request.setParameter( "val1", "%5Chome" );
-    request.setParameter( "val2", "%2Fhome" );
-    request.setParameter( "val%5C", "something" );
-    request.setQueryString( "val1=%255Chome&val2=%252Fpublic" );
-    decodingFilter.doFilter( request, new MockHttpServletResponse(), new MockFilterChain() {
-      @Override public void doFilter( ServletRequest request, ServletResponse response ) {
-
-        assertEquals( "\\home", request.getParameter( "val1" ) );
-        assertEquals( "/home", request.getParameter( "val2" ) );
-
-        assertEquals( "val1=%5Chome&val2=%2Fpublic", ( (HttpServletRequest) request ).getQueryString() );
-        final Enumeration parameterNames = request.getParameterNames();
-        boolean foundDecodedParam  = false;
-        while ( parameterNames.hasMoreElements() ) {
-          String o =  parameterNames.nextElement().toString();
-          if(o.equals( "val\\" ) ){
-            foundDecodedParam = true;
-            break;
-          }
-        }
-        assertTrue( foundDecodedParam  );
-      }
-    } );
-
-    // Test query-string and parameter values with POST request
-    request =
-      new MockHttpServletRequest( "POST",
-        "http://localhost:8080/pentaho/something" );
-    request.setParameter( "val1", "\\home" );
-    request.setParameter( "val2", "/home" );
-    request.setParameter( "val%255C", "something" );
-
-    decodingFilter.doFilter( request, new MockHttpServletResponse(), new MockFilterChain() {
-      @Override public void doFilter( ServletRequest request, ServletResponse response ) {
-
-        assertEquals( "\\home", request.getParameter( "val1" ) );
-        assertEquals( "/home", request.getParameter( "val2" ) );
-        assertEquals( "something", request.getParameter( "val%255C" ) );
-
-      }
-    } );
+    });
 
 
   }
