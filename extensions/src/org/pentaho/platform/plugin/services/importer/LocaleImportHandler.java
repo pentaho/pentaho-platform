5979fe5dba3983a0ed0b1e14eb63d04cedf457fd
Anna Popova1 <Anna_Popova1@epam.com>
2014-05-27 11:24:13 +0400

    BISERVER-11537 - Folder Name in PUC different than name attribute used in index.xml after import utility

--- extensions/src/org/pentaho/platform/plugin/services/importer/LocaleImportHandler.java
+++ extensions/src/org/pentaho/platform/plugin/services/importer/LocaleImportHandler.java
@@ -17,6 +17,17 @@
 
 package org.pentaho.platform.plugin.services.importer;
 
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
@@ -27,17 +38,11 @@ import org.pentaho.platform.plugin.services.importer.mimeType.MimeType;
 import org.pentaho.platform.plugin.services.importexport.ExportFileNameEncoder;
 import org.pentaho.platform.plugin.services.importexport.ImportSession;
 import org.pentaho.platform.repository.RepositoryFilenameUtils;
-
-import java.io.ByteArrayInputStream;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.PropertyResourceBundle;
+import org.w3c.dom.Document;
 
 public class LocaleImportHandler extends RepositoryFileImportFileHandler implements IPlatformImportHandler {
 
   private static final String FILE_DESCRIPTION = "file.description";
-
   private static final String FILE_TITLE = "file.title";
 
   private static final String DIRECTORY_NAME = "directory_name";
@@ -47,6 +52,11 @@ public class LocaleImportHandler extends RepositoryFileImportFileHandler impleme
 
   private static final String LOCALE_EXT = ".locale";
   private static final String OLD_LOCALE_EXT = ".properties";
+  private static final String XML_LOCALE_EXT = ".xml";
+
+  private static final String VARIABLE_SYMBOL_FROM_INDEX = "%";
+  private static final String TITLE_PROPERTY_NAME = "name";
+  private static final String DESC_PROPERTY_NAME = "description";
 
   private List<String> artifacts; // spring injected file extensions
 
@@ -60,16 +70,18 @@ public class LocaleImportHandler extends RepositoryFileImportFileHandler impleme
 
   public void importFile( IPlatformImportBundle bundle ) throws PlatformImportException {
     RepositoryFileImportBundle localeBundle = (RepositoryFileImportBundle) bundle;
-    RepositoryFile localeParent = getLocaleParent( localeBundle );
 
-    Properties localeProperties = buildLocaleProperties( localeBundle );
+    Properties localePropertiesFromIndex = loadPropertiesByXml( localeBundle );
+    RepositoryFile localeParent = getLocaleParent( localeBundle, localePropertiesFromIndex );
+
+    Properties localeProperties = buildLocaleProperties( localeBundle, localePropertiesFromIndex );
 
     if ( localeParent != null && unifiedRepository != null && localeBundle.getFile() != null ) {
       // If the parent file (content) got skipped because it existed then we will not import the locale information
       String fullPath = RepositoryFilenameUtils.concat( localeBundle.getPath(), localeBundle.getFile().getName() );
       if ( ImportSession.getSession().getSkippedFiles().contains( fullPath ) ) {
         getLogger().trace(
-          "Not importing Locale [" + localeBundle.getFile().getName() + "] since parent file not written " );
+            "Not importing Locale [" + localeBundle.getFile().getName() + "] since parent file not written " );
       } else {
         getLogger().trace( "Processing Locale [" + localeBundle.getFile().getName() + "]" );
         unifiedRepository
@@ -84,41 +96,45 @@ public class LocaleImportHandler extends RepositoryFileImportFileHandler impleme
    * @param locale
    * @return
    */
-  private Properties buildLocaleProperties( RepositoryFileImportBundle locale ) {
+  private Properties buildLocaleProperties( RepositoryFileImportBundle locale, Properties localePropertiesFromIndex ) {
     Properties localeProperties = new Properties();
     PropertyResourceBundle rb = null;
     String comment = locale.getComment();
     String fileTitle = locale.getName();
 
-    try {
-      byte[] bytes = IOUtils.toByteArray( locale.getInputStream() );
-      java.io.InputStream bundleInputStream = new ByteArrayInputStream( bytes );
-      rb = new PropertyResourceBundle( bundleInputStream );
-    } catch ( Exception returnEmptyIfError ) {
-      getLogger().error( returnEmptyIfError.getMessage() );
-      return localeProperties;
+    if ( !localePropertiesFromIndex.isEmpty() ) {
+      // for old style index.xml as locale
+      comment = localePropertiesFromIndex.getProperty( DESC_PROPERTY_NAME );
+      fileTitle = localePropertiesFromIndex.getProperty( TITLE_PROPERTY_NAME );
+    } else {
+      try {
+        byte[] bytes = IOUtils.toByteArray( locale.getInputStream() );
+        java.io.InputStream bundleInputStream = new ByteArrayInputStream( bytes );
+        rb = new PropertyResourceBundle( bundleInputStream );
+      } catch ( Exception returnEmptyIfError ) {
+        getLogger().error( returnEmptyIfError.getMessage() );
+        return localeProperties;
+      }
+
+      if ( rb != null ) {
+        // this is the 4.8 style - name and description
+        // First try file desc. If no file desc, try for a directory desc, else try fallback
+        comment =
+            rb.containsKey( DESC_PROPERTY_NAME ) ? rb.getString( DESC_PROPERTY_NAME ) : rb
+                .containsKey( FILE_DESCRIPTION ) ? rb.getString( FILE_DESCRIPTION ) : rb
+                .containsKey( DIRECTORY_DESCRIPTION ) ? rb.getString( DIRECTORY_DESCRIPTION ) : comment;
+
+        // First try name. If no name, try title. If no title, try for a directory name, else use filename.
+        fileTitle =
+            rb.containsKey( TITLE_PROPERTY_NAME ) ? rb.getString( TITLE_PROPERTY_NAME ) : rb.containsKey( "title" )
+                ? rb.getString( "title" ) : rb.containsKey( FILE_TITLE ) ? rb.getString( FILE_TITLE ) : rb
+                    .containsKey( DIRECTORY_NAME ) ? rb.getString( DIRECTORY_NAME ) : fileTitle;
+
+      }
     }
-
-    if ( rb != null ) {
-      // this is the 4.8 style - name and description
-      // First try file desc. If no file desc, try for a directory desc, else try fallback
-      comment =
-          rb.containsKey( "description" ) ? rb.getString( "description" ) : rb.containsKey( FILE_DESCRIPTION ) ? rb
-              .getString( FILE_DESCRIPTION ) : rb.containsKey( DIRECTORY_DESCRIPTION ) ? rb
-              .getString( DIRECTORY_DESCRIPTION ) : comment;
-
-      // First try name. If no name, try title. If no title, try for a directory name, else use filename.
-      fileTitle =
-          rb.containsKey( "name" ) ? rb.getString( "name" ) : rb.containsKey( "title" ) ? rb.getString( "title" ) : rb
-              .containsKey( FILE_TITLE ) ? rb.getString( FILE_TITLE ) : rb.containsKey( DIRECTORY_NAME ) ? rb
-              .getString( DIRECTORY_NAME ) : fileTitle;
-
-    }
-
     // this is the new .locale Jcr property names
-    localeProperties.setProperty( FILE_DESCRIPTION, comment != null ? comment : "" );
-    localeProperties.setProperty( FILE_TITLE, fileTitle != null ? fileTitle : "" );
-
+    localeProperties.setProperty( FILE_DESCRIPTION, comment != null ? comment : StringUtils.EMPTY );
+    localeProperties.setProperty( FILE_TITLE, fileTitle != null ? fileTitle : StringUtils.EMPTY );
     return localeProperties;
   }
 
@@ -144,7 +160,7 @@ public class LocaleImportHandler extends RepositoryFileImportFileHandler impleme
     return localeCode;
   }
 
-  private RepositoryFile getLocaleParent( RepositoryFileImportBundle locale ) {
+  private RepositoryFile getLocaleParent( RepositoryFileImportBundle locale, Properties localePropertiesFromIndex ) {
     if ( unifiedRepository == null ) {
       return null;
     }
@@ -156,35 +172,40 @@ public class LocaleImportHandler extends RepositoryFileImportFileHandler impleme
     }
     RepositoryFile localeFolder = unifiedRepository.getFile( locale.getPath() );
 
-    if ( isLocaleFolder( localeFileName ) ) {
+    if ( isLocaleFolder( localeFileName, localePropertiesFromIndex ) ) {
       localeParent = localeFolder;
-    } else {
+    } else if ( localeFolder != null ) {
       List<RepositoryFile> localeFolderChildren = unifiedRepository.getChildren( localeFolder.getId() );
       for ( RepositoryFile localeChild : localeFolderChildren ) {
 
         String localeChildName = extractFileName( localeChild.getName() );
         String localeChildExtension = extractExtension( localeChild.getName() );
 
-        // [BISERVER-10444] locale is not being applied to correct file extension (report1.prpt.locale vs. report1.xaction.locale)
-        boolean localeFileNameAlsoContainsFileExtension = checkIfLocaleFileNameAlsoContainsAFileExtension(localeFileName);
-        
-        if( localeFileNameAlsoContainsFileExtension ){
-          //FilenameUtils.getBaseName() returns name of file or empty string if none exists (NPE safe)
-          //FilenameUtils.getExtension() returns extension of file or empty string if none exists (NPE safe)
+        // [BISERVER-10444] locale is not being applied to correct file extension (report1.prpt.locale vs.
+        // report1.xaction.locale)
+        boolean localeFileNameAlsoContainsFileExtension =
+            checkIfLocaleFileNameAlsoContainsAFileExtension( localeFileName );
+
+        if ( localeFileNameAlsoContainsFileExtension ) {
+          // FilenameUtils.getBaseName() returns name of file or empty string if none exists (NPE safe)
+          // FilenameUtils.getExtension() returns extension of file or empty string if none exists (NPE safe)
           String localeFileExtension = FilenameUtils.getExtension( FilenameUtils.getBaseName( localeFileName ) );
-          String localeFileNameWithoutExtensions = FilenameUtils.getBaseName( FilenameUtils.getBaseName( localeFileName ) );
-          
+          String localeFileNameWithoutExtensions =
+              FilenameUtils.getBaseName( FilenameUtils.getBaseName( localeFileName ) );
+
           if(localeFileExtension.contains( "_" )) {
             localeFileExtension = localeFileExtension.substring( 0, localeFileExtension.indexOf( "_" ) );
           }
           
-          if ( localeFileNameWithoutExtensions.equals( localeChildName ) 
-              && localeFileExtension.equalsIgnoreCase( localeChildExtension ) && artifacts.contains( localeChildExtension ) ) {
+          if ( localeFileNameWithoutExtensions.equals( localeChildName )
+              && localeFileExtension.equalsIgnoreCase( localeChildExtension )
+              && artifacts.contains( localeChildExtension ) ) {
             localeParent = localeChild;
             break;
           }
-          
-        } else if ( extractFileName( localeFileName ).equals( localeChildName ) && artifacts.contains( localeChildExtension ) ) {
+
+        } else if ( extractFileName( localeFileName ).equals( localeChildName )
+            && artifacts.contains( localeChildExtension ) ) {
           localeParent = localeChild;
           break;
         }
@@ -193,9 +214,10 @@ public class LocaleImportHandler extends RepositoryFileImportFileHandler impleme
     return localeParent;
   }
 
-  private boolean isLocaleFolder( String localeFileName ) {
+  private boolean isLocaleFolder( String localeFileName, Properties localePropertiesFromIndex ) {
     return ( localeFileName.startsWith( LOCALE_FOLDER ) && localeFileName.endsWith( LOCALE_EXT ) )
-        || ( localeFileName.startsWith( LOCALE_FOLDER ) && localeFileName.endsWith( OLD_LOCALE_EXT ) );
+        || ( localeFileName.startsWith( LOCALE_FOLDER ) && localeFileName.endsWith( OLD_LOCALE_EXT ) )
+        || ( localeFileName.equals( LOCALE_FOLDER + XML_LOCALE_EXT ) && !localePropertiesFromIndex.isEmpty() );
   }
 
   private String extractExtension( String name ) {
@@ -213,15 +235,44 @@ public class LocaleImportHandler extends RepositoryFileImportFileHandler impleme
     }
     return name.substring( 0, idx );
   }
-  
-  private boolean checkIfLocaleFileNameAlsoContainsAFileExtension( String locale ){
-    
-    //ex: report1.prpt.locale    
-    if( !StringUtils.isEmpty( locale ) ){
-      //FilenameUtils.getBaseName() returns name of file or empty string if none exists (NPE safe)
-      //FilenameUtils.getExtension() returns extension of file or empty string if none exists (NPE safe)
-      return !StringUtils.isEmpty( FilenameUtils.getExtension( FilenameUtils.getBaseName( locale ) ) );
+
+  private boolean checkIfLocaleFileNameAlsoContainsAFileExtension( String locale ) {
+
+    // ex: report1.prpt.locale
+    if ( StringUtils.isNotEmpty( locale ) ) {
+      // FilenameUtils.getBaseName() returns name of file or empty string if none exists (NPE safe)
+      // FilenameUtils.getExtension() returns extension of file or empty string if none exists (NPE safe)
+      return StringUtils.isNotEmpty( FilenameUtils.getExtension( FilenameUtils.getBaseName( locale ) ) );
     }
     return false;
   }
+
+  private Properties loadPropertiesByXml( RepositoryFileImportBundle localeBundle ) {
+    final Properties properties = new Properties();
+    String fileTitle = localeBundle.getName();
+
+    if ( ( LOCALE_FOLDER + XML_LOCALE_EXT ).equals( fileTitle ) ) {
+      DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = null;
+      try {
+        builder = builderFactory.newDocumentBuilder();
+        Document document = builder.parse( localeBundle.getInputStream() );
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        String name = xPath.compile( "/index/name" ).evaluate( document );
+        String desc = xPath.compile( "/index/description" ).evaluate( document );
+
+        if ( StringUtils.isNotBlank( name ) && !name.equals( VARIABLE_SYMBOL_FROM_INDEX + TITLE_PROPERTY_NAME ) ) {
+          properties.put( TITLE_PROPERTY_NAME, name );
+        }
+        if ( StringUtils.isNotBlank( desc ) && !desc.equals( VARIABLE_SYMBOL_FROM_INDEX + DESC_PROPERTY_NAME ) ) {
+          properties.put( DESC_PROPERTY_NAME, desc );
+        }
+      } catch ( Exception e ) {
+        getLogger().error( e.getMessage() );
+      }
+    }
+    return properties;
+  }
+
 }
