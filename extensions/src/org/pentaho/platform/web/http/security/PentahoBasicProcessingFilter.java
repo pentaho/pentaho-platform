ce77e4e3efa6d0be18e871f208bd7eb00d6aff87
Nick Baker <nbaker@pentaho.com>
2014-06-02 09:35:43 -0400

    [BISERVER-10850] Firing AuthenticationSucccess from Basic Filter to ensure session startup actions run

--- extensions/src/org/pentaho/platform/web/http/security/PentahoBasicProcessingFilter.java
+++ extensions/src/org/pentaho/platform/web/http/security/PentahoBasicProcessingFilter.java
@@ -17,9 +17,12 @@
 
 package org.pentaho.platform.web.http.security;
 
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.security.Authentication;
 import org.springframework.security.BadCredentialsException;
 import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.event.authentication.AuthenticationSuccessEvent;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -31,18 +34,25 @@ import java.io.IOException;
 /**
  * This class's sole purpose is to defeat the persistence of Basic-Auth credentials in the browser. The mechanism used
  * to accomplish this is to detect an expired (invalid) HttpSession from the client.
- * 
+ * <p/>
  * If the first request after a session becomes invalid is a Basic-Auth request, we automatically deny, forcing
  * re-authentication.
- * 
+ * <p/>
  * The second path is if the first request after session invalidation is not a basic-auth (user manually logged out and
  * was presented with the login page), we drop a cookie in the response noting the event. The next request with
  * Basic-Auth and a valid HttpSession checks for this cookie and if present, forces reauthentication.
- * 
- * 
+ * <p/>
+ * <p/>
  * User: nbaker Date: 8/15/13
  */
-public class PentahoBasicProcessingFilter extends org.springframework.security.ui.basicauth.BasicProcessingFilter {
+public class PentahoBasicProcessingFilter extends org.springframework.security.ui.basicauth.BasicProcessingFilter
+  implements ApplicationEventPublisherAware {
+
+  private ApplicationEventPublisher applicationEventPublisher;
+
+  public void setApplicationEventPublisher( ApplicationEventPublisher applicationEventPublisher ) {
+    this.applicationEventPublisher = applicationEventPublisher;
+  }
 
   @Override
   public void doFilterHttp( HttpServletRequest request, HttpServletResponse response, FilterChain chain )
@@ -69,7 +79,7 @@ public class PentahoBasicProcessingFilter extends org.springframework.security.u
         }
         // force the prompt for credentials
         getAuthenticationEntryPoint()
-            .commence( request, response, new BadCredentialsException( "Clearing Basic-Auth" ) );
+          .commence( request, response, new BadCredentialsException( "Clearing Basic-Auth" ) );
         return;
       } else if ( expiredCookie != null ) {
         // Session is expired but this request does not include basic-auth, drop a cookie to keep track of this event.
@@ -81,7 +91,7 @@ public class PentahoBasicProcessingFilter extends org.springframework.security.u
     } else {
       String header = request.getHeader( "Authorization" );
       if ( header != null && header.indexOf( "Basic" ) == 0
-          && SecurityContextHolder.getContext().getAuthentication() == null ) {
+        && SecurityContextHolder.getContext().getAuthentication() == null ) {
         // Session is valid, but Basic-auth is supplied. Check to see if the session end cookie we created is present,
         // if so, force reauthentication.
 
@@ -94,7 +104,7 @@ public class PentahoBasicProcessingFilter extends org.springframework.security.u
               c.setPath( request.getContextPath() != null ? request.getContextPath() : "/" );
               response.addCookie( c );
               getAuthenticationEntryPoint().commence( request, response,
-                  new BadCredentialsException( "Clearing Basic-Auth" ) );
+                new BadCredentialsException( "Clearing Basic-Auth" ) );
               return;
             }
           }
@@ -102,14 +112,16 @@ public class PentahoBasicProcessingFilter extends org.springframework.security.u
       }
     }
 
-    super.doFilterHttp( request, response, chain ); // To change body of overridden methods use File | Settings | File
-                                                    // Templates.
+    super.doFilterHttp( request, response, chain );
   }
 
   @Override
   protected void onSuccessfulAuthentication( HttpServletRequest request, HttpServletResponse response,
-      Authentication authResult ) throws IOException {
+                                             Authentication authResult ) throws IOException {
     super.onSuccessfulAuthentication( request, response, authResult );
     request.getSession().setAttribute( "BasicAuth", "true" );
+    if( applicationEventPublisher != null ) {
+      applicationEventPublisher.publishEvent( new AuthenticationSuccessEvent( authResult ) );
+    }
   }
 }
