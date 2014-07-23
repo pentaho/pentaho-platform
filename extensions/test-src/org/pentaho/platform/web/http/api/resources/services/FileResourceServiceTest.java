b29d7e44c0af5a0d0dc966c51274c5fed463d8eb
Brendan Lynch <blynch@pentaho.com>
2014-07-23 11:46:07 -0400

    [BACKLOG-10] - Create example of refactor

---
+++ extensions/test-src/org/pentaho/platform/web/http/api/resources/services/FileResourceServiceTest.java
@@ -0,0 +1,16 @@
+package org.pentaho.platform.web.http.api.resources.services;
+
+import org.junit.Test;
+
+public class FileResourceServiceTest {
+
+  @Test
+  public void testDoDeleteFiles() throws Exception {
+
+  }
+
+  @Test
+  public void testGetRepoWs() throws Exception {
+
+  }
+}
