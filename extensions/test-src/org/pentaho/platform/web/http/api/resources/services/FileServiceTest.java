53c5cf47069fb411530d499ae6a4fee2d703cc87
wseyler <wseyler@pentaho.com>
2014-08-01 14:36:52 -0400

    [BACKLOG-194] - Coded Tests doGetFileOrDirAsDownload using the FileService.  Refactored FileService so that it can be tested easier.

--- extensions/test-src/org/pentaho/platform/web/http/api/resources/services/FileServiceTest.java
+++ extensions/test-src/org/pentaho/platform/web/http/api/resources/services/FileServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyList;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
