8a43534492794d88f3275d524a9a2e55aa7bdc4d
Benjamin Morrise <bmorrise@pentaho.com>
2014-08-04 13:19:31 -0400

    [BACKLOG-194] Refactor and docs for /repo/files/{pathId : .+}/metadata

--- extensions/src/org/pentaho/platform/web/http/api/resources/services/FileService.java
+++ extensions/src/org/pentaho/platform/web/http/api/resources/services/FileService.java
@@ -800,6 +800,49 @@ public class FileService {
     return getRepoWs().getDeletedFiles();
   }
 
+  /**
+   * Get metadata for a file by path id
+   *
+   * @param pathId
+   * @return
+   */
+  public List<StringKeyStringValueDto> doGetMetadata( String pathId ) throws FileNotFoundException {
+    List<StringKeyStringValueDto> list = null;
+    String path = null;
+    if ( pathId == null || pathId.equals( FileUtils.PATH_SEPARATOR ) ) {
+      path = FileUtils.PATH_SEPARATOR;
+    } else {
+      if ( !pathId.startsWith( FileUtils.PATH_SEPARATOR ) ) {
+        path = idToPath( pathId );
+      }
+    }
+    final RepositoryFileDto file = getRepoWs().getFile( path );
+    if ( file == null ) {
+      throw new FileNotFoundException();
+    }
+
+    list = getRepoWs().getFileMetadata( file.getId() );
+
+    if ( list != null ) {
+      boolean hasSchedulable = false;
+      for ( StringKeyStringValueDto value : list ) {
+        if ( value.getKey().equals( "_PERM_SCHEDULABLE" ) ) {
+          hasSchedulable = true;
+          break;
+        }
+      }
+      if ( !hasSchedulable ) {
+        StringKeyStringValueDto schedPerm = new StringKeyStringValueDto( "_PERM_SCHEDULABLE", "true" );
+        list.add( schedPerm );
+      }
+
+      // check file object for hidden value and add it to the list
+      list.add( new StringKeyStringValueDto( "_PERM_HIDDEN", String.valueOf( file.isHidden() ) ) );
+    }
+
+    return list;
+  }
+
   protected RepositoryDownloadWhitelist getWhitelist() {
     if ( whitelist == null ) {
       whitelist = new RepositoryDownloadWhitelist();
