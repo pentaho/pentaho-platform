14e0302c5a556fc8e72113e63b2df6b7aae79e79
Gretchen Moran <gmoran@pentaho.com>
2014-08-13 12:43:50 -0400

    [PDI-10610]: 	distinguish file extension from document type; add mime type.

--- api/src/org/pentaho/platform/api/metaverse/IMetaverseDocument.java
+++ api/src/org/pentaho/platform/api/metaverse/IMetaverseDocument.java
@@ -27,7 +27,7 @@ public interface IMetaverseDocument extends IMetaverseComponentDescriptor, IIden
    * 
    * @return the content of this object
    */
-  Object getContent();
+  public Object getContent();
 
   /**
    * Sets the content object for this document.
@@ -35,5 +35,37 @@ public interface IMetaverseDocument extends IMetaverseComponentDescriptor, IIden
    * @param content
    *          the new content
    */
-  void setContent( Object content );
+  public void setContent( Object content );
+
+  /**
+   * Gets the file extension for this document;
+   * characters only, the dot (.) is excluded
+   *
+   * @return the extension associated with this document
+   */
+  public String getExtension();
+
+  /**
+   *  Set the extension for this document
+   *
+   * @param extension
+   */
+  public void setExtension( String extension );
+
+  /**
+   * Returns the RFC compliant string version of the MIME content type,
+   * if it can be determined for this document.
+   *
+   * @return MIME content type if available; otherwise null
+   */
+  public String getMimeType();
+
+  /**
+   *  Set the MIME content type for this document.
+   *
+   * @param type
+   */
+  public void setMimeType( String type );
+
+
 }
