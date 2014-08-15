b759c2435d2d6adda1fe60cf277b2cd02c8b098c
Matt Burgess <mburgess@pentaho.com>
2014-08-14 22:26:04 -0400

    [PDI-10610]: Unifying properties-related interfaces

--- api/src/org/pentaho/platform/api/metaverse/IMetaverseElement.java
+++ api/src/org/pentaho/platform/api/metaverse/IMetaverseElement.java
@@ -20,45 +20,8 @@ package org.pentaho.platform.api.metaverse;
 import java.util.Set;
 
 /**
- * The IMetaverseElement interface provides methods common to metaverse elements (node, links, e.g.)
+ * The IMetaverseElement interface provides a contract for methods common to metaverse elements (node, links, e.g.)
  * 
  */
-public interface IMetaverseElement {
-
-  /**
-   * Return the object value associated with the provided string key. If no value exists for that key, return null.
-   * 
-   * @param key
-   *          the key of the key/value property
-   * @return the object value related to the string key
-   */
-  public <T> T getProperty( String key );
-
-  /**
-   * Return all the keys associated with the element.
-   * 
-   * @return the set of all string keys associated with the element
-   */
-  public Set<String> getPropertyKeys();
-
-  /**
-   * Assign a key/value property to the element. If a value already exists for this key, then the previous key/value is
-   * overwritten.
-   * 
-   * @param key
-   *          the string key of the property
-   * @param value
-   *          the object value o the property
-   */
-  public void setProperty( String key, Object value );
-
-  /**
-   * Un-assigns a key/value property from the element. The object value of the removed property is returned.
-   * 
-   * @param key
-   *          the key of the property to remove from the element
-   * @return the object value associated with that key prior to removal
-   */
-  public <T> T removeProperty( String key );
-
+public interface IMetaverseElement extends IHasProperties {
 }
