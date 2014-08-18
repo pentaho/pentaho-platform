05ecd40934fbc46bb6288ec1772bdc09c0ea4ead
Pedro Teixeira <pedrofvteixeira@gmail.com>
2014-08-18 12:29:38 +0100

    [BACKLOG-218] Add event bus to the platform and make sure it is available on the DI Server 	- added unit tests

---
+++ extensions/test-src/org/pentaho/platform/monitoring/MonitoringDeadEventSubscriberTest.java
@@ -0,0 +1,36 @@
+/*!
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* Copyright (c) 2002-2014 Pentaho Corporation..  All rights reserved.
+*/
+package org.pentaho.platform.monitoring;
+
+import com.google.common.eventbus.DeadEvent;
+import org.junit.Assert;
+import org.junit.Test;
+import org.pentaho.platform.monitoring.subscribers.MonitoringDeadEventSubscriber;
+
+public class MonitoringDeadEventSubscriberTest {
+
+
+  @Test
+  public void testGetSubscriberId() {
+    Assert.assertNotNull( new MonitoringDeadEventSubscriber().getSubscriberId() );
+  }
+
+  @Test
+  public void testHandleDeadEvent() {
+    new MonitoringDeadEventSubscriber().handleDeadEvent( new DeadEvent( new String("mock"), new String("mock") ) );
+  }
+}
