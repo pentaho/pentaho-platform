02679464f782f3d6898612813be423a748622688
Matt Burgess <mburgess@pentaho.com>
2014-08-26 15:53:44 -0400

    [PDI-10610]: Added get/setContext to IMetaverseComponentDescriptor

--- api/src/org/pentaho/platform/api/metaverse/IMetaverseComponentDescriptor.java
+++ api/src/org/pentaho/platform/api/metaverse/IMetaverseComponentDescriptor.java
@@ -1,3 +1,20 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2014 Pentaho Corporation..  All rights reserved.
+ */
+
 package org.pentaho.platform.api.metaverse;
 
 /**
@@ -7,7 +24,31 @@ package org.pentaho.platform.api.metaverse;
  */
 public interface IMetaverseComponentDescriptor extends IIdentifiable, INamespace {
 
-  public void setNamespace( INamespace namespace );
+  /**
+   * Sets the namespace for the component described by this descriptor.
+   *
+   * @param namespace the namespace to set
+   */
+  void setNamespace( INamespace namespace );
 
-  public INamespace getNamespace();
+  /**
+   * Gets the namespace for the component described by this descriptor.
+   *
+   * @return the namespace of the described component
+   */
+  INamespace getNamespace();
+
+  /**
+   * Gets the context ("static", "runtime", e.g.) associated with the component described by this descriptor.
+   *
+   * @return A string containing a description of the context associated with the described component
+   */
+  String getContext();
+
+  /**
+   * Sets the context ("static", "runtime", e.g.) associated with the component described by this descriptor.
+   *
+   * @param context the context for the described component
+   */
+  void setContext( String context );
 }
