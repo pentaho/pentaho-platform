c8cfdf7d194c8d4625cf5d11601294e22ca2199c
jeffreythomas <jthomas@pentaho.com>
2014-09-02 11:42:55 -0400

    [BISERVER-12037] - HttpMimeTypeListener is in two packages (same code)
    Talked with Will G. and deprecated the class that was created second (went back to svn to verify).

--- extensions/src/org/pentaho/platform/web/http/HttpMimeTypeListener.java
+++ extensions/src/org/pentaho/platform/web/http/HttpMimeTypeListener.java
@@ -24,52 +24,30 @@ import org.pentaho.platform.util.web.MimeHelper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Deprecated
 public class HttpMimeTypeListener implements IMimeTypeListener {
 
-  private HttpServletResponse response;
+  private org.pentaho.platform.web.servlet.HttpMimeTypeListener mimeTypeListener;
 
-  private HttpServletRequest request;
-
-  private String title;
-
+  @Deprecated
   public HttpMimeTypeListener( final HttpServletRequest request, final HttpServletResponse response ) {
-    this( request, response, null );
+    mimeTypeListener = new org.pentaho.platform.web.servlet.HttpMimeTypeListener( request, response );
   }
 
+  @Deprecated
   public HttpMimeTypeListener( final HttpServletRequest request, final HttpServletResponse response,
                                final String title ) {
-    this.request = request;
-    this.response = response;
-    this.title = title;
+    mimeTypeListener = new org.pentaho.platform.web.servlet.HttpMimeTypeListener( request, response, title );
   }
 
+  @Deprecated
   public void setName( String name ) {
-    title = name;
+    mimeTypeListener.setName( name );
   }
 
+  @Deprecated
   public void setMimeType( final String mimeType ) {
-    response.setContentType( mimeType );
-    response.setCharacterEncoding( LocaleHelper.getSystemEncoding() );
-    //
-    // Since this is an HTTP thing, allow users to override default behavior by allowing them
-    // the mime object as an attachment. Forcing everything non-html to be an attachment
-    // means that users can't have their excel reports appear in their IE browser, it makes
-    // SVG not work unless you have an external SVG viewer, it makes PDF files no longer
-    // be viewable in the browser. Please see PLATFORM-1018.
-    // also see: http://support.microsoft.com/kb/260519 for info on content-disposition header
-    //
-    String forceAttachment = request.getParameter( "forceAttachment" ); //$NON-NLS-1$
-    String fileName = title;
-    if ( fileName == null ) {
-      fileName = "default"; //$NON-NLS-1$
-    }
-    fileName = fileName.replace( ' ', '_' );
-    fileName += MimeHelper.getExtension( mimeType );
-    if ( ( forceAttachment != null ) && ( "true".equalsIgnoreCase( forceAttachment ) ) ) { //$NON-NLS-1$
-      response.setHeader( "content-disposition", "attachment;filename=" + fileName ); //$NON-NLS-1$ //$NON-NLS-2$
-    } else {
-      response.setHeader( "content-disposition", "inline;filename=" + fileName ); //$NON-NLS-1$ //$NON-NLS-2$
-    }
+    mimeTypeListener.setMimeType( mimeType );
   }
 
 }
