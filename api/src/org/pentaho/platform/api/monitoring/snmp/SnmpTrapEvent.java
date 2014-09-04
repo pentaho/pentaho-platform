e3c1a65f174207d542ab0054b883e83397a71ad3
Nick Baker <nbaker@pentaho.com>
2014-09-03 21:54:42 -0400

    [BACKLOG-425] Introducing Annotation-based Transformer replacing BeanUtils one.

---
+++ api/src/org/pentaho/platform/api/monitoring/snmp/SnmpTrapEvent.java
@@ -0,0 +1,36 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2014 Pentaho Corporation. All rights reserved.
+ */
+
+package org.pentaho.platform.api.monitoring.snmp;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a class as an SNMP Trap entity.
+ * Created by nbaker on 9/3/14.
+ */
+
+@Retention( RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE})
+@Inherited
+public @interface SnmpTrapEvent {
+  String oid();
+}
