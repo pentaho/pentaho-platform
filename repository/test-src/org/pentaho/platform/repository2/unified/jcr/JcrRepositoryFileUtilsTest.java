d7d71498185a2f10cb48b7037fd5f01c65ebc660
pminutillo <pminutillo@pentaho.com>
2014-09-11 11:28:06 -0400

    [BACKLOG-750] Fix for saving files when versioning is disabled. Save a new version, but remove previous to prevent incrementing number of versions. A version node is still required

--- repository/test-src/org/pentaho/platform/repository2/unified/jcr/JcrRepositoryFileUtilsTest.java
+++ repository/test-src/org/pentaho/platform/repository2/unified/jcr/JcrRepositoryFileUtilsTest.java
@@ -21,12 +21,13 @@
  ******************************************************************************/
 package org.pentaho.platform.repository2.unified.jcr;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import org.apache.jackrabbit.core.VersionManagerImpl;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.pentaho.platform.api.engine.IPentahoSession;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
@@ -34,29 +35,33 @@ import javax.jcr.Session;
 import javax.jcr.UnsupportedRepositoryOperationException;
 import javax.jcr.Workspace;
 import javax.jcr.version.Version;
+import javax.jcr.version.VersionHistory;
 import javax.jcr.version.VersionManager;
+import java.util.Calendar;
+import java.util.Date;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Tatsiana_Kasiankova
- * 
  */
 public class JcrRepositoryFileUtilsTest {
   /**
-   * 
+   *
    */
   private static final String VERSION_NAME_TEST = "Version Name Test";
   /**
-   * 
+   *
    */
   private static final String UNSUPPORTED_REPOSITORY_OPERATION_EXCEPTION_TEST_MESSAGE =
-      "Unsupported Repository Operation Exception Test Message";
+    "Unsupported Repository Operation Exception Test Message";
   /**
-   * 
+   *
    */
   private static final String REPOSITORY_EXCEPTION_TEST_MESSAGE = "Repository Exception Test Message";
   private PentahoJcrConstants pJcrConstMock = mock( PentahoJcrConstants.class );
@@ -73,7 +78,7 @@ public class JcrRepositoryFileUtilsTest {
 
   /**
    * This test covers the change for avoiding NPE from Jackrabbit: see http://jira.pentaho.com/browse/BACKLOG-175
-   * 
+   *
    * @throws UnsupportedRepositoryOperationException
    * @throws RepositoryException
    */
@@ -95,7 +100,7 @@ public class JcrRepositoryFileUtilsTest {
   @Test
   public void testRepositoryExceptionThrows() throws Exception {
     when( vmanagerMock.getBaseVersion( anyString() ) ).thenThrow(
-        new RepositoryException( REPOSITORY_EXCEPTION_TEST_MESSAGE ) );
+      new RepositoryException( REPOSITORY_EXCEPTION_TEST_MESSAGE ) );
     exception.expect( RepositoryException.class );
     exception.expectMessage( REPOSITORY_EXCEPTION_TEST_MESSAGE );
     JcrRepositoryFileUtils.getVersionId( sessionMock, pJcrConstMock, nodeMock );
@@ -104,7 +109,7 @@ public class JcrRepositoryFileUtilsTest {
   @Test
   public void testUnsupportedRepositoryOperationExceptionThrows() throws Exception {
     when( workspaceMock.getVersionManager() ).thenThrow(
-        new UnsupportedRepositoryOperationException( UNSUPPORTED_REPOSITORY_OPERATION_EXCEPTION_TEST_MESSAGE ) );
+      new UnsupportedRepositoryOperationException( UNSUPPORTED_REPOSITORY_OPERATION_EXCEPTION_TEST_MESSAGE ) );
     exception.expect( UnsupportedRepositoryOperationException.class );
     exception.expectMessage( UNSUPPORTED_REPOSITORY_OPERATION_EXCEPTION_TEST_MESSAGE );
     JcrRepositoryFileUtils.getVersionId( sessionMock, pJcrConstMock, nodeMock );
@@ -132,6 +137,113 @@ public class JcrRepositoryFileUtilsTest {
     } catch ( Exception e ) {
       fail( "No exception shold be here but it is: " + e );
     }
+  }
+
+  @Test
+  public void testCheckinNearestVersionableNodeIfNecessary() throws Exception {
+    String versionMessage = "Version message";
+    String username = "admin";
+
+    Date curDate = new Date();
+
+    Node mockNode = mock( Node.class );
+    Node parentNode = mock( Node.class );
+    IPentahoSession pentahoSession = mock( IPentahoSession.class );
+    when( pentahoSession.getName() ).thenReturn( username );
+
+    JcrRepositoryFileUtils.setVersioningEnabled( Boolean.TRUE );
+
+    VersionManagerImpl versionManager = mock( VersionManagerImpl.class );
+    when( workspaceMock.getVersionManager() ).thenReturn( versionManager );
+
+    when( mockNode.getParent() ).thenReturn( parentNode );
+
+    PentahoJcrConstants pentahoJcrConstants = new PentahoJcrConstants( sessionMock );
+
+    when( mockNode.isNodeType( pentahoJcrConstants.getPHO_MIX_VERSIONABLE() ) ).thenReturn( Boolean.TRUE );
+
+    PentahoSessionHolder.setSession( pentahoSession );
+
+    JcrRepositoryFileUtils
+      .checkinNearestVersionableNodeIfNecessary( sessionMock, pentahoJcrConstants, mockNode, versionMessage, curDate,
+        true );
+
+    Calendar cal = Calendar.getInstance();
+    if ( curDate != null ) {
+      cal.setTime( curDate );
+    } else {
+      cal.setTime( new Date() );
+    }
+
+    // validate checkin was called
+    verify( versionManager ).checkin( mockNode.getPath(), cal );
+  }
+
+  @Test
+  public void testCheckinNearestVersionableNodeIfNecessaryWithoutVersioning() throws Exception {
+    String versionMessage = "Version message";
+    String username = "admin";
+    String mockNodeId = "12345";
+    String versionName = "versionName";
+    String versionableIdentifier = "versionableIdentifier";
+
+    Date curDate = new Date();
+
+    Calendar cal = Calendar.getInstance();
+    if ( curDate != null ) {
+      cal.setTime( curDate );
+    } else {
+      cal.setTime( new Date() );
+    }
+
+    // disable versioning
+    JcrRepositoryFileUtils.setVersioningEnabled( Boolean.FALSE );
+
+    Node mockNode = mock( Node.class );
+    Node parentNode = mock( Node.class );
+    Node versionNode = mock( Node.class );
+    Version mockVersion = mock( Version.class );
+    Version mockVersion2 = mock( Version.class );
+    VersionManagerImpl versionManager = mock( VersionManagerImpl.class );
+    VersionHistory mockVersionHistory = mock( VersionHistory.class );
+    IPentahoSession pentahoSession = mock( IPentahoSession.class );
+
+    when( pentahoSession.getName() ).thenReturn( username );
+    when( sessionMock.getNodeByIdentifier( anyString() ) ).thenReturn( mockNode );
+
+    String[] mockVersionLabels = { "label1" };
+    when( mockVersionHistory.getVersionLabels( mockVersion ) ).thenReturn( mockVersionLabels );
+    when( mockVersionHistory.getVersionableIdentifier() ).thenReturn( versionableIdentifier );
+
+    Version[] mockVersionsList = { mockVersion, mockVersion2 };
+    Version[] mockVersionsList2 = { mockVersion };
+    when( mockVersion.getSuccessors() ).thenReturn( mockVersionsList, mockVersionsList2, null );
+    when( mockVersion.getNode( (String) anyObject() ) ).thenReturn( versionNode );
+    when( mockVersion.getName() ).thenReturn( versionName );
+    when( mockVersion.getCreated() ).thenReturn( cal );
+
+    when( versionManager.getVersionHistory( mockNode.getPath() ) ).thenReturn( mockVersionHistory );
+    when( workspaceMock.getVersionManager() ).thenReturn( versionManager );
+    when( mockVersionHistory.getRootVersion() ).thenReturn( mockVersion );
+
+    when( mockNode.getParent() ).thenReturn( parentNode );
+    when( mockNode.getIdentifier() ).thenReturn( mockNodeId );
+
+    PentahoJcrConstants pentahoJcrConstants = new PentahoJcrConstants( sessionMock );
+
+    when( mockNode.isNodeType( pentahoJcrConstants.getPHO_MIX_VERSIONABLE() ) ).thenReturn( Boolean.TRUE );
+
+    PentahoSessionHolder.setSession( pentahoSession );
+
+    JcrRepositoryFileUtils
+      .checkinNearestVersionableNodeIfNecessary( sessionMock, pentahoJcrConstants, mockNode, versionMessage, curDate,
+        true );
+
+    // verify checkin was called
+    verify( versionManager ).checkin( mockNode.getPath(), cal );
+
+    // verify version is deleted
+    verify( mockVersionHistory ).removeVersion( (String) anyObject() );
   }
 
 }
