5ada54774e3f82a9c75f05d19a8c999b0af3f454
Anna Popova1 <Anna_Popova1@epam.com>
2014-09-16 16:57:22 +0400

    [TEST] BACKLOG-774 - uncomment and refactoring XQueryTest

--- extensions/test-src/org/pentaho/platform/plugin/services/connections/xquery/XQueryTest.java
+++ extensions/test-src/org/pentaho/platform/plugin/services/connections/xquery/XQueryTest.java
@@ -17,17 +17,33 @@
 
 package org.pentaho.platform.plugin.services.connections.xquery;
 
-import java.io.File;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.util.Properties;
+
+import net.sf.saxon.query.StaticQueryContext;
+import net.sf.saxon.trans.XPathException;
+
+import org.apache.commons.collections.ListUtils;
+import org.apache.commons.lang.StringUtils;
 import org.pentaho.commons.connection.IPeekable;
+import org.pentaho.commons.connection.IPentahoConnection;
 import org.pentaho.commons.connection.IPentahoResultSet;
+import org.pentaho.commons.connection.memory.MemoryMetaData;
+import org.pentaho.commons.connection.memory.MemoryResultSet;
+import org.pentaho.platform.api.engine.ILogger;
 import org.pentaho.platform.engine.core.BaseTest;
+import org.pentaho.platform.plugin.services.connections.javascript.JavaScriptResultSet;
 
 @SuppressWarnings( { "all" } )
 public class XQueryTest extends BaseTest {
   private static final String SOLUTION_PATH = "test-src/connections-solution";
   private static final String ALT_SOLUTION_PATH = "test-src/connections-solution";
   private static final String PENTAHO_XML_PATH = "/system/pentaho.xml";
+
+  private static final String TEST_QUERY = "doc(\"" + SOLUTION_PATH + "/xquery/books.xml\")/bookstore/book";
 
   public String getSolutionPath() {
     File file = new File( SOLUTION_PATH + PENTAHO_XML_PATH );
@@ -38,13 +54,12 @@ public class XQueryTest extends BaseTest {
       System.out.println( "File does not exist returning " + ALT_SOLUTION_PATH );
       return ALT_SOLUTION_PATH;
     }
-
   }
 
-  public void testQuery1() throws Exception {
+  public void testExecuteQuery() throws Exception {
 
     XQConnection connection = new XQConnection();
-    IPentahoResultSet data = connection.executeQuery( "doc(\"" + SOLUTION_PATH + "/xquery/books.xml\")/bookstore/book" );
+    IPentahoResultSet data = connection.executeQuery( TEST_QUERY );
     assertNotNull( "result set is null", data );
 
     assertTrue( "result set is wrong type", data instanceof XQResultSet );
@@ -63,9 +78,23 @@ public class XQueryTest extends BaseTest {
     data.closeConnection();
   }
 
+  public void testExecuteWrongQuery() throws Exception {
+    try {
+      XQConnection connection = new XQConnection();
+      StaticQueryContext mockContext = mock( StaticQueryContext.class );
+      when( mockContext.compileQuery( TEST_QUERY ) ).thenThrow( new XPathException( "Test XPathException" ) );
+      connection.sqc = mockContext;
+
+      IPentahoResultSet data = connection.executeQuery( TEST_QUERY );
+      fail( "Should throw XPathException" );
+    } catch ( XPathException e ) {
+      // valid
+    }
+  }
+
   public void testGetDataRow() throws Exception {
     XQConnection connection = new XQConnection();
-    IPentahoResultSet data = connection.executeQuery( "doc(\"" + SOLUTION_PATH + "/xquery/books.xml\")/bookstore/book" );
+    IPentahoResultSet data = connection.executeQuery( TEST_QUERY );
     assertNotNull( "result set is null", data );
 
     Object[] row = data.getDataRow( 1 );
@@ -86,7 +115,7 @@ public class XQueryTest extends BaseTest {
 
   public void testGetDataColumn() throws Exception {
     XQConnection connection = new XQConnection();
-    IPentahoResultSet data = connection.executeQuery( "doc(\"" + SOLUTION_PATH + "/xquery/books.xml\")/bookstore/book" );
+    IPentahoResultSet data = connection.executeQuery( TEST_QUERY );
     assertNotNull( "result set is null", data );
 
     Object[] col = data.getDataColumn( 2 );
@@ -105,7 +134,7 @@ public class XQueryTest extends BaseTest {
 
     XQConnection connection = new XQConnection();
     connection.setMaxRows( 2 );
-    IPentahoResultSet data = connection.executeQuery( "doc(\"" + SOLUTION_PATH + "/xquery/books.xml\")/bookstore/book" );
+    IPentahoResultSet data = connection.executeQuery( TEST_QUERY );
     assertNotNull( "result set is null", data );
 
     assertTrue( "result set is wrong type", data instanceof XQResultSet );
@@ -135,7 +164,7 @@ public class XQueryTest extends BaseTest {
   public void testValueAt() throws Exception {
 
     XQConnection connection = new XQConnection();
-    IPentahoResultSet data = connection.executeQuery( "doc(\"" + SOLUTION_PATH + "/xquery/books.xml\")/bookstore/book" );
+    IPentahoResultSet data = connection.executeQuery( TEST_QUERY );
     assertNotNull( "result set is null", data );
 
     assertEquals( "2005", data.getValueAt( 0, 2 ) );
@@ -150,9 +179,8 @@ public class XQueryTest extends BaseTest {
   }
 
   public void testPeek() throws Exception {
-
     XQConnection connection = new XQConnection();
-    IPentahoResultSet data = connection.executeQuery( "doc(\"" + SOLUTION_PATH + "/xquery/books.xml\")/bookstore/book" );
+    IPentahoResultSet data = connection.executeQuery( TEST_QUERY );
     assertNotNull( "result set is null", data );
 
     assertTrue( "result set is wrong type", data instanceof XQResultSet );
@@ -226,169 +254,154 @@ public class XQueryTest extends BaseTest {
 
     row = peekable.peek();
     assertNull( row );
-
   }
 
-  /*
-   * public void testAddRow() {
-   * 
-   * MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
-   * 
-   * MemoryResultSet dataSet = new MemoryResultSet( metadata ); JavaScriptResultSet data = new JavaScriptResultSet( );
-   * data.setResultSet(dataSet);
-   * 
-   * data.addRow( new Object[] {"a", new Integer(1) } ); data.addRow( new Object[] {"b", new Integer(2) } );
-   * data.addRow( new Object[] {"c", new Integer(3) } );
-   * 
-   * assertEquals( 3, data.getRowCount() ); assertEquals( "a", data.getValueAt(0, 0) ); assertEquals( 1,
-   * data.getValueAt(0, 1) ); assertEquals( "b", data.getValueAt(1, 0) ); assertEquals( 2, data.getValueAt(1, 1) );
-   * assertEquals( "c", data.getValueAt(2, 0) ); assertEquals( 3, data.getValueAt(2, 1) );
-   * 
-   * }
-   * 
-   * public void testIterators() {
-   * 
-   * MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
-   * 
-   * MemoryResultSet dataSet = new MemoryResultSet( metadata ); JavaScriptResultSet data = new JavaScriptResultSet( );
-   * data.setResultSet(dataSet);
-   * 
-   * data.addRow( new Object[] {"a", new Integer(1) } ); data.addRow( new Object[] {"b", new Integer(2) } );
-   * data.addRow( new Object[] {"c", new Integer(3) } );
-   * 
-   * assertEquals( 3, data.getRowCount() ); assertEquals( "a", data.getValueAt(0, 0) ); assertEquals( 1,
-   * data.getValueAt(0, 1) ); assertEquals( "b", data.getValueAt(1, 0) ); assertEquals( 2, data.getValueAt(1, 1) );
-   * assertEquals( "c", data.getValueAt(2, 0) ); assertEquals( 3, data.getValueAt(2, 1) );
-   * 
-   * assertEquals( "a", data.next()[0] ); assertEquals( "b", data.next()[0] ); assertEquals( "c", data.next()[0] );
-   * assertNull( data.next() );
-   * 
-   * data.beforeFirst();
-   * 
-   * assertEquals( "a", data.next()[0] ); assertEquals( "b", data.next()[0] ); assertEquals( "c", data.next()[0] );
-   * assertNull( data.next() );
-   * 
-   * data.close();
-   * 
-   * assertEquals( "a", data.next()[0] ); assertEquals( "b", data.next()[0] ); assertEquals( "c", data.next()[0] );
-   * assertNull( data.next() );
-   * 
-   * data.closeConnection();
-   * 
-   * assertEquals( "a", data.next()[0] ); assertEquals( "b", data.next()[0] ); assertEquals( "c", data.next()[0] );
-   * assertNull( data.next() );
-   * 
-   * data.dispose();
-   * 
-   * assertEquals( "a", data.next()[0] ); assertEquals( "b", data.next()[0] ); assertEquals( "c", data.next()[0] );
-   * assertNull( data.next() );
-   * 
-   * }
-   * 
-   * public void testGetDataColumn() {
-   * 
-   * MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
-   * 
-   * MemoryResultSet dataSet = new MemoryResultSet( metadata ); JavaScriptResultSet data = new JavaScriptResultSet( );
-   * data.setResultSet(dataSet);
-   * 
-   * data.addRow( new Object[] {"a", new Integer(1) } ); data.addRow( new Object[] {"b", new Integer(2) } );
-   * data.addRow( new Object[] {"c", new Integer(3) } );
-   * 
-   * Object col[] = data.getDataColumn( 0 ); assertEquals( 3, col.length ); assertEquals( "a", col[0] ); assertEquals(
-   * "b", col[1] ); assertEquals( "c", col[2] );
-   * 
-   * col = data.getDataColumn( 1 ); assertEquals( 3, col.length ); assertEquals( 1, col[0] ); assertEquals( 2, col[1] );
-   * assertEquals( 3, col[2] ); }
-   * 
-   * public void testGetDataRow() {
-   * 
-   * MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
-   * 
-   * MemoryResultSet dataSet = new MemoryResultSet( metadata ); JavaScriptResultSet data = new JavaScriptResultSet( );
-   * data.setResultSet(dataSet);
-   * 
-   * data.addRow( new Object[] {"a", new Integer(1) } ); data.addRow( new Object[] {"b", new Integer(2) } );
-   * data.addRow( new Object[] {"c", new Integer(3) } );
-   * 
-   * Object row[] = data.getDataRow( 0 ); assertEquals( 2, row.length ); assertEquals( "a", row[0] ); assertEquals( 1,
-   * row[1] ); row = data.getDataRow( 1 ); assertEquals( "b", row[0] ); assertEquals( 2, row[1] ); row =
-   * data.getDataRow( 2 ); assertEquals( "c", row[0] ); assertEquals( 3, row[1] );
-   * 
-   * assertNull( data.getDataRow( 99 )); }
-   * 
-   * public void testPeek() {
-   * 
-   * MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
-   * 
-   * MemoryResultSet dataSet = new MemoryResultSet( metadata ); JavaScriptResultSet data = new JavaScriptResultSet( );
-   * data.setResultSet(dataSet);
-   * 
-   * data.addRow( new Object[] {"a", new Integer(1) } ); data.addRow( new Object[] {"b", new Integer(2) } );
-   * data.addRow( new Object[] {"c", new Integer(3) } );
-   * 
-   * Object row[] = data.peek( ); assertEquals( "a", row[0] ); assertEquals( 1, row[1] );
-   * 
-   * row = data.peek( ); assertEquals( "a", row[0] ); assertEquals( 1, row[1] );
-   * 
-   * row = data.peek( ); assertEquals( "a", row[0] ); assertEquals( 1, row[1] );
-   * 
-   * row = data.next(); assertEquals( "a", row[0] ); assertEquals( 1, row[1] );
-   * 
-   * row = data.peek( ); assertEquals( "b", row[0] ); assertEquals( 2, row[1] );
-   * 
-   * row = data.peek( ); assertEquals( "b", row[0] ); assertEquals( 2, row[1] );
-   * 
-   * row = data.next(); assertEquals( "b", row[0] ); assertEquals( 2, row[1] );
-   * 
-   * row = data.peek( ); assertEquals( "c", row[0] ); assertEquals( 3, row[1] );
-   * 
-   * row = data.peek( ); assertEquals( "c", row[0] ); assertEquals( 3, row[1] );
-   * 
-   * row = data.next( ); assertEquals( "c", row[0] ); assertEquals( 3, row[1] );
-   * 
-   * row = data.peek( ); assertNull( row );
-   * 
-   * row = data.peek( ); assertNull( row );
-   * 
-   * row = data.peek( ); assertNull( row );
-   * 
-   * row = data.next( ); assertNull( row );
-   * 
-   * data.beforeFirst(); row = data.peek( ); assertEquals( "a", row[0] ); assertEquals( 1, row[1] );
-   * 
-   * }
-   * 
-   * public void testCopy() {
-   * 
-   * MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
-   * 
-   * MemoryResultSet data1 = new MemoryResultSet( metadata );
-   * 
-   * data1.addRow( new Object[] {"a", new Integer(1) } ); data1.addRow( new Object[] {"b", new Integer(2) } );
-   * data1.addRow( new Object[] {"c", new Integer(3) } );
-   * 
-   * MemoryResultSet data = (MemoryResultSet) data1.memoryCopy();
-   * 
-   * assertNotNull( data.getMetaData() ); assertNotNull( data.getMetaData().getColumnHeaders() ); assertNull(
-   * data.getMetaData().getRowHeaders() );
-   * 
-   * assertEquals( 2, data.getMetaData().getColumnCount() ); assertEquals( 1,
-   * data.getMetaData().getColumnHeaders().length ); assertEquals( 2, data.getMetaData().getColumnHeaders()[0].length );
-   * assertEquals( "col1", data.getMetaData().getColumnHeaders()[0][0] ); assertEquals( "col2",
-   * data.getMetaData().getColumnHeaders()[0][1] );
-   * 
-   * assertEquals( 3, data.getRowCount() ); assertEquals( "a", data.getValueAt(0, 0) ); assertEquals( 1,
-   * data.getValueAt(0, 1) ); assertEquals( "b", data.getValueAt(1, 0) ); assertEquals( 2, data.getValueAt(1, 1) );
-   * assertEquals( "c", data.getValueAt(2, 0) ); assertEquals( 3, data.getValueAt(2, 1) );
-   * 
-   * }
-   * 
-   * // public void testRSCompareNotOK5() // { // startTest(); // IRuntimeContext context = run("samples", "rules",
-   * "ResultSetCompareTest_error5.xaction"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //
-   * assertEquals(context.getStatus(), IRuntimeContext.RUNTIME_STATUS_FAILURE); // // finishTest(); // // } public
-   * static void main(String[] args) { XQueryTest test = new XQueryTest(); try { test.setUp(); //
-   * test.testRSCompareOK(); // test.testRSCompareNotOK1(); // test.testRSCompareNotOK2(); test.testRSCompareNotOK3();
-   * test.testRSCompareNotOK4(); // test.testRSCompareNotOK5(); } finally { test.tearDown(); BaseTest.shutdown(); } }
-   */
+  public void testAddRow() {
+    MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
+
+    MemoryResultSet dataSet = new MemoryResultSet( metadata );
+    JavaScriptResultSet data = new JavaScriptResultSet();
+    data.setResultSet( dataSet );
+
+    data.addRow( new Object[] { "a", new Integer( 1 ) } );
+    data.addRow( new Object[] { "b", new Integer( 2 ) } );
+    data.addRow( new Object[] { "c", new Integer( 3 ) } );
+
+    assertEquals( 3, data.getRowCount() );
+    assertEquals( "a", data.getValueAt( 0, 0 ) );
+    assertEquals( 1, data.getValueAt( 0, 1 ) );
+    assertEquals( "b", data.getValueAt( 1, 0 ) );
+    assertEquals( 2, data.getValueAt( 1, 1 ) );
+    assertEquals( "c", data.getValueAt( 2, 0 ) );
+    assertEquals( 3, data.getValueAt( 2, 1 ) );
+  }
+
+  public void testIterators() {
+    MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
+
+    MemoryResultSet dataSet = new MemoryResultSet( metadata );
+    JavaScriptResultSet data = new JavaScriptResultSet();
+    data.setResultSet( dataSet );
+
+    data.addRow( new Object[] { "a", new Integer( 1 ) } );
+    data.addRow( new Object[] { "b", new Integer( 2 ) } );
+    data.addRow( new Object[] { "c", new Integer( 3 ) } );
+
+    assertEquals( 3, data.getRowCount() );
+    assertEquals( "a", data.getValueAt( 0, 0 ) );
+    assertEquals( 1, data.getValueAt( 0, 1 ) );
+    assertEquals( "b", data.getValueAt( 1, 0 ) );
+    assertEquals( 2, data.getValueAt( 1, 1 ) );
+    assertEquals( "c", data.getValueAt( 2, 0 ) );
+    assertEquals( 3, data.getValueAt( 2, 1 ) );
+
+    assertEquals( "a", data.next()[0] );
+    assertEquals( "b", data.next()[0] );
+    assertEquals( "c", data.next()[0] );
+    assertNull( data.next() );
+
+    data.beforeFirst();
+
+    assertEquals( "a", data.next()[0] );
+    assertEquals( "b", data.next()[0] );
+    assertEquals( "c", data.next()[0] );
+    assertNull( data.next() );
+
+    data.close();
+
+    assertEquals( "a", data.next()[0] );
+    assertEquals( "b", data.next()[0] );
+    assertEquals( "c", data.next()[0] );
+    assertNull( data.next() );
+
+    data.closeConnection();
+
+    assertEquals( "a", data.next()[0] );
+    assertEquals( "b", data.next()[0] );
+    assertEquals( "c", data.next()[0] );
+    assertNull( data.next() );
+
+    data.dispose();
+
+    assertEquals( "a", data.next()[0] );
+    assertEquals( "b", data.next()[0] );
+    assertEquals( "c", data.next()[0] );
+    assertNull( data.next() );
+  }
+
+  public void testCopyMemoryResultSet() {
+    MemoryMetaData metadata = new MemoryMetaData( new String[][] { { "col1", "col2" } }, null );
+
+    MemoryResultSet data1 = new MemoryResultSet( metadata );
+
+    data1.addRow( new Object[] { "a", new Integer( 1 ) } );
+    data1.addRow( new Object[] { "b", new Integer( 2 ) } );
+    data1.addRow( new Object[] { "c", new Integer( 3 ) } );
+
+    MemoryResultSet data = (MemoryResultSet) data1.memoryCopy();
+
+    assertNotNull( data.getMetaData() );
+    assertNotNull( data.getMetaData().getColumnHeaders() );
+    assertNull( data.getMetaData().getRowHeaders() );
+
+    assertEquals( 2, data.getMetaData().getColumnCount() );
+    assertEquals( 1, data.getMetaData().getColumnHeaders().length );
+    assertEquals( 2, data.getMetaData().getColumnHeaders()[0].length );
+    assertEquals( "col1", data.getMetaData().getColumnHeaders()[0][0] );
+    assertEquals( "col2", data.getMetaData().getColumnHeaders()[0][1] );
+
+    assertEquals( 3, data.getRowCount() );
+    assertEquals( "a", data.getValueAt( 0, 0 ) );
+    assertEquals( 1, data.getValueAt( 0, 1 ) );
+    assertEquals( "b", data.getValueAt( 1, 0 ) );
+    assertEquals( 2, data.getValueAt( 1, 1 ) );
+    assertEquals( "c", data.getValueAt( 2, 0 ) );
+    assertEquals( 3, data.getValueAt( 2, 1 ) );
+  }
+
+  public void testXQConnectionExequtePreparedQuery() {
+    try {
+      XQConnection connection = new XQConnection();
+      connection.prepareAndExecuteQuery( TEST_QUERY, ListUtils.EMPTY_LIST );
+      fail( "Should throw UnsupportedOperationException" );
+    } catch ( UnsupportedOperationException e ) {
+      // valid
+    } catch ( Exception e ) {
+      fail( "Should throw UnsupportedOperationException" );
+    }
+  }
+
+  public void testXQConnectionConnect() {
+    try {
+      XQConnection connection = new XQConnection();
+      ILogger mockLogger = mock( ILogger.class );
+      connection.setLogger( mockLogger );
+
+      Properties properties = null;
+      boolean isConnected = connection.connect( properties );
+      assertTrue( isConnected );
+
+      properties = new Properties();
+      isConnected = connection.connect( properties );
+      assertTrue( isConnected );
+
+      properties.setProperty( IPentahoConnection.QUERY_KEY, StringUtils.EMPTY );
+      isConnected = connection.connect( properties );
+      assertTrue( isConnected );
+
+      properties.setProperty( IPentahoConnection.QUERY_KEY, TEST_QUERY );
+      isConnected = connection.connect( properties );
+      assertTrue( isConnected );
+
+      StaticQueryContext mockContext = mock( StaticQueryContext.class );
+      when( mockContext.compileQuery( TEST_QUERY ) ).thenThrow( new XPathException( "Test XPathException" ) );
+      connection.sqc = mockContext;
+
+      isConnected = connection.connect( properties );
+      assertFalse( isConnected );
+    } catch ( Exception e ) {
+      e.printStackTrace();
+      fail( "Should not throw Exception" );
+    }
+  }
 }
