4760f073b31b70e7ebc37ca66080d92ff747b336
e-cuellar <ecuellar@pentaho.com>
2014-09-26 15:32:56 -0400

    [BACKLOG-831] Export from command line and UI doesn't work for reports with special chars in the name

--- extensions/src/org/pentaho/platform/plugin/services/importexport/CommandLineProcessor.java
+++ extensions/src/org/pentaho/platform/plugin/services/importexport/CommandLineProcessor.java
@@ -276,6 +276,7 @@ public class CommandLineProcessor {
     String exportURL = contextURL + "/api/repo/files/";
     if ( path != null ) {
       String effPath = RepositoryPathEncoder.encodeRepositoryPath( path );
+      effPath = RepositoryPathEncoder.encodeURIComponent( effPath );
       exportURL += effPath;
     }
     String service =
