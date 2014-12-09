288258a53ffed5a719aeaf0594116fd024adb87e
Diogo Mariano <diogo.mariano@webdetails.pt>
2014-12-09 18:42:40 +0000

    [BACKLOG-1562] - Add support for acl nodes on the RepositoryFile and jcr nodes. Updated unit tests.

--- repository/src/org/pentaho/platform/repository2/unified/webservices/RepositoryFileAdapter.java
+++ repository/src/org/pentaho/platform/repository2/unified/webservices/RepositoryFileAdapter.java
@@ -86,6 +86,9 @@ public class RepositoryFileAdapter extends XmlAdapter<RepositoryFileDto, Reposit
     if ( include( "hidden", memberSet, exclude ) ) {
       f.hidden = v.isHidden();
     }
+    if ( include( "aclNode", memberSet, exclude ) ) {
+      f.aclNode = v.isAclNode();
+    }
     if ( include( "createDate", memberSet, exclude ) ) {
       f.createdDate = v.getCreatedDate();
     }
@@ -220,7 +223,7 @@ public class RepositoryFileAdapter extends XmlAdapter<RepositoryFileDto, Reposit
       .folder( v.folder ).fileSize( v.fileSize ).lastModificationDate( v.lastModifiedDate ).locale( v.locale )
       .lockDate( v.lockDate ).locked( v.locked ).lockMessage( v.lockMessage ).lockOwner( v.lockOwner )
       .title( v.title ).versioned( v.versioned ).versionId( v.versionId ).originalParentFolderPath(
-        v.originalParentFolderPath ).deletedDate( v.deletedDate ).hidden( v.hidden ).build();
+        v.originalParentFolderPath ).deletedDate( v.deletedDate ).hidden( v.hidden ).aclNode( v.aclNode ).build();
   }
 
   private static DefaultUnifiedRepositoryWebService getRepoWs() {
