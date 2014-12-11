74d062abe7120b76369ce84d935b743c4338e59e
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2014-12-11 19:36:45 +0400

    [BACKLOG-1495] Add the ability to change the visibility of a data source

--- extensions/src/org/pentaho/platform/plugin/services/metadata/IPentahoMetadataDomainRepositoryImporter.java
+++ extensions/src/org/pentaho/platform/plugin/services/metadata/IPentahoMetadataDomainRepositoryImporter.java
@@ -21,20 +21,21 @@ package org.pentaho.platform.plugin.services.metadata;
 import org.pentaho.metadata.repository.DomainAlreadyExistsException;
 import org.pentaho.metadata.repository.DomainIdNullException;
 import org.pentaho.metadata.repository.DomainStorageException;
+import org.pentaho.platform.api.repository2.unified.RepositoryFileAcl;
 
 import java.io.InputStream;
 
 /**
  * Class Description
- * 
+ *
  * @author <a href="mailto:dkincade@pentaho.com">David M. Kincade</a>
  */
 public interface IPentahoMetadataDomainRepositoryImporter {
-  public void storeDomain( final InputStream inputStream, final String domainId, final boolean overwrite )
+  public void storeDomain( InputStream inputStream, String domainId, boolean overwrite, RepositoryFileAcl acl )
     throws DomainIdNullException, DomainAlreadyExistsException, DomainStorageException;
 
   public void addLocalizationFile( final String domainId, final String locale, final InputStream inputStream,
-      final boolean overwrite ) throws DomainStorageException;
+                                   final boolean overwrite ) throws DomainStorageException;
 
   public void removeDomain( String domainId );
 }
