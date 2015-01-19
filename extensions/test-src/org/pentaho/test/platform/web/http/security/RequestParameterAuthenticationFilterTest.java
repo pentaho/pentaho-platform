4c4b22a36b528e2de3ddc51eb083d033c5343230
Scott Aslan <saslan@pentaho.com>
2015-01-19 16:08:04 -0500

    [TEST] fix failing tests in extensions

--- extensions/test-src/org/pentaho/test/platform/web/http/security/RequestParameterAuthenticationFilterTest.java
+++ extensions/test-src/org/pentaho/test/platform/web/http/security/RequestParameterAuthenticationFilterTest.java
@@ -1,17 +1,12 @@
 package org.pentaho.test.platform.web.http.security;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.pentaho.di.core.KettleClientEnvironment;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.platform.api.engine.IConfiguration;
+import org.pentaho.platform.api.engine.ISystemConfig;
 import org.pentaho.platform.web.http.security.RequestParameterAuthenticationFilter;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -20,6 +15,14 @@ import org.springframework.security.AuthenticationManager;
 import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
 import org.springframework.security.ui.WebAuthenticationDetails;
 
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class RequestParameterAuthenticationFilterTest {
 
   private RequestParameterAuthenticationFilter filter;
@@ -27,20 +30,28 @@ public class RequestParameterAuthenticationFilterTest {
   private AuthenticationManager authManagerMock;
 
   @Before
-  public void beforeTest() throws KettleException {
+  public void beforeTest() throws KettleException, IOException {
     KettleClientEnvironment.init();
     filter = new RequestParameterAuthenticationFilter();
     authManagerMock = mock( AuthenticationManager.class );
     filter.setAuthenticationManager( authManagerMock );
-
+    final Properties properties = new Properties();
+    properties.setProperty( "requestParameterAuthenticationEnabled", "true" );
+    IConfiguration config = mock( IConfiguration.class );
+    ISystemConfig mockISystemConfig = mock( ISystemConfig.class );
+    mockISystemConfig.registerConfiguration( config );
+    filter.setSystemConfig( mockISystemConfig );
+    doReturn( config ).when( mockISystemConfig ).getConfiguration( "security" );
+    doReturn( properties ).when( config ).getProperties();
   }
 
   @Test
   public void userNamePasswordEncrypted() throws IOException, ServletException {
     final MockHttpServletRequest request =
-        new MockHttpServletRequest(
-            "GET",
-            "http://localhost:9080/pentaho-di/kettle/executeTrans/?rep=dev&userid=admin&password=Encrypted%202be98afc86aa7f2e4bb18bd63c99dbdde&trans=/home/admin/Trans1" );
+      new MockHttpServletRequest(
+        "GET",
+        "http://localhost:9080/pentaho-di/kettle/executeTrans/?rep=dev&userid=admin&password=Encrypted"
+          + "%202be98afc86aa7f2e4bb18bd63c99dbdde&trans=/home/admin/Trans1" );
     request.addParameter( "userid", "admin" );
     request.addParameter( "password", "Encrypted 2be98afc86aa7f2e4bb18bd63c99dbdde" );
 
@@ -54,8 +65,9 @@ public class RequestParameterAuthenticationFilterTest {
   @Test
   public void userNamePasswordUnencrypted() throws IOException, ServletException {
     final MockHttpServletRequest request =
-        new MockHttpServletRequest( "GET",
-            "http://localhost:9080/pentaho-di/kettle/executeTrans/?rep=dev&userid=admin&password=password&trans=/home/admin/Trans1" );
+      new MockHttpServletRequest( "GET",
+        "http://localhost:9080/pentaho-di/kettle/executeTrans/?rep=dev&userid=admin&password=password&trans=/home"
+          + "/admin/Trans1" );
     request.addParameter( "userid", "admin" );
     request.addParameter( "password", "password" );
 
