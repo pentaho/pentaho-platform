7759e524e15036eaab8b7eac0fccbca72ff95638
Kanstantsin Karneliuk <kanstantsin_karneliuk@epam.com>
2015-05-29 13:01:47 +0300

    BISERVER-12584 -- Encrypted repository passwords with Import-Export utility does not work
    What was done:
    1) added usage of KettleEncoder

--- extensions/src/org/pentaho/platform/plugin/services/importexport/CommandLineProcessor.java
+++ extensions/src/org/pentaho/platform/plugin/services/importexport/CommandLineProcessor.java
@@ -47,6 +47,7 @@ import org.pentaho.platform.repository2.unified.webservices.jaxws.IUnifiedReposi
 import org.pentaho.platform.repository2.unified.webservices.jaxws.UnifiedRepositoryToWebServiceAdapter;
 import org.pentaho.platform.security.policy.rolebased.actions.AdministerSecurityAction;
 import org.pentaho.platform.util.RepositoryPathEncoder;
+import org.pentaho.di.core.encryption.KettleTwoWayPasswordEncoder;
 
 import javax.ws.rs.core.MediaType;
 import javax.xml.namespace.QName;
@@ -325,6 +326,7 @@ public class CommandLineProcessor {
     String password =
         getOptionValue( Messages.getInstance().getString( "CommandLineProcessor.INFO_OPTION_PASSWORD_KEY" ), Messages
             .getInstance().getString( "CommandLineProcessor.INFO_OPTION_PASSWORD_NAME" ), true, false );
+    password = KettleTwoWayPasswordEncoder.decryptPasswordOptionallyEncrypted( password );
     ClientConfig clientConfig = new DefaultClientConfig();
     clientConfig.getFeatures().put( JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE );
     client = Client.create( clientConfig );
