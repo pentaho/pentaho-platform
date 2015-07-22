47c11d84eb560a89458b2610ebd61cd42a20b98d
Brendan Lynch <blynch@pentaho.com>
2015-07-22 10:59:10 -0400

    [BACKLOG-4343] - Pentaho Fails to start with a space in the path directory that Pentaho is installed in
    Updating the start-pentaho scripts to allow spaces in the installation path.

--- assembly/package-res/biserver/start-pentaho.sh
+++ assembly/package-res/biserver/start-pentaho.sh
@@ -19,7 +19,7 @@ setPentahoEnv "$DIR/jre"
 ## The plugin loading system for kettle needs this set to know   ##
 ## where to load the plugins from                                ##
 ### =========================================================== ###
-DI_HOME=$DIR/pentaho-solutions/system/kettle
+DI_HOME="$DIR"/pentaho-solutions/system/kettle
 
 errCode=0
 if [ -f "$DIR/promptuser.sh" ]; then
@@ -29,7 +29,7 @@ if [ -f "$DIR/promptuser.sh" ]; then
 fi
 if [ "$errCode" = 0 ]; then
   cd "$DIR/tomcat/bin"
-  CATALINA_OPTS="-Xms1024m -Xmx2048m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -DDI_HOME=$DI_HOME"
+  CATALINA_OPTS="-Xms1024m -Xmx2048m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -DDI_HOME=\"$DI_HOME\""
   export CATALINA_OPTS
   JAVA_HOME=$_PENTAHO_JAVA_HOME
   sh startup.sh
