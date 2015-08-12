da47ed92cd069071309d822db728c8547fcf000a
Brendan Lynch <blynch@pentaho.com>
2015-08-12 17:12:58 -0400

    [TEST] - Increasing unit test coverage.
    Adding service layer unit tests for deleting users and adding roles to a user.

--- extensions/test-src/org/pentaho/platform/web/http/api/resources/services/UserRoleListServiceTest.java
+++ extensions/test-src/org/pentaho/platform/web/http/api/resources/services/UserRoleListServiceTest.java
@@ -19,52 +19,80 @@ package org.pentaho.platform.web.http.api.resources.services;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.pentaho.platform.api.engine.IAuthorizationPolicy;
 import org.pentaho.platform.api.engine.IUserRoleListService;
+import org.pentaho.platform.api.engine.security.userroledao.IPentahoUser;
+import org.pentaho.platform.api.engine.security.userroledao.IUserRoleDao;
+import org.pentaho.platform.api.engine.security.userroledao.UncategorizedUserRoleDaoException;
+import org.pentaho.platform.api.mt.ITenant;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.engine.security.DefaultRoleComparator;
+import org.pentaho.platform.engine.security.DefaultUsernameComparator;
 import org.pentaho.platform.web.http.api.resources.RoleListWrapper;
 import org.pentaho.platform.web.http.api.resources.UserListWrapper;
 
 public class UserRoleListServiceTest {
 
+  private UserRoleListService userRoleListServiceSpy;
   private UserRoleListService userRoleListService;
+
+  @BeforeClass
+  public static void startup() {
+    PentahoSystem.init();
+  }
+
+  @AfterClass
+  public static void shutdown() {
+    PentahoSystem.shutdown();
+  }
+
 
   @Before
   public void setUp() {
-    userRoleListService = spy( new UserRoleListService() );
+    userRoleListServiceSpy = spy( new UserRoleListService() );
+
+    userRoleListService = new UserRoleListService();
+    userRoleListService.setExtraRoles( new ArrayList<String>() );
+    userRoleListService.setSystemRoles( new ArrayList<String>() );
+    userRoleListService.setRoleComparator( mock( DefaultRoleComparator.class ) );
+    userRoleListService.setUserComparator( mock( DefaultUsernameComparator.class ) );
   }
 
   @After
   public void cleanup() {
-    userRoleListService = null;
+    userRoleListServiceSpy = null;
+    PentahoSystem.clearObjectFactory();
   }
 
   @Test
   public void testDoGetRolesForUser() throws Exception {
-    doReturn( "admin, guest" ).when( userRoleListService ).getRolesForUser( "Administrator" );
-    String roles = userRoleListService.doGetRolesForUser( "Administrator" );
+    doReturn( "admin, guest" ).when( userRoleListServiceSpy ).getRolesForUser( "Administrator" );
+    String roles = userRoleListServiceSpy.doGetRolesForUser( "Administrator" );
     assertTrue( roles.length() > 0 );
   }
 
   @Test
   public void testDoGetUsersInRole() throws Exception {
-    doReturn( true ).when( userRoleListService ).canAdminister();
-    doReturn( "Administrator, Guest" ).when( userRoleListService ).getUsersInRole( "admin" );
-    String users = userRoleListService.doGetUsersInRole( "admin" );
+    doReturn( true ).when( userRoleListServiceSpy ).canAdminister();
+    doReturn( "Administrator, Guest" ).when( userRoleListServiceSpy ).getUsersInRole( "admin" );
+    String users = userRoleListServiceSpy.doGetUsersInRole( "admin" );
     assertTrue( users.length() > 0 );
 
     try {
-      doReturn( false ).when( userRoleListService ).canAdminister();
-      userRoleListService.doGetUsersInRole( "unauthorized" );
+      doReturn( false ).when( userRoleListServiceSpy ).canAdminister();
+      userRoleListServiceSpy.doGetUsersInRole( "unauthorized" );
     } catch ( Exception e ) {
       assertTrue( e instanceof UserRoleListService.UnauthorizedException );
     }
@@ -73,7 +101,7 @@ public class UserRoleListServiceTest {
   @Test
   public void testGetUsers() {
     IUserRoleListService service = mock( IUserRoleListService.class );
-    doReturn( service ).when( userRoleListService ).getUserRoleListService();
+    doReturn( service ).when( userRoleListServiceSpy ).getUserRoleListService();
 
     List<String> users = new ArrayList<String>();
     users.add( "admin" );
@@ -81,8 +109,68 @@ public class UserRoleListServiceTest {
     users.add( "suzy" );
     doReturn( users ).when( service ).getAllUsers();
 
-    UserListWrapper usersWrapper = userRoleListService.getUsers();
+    UserListWrapper usersWrapper = userRoleListServiceSpy.getUsers();
     assertTrue( usersWrapper.getUsers().size() == 3 );
+  }
+
+  @Test
+  public void testDeleteUsers() {
+    String users = "admin\tsuzy\t";
+
+    IAuthorizationPolicy policy = mock( IAuthorizationPolicy.class );
+    when( policy.isAllowed( anyString() ) ).thenReturn( true );
+    PentahoSystem.registerObject( policy );
+
+    IPentahoUser user = mock( IPentahoUser.class );
+    IUserRoleDao roleDao = mock( IUserRoleDao.class );
+    when( roleDao.getUser( any( ITenant.class ), anyString() ) ).thenReturn( user );
+    PentahoSystem.registerObject( roleDao );
+
+    try {
+      userRoleListService.deleteUsers( users );
+    } catch ( UserRoleListService.UnauthorizedException e ) {
+      e.printStackTrace();
+    }
+
+    verify( roleDao, times( 2 ) ).deleteUser( any( IPentahoUser.class ) );
+  }
+
+  @Test
+  public void testDeleteUsersUnauthorizedException() {
+    String users = "admin\tsuzy\t";
+
+    IAuthorizationPolicy policy = mock( IAuthorizationPolicy.class );
+    when( policy.isAllowed( anyString() ) ).thenReturn( false );
+    PentahoSystem.registerObject( policy );
+
+    try {
+      userRoleListService.deleteUsers( users );
+    } catch ( UserRoleListService.UnauthorizedException e ) {
+      //Expected test failure
+    }
+  }
+
+  @Test
+  public void testDeleteUsersUncategorizedUserRoleDaoException() {
+    String users = "admin\tsuzy\t";
+
+    IAuthorizationPolicy policy = mock( IAuthorizationPolicy.class );
+    when( policy.isAllowed( anyString() ) ).thenReturn( true );
+    PentahoSystem.registerObject( policy );
+
+    IPentahoUser user = mock( IPentahoUser.class );
+    IUserRoleDao roleDao = mock( IUserRoleDao.class );
+    when( roleDao.getUser( any( ITenant.class ), anyString() ) )
+      .thenThrow( new UncategorizedUserRoleDaoException( "expectedTestException" ) );
+    PentahoSystem.registerObject( roleDao );
+
+    try {
+      userRoleListService.deleteUsers( users );
+    } catch ( UncategorizedUserRoleDaoException e ) {
+      //Expected test failure
+    } catch ( UserRoleListService.UnauthorizedException e ) {
+      fail(); // We should not be reaching this line of code
+    }
   }
 
   @Test
@@ -93,12 +181,12 @@ public class UserRoleListServiceTest {
 
     IUserRoleListService userRoleListService1 = mock( IUserRoleListService.class );
 
-    doReturn( userRoleListService1 ).when( userRoleListService ).getUserRoleListService();
+    doReturn( userRoleListService1 ).when( userRoleListServiceSpy ).getUserRoleListService();
     doReturn( roles ).when( userRoleListService1 ).getAllRoles();
 
-    RoleListWrapper roleListWrapper = userRoleListService.getRoles();
+    RoleListWrapper roleListWrapper = userRoleListServiceSpy.getRoles();
 
-    verify( userRoleListService ).getUserRoleListService();
+    verify( userRoleListServiceSpy ).getUserRoleListService();
     verify( userRoleListService1 ).getAllRoles();
 
     assertEquals( roles, roleListWrapper.getRoles() );
@@ -113,15 +201,15 @@ public class UserRoleListServiceTest {
     roles.add( "ADMIN_ROLE" );
 
     IUserRoleListService userRoleListService1 = mock( IUserRoleListService.class );
-    doReturn( userRoleListService1 ).when( userRoleListService ).getUserRoleListService();
+    doReturn( userRoleListService1 ).when( userRoleListServiceSpy ).getUserRoleListService();
     doReturn( roles ).when( userRoleListService1 ).getAllRoles();
 
     ArrayList<String> extraRoles = new ArrayList<String>();
     extraRoles.add( "EXTRA_ROLE1" );
     extraRoles.add( "EXTRA_ROLE2" );
-    userRoleListService.setExtraRoles( extraRoles );
+    userRoleListServiceSpy.setExtraRoles( extraRoles );
 
-    RoleListWrapper roleWrapper = userRoleListService.getPermissionRoles( "ADMIN_ROLE" );
+    RoleListWrapper roleWrapper = userRoleListServiceSpy.getPermissionRoles( "ADMIN_ROLE" );
     assertTrue( !roleWrapper.getRoles().contains( "ADMIN_ROLE" ) );
     assertTrue( roleWrapper.getRoles().size() == 4 );
   }
@@ -138,15 +226,15 @@ public class UserRoleListServiceTest {
 
     IUserRoleListService userRoleListService1 = mock( IUserRoleListService.class );
 
-    doReturn( userRoleListService1 ).when( userRoleListService ).getUserRoleListService();
+    doReturn( userRoleListService1 ).when( userRoleListServiceSpy ).getUserRoleListService();
     doReturn( roles ).when( userRoleListService1 ).getAllRoles();
-    doReturn( extraRoles ).when( userRoleListService ).getExtraRoles();
+    doReturn( extraRoles ).when( userRoleListServiceSpy ).getExtraRoles();
 
-    RoleListWrapper roleListWrapper = userRoleListService.getAllRoles();
+    RoleListWrapper roleListWrapper = userRoleListServiceSpy.getAllRoles();
 
-    verify( userRoleListService ).getUserRoleListService();
+    verify( userRoleListServiceSpy ).getUserRoleListService();
     verify( userRoleListService1 ).getAllRoles();
-    verify( userRoleListService ).getExtraRoles();
+    verify( userRoleListServiceSpy ).getExtraRoles();
 
     assertEquals( 4, roleListWrapper.getRoles().size() );
   }
@@ -159,11 +247,11 @@ public class UserRoleListServiceTest {
     extraRoles.add( "ROLE3" );
     extraRoles.add( "ROLE4" );
 
-    doReturn( extraRoles ).when( userRoleListService ).getExtraRoles();
+    doReturn( extraRoles ).when( userRoleListServiceSpy ).getExtraRoles();
 
-    RoleListWrapper roleListWrapper = userRoleListService.getExtraRolesList();
+    RoleListWrapper roleListWrapper = userRoleListServiceSpy.getExtraRolesList();
 
-    verify( userRoleListService ).getExtraRoles();
+    verify( userRoleListServiceSpy ).getExtraRoles();
 
     assertEquals( 4, roleListWrapper.getRoles().size() );
     assertEquals( extraRoles, roleListWrapper.getRoles() );
