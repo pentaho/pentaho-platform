11dfafd4b2218bc729316aa74ac96c3aec6cf680
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2015-08-20 01:42:07 +0400

    [PDI-13961] Spoon firstly tries to conect via JNDI although connection type is JDBC - introduce two interfaces for sharing different data source providers - update PlatformKettleDataSourceProvider to implement new method in DataSourceProviderInterface

--- core/src/org/pentaho/platform/engine/services/connection/datasource/dbcp/JndiDatasourceService.java
+++ core/src/org/pentaho/platform/engine/services/connection/datasource/dbcp/JndiDatasourceService.java
@@ -20,23 +20,21 @@ package org.pentaho.platform.engine.services.connection.datasource.dbcp;
 
 import org.pentaho.platform.api.data.DBDatasourceServiceException;
 import org.pentaho.platform.api.data.IDBDatasourceService;
+import org.pentaho.platform.api.data.IJndiDatasourceService;
 import org.pentaho.platform.engine.services.messages.Messages;
 
 import javax.sql.DataSource;
 
-public class JndiDatasourceService extends BaseDatasourceService {
+public class JndiDatasourceService extends BaseDatasourceService implements IJndiDatasourceService {
 
   @Override
   protected DataSource retrieve( String dsName ) throws DBDatasourceServiceException {
-
-    DataSource ds = null;
+    DataSource ds;
     try {
-
       ds =  getJndiDataSource( dsName );
       if ( ds != null ) {
         cacheManager.putInRegionCache( IDBDatasourceService.JDBC_DATASOURCE, dsName, ds );
       }
-
     } catch ( DBDatasourceServiceException dse ) {
       throw new DBDatasourceServiceException( Messages.getInstance().getErrorString(
           "DatasourceService.ERROR_0003_UNABLE_TO_GET_JNDI_DATASOURCE" ), dse ); //$NON-NLS-1$
