16510ce602c9211c7e79ce23cf816f99654fe20d
Nick Baker <nbaker@pentaho.com>
2015-09-03 14:30:12 -0400

    [BACKLOG-5038] Loading Default Content is separate thread to increase startup performance

--- extensions/src/org/pentaho/platform/plugin/action/defaultcontent/DefaultContentSystemListener.java
+++ extensions/src/org/pentaho/platform/plugin/action/defaultcontent/DefaultContentSystemListener.java
@@ -19,6 +19,7 @@ package org.pentaho.platform.plugin.action.defaultcontent;
 
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.api.engine.IPentahoSystemListener;
+import org.pentaho.platform.api.engine.ISystemConfig;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.security.SecurityHelper;
 import org.pentaho.platform.plugin.services.importer.ArchiveLoader;
@@ -39,30 +40,47 @@ public class DefaultContentSystemListener implements IPentahoSystemListener {
   @Override
   public boolean startup( IPentahoSession arg0 ) {
 
-    try {
-      SecurityHelper.getInstance().runAsSystem( new Callable<Void>() {
-
-        @Override
-        public Void call() throws Exception {
-          Logger.info( this.getClass().getName(), "Default content importer has started" );
-
-          // get a File reference to the directory
-          String solutionPath = PentahoSystem.getApplicationContext().getSolutionPath( DEFAULT_CONTENT_FOLDER );
-          File directory = new File( solutionPath );
-
-          // Instantiate the importer
-          IPlatformImporter importer = PentahoSystem.get( IPlatformImporter.class );
-          ArchiveLoader archiveLoader = new ArchiveLoader( importer );
-          archiveLoader.loadAll( directory, ArchiveLoader.ZIPS_FILTER );
-          return null;
-        }
-      } );
-    } catch ( Exception e ) {
-      Logger.error( this.getClass().getName(), e.getMessage() );
+    // By default we'll run in a separate thread. This checks to see if someone has disabled this.
+    ISystemConfig systemSettings = PentahoSystem.get( ISystemConfig.class );
+    Boolean disableLoadAsync = false;
+    if( systemSettings != null ) {
+      String disableLoadAsyncStr = systemSettings.getProperty( "system.disable-async-default-content-loading" );
+      disableLoadAsync = Boolean.valueOf( disableLoadAsyncStr );
     }
 
-    // we return true even of an exception is caught.
-    // if we would return a false then the server will not start.
+    Runnable runnable = new Runnable() {
+      @Override public void run() {
+        try {
+          SecurityHelper.getInstance().runAsSystem( new Callable<Void>() {
+
+            @Override
+            public Void call() throws Exception {
+              Logger.info( this.getClass().getName(), "Default content importer has started" );
+
+              // get a File reference to the directory
+              String solutionPath = PentahoSystem.getApplicationContext().getSolutionPath( DEFAULT_CONTENT_FOLDER );
+              File directory = new File( solutionPath );
+
+              // Instantiate the importer
+              IPlatformImporter importer = PentahoSystem.get( IPlatformImporter.class );
+              ArchiveLoader archiveLoader = new ArchiveLoader( importer );
+              archiveLoader.loadAll( directory, ArchiveLoader.ZIPS_FILTER );
+              return null;
+            }
+          } );
+        } catch ( Exception e ) {
+          Logger.error( this.getClass().getName(), e.getMessage() );
+        }
+      }
+    };
+    if( ! disableLoadAsync ) {
+      Thread t = new Thread( runnable );
+      t.setDaemon( true );
+      t.setName( "Default Content Loader Thread" );
+      t.start();
+    } else {
+      runnable.run();
+    }
     return true;
   }
 }
