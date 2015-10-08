a36841f3fe0da1846fa1327dae9d884828f2b427
Andrey_Markelov <Andrey_Markelov@epam.com>
2015-10-08 11:43:49 -0300

    [BISERVER-12297] created new endpoint for updating a schedule saving the original user

--- user-console/source/org/pentaho/mantle/client/workspace/SchedulesPanel.java
+++ user-console/source/org/pentaho/mantle/client/workspace/SchedulesPanel.java
@@ -72,6 +72,7 @@ import com.google.gwt.user.cellview.client.SimplePager.TextLocation;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Label;
@@ -177,8 +178,6 @@ public class SchedulesPanel extends SimplePanel {
 
   private IDialogCallback scheduleDialogCallback = new IDialogCallback() {
     public void okPressed() {
-      // Remove Next Line to disable deletion of old job
-      controlJobs( getSelectedJobs(), "removeJob", RequestBuilder.DELETE, true );
       refresh();
 
       MessageDialogBox dialogBox =
