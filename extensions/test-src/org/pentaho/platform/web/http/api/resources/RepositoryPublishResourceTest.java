3a32e24e16232127d8a6268b3a2a9356da474906
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2015-10-13 17:38:27 +0400

    [BISERVER-11565] - File upload names fail if the name has double quotes - introduce new endpoint '/repo/publish/file' which expects path to be encoded - deprecate existing and fix their javadocs - add tests - update jersey-related artifacts in test condiguration to 1.16 that is used in compile configuration

--- extensions/test-src/org/pentaho/platform/web/http/api/resources/RepositoryPublishResourceTest.java
+++ extensions/test-src/org/pentaho/platform/web/http/api/resources/RepositoryPublishResourceTest.java
@@ -22,12 +22,17 @@ import org.pentaho.platform.plugin.services.importer.PlatformImportException;
 import org.pentaho.platform.web.http.api.resources.services.RepositoryPublishService;
 
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Date;
 
-import static javax.ws.rs.core.Response.Status.PRECONDITION_FAILED;
-import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import static javax.ws.rs.core.Response.Status.*;
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+import static org.pentaho.platform.plugin.services.importer.PlatformImportException.PUBLISH_GENERAL_ERROR;
+import static org.pentaho.platform.plugin.services.importer.PlatformImportException.PUBLISH_USERNAME_PASSWORD_FAIL;
 
 public class RepositoryPublishResourceTest {
 
@@ -47,24 +52,20 @@ public class RepositoryPublishResourceTest {
   @Test
   public void testWriteFile() throws Exception {
     String pathId = "pathId";
-    InputStream fileContents = mock( InputStream.class );
-    Boolean overwriteFile = Boolean.TRUE;
+    InputStream fileContents = emptyStream();
     FormDataContentDisposition mockFormDataContentDisposition = mock( FormDataContentDisposition.class );
 
     doNothing().when( repositoryPublishResource.repositoryPublishService )
-      .writeFile( pathId, fileContents, overwriteFile );
-
-    Response mockResponse = mock( Response.class );
+      .writeFile( pathId, fileContents, true );
 
     String okResponseText = "SUCCESS";
-    doReturn( mockResponse ).when( repositoryPublishResource ).buildPlainTextOkResponse( okResponseText );
 
     Response testResponse =
-      repositoryPublishResource.writeFile( pathId, fileContents, overwriteFile, mockFormDataContentDisposition );
-    assertEquals( mockResponse, testResponse );
+      repositoryPublishResource.writeFile( pathId, fileContents, true, mockFormDataContentDisposition );
+    assertResponse( testResponse, OK, okResponseText );
 
     verify( repositoryPublishResource.repositoryPublishService, times( 1 ) )
-      .writeFile( pathId, fileContents, overwriteFile );
+      .writeFile( pathId, fileContents, true );
     verify( repositoryPublishResource, times( 1 ) ).buildPlainTextOkResponse( okResponseText );
   }
 
@@ -87,7 +88,7 @@ public class RepositoryPublishResourceTest {
 
     Response mockServerErrorResponse = mock( Response.class );
     doReturn( mockServerErrorResponse ).when( repositoryPublishResource )
-      .buildServerErrorResponse( PlatformImportException.PUBLISH_GENERAL_ERROR );
+      .buildServerErrorResponse( PUBLISH_GENERAL_ERROR );
 
     // Test 1
     Exception mockPentahoAccessControlException = mock( PentahoAccessControlException.class );
@@ -123,6 +124,118 @@ public class RepositoryPublishResourceTest {
       PlatformImportException.PUBLISH_USERNAME_PASSWORD_FAIL );
     verify( repositoryPublishResource, times( 1 ) ).buildStatusResponse( PRECONDITION_FAILED, errorStatus );
     verify( repositoryPublishResource, times( 1 ) )
-      .buildServerErrorResponse( PlatformImportException.PUBLISH_GENERAL_ERROR );
+      .buildServerErrorResponse( PUBLISH_GENERAL_ERROR );
+  }
+
+
+  @Test
+  public void writeFileWithEncodedName_Returns200_OnSuccess() throws Exception {
+    final String originalFile = "my-ktr.ktr";
+    final String originalPath = "/public/" + originalFile;
+
+    doReturn( false ).when( repositoryPublishResource ).invalidPath( originalPath );
+
+    Response shouldBeOk = repositoryPublishResource
+      .writeFileWithEncodedName( encode( originalPath ), emptyStream(), true, dummyInfo( originalFile ) );
+
+    assertResponse( shouldBeOk, OK, "SUCCESS" );
+    verify( repositoryPublishResource.repositoryPublishService, times( 1 ) )
+      .publishFile( eq( originalPath ), any( InputStream.class ), eq( true ) );
+  }
+
+  @Test
+  public void writeFileWithEncodedName_Returns422_OnPathWithReservedChars() throws Exception {
+    final String originalFile = "my\nktr.ktr";
+    final String originalPath = "/public/" + originalFile;
+
+    doReturn( true ).when( repositoryPublishResource ).invalidPath( originalPath );
+
+    Response shouldBeUnprocessableEntity = repositoryPublishResource
+      .writeFileWithEncodedName( encode( originalPath ), emptyStream(), true, dummyInfo( originalFile ) );
+
+    assertResponse( shouldBeUnprocessableEntity, 422,
+      "Cannot publish [" + originalPath + "] because it contains reserved character(s)" );
+    verify( repositoryPublishResource.repositoryPublishService, never() )
+      .writeFile( eq( originalPath ), any( InputStream.class ), eq( true ) );
+  }
+
+  @Test
+  public void writeFileWithEncodedName_Returns500_OnUnexpectedError() throws Exception {
+    testWriteFileWithEncodedName_OnError(
+      new RuntimeException( "unexpected" ),
+      INTERNAL_SERVER_ERROR, PUBLISH_GENERAL_ERROR );
+  }
+
+  @Test
+  public void writeFileWithEncodedName_Returns401_OnUnauthorizedAccess() throws Exception {
+    testWriteFileWithEncodedName_OnError(
+      new PentahoAccessControlException( "unauthorized" ),
+      UNAUTHORIZED, PUBLISH_USERNAME_PASSWORD_FAIL );
+  }
+
+  @Test
+  public void writeFileWithEncodedName_Returns412_OnPlatformImportException() throws Exception {
+    final int someCode = 50;
+    testWriteFileWithEncodedName_OnError(
+      new PlatformImportException( "import exception", someCode ),
+      PRECONDITION_FAILED, someCode );
+  }
+
+  private void testWriteFileWithEncodedName_OnError( Exception thrownException, Response.Status expectedStatus,
+                                                     int expectedErrorCode ) throws Exception {
+    final String originalFile = "my-ktr.ktr";
+    final String originalPath = "/public/" + originalFile;
+
+    doReturn( false ).when( repositoryPublishResource ).invalidPath( originalPath );
+    doThrow( thrownException ).when( repositoryPublishResource.repositoryPublishService )
+      .publishFile( eq( originalPath ), any( InputStream.class ), eq( true ) );
+
+    Response response = repositoryPublishResource
+      .writeFileWithEncodedName( encode( originalPath ), emptyStream(), true, dummyInfo( originalFile ) );
+    assertResponse( response, expectedStatus, String.valueOf( expectedErrorCode ) );
+  }
+
+  @Test
+  public void writeFileWithEncodedName_DecodesPathAndName() throws Exception {
+    final String originalFile = "my-\"quoted\".ktr";
+    final String originalPath = "/public/" + originalFile;
+
+    doReturn( false ).when( repositoryPublishResource ).invalidPath( originalPath );
+
+    repositoryPublishResource.writeFileWithEncodedName(
+      encode( originalPath ), emptyStream(), true, dummyInfo( originalFile ) );
+
+    // decodes path
+    verify( repositoryPublishResource.repositoryPublishService, times( 1 ) )
+      .publishFile( eq( originalPath ), any( InputStream.class ), eq( true ) );
+  }
+
+
+  private static String encode( String originalPath ) throws UnsupportedEncodingException {
+    return URLEncoder.encode( originalPath, "UTF-8" );
+  }
+
+  private static InputStream emptyStream() {
+    return new ByteArrayInputStream( new byte[ 0 ] );
+  }
+
+  private static FormDataContentDisposition dummyInfo( String fileName ) throws Exception {
+    return FormDataContentDisposition
+      .name( "name" ).size( 1 ).fileName( encode( fileName ) )
+      .creationDate( new Date( 0 ) )
+      .modificationDate( new Date( 1 ) )
+      .readDate( new Date( 2 ) )
+      .build();
+  }
+
+
+  private static void assertResponse( Response actual, Response.Status expectedStatus, Object expectedEntity ) {
+    assertResponse( actual, expectedStatus.getStatusCode(), expectedEntity );
+  }
+
+  private static void assertResponse( Response actual, int expectedCode, Object expectedEntity ) {
+    assertNotNull( "Response should not be null", actual );
+    assertEquals( expectedCode, actual.getStatus() );
+    assertEquals( expectedEntity, actual.getEntity() );
   }
 }
