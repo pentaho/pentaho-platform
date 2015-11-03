028f2fb64cf28dcf61b0f7f376e031d030bf6283
Brandon Groves <brandon.groves@gmail.com>
2015-11-03 15:44:18 -0500

    [TEST] increase test coverage

--- api/test-src/org/pentaho/platform/api/repository2/unified/RepositoryFileAclTest.java
+++ api/test-src/org/pentaho/platform/api/repository2/unified/RepositoryFileAclTest.java
@@ -21,6 +21,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -30,17 +31,15 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.anyBoolean;
-import static org.mockito.Matchers.anyString;
 
 /**
  * Created by bgroves on 10/23/15.
  */
 public class RepositoryFileAclTest {
 
-  private static final String ID = anyString();
+  private static final String ID = "id";
   private static final RepositoryFileSid SID = new RepositoryFileSid( "SID" );
-  private static final boolean INHERITING = anyBoolean();
+  private static final boolean INHERITING = false;
   private static final RepositoryFileAce ACE_FILE_ONE = new RepositoryFileAce( SID, RepositoryFilePermission.READ );
   private static final RepositoryFileAce ACE_FILE_TWO = new RepositoryFileAce( SID, RepositoryFilePermission.WRITE );
   private static final List<RepositoryFileAce> ACES = Arrays.asList( ACE_FILE_ONE, ACE_FILE_TWO );
@@ -63,15 +62,6 @@ public class RepositoryFileAclTest {
     assertNotEquals( 923521, acl.hashCode() );
     assertNotNull( acl.toString() );
 
-    // Testing variations of equals
-    assertFalse( acl.equals( null ) );
-    assertFalse( acl.equals( new String() ) );
-
-    assertTrue( acl.equals( acl ) );
-    RepositoryFileAcl.Builder builder = new RepositoryFileAcl.Builder( acl );
-    RepositoryFileAcl dupAcl = builder.build();
-    assertTrue( acl.equals( dupAcl ) );
-
     // Testing Exceptions
     try {
       new RepositoryFileAcl( null, null, true, null );
@@ -82,7 +72,7 @@ public class RepositoryFileAclTest {
   }
 
   @Test
-  public void testBuilder() {
+  public void testBuilderAndEquals() {
     RepositoryFileAcl.Builder builder = new RepositoryFileAcl.Builder( SID );
     RepositoryFileAcl newAcl = builder.build();
     assertEquals( SID, newAcl.getOwner() );
@@ -90,9 +80,17 @@ public class RepositoryFileAclTest {
     assertTrue( newAcl.isEntriesInheriting() );
     assertEquals( 0, newAcl.getAces().size() );
 
-    // Testing more equals
+    // Testing equals
+    assertFalse( acl.equals( null ) );
+    assertFalse( acl.equals( new String() ) );
+
+    assertTrue( acl.equals( acl ) );
     builder = new RepositoryFileAcl.Builder( acl );
     RepositoryFileAcl dupAcl = builder.build();
+    assertTrue( acl.equals( dupAcl ) );
+
+    builder = new RepositoryFileAcl.Builder( acl );
+    dupAcl = builder.build();
     assertEquals( acl.getAces().size(), dupAcl.getAces().size() );
 
     builder.owner( new RepositoryFileSid( "diffSid" ) );
@@ -113,5 +111,51 @@ public class RepositoryFileAclTest {
     builder.aces( Arrays.asList( ACE_FILE_ONE ) );
     diffAcl = builder.build();
     assertFalse( acl.equals( diffAcl ) );
+
+    // Testing build constructors
+    String newOwner = "newOwner";
+    builder = new RepositoryFileAcl.Builder( newOwner );
+    newAcl = builder.build();
+    assertEquals( newOwner, newAcl.getOwner().getName() );
+    assertEquals( RepositoryFileSid.Type.USER, newAcl.getOwner().getType() );
+
+    String newName = "newName";
+    builder = new RepositoryFileAcl.Builder( newName, RepositoryFileSid.Type.ROLE );
+    newAcl = builder.build();
+    assertEquals( newName, newAcl.getOwner().getName() );
+    assertEquals( RepositoryFileSid.Type.ROLE, newAcl.getOwner().getType() );
+
+    String newId = "newId";
+    builder = new RepositoryFileAcl.Builder( newId, newName, RepositoryFileSid.Type.ROLE );
+    newAcl = builder.build();
+    assertEquals( newName, newAcl.getOwner().getName() );
+    assertEquals( RepositoryFileSid.Type.ROLE, newAcl.getOwner().getType() );
+
+    builder.ace( SID, RepositoryFilePermission.DELETE );
+    newAcl = builder.build();
+    assertTrue( newAcl.getAces().get( 0 ).getPermissions().contains( RepositoryFilePermission.DELETE ) );
+
+    EnumSet<RepositoryFilePermission> permissions = EnumSet.of( RepositoryFilePermission.READ,
+      RepositoryFilePermission.WRITE );
+    builder.clearAces();
+    builder.ace( SID, permissions );
+    newAcl = builder.build();
+    assertTrue( newAcl.getAces().get( 0 ).getPermissions().contains( RepositoryFilePermission.READ ) );
+    assertTrue( newAcl.getAces().get( 0 ).getPermissions().contains( RepositoryFilePermission.WRITE ) );
+
+    builder.clearAces();
+    builder.ace( "SID", RepositoryFileSid.Type.ROLE, RepositoryFilePermission.DELETE );
+    newAcl = builder.build();
+    assertEquals( "SID", newAcl.getAces().get( 0 ).getSid().getName() );
+    assertEquals( RepositoryFileSid.Type.ROLE, newAcl.getAces().get( 0 ).getSid().getType() );
+    assertTrue( newAcl.getAces().get( 0 ).getPermissions().contains( RepositoryFilePermission.DELETE ) );
+
+    builder.clearAces();
+    builder.ace( "SID", RepositoryFileSid.Type.ROLE, permissions );
+    newAcl = builder.build();
+    assertEquals( "SID", newAcl.getAces().get( 0 ).getSid().getName() );
+    assertEquals( RepositoryFileSid.Type.ROLE, newAcl.getAces().get( 0 ).getSid().getType() );
+    assertTrue( newAcl.getAces().get( 0 ).getPermissions().contains( RepositoryFilePermission.READ ) );
+    assertTrue( newAcl.getAces().get( 0 ).getPermissions().contains( RepositoryFilePermission.WRITE ) );
   }
 }
