9696acf2670c7f588e7574c21007368212c020b4
AliaksandrDrebenchuk <Aliaksandr_Drebenchuk@epam.com>
2016-01-20 13:17:47 +0300

    BACKLOG-6086 Implement the solution for repository usage error in the DI server after upgrade
    PentahoSolutionSpringApplicationContext.java import "importExport.xml" resource into pentaho-spring-beans.xml

--- extensions/src/org/pentaho/platform/web/http/context/PentahoSolutionSpringApplicationContext.java
+++ extensions/src/org/pentaho/platform/web/http/context/PentahoSolutionSpringApplicationContext.java
@@ -21,8 +21,29 @@ import org.pentaho.platform.web.http.PentahoHttpSessionHelper;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.text.MessageFormat;
+import java.util.Iterator;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 /**
  * Overrides <code>getResourceByPath</code> so that relative paths are relative to the Pentaho solution repository's
@@ -32,16 +53,113 @@ import java.io.File;
  */
 public class PentahoSolutionSpringApplicationContext extends XmlWebApplicationContext {
 
+  private final static String DEFAULT_NAMESPASE = "ns";
+
+  private final static String SYSTEM_FOLDER = "system";
+
+  private final static String DEFAULT_SPRING_XML = "pentaho-spring-beans.xml";
+  private final static String IMPORT_TAG = "import";
+  private final static String RESOURCE_ATTR = "resource";
+  private final static String RESOURCE = "importExport.xml";
+
+  private final static String IMPORT_COMMENT = "Import \"{0}\" was added by \"{1}\" class automatically";
+
+  private final static XPathFactory XPATH_FACTORY = XPathFactory.newInstance();
+
   protected Resource getResourceByPath( String path ) {
     Resource resource = null;
     String solutionPath = PentahoHttpSessionHelper.getSolutionPath( getServletContext() );
     if ( solutionPath != null ) {
-      File file = new File( solutionPath + File.separator + "system" + File.separator + path ); //$NON-NLS-1$
+      File file = new File( solutionPath + File.separator + SYSTEM_FOLDER + File.separator + path ); //$NON-NLS-1$
       resource = new FileSystemResource( file );
     } else {
       resource = super.getResourceByPath( path );
     }
+
+    if ( path.toLowerCase().endsWith( DEFAULT_SPRING_XML ) ) {
+      try {
+
+        DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+        dFactory.setNamespaceAware( true );
+        DocumentBuilder dBuilder = dFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse( resource.getInputStream() );
+
+        Node node = doc.getDocumentElement();
+
+        NodeList nodes =
+            evaluateXPath( node, MessageFormat.format( "./{0}:{1}[@{2}=''{3}'']", DEFAULT_NAMESPASE, IMPORT_TAG,
+                RESOURCE_ATTR, RESOURCE ), XPathConstants.NODESET );
+
+        if ( nodes.getLength() > 0 ) {
+          return resource;
+        }
+
+        Element importEl = doc.createElementNS( node.getNamespaceURI(), IMPORT_TAG );
+        importEl.setAttribute( RESOURCE_ATTR, RESOURCE );
+        Comment comment =
+            doc.createComment( MessageFormat.format( IMPORT_COMMENT, RESOURCE, this.getClass().getSimpleName() ) );
+        addLineBreak( node );
+        node.appendChild( comment );
+        addLineBreak( node );
+        node.appendChild( importEl );
+        addLineBreak( node );
+
+        TransformerFactory tFactory = TransformerFactory.newInstance();
+        Transformer transformer = tFactory.newTransformer();
+        DOMSource source = new DOMSource( doc );
+        FileOutputStream out = new FileOutputStream( resource.getFile() );
+        try {
+          StreamResult result = new StreamResult( out );
+          transformer.transform( source, result );
+        } finally {
+          out.flush();
+          out.close();
+        }
+      } catch ( Exception e ) {
+        e.printStackTrace();
+      }
+    }
+
     return resource;
+  }
+
+  private void addLineBreak( Node node ) {
+    node.appendChild( node.getOwnerDocument().createTextNode( "\n" ) );
+  }
+
+  @SuppressWarnings( "unchecked" )
+  private <T> T evaluateXPath( Node node, String exp, QName res ) throws XPathExpressionException {
+    XPath xpath = XPATH_FACTORY.newXPath();
+    xpath.setNamespaceContext( new DefNamespaceContext( node.getNamespaceURI() ) );
+    return (T) xpath.compile( exp ).evaluate( node, res );
+  }
+
+  private static class DefNamespaceContext implements NamespaceContext {
+
+    String defaultNS;
+
+    public DefNamespaceContext( String defaultNS ) {
+      this.defaultNS = defaultNS;
+    }
+
+    @Override
+    public String getNamespaceURI( String prefix ) {
+      if ( DEFAULT_NAMESPASE.equals( prefix ) ) {
+        return defaultNS;
+      }
+      return null;
+    }
+
+    @Override
+    public String getPrefix( String namespaceURI ) {
+      return null;
+    }
+
+    @Override
+    public Iterator<?> getPrefixes( String namespaceURI ) {
+      return null;
+    }
+
   }
 
 }
