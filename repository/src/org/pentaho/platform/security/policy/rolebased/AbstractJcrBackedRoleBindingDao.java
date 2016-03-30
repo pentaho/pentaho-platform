580c23786c91eb408bcce35a5e12bf011b6e77b3
Mikhail Chen-Len-Son <mikhail_chen-len-son@epam.com>
2016-03-30 13:40:16 +0400

    [SP2607] Backport of BISERVER-13057 - User with multiple roles/groups gets blank screen when logs in to Pentaho User Console (5.4 Suite) - fixed checkstyle violations

--- repository/src/org/pentaho/platform/security/policy/rolebased/AbstractJcrBackedRoleBindingDao.java
+++ repository/src/org/pentaho/platform/security/policy/rolebased/AbstractJcrBackedRoleBindingDao.java
@@ -27,7 +27,6 @@ import org.pentaho.platform.api.mt.ITenantedPrincipleNameResolver;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.core.system.TenantUtils;
 import org.pentaho.platform.repository2.unified.ServerRepositoryPaths;
-import org.pentaho.platform.repository2.unified.jcr.JcrRepositoryFileUtils;
 import org.pentaho.platform.repository2.unified.jcr.JcrStringHelper;
 import org.pentaho.platform.repository2.unified.jcr.JcrTenantUtils;
 import org.pentaho.platform.repository2.unified.jcr.NodeHelper;
@@ -48,12 +47,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.TreeSet;
 
 public abstract class AbstractJcrBackedRoleBindingDao implements IRoleAuthorizationPolicyRoleBindingDao {
 
@@ -100,7 +96,7 @@ public abstract class AbstractJcrBackedRoleBindingDao implements IRoleAuthorizat
     Assert.notNull( superAdminRoleName );
     Assert.notNull( authorizationActions );
 
-    this.authorizationActions.addAll(authorizationActions);
+    this.authorizationActions.addAll( authorizationActions );
 
     this.immutableRoleBindings = immutableRoleBindings;
     this.bootstrapRoleBindings = bootstrapRoleBindings;
@@ -309,7 +305,7 @@ public abstract class AbstractJcrBackedRoleBindingDao implements IRoleAuthorizat
         if ( runtimeRoleNode.hasProperty( pentahoJcrConstants.getPHO_BOUNDROLES() ) ) {
           // get clean runtime role name
           String runtimeRoleName = JcrStringHelper.fileNameDecode(
-                  runtimeRoleNode.getName().substring(phoNsPrefix.length())
+                  runtimeRoleNode.getName().substring( phoNsPrefix.length() )
           );
           // get logical role names
           List<String> logicalRoleNames = new ArrayList<String>();
