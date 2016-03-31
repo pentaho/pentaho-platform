2586f914bfbb3ab3a44a7cf326dae5c17ed68267
Nick Baker <nbaker@pentaho.com>
2016-03-31 15:52:34 -0400

    [BACKLOG-7324, BACKLOG-7407] Fixing case where two instances resolve the same port before opening it to mark it used.

--- extensions/src/org/pentaho/platform/osgi/ServerSocketBasedKarafInstanceResolver.java
+++ extensions/src/org/pentaho/platform/osgi/ServerSocketBasedKarafInstanceResolver.java
@@ -16,6 +16,7 @@
  */
 package org.pentaho.platform.osgi;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,7 +42,15 @@ import java.util.regex.Pattern;
  * Created by nbaker on 3/21/16.
  */
 class ServerSocketBasedKarafInstanceResolver implements IKarafInstanceResolver {
-  private static final int START_PORT_NUMBER = 10000;
+  private static final int START_PORT_NUMBER = determineStartPort();
+  public static final String PENTAHO_KARAF_INSTANCE_START_PORT = "pentaho.karaf.instance.start.port";
+
+  static int determineStartPort() {
+    String portStr = System.getProperty( PENTAHO_KARAF_INSTANCE_START_PORT, "11000" );
+    int portNo = NumberUtils.toInt( portStr );
+    return portNo != 0 ? portNo : 11000;
+  }
+
   private static final int MAX_NUMBER_OF_KARAF_INSTANCES = 1000;
   public static final String DATA = "data";
   private Logger logger = LoggerFactory.getLogger( getClass() );
@@ -62,23 +71,33 @@ class ServerSocketBasedKarafInstanceResolver implements IKarafInstanceResolver {
   }
 
   private void assignAvailableCacheFolderForType( KarafInstance instance ) {
+
+    // something like karaf/caches
     String cacheParentFolder = instance.getCacheParentFolder();
+
+    // We separate the caches by client type to avoid reuse of an inappropriate data folder
     File clientTypeCacheFolder = new File( cacheParentFolder + "/" + instance.getClientType() );
     clientTypeCacheFolder.mkdirs();
+
     File[] dataDirectories = clientTypeCacheFolder.listFiles( new FilenameFilter() {
       @Override public boolean accept( File dir, String name ) {
         return name.startsWith( DATA );
       }
     } );
+
     int maxInstanceNoFound = 0;
     Pattern pattern = Pattern.compile( DATA + "\\-([0-9]+)" );
 
+    // Go through existing data directories. If one is not in-use, use that. If not find the highest number and create
+    // one greater
     for ( File dataDirectory : dataDirectories ) {
       boolean locked = true;
       Matcher matcher = pattern.matcher( dataDirectory.getName() );
       if ( !matcher.matches() ) {
+        // unexpected directory, not a data folder, skipping
         continue;
       }
+      // extract the data folder number
       int instanceNo = Integer.parseInt( matcher.group( 1 ) );
 
       maxInstanceNoFound = Math.max( maxInstanceNoFound, instanceNo );
@@ -86,13 +105,16 @@ class ServerSocketBasedKarafInstanceResolver implements IKarafInstanceResolver {
       File lockFile = new File( dataDirectory, ".lock" );
       FileLock lock = null;
       if ( !lockFile.exists() ) {
+        // Lock file was not present, we can use it
         locked = false;
       } else {
+        // Lock file was there, see if another process actually holds a lock on it
         try {
           FileOutputStream fileOutputStream = new FileOutputStream( lockFile );
           try {
             lock = fileOutputStream.getChannel().tryLock();
-            if ( lock != null ) { // not locked by another program
+            if ( lock != null ) {
+              // not locked by another program
               instance.setCacheLock( lock );
               locked = false;
             }
@@ -106,10 +128,13 @@ class ServerSocketBasedKarafInstanceResolver implements IKarafInstanceResolver {
 
       if ( !locked ) {
         instance.setCachePath( dataDirectory.getPath() );
+        // we're good to use this one, break out of existing directory loop
         break;
       }
     }
+
     if ( instance.getCachePath() == null ) {
+      // Create a new cache folder
       File newCacheFolder = null;
       while ( newCacheFolder == null ) {
         maxInstanceNoFound++;
@@ -120,9 +145,12 @@ class ServerSocketBasedKarafInstanceResolver implements IKarafInstanceResolver {
         }
         newCacheFolder = candidate;
       }
+
       FileOutputStream fileOutputStream = null;
       try {
         newCacheFolder.mkdir();
+        // create lock file and lock it for this process
+
         File lockFile = new File( newCacheFolder, ".lock" );
         fileOutputStream = new FileOutputStream( lockFile );
         FileLock lock = fileOutputStream.getChannel().lock();
@@ -152,7 +180,7 @@ class ServerSocketBasedKarafInstanceResolver implements IKarafInstanceResolver {
 
   private boolean isPortAvailable( int port ) {
 
-    try ( Socket socket = new Socket( "localhost", port ) ) {
+    try ( Socket ignored = new Socket( "localhost", port ) ) {
       return false;
     } catch ( IOException e ) {
       return true;
@@ -169,20 +197,22 @@ class ServerSocketBasedKarafInstanceResolver implements IKarafInstanceResolver {
       int candidate = START_PORT_NUMBER + testInstance;
       Socket socket = null;
       try {
+        logger.debug( "Instance test. Trying port " + candidate );
         socket = new Socket( "localhost", candidate );
         socket.close();
       } catch ( ConnectException e ) {
         // port not in-use
-        instanceNo = testInstance;
+
         try {
           ServerSocket ssocket = new ServerSocket( candidate );
+          instanceNo = testInstance;
           instance.setInstanceSocket( ssocket );
+          instance.setInstanceNumber( instanceNo );
+          logger.debug( "Karaf instance resolved to: " + instanceNo );
         } catch ( IOException e1 ) {
           logger.error( "Error creating ServerSocket", e1 );
         }
 
-        logger.debug( "Karaf instance resolved to: " + instanceNo );
-        instance.setInstanceNumber( instanceNo );
 
       } catch ( IOException ignored ) {
         // Some other error, move to next candidate
