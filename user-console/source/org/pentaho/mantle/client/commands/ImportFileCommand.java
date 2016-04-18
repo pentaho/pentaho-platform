115e479e5967c89b166739ff37afea77c5165b33
Kafalas Tim <tkafalas@pentaho.com>
2016-04-18 14:03:55 -0400

    [BACKLOG-7846] Hide the advance section of import dialog for non-admins

--- user-console/source/org/pentaho/mantle/client/commands/ImportFileCommand.java
+++ user-console/source/org/pentaho/mantle/client/commands/ImportFileCommand.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.mantle.client.commands;
@@ -65,7 +65,7 @@ public class ImportFileCommand extends AbstractCommand {
 
   protected void performOperation( boolean feedback ) {
     // delete file
-    importDialog = new ImportDialog( repositoryFile );
+    importDialog = new ImportDialog( repositoryFile, SolutionBrowserPanel.getInstance().isAdministrator() );
     importDialog.getForm().addSubmitCompleteHandler( new SubmitCompleteHandler() {
       @Override
       public void onSubmitComplete( SubmitCompleteEvent sce ) {
