9481387376aa4c1a18cabead8f6cf035862361e6
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2016-07-27 12:54:57 +0300

    [SP-2760] - Backport of BISERVER-13116 - API call asking internally for non existing pentaho/js/postAuth.js file (6.1 Suite)

--- extensions/src/org/pentaho/platform/web/http/filters/PentahoWebContextFilter.java
+++ extensions/src/org/pentaho/platform/web/http/filters/PentahoWebContextFilter.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.platform.web.http.filters;
@@ -125,13 +125,6 @@ public class PentahoWebContextFilter implements Filter {
             requireScriptBytes = requireScript.getBytes();
             THREAD_LOCAL_REQUIRE_SCRIPT.set( requireScriptBytes );
           }
-        }
-
-        String basicAuthFlag = (String) httpRequest.getSession().getAttribute( "BasicAuth" );
-        if ( basicAuthFlag != null && basicAuthFlag.equals( "true" ) ) {
-          out.write( ( "document.write(\"<script type='text/javascript' src='"
-            + contextPath + "js/postAuth.js'></scr\"+\"ipt>\");\n" )
-              .getBytes( "UTF-8" ) );
         }
 
         out.write( contextPathBytes );
