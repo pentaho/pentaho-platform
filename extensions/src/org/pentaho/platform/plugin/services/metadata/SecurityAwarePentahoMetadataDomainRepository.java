c1680ea193578ca7d7636368d0f9a8c219014e53
nbaker <nbaker@pentaho.com>
2016-11-03 21:26:06 -0400

    [BACKLOG-12120] Fixing typing to ensure compatibility.

--- extensions/src/org/pentaho/platform/plugin/services/metadata/SecurityAwarePentahoMetadataDomainRepository.java
+++ extensions/src/org/pentaho/platform/plugin/services/metadata/SecurityAwarePentahoMetadataDomainRepository.java
@@ -17,10 +17,6 @@
 
 package org.pentaho.platform.plugin.services.metadata;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.pentaho.metadata.model.LogicalModel;
@@ -33,11 +29,11 @@ import org.pentaho.platform.api.repository2.unified.IUnifiedRepository;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.security.SecurityHelper;
 import org.pentaho.platform.plugin.services.messages.Messages;
-import org.pentaho.platform.web.http.api.resources.utils.SystemUtils;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * This is the platform implementation which implements security. NOTE: this class will be moved after integration
@@ -73,7 +69,7 @@ public class SecurityAwarePentahoMetadataDomainRepository extends PentahoMetadat
     String username = auth.getName();
     HashSet<String> roles = null;
     roles = new HashSet<String>(  );
-    for ( SimpleGrantedAuthority role : (List<SimpleGrantedAuthority>) auth.getAttribute( "roles" ) ) {
+    for ( GrantedAuthority role : (List<GrantedAuthority>) auth.getAttribute( "roles" ) ) {
       roles.add( role.getAuthority() );
     }
 
