8f49c8502a9f684df69ad6ef93ffb2c92c24efdf
Dzmitry_Hryhoryeu <Dzmitry_Hryhoryeu@epam.com>
2016-12-29 17:15:14 +0300

    [BISERVER-13444] - Scheduled PRPTs and PIR Reports are not recorded to pro_audit table.

--- scheduler/src/org/pentaho/platform/scheduler2/quartz/BlockingQuartzJob.java
+++ scheduler/src/org/pentaho/platform/scheduler2/quartz/BlockingQuartzJob.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.platform.scheduler2.quartz;
@@ -20,9 +20,13 @@ package org.pentaho.platform.scheduler2.quartz;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.scheduler2.IBlockoutManager;
+import org.pentaho.platform.engine.core.audit.AuditHelper;
+import org.pentaho.platform.engine.core.audit.MessageTypes;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.scheduler2.blockout.BlockoutAction;
 import org.pentaho.platform.scheduler2.blockout.PentahoBlockoutManager;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
@@ -36,7 +40,11 @@ public class BlockingQuartzJob implements Job {
   public void execute( final JobExecutionContext jobExecutionContext ) throws JobExecutionException {
     try {
       if ( getBlockoutManager().shouldFireNow() || isBlockoutAction( jobExecutionContext ) ) { // We should always let the blockouts fire //$NON-NLS-1$
+        final long start = System.currentTimeMillis();
+        makeAuditRecord( 0, MessageTypes.INSTANCE_START, jobExecutionContext );
         createUnderlyingJob().execute( jobExecutionContext );
+        final long end = System.currentTimeMillis();
+        makeAuditRecord( ( (float) ( end - start ) / 1000 ), MessageTypes.INSTANCE_END, jobExecutionContext );
       } else {
         getLogger().warn(
             "Job '" + jobExecutionContext.getJobDetail().getName()
@@ -61,7 +69,7 @@ public class BlockingQuartzJob implements Job {
   Log getLogger() {
     return LogFactory.getLog( BlockingQuartzJob.class );
   }
-  
+
   protected boolean isBlockoutAction( JobExecutionContext ctx ) {
     try {
       String actionClass = ctx.getJobDetail().getJobDataMap().getString( QuartzScheduler.RESERVEDMAPKEY_ACTIONCLASS );
@@ -70,6 +78,22 @@ public class BlockingQuartzJob implements Job {
       getLogger().warn( t.getMessage(), t );
       return false;
     }
-  }    
-  
+  }
+
+  protected void makeAuditRecord( final float time, final String messageType,
+                                  final JobExecutionContext jobExecutionContext ) {
+    if ( jobExecutionContext != null && jobExecutionContext.getJobDetail() != null ) {
+      final JobDataMap jobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+      AuditHelper.audit( PentahoSessionHolder.getSession() != null ? PentahoSessionHolder.getSession().getId() : null,
+              jobDataMap.get( QuartzScheduler.RESERVEDMAPKEY_ACTIONUSER ) != null ? jobDataMap.get( QuartzScheduler.RESERVEDMAPKEY_ACTIONUSER ).toString() : null,
+              jobDataMap.get( QuartzScheduler.RESERVEDMAPKEY_STREAMPROVIDER ) != null ? jobDataMap.get( QuartzScheduler.RESERVEDMAPKEY_STREAMPROVIDER ).toString() : null,
+              jobExecutionContext.getJobDetail().getJobClass() != null ? jobExecutionContext.getJobDetail().getJobClass().getName() : null,
+              jobDataMap.get( QuartzScheduler.RESERVEDMAPKEY_ACTIONID ) != null ? jobDataMap.get( QuartzScheduler.RESERVEDMAPKEY_ACTIONID ).toString() : null,
+              messageType,
+              jobDataMap.get( "lineage-id" ) != null ? jobDataMap.get( "lineage-id" ).toString() : null,
+              null,
+              time,
+              null ); //$NON-NLS-1$
+    }
+  }
 }
--- scheduler/test-src/org/pentaho/platform/scheduler2/quartz/BlockingQuartzJobTest.java
+++ scheduler/test-src/org/pentaho/platform/scheduler2/quartz/BlockingQuartzJobTest.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.platform.scheduler2.quartz;
@@ -136,6 +136,11 @@ public class BlockingQuartzJobTest {
       @Override
       Log getLogger() {
         return logger;
+      }
+
+      @Override
+      protected void makeAuditRecord( float time, String messageType, JobExecutionContext jobExecutionContext ) {
+
       }
     };
   }
