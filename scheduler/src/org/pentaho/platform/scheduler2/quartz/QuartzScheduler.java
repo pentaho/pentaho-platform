0b1500131be79e887e90bcb9af613c556edacb28
Andrei Abramov <andrei_abramov@epam.com>
2017-01-30 15:40:27 +0400

    Backport of PPP-3398 - Schedules shows wrong next run day when Manual Rerun of reports. (7.0.0.3 Suite) backport of #3374

--- scheduler/src/org/pentaho/platform/scheduler2/quartz/QuartzScheduler.java
+++ scheduler/src/org/pentaho/platform/scheduler2/quartz/QuartzScheduler.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.platform.scheduler2.quartz;
@@ -400,6 +400,12 @@ public class QuartzScheduler implements IScheduler {
           ( (SimpleTrigger) trigger ).setPreviousFireTime( new Date() );
         } else if ( trigger instanceof CronTrigger ) {
           ( (CronTrigger) trigger ).setPreviousFireTime( new Date() );
+        }
+        if ( trigger.getStartTime() != null && trigger.getStartTime().before( new Date() ) ) {
+          Date newStartTime = trigger.getFireTimeAfter( new Date() );
+          if ( newStartTime != null ) {
+            trigger.setStartTime( newStartTime );
+          }
         }
         // force the trigger to be updated with the previous fire time
         scheduler.rescheduleJob( jobId, jobKey.getUserName(), trigger );
