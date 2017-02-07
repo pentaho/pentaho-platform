e6a7cd048e5bb400b4d3177f35040c9035ba4165
U-PENTAHO\kbryant <kbryant@PC0BMRX8.pentaho.com>
2017-02-07 14:24:52 -0500

    [TEST] Increasing code coverage in core package

--- core/src/test/java/org/pentaho/platform/util/ColorHelperTest.java
+++ core/src/test/java/org/pentaho/platform/util/ColorHelperTest.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.platform.util;
@@ -37,6 +37,16 @@ public class ColorHelperTest extends TestCase {
 
   public void testUsingDefaultWhenNotFound() {
     assertEquals( ColorHelper.lookupColor( "noSuchColorInMap", Color.BLACK ), Color.BLACK );
+  }
+
+  public void testUsingNullString() {
+    Color color = ColorHelper.lookupColor( null );
+    assertEquals( color, null);
+  }
+
+  public void testUsingNullStringAnColor() {
+    Color color = ColorHelper.lookupColor( null, Color.BLACK);
+    assertEquals( color, Color.BLACK);
   }
 
 }
