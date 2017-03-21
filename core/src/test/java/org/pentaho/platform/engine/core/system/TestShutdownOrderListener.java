8c162ed2c1e805c298fda466cb7659593b7ca081
Emily Sager <esager@pentaho.com>
2017-03-21 12:06:42 -0400

    [BACKLOG-14605] added a simple test case to ensure the shutdown order occurs in reverse

---
+++ core/src/test/java/org/pentaho/platform/engine/core/system/TestShutdownOrderListener.java
@@ -0,0 +1,42 @@
+package org.pentaho.platform.engine.core.system;
+
+import org.pentaho.platform.api.engine.IPentahoSession;
+import org.pentaho.platform.api.engine.IPentahoSystemListener;
+
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2017 Pentaho Corporation.  All rights reserved.
+ */
+public class TestShutdownOrderListener implements IPentahoSystemListener {
+
+  private  static int orderGlobal = 0;
+
+  private int order = 0;
+  public void shutdown() {
+    order = orderGlobal;
+    orderGlobal++;
+  }
+
+  public int getOrder() {
+    return order;
+  }
+
+  public boolean startup( IPentahoSession arg0 ) {
+   //not testing this, do nothing
+    return true;
+  }
+
+}
