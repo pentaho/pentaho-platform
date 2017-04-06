a76dd582e4cddd3e3a07093a49dfcdb4c74d31ef
Guilherme Raimundo <graimundo@pentaho.com>
2017-04-06 18:02:45 +0100

    [BACKLOG-14852] - Incorporated review suggestions and fixed config name.

--- assemblies/pentaho-solutions/src/main/resources/pentaho-solutions/system/client-config-folder-enabler/web/client-config-enabler-require-js-cfg.js
+++ assemblies/pentaho-solutions/src/main/resources/pentaho-solutions/system/client-config-folder-enabler/web/client-config-enabler-require-js-cfg.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2017 Pentaho Corporation.  All rights reserved.
+ * Copyright 2017 Pentaho Corporation.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 (function(global) {
-  /* globals requireCfg, CONTEXT_PATH, KARMA_RUN, SESSION_LOCALE, active_theme, ENVIRONMENT_CONFIG */
+  /* globals requireCfg, CONTEXT_PATH */
   /* eslint dot-notation: 0, require-jsdoc: 0 */
 
   var requireTypes = requireCfg.config["pentaho/service"] || (requireCfg.config["pentaho/service"] = {});
