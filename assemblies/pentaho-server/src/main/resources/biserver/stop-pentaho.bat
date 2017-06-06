20465b0ebfbc7daf31c556a9cc507bfc3e98bba9
Andrei Abramov <Andrei_Abramov@epam.com>
2017-06-06 12:39:15 +0400

    [PSW-229] Script files don't have an header with Copyright data - license header was added

--- assemblies/pentaho-server/src/main/resources/biserver/stop-pentaho.bat
+++ assemblies/pentaho-server/src/main/resources/biserver/stop-pentaho.bat
@@ -1,4 +1,23 @@
 @echo off
+
+REM *******************************************************************************************
+REM This program is free software; you can redistribute it and/or modify it under the
+REM terms of the GNU General Public License, version 2 as published by the Free Software
+REM Foundation.
+REM
+REM You should have received a copy of the GNU General Public License along with this
+REM program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+REM or from the Free Software Foundation, Inc.,
+REM 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+REM
+REM This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+REM without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+REM See the GNU General Public License for more details.
+REM
+REM
+REM Copyright 2011 - 2017 Pentaho Corporation.  All rights reserved.
+REM *******************************************************************************************
+
 setlocal
 cd /D %~dp0
 call set-pentaho-env.bat "%~dp0jre"
