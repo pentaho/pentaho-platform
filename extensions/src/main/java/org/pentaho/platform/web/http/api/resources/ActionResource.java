e2e8e07abed7f8f4095decd62e8c9bb485a44213
Diogo Mariano <dmariano@pentaho.com>
2017-06-06 12:05:03 +0100

    [BACKLOG-16372] - Removed unrequired dependency - Fixed checkstyle for ActionResource

--- extensions/src/main/java/org/pentaho/platform/web/http/api/resources/ActionResource.java
+++ extensions/src/main/java/org/pentaho/platform/web/http/api/resources/ActionResource.java
@@ -68,14 +68,12 @@ public class ActionResource {
   @POST
   @Path( "/runInBackground" )
   @Consumes( { TEXT_PLAIN } )
-  @StatusCodes(
-    {
+  @StatusCodes( {
       @ResponseCode( code = 200, condition = "Action invoked successfully." ),
       @ResponseCode( code = 400, condition = "Bad input - could not invoke action." ),
       @ResponseCode( code = 401, condition = "User does not have permissions to invoke action" ),
       @ResponseCode( code = 500, condition = "Error while retrieving system resources." ),
-    }
-  )
+    } )
   public Response runInBackground(
     @QueryParam( ActionUtil.INVOKER_ACTIONID ) String actionId,
     @QueryParam( ActionUtil.INVOKER_ACTIONCLASS ) String actionClass,
