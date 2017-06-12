ee9db6044575568628cade013118af08d2e1cbfe
Emily Sager <esager@pentaho.com>
2017-06-12 13:00:20 -0400

    [BACKLOG-16708] Changed behaviour of isPasswordValid in DefaultPasswordEncoder.java so that when a null password is valid the method returns false, rather than throwing an IllegalArgumentException.

---
+++ repository/src/test/java/org/pentaho/platform/security/userroledao/jackrabbit/DefaultPentahoPasswordEncoderTest.java
@@ -0,0 +1,42 @@
+package org.pentaho.platform.security.userroledao.jackrabbit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.pentaho.platform.repository2.userroledao.jackrabbit.security.DefaultPentahoPasswordEncoder;
+import org.pentaho.test.platform.security.userroledao.ws.UserRoleWebServiceBase;
+
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2006 - 2017 Pentaho Corporation.  All rights reserved.
+ */
+public class DefaultPentahoPasswordEncoderTest {
+
+  @Test
+  public void testValidCredentials( ) {
+    DefaultPentahoPasswordEncoder passwordEncoder = new DefaultPentahoPasswordEncoder();
+    String password = "helloworld";
+    String encryptedPassword =  new UserRoleWebServiceBase.PasswordEncoderMock().encodePassword( password, null );
+    Assert.assertTrue( passwordEncoder.isPasswordValid( encryptedPassword, password, null ) );
+  }
+
+  @Test
+  public void testInvalidCredentials( ) {
+    DefaultPentahoPasswordEncoder passwordEncoder = new DefaultPentahoPasswordEncoder( );
+    Assert.assertFalse( passwordEncoder.isPasswordValid( "password", null, null ) );
+    Assert.assertFalse( passwordEncoder.isPasswordValid( passwordEncoder.encodePassword( "", null ), "password", null ) );
+    Assert.assertFalse( passwordEncoder.isPasswordValid( null, null, null ) );
+  }
+}
