495c04b0c0474af925a4cca3576990186594dfc2
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2017-06-15 16:35:03 +0300

    [PPP-3735] - Use of vulnerable component httpclient-3.0.1.jar, commons-httpclient-3.1 - httpclient-4.0.1.jar, SONATYPE-2007-0004, CVE-2012-6153, CVE-2011-1498, CVE-2014-3577

--- extensions/src/main/java/org/pentaho/platform/web/servlet/ProxyServlet.java
+++ extensions/src/main/java/org/pentaho/platform/web/servlet/ProxyServlet.java
@@ -12,19 +12,25 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.platform.web.servlet;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.pentaho.di.core.util.HttpClientManager;
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.web.servlet.messages.Messages;
@@ -36,20 +42,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
- * 
  * This servlet is used to Proxy a Servlet request to another server for processing and returns that result to the
  * caller as if this Servlet actiually serviced it. Setup the proxy by editing the <b>web.xml</b> to map the servlet
  * name you want to proxy to the Proxy Servlet class.
  * <p>
- * 
+ * <p>
  * <pre>
  *  &lt;servlet&gt;
- *    &lt;servlet-name&gt;ViewAction&lt;/servlet-name&gt; 
+ *    &lt;servlet-name&gt;ViewAction&lt;/servlet-name&gt;
  *    &lt;servlet-class&gt;com.pentaho.ui.servlet.ProxyServlet&lt;/servlet-class&gt;
  *    &lt;init-param&gt;
  *       &lt;param-name&gt;ProxyURL&lt;/param-name&gt;
@@ -57,28 +64,27 @@ import java.util.Map;
  *    &lt;/init-param&gt;
  *   &lt;/servlet&gt;
  * </pre>
- * 
+ * <p>
  * In the above example, all requests to /ViewAction will be forwarded to the ViewAction Servlet running on the Pentaho
  * server atmy.remoteserver.com:8080
- * 
+ * <p>
  * <p>
  * NOTES:
  * <p>
- * 
+ * <p>
  * For this to be useful, both Pentaho servers should be using the same database repository.
  * <p>
  * The recieving server should have the ProxyTrustingFilter enabled to handle authicentation.
  * <p>
  * This Servlet only works with GET requests. All requests in the Pentaho BI Platform are currently gets.
- * 
- * @see com.pentaho.test.servlet.ProxyTrustingFilter
+ *
  * @author Doug Moran
- * 
+ * @see org.pentaho.platform.web.http.filters.ProxyTrustingFilter
  */
 public class ProxyServlet extends ServletBase {
 
   /**
-   * 
+   *
    */
   private static final long serialVersionUID = 4680027723733552639L;
 
@@ -89,9 +95,9 @@ public class ProxyServlet extends ServletBase {
     return ProxyServlet.logger;
   }
 
-  String proxyURL = null; // "http://localhost:8080/pentaho";
+  private String proxyURL = null; // "http://localhost:8080/pentaho";
 
-  String errorURL = null; // The URL to redirect to if the user is invalid
+  private String errorURL = null; // The URL to redirect to if the user is invalid
 
   /**
    * Base Constructor
@@ -106,18 +112,18 @@ public class ProxyServlet extends ServletBase {
     if ( ( proxyURL == null ) ) {
       error( Messages.getInstance().getString( "ProxyServlet.ERROR_0001_NO_PROXY_URL_SPECIFIED" ) ); //$NON-NLS-1$
     } else {
-      proxyURL.trim();
       try {
-        URL url = new URL( proxyURL ); // Just doing this to verify
+        URL url = new URL( proxyURL.trim() ); // Just doing this to verify
         // it's good
-        info( Messages.getInstance().getString( "ProxyServlet.INFO_0001_URL_SELECTED", url.toExternalForm() ) ); // using 'url' to get rid of unused var compiler warning //$NON-NLS-1$
+        info( Messages.getInstance().getString( "ProxyServlet.INFO_0001_URL_SELECTED",
+          url.toExternalForm() ) ); // using 'url' to get rid of unused var compiler warning //$NON-NLS-1$
       } catch ( Throwable t ) {
         error( Messages.getInstance().getErrorString( "ProxyServlet.ERROR_0002_INVALID_URL", proxyURL ) ); //$NON-NLS-1$
         proxyURL = null;
       }
     }
 
-    errorURL = servletConfig.getInitParameter( "ErrorURL" );
+    errorURL = servletConfig.getInitParameter( "ErrorURL" ); //$NON-NLS-1$
     super.init( servletConfig );
   }
 
@@ -127,88 +133,94 @@ public class ProxyServlet extends ServletBase {
     }
 
     String servletPath = request.getServletPath();
-    // System .out.println( ">>>>>>>> REQ: " + request.getRequestURL().toString() ); //$NON-NLS-1$//$NON-NLS-2$
     PentahoSystem.systemEntryPoint();
     try {
       String theUrl = proxyURL + servletPath;
-      PostMethod method = new PostMethod( theUrl );
-
-      // Copy the parameters from the request to the proxy
-      // System .out.print( ">>>>>>>> PARAMS: " ); //$NON-NLS-1$
-      Map paramMap = request.getParameterMap();
-      Map.Entry entry;
-      String[] array;
-      for ( Iterator it = paramMap.entrySet().iterator(); it.hasNext(); ) {
-        entry = (Map.Entry) it.next();
-        array = (String[]) entry.getValue();
-        for ( String element : array ) {
-          method.addParameter( (String) entry.getKey(), element );
-          // System.out.print( (String)entry.getKey() + "=" + array[i]
-          // + "&" ); //$NON-NLS-1$ //$NON-NLS-2$
-        }
-      }
-      // System.out.println( "" ); //$NON-NLS-1$
-
-      // Just in case someone is trying to spoof the proxy
-      method.removeParameter( "_TRUST_USER_" ); //$NON-NLS-1$
-
-      // Get the user from the session
-      IPentahoSession userSession = getPentahoSession( request );
-      String name = userSession.getName();
-
-      // Add the trusted user from the session
-      if ( ( name != null ) && ( name.length() > 0 ) ) {
-        method.addParameter( "_TRUST_USER_", name ); //$NON-NLS-1$
-        // System.out.println( ">>>>>>>> USR: " + name ); //$NON-NLS-1$
-      } else if ( ( errorURL != null ) && ( errorURL.trim().length() > 0 ) ) {
-        response.sendRedirect( errorURL );
-        // System.out.println( ">>>>>>>> REDIR: " + errorURL );
-        // //$NON-NLS-1$
-        return;
-      }
-
-      // System.out.println( ">>>>>>>> PROXY: " + theUrl ); //$NON-NLS-1$
-      debug( Messages.getInstance().getString( "ProxyServlet.DEBUG_0001_OUTPUT_URL", theUrl ) ); //$NON-NLS-1$
-
-      // Now do the request
-      HttpClient client = new HttpClient();
-
+      URIBuilder uriBuilder;
+      String trustUserParam = "_TRUST_USER_"; //$NON-NLS-1$
       try {
-        // Execute the method.
-        int statusCode = client.executeMethod( method );
+        uriBuilder = new URIBuilder( theUrl );
+        List<NameValuePair> queryParams = uriBuilder.getQueryParams();
+        for ( NameValuePair nameValuePair : queryParams ) {
+          // Just in case someone is trying to spoof the proxy
+          if ( nameValuePair.getName().equals( trustUserParam ) ) {
+            queryParams.remove( nameValuePair );
+          }
+        }
+        // Copy the parameters from the request to the proxy
+        Map paramMap = request.getParameterMap();
+        Map.Entry entry;
+        String[] array;
+        for ( Iterator it = paramMap.entrySet().iterator(); it.hasNext(); ) {
+          entry = (Map.Entry) it.next();
+          array = (String[]) entry.getValue();
+          for ( String element : array ) {
+            queryParams.add( new BasicNameValuePair( (String) entry.getKey(), element ) );
+          }
+        }
 
-        if ( statusCode != HttpStatus.SC_OK ) {
-          error( Messages.getInstance().getErrorString(
-              "ProxyServlet.ERROR_0003_REMOTE_HTTP_CALL_FAILED", method.getStatusLine().toString() ) ); //$NON-NLS-1$
+        // Get the user from the session
+        IPentahoSession userSession = getPentahoSession( request );
+        String name = userSession.getName();
+
+        // Add the trusted user from the session
+        if ( StringUtils.isNotEmpty( name ) ) {
+          queryParams.add( new BasicNameValuePair( trustUserParam, name ) );
+        } else if ( ( errorURL != null ) && ( errorURL.trim().length() > 0 ) ) {
+          response.sendRedirect( errorURL );
           return;
         }
-        setHeader( "Content-Type", method, response ); //$NON-NLS-1$
-        setHeader( "Content-Length", method, response ); //$NON-NLS-1$
+        uriBuilder.setParameters( queryParams );
+        debug( Messages.getInstance().getString(
+          "ProxyServlet.DEBUG_0001_OUTPUT_URL", uriBuilder.toString() ) ); //$NON-NLS-1$
 
-        InputStream inStr = method.getResponseBodyAsStream();
-        ServletOutputStream outStr = response.getOutputStream();
+        HttpPost method = new HttpPost( uriBuilder.build() );
 
-        int inCnt = 0;
-        byte[] buf = new byte[2048];
-        while ( -1 != ( inCnt = inStr.read( buf ) ) ) {
-          outStr.write( buf, 0, inCnt );
+        // Now do the request
+        HttpClient client = HttpClientManager.getInstance().createDefaultClient();
+
+        try {
+          // Execute the method.
+          HttpResponse httpResponse = client.execute( method );
+          StatusLine statusLine = httpResponse.getStatusLine();
+          int statusCode = statusLine.getStatusCode();
+          if ( statusCode != HttpStatus.SC_OK ) {
+            error( Messages.getInstance().getErrorString(
+              "ProxyServlet.ERROR_0003_REMOTE_HTTP_CALL_FAILED", statusLine.toString() ) ); //$NON-NLS-1$
+            return;
+          }
+          setHeader( "Content-Type", method, response ); //$NON-NLS-1$
+          setHeader( "Content-Length", method, response ); //$NON-NLS-1$
+
+          InputStream inStr = httpResponse.getEntity().getContent();
+          ServletOutputStream outStr = response.getOutputStream();
+
+          int inCnt;
+          byte[] buf = new byte[ 2048 ];
+          while ( -1 != ( inCnt = inStr.read( buf ) ) ) {
+            outStr.write( buf, 0, inCnt );
+          }
+
+        } catch ( IOException e ) {
+          error( Messages.getInstance().getErrorString( "ProxyServlet.ERROR_0005_TRANSPORT_FAILURE" ),
+            e ); //$NON-NLS-1$
+          e.printStackTrace();
+        } finally {
+          method.releaseConnection();
         }
-      } catch ( HttpException e ) {
-        error( Messages.getInstance().getErrorString( "ProxyServlet.ERROR_0004_PROTOCOL_FAILURE" ), e ); //$NON-NLS-1$
+
+      } catch ( URISyntaxException e ) {
+        error( Messages.getInstance().getErrorString(
+          "ProxyServlet.ERROR_0006_URI_SYNTAX_EXCEPTION", theUrl ) ); //$NON-NLS-1$
         e.printStackTrace();
-      } catch ( IOException e ) {
-        error( Messages.getInstance().getErrorString( "ProxyServlet.ERROR_0005_TRANSPORT_FAILURE" ), e ); //$NON-NLS-1$
-        e.printStackTrace();
-      } finally {
-        method.releaseConnection();
       }
     } finally {
       PentahoSystem.systemExitPoint();
     }
   }
 
-  void setHeader( final String headerStr, final HttpMethod method, final HttpServletResponse response ) {
-    Header header = method.getResponseHeader( headerStr );
+  private void setHeader( final String headerStr, final HttpRequestBase method, final HttpServletResponse response ) {
+    Header header = method.getHeaders( headerStr )[ 0 ];
     if ( header != null ) {
       response.setHeader( headerStr, header.getValue() );
     }
