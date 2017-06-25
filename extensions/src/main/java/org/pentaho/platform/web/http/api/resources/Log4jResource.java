b4a1be9d28bea27e316073284b684de79b5e3faf
Johnny Nieh <jnieh@pentaho.com>
2017-06-25 15:16:00 -0400

    [BACKLOG-17201] Add ability to change log level at runtime.

---
+++ extensions/src/main/java/org/pentaho/platform/web/http/api/resources/Log4jResource.java
@@ -0,0 +1,99 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2017 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.platform.web.http.api.resources;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.Loader;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.codehaus.enunciate.jaxrs.ResponseCode;
+import org.codehaus.enunciate.jaxrs.StatusCodes;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Enumeration;
+
+
+@Path( "/logconfig" )
+public class Log4jResource {
+
+  private static final Logger LOGGER = Logger.getLogger( Log4jResource.class );
+  private static final String CONFIG = "log4j.xml";
+
+  @PUT
+  @Path ( "/reload" )
+  @StatusCodes( {
+    @ResponseCode( code = 200, condition = "Successfully reload from configuration." )
+    } )
+  @Produces( { MediaType.TEXT_PLAIN } )
+  public Response reloadConfiguration() throws Exception {
+    LOGGER.setLevel( Level.INFO );
+    LOGGER.info( "Reloading configuration..." );
+
+    DOMConfigurator.configure( Loader.getResource( CONFIG ) );
+    return Response.ok( "Done" ).build();
+  }
+
+  @PUT
+  @Path ( "/update" )
+  @StatusCodes( {
+    @ResponseCode( code = 200, condition = "Successfully update log level." ),
+    @ResponseCode( code = 304, condition = "Log level is not modified." )
+    } )
+  @Produces( { MediaType.TEXT_PLAIN } )
+  public Response updateLogLevel( @FormParam(  "level" ) String targetLevel, @FormParam( "category" ) String category ) throws Exception {
+    LOGGER.setLevel( Level.INFO );
+
+    if ( StringUtils.isBlank( targetLevel ) && StringUtils.isBlank( category ) ) {
+      return Response.notModified( "No parameter provided, log level not modified." ).build();
+    }
+
+    Logger root = LogManager.getRootLogger();
+
+    if ( StringUtils.isNotBlank( targetLevel ) ) {
+      LOGGER.info( "Request to set log level: " + targetLevel );
+
+      if ( StringUtils.isNotBlank( category ) ) {
+        LOGGER.info( "Request to set log level for package: " + category );
+        Logger catLog = LogManager.exists( category );
+        if ( catLog != null ) {
+          catLog.setLevel( Level.toLevel( targetLevel, root.getLevel() ) );
+          return Response.ok( "Setting log level for: '" + catLog.getName() + "' to be: " + catLog.getLevel() ).build();
+        }
+        return Response.notModified( "Category: '" + category + "' not found, log level not modified." ).build();
+      }
+
+      root.setLevel( Level.toLevel( targetLevel, root.getLevel() ) );
+      LOGGER.info( "Root logger level set to: " + root.getLevel() );
+
+      Enumeration e = LogManager.getCurrentLoggers();
+      while ( e.hasMoreElements() ) {
+        Logger logger = (Logger) e.nextElement();
+        logger.setLevel( Level.toLevel( targetLevel, root.getLevel() ) );
+      }
+    }
+
+    return Response.ok( "Log level updated." ).build();
+  }
+}
---
+++ extensions/src/test/java/org/pentaho/platform/web/http/api/resources/Log4jResourceTest.java
@@ -0,0 +1,87 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2017 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.platform.web.http.api.resources;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.Loader;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.util.Enumeration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class Log4jResourceTest {
+
+  private static final String CONFIG = "log4j.xml";
+  private Log4jResource target = new Log4jResource();
+
+  @Test
+  public void resetLogLevel() throws Exception {
+    Logger root = LogManager.getRootLogger();
+    Level initialLevel = root.getLevel();
+    assertNotEquals( initialLevel, Level.ALL );
+    root.setLevel( Level.ALL );
+    assertEquals( Level.ALL, root.getLevel() );
+
+    Response res = target.reloadConfiguration();
+    assertNotEquals( Level.ALL,  LogManager.getRootLogger().getLevel() );
+
+    assertEquals( "Done", res.getEntity().toString() );
+  }
+
+  @Test
+  public void updateLogLevel() throws Exception {
+    Response res = target.updateLogLevel( null, null );
+    assertEquals( 304, res.getStatus() );
+
+    res = target.updateLogLevel( "debug", null );
+    int notDebug = 0;
+    Enumeration e = LogManager.getCurrentLoggers();
+    while ( e.hasMoreElements() ) {
+      Logger logger = (Logger) e.nextElement();
+      if ( !Level.DEBUG.equals( logger.getLevel() ) ) {
+        notDebug++;
+      }
+    }
+    assertEquals( 0, notDebug );
+    assertEquals( 200, res.getStatus() );
+    assertEquals( "Log level updated.", res.getEntity().toString() );
+
+    Level original = LogManager.getLogger( "org.pentaho" ).getLevel();
+    assertNotEquals( Level.ALL, original );
+    res = target.updateLogLevel( "ALL", "org.pentaho" );
+    assertEquals( Level.ALL,  LogManager.getLogger( "org.pentaho" ).getLevel() );
+    assertEquals( 200, res.getStatus() );
+    assertEquals( "Setting log level for: 'org.pentaho' to be: ALL", res.getEntity().toString() );
+
+    res = target.updateLogLevel( "ALL", "foo.bar" );
+    assertEquals( 304, res.getStatus() );
+  }
+
+  @Before
+  public void readFromConfig() {
+    DOMConfigurator.configure( Loader.getResource( CONFIG ) );
+  }
+
+}
