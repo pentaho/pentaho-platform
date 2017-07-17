7242530a60db8b9e776d21ba38060be1c9d0ddad
Mordechai Zur <mzur@pentaho.com>
2017-07-17 18:26:30 -0400

    [BACKLOG-1700] Add needed support for spring to properly honor both sync and async events, and correctly honor Async annotation. Fixed checkstyle issues.

--- extensions/src/main/java/org/pentaho/platform/spring/ApplicationEventDispatcher.java
+++ extensions/src/main/java/org/pentaho/platform/spring/ApplicationEventDispatcher.java
@@ -28,7 +28,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class ApplicationEventDispatcher extends AbstractApplicationEventMulticaster {
-  public final static String ASYNC_ANNOTATION_QUALIFIER = "dispatcher";
+  public static final String ASYNC_ANNOTATION_QUALIFIER = "dispatcher";
 
   private ApplicationEventMulticaster asyncMulticaster;
   private ApplicationEventMulticaster defaultMulticaster;
@@ -67,7 +67,7 @@ public class ApplicationEventDispatcher extends AbstractApplicationEventMulticas
     asyncMulticaster.multicastEvent( event );
   }
 
-  public void multicastEvent(final ApplicationEvent event, final ResolvableType type ) {
+  public void multicastEvent( final ApplicationEvent event, final ResolvableType type ) {
     defaultMulticaster.multicastEvent( event, type );
     asyncMulticaster.multicastEvent( event, type );
   }
@@ -116,23 +116,25 @@ public class ApplicationEventDispatcher extends AbstractApplicationEventMulticas
     return ( annotation != null && annotation.value().equals( ASYNC_ANNOTATION_QUALIFIER ) );
   }
 
-  private Async getAsyncAnnotationDirect(final ApplicationListener listener ) {
+  private Async getAsyncAnnotationDirect( final ApplicationListener listener ) {
     return listener.getClass().getDeclaredAnnotation( Async.class );
   }
 
-  private Async getAsyncAnnotationThroughProxy(final ApplicationListener listener ) {
+  private Async getAsyncAnnotationThroughProxy( final ApplicationListener listener ) {
     // The listener could actually be an proxy/wrapper implementing the ApplicationListener.
     // Such a listener (spring implementation details here) has a "method" field representing the
     // target method which the proxy delegates events to.
     //
     try {
-      final Field method = listener.getClass().getDeclaredField("method" );
+      final Field method = listener.getClass().getDeclaredField( "method" );
       method.setAccessible( true );
       final Method listeningMethod = (Method) method.get( listener );
       return listeningMethod.getDeclaredAnnotation( Async.class );
 
     } catch ( final NoSuchFieldException e ) {
+      // silently ignore falling back accordingly returning null
     } catch ( final IllegalAccessException e ) {
+      // silently ignore falling back accordingly returning null
     }
 
     return null;
