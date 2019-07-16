f8945276d87e3d0a7c3aae24df12111eb5ab6a0a
Duarte Cunha Leão <duarte.leao@gmail.com>
2019-07-16 15:20:03 +0100

    [BACKLOG-29696] David's linting 3 !

--- extensions/src/test/java/org/pentaho/platform/web/WebUtilTest.java
+++ extensions/src/test/java/org/pentaho/platform/web/WebUtilTest.java
@@ -36,7 +36,10 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
