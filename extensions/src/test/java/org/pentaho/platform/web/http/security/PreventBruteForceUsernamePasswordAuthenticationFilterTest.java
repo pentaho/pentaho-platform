09485010ed947f2c54fc570d4cf9b23adb46b423
Bruno Costa <bruno.costa@hitachivantara.com>
2020-03-25 14:20:11 +0000

    [PPP-4431] - IBM ASoC DAST Scan: Improper Restriction of Excessive Authentication Attempts (/pentaho/j_spring_security_check: j_password)

--- extensions/src/test/java/org/pentaho/platform/web/http/security/PreventBruteForceUsernamePasswordAuthenticationFilterTest.java
+++ extensions/src/test/java/org/pentaho/platform/web/http/security/PreventBruteForceUsernamePasswordAuthenticationFilterTest.java
@@ -1,3 +1,22 @@
+/*!
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ *
+ * Copyright (c) 2020 Hitachi Vantara. All rights reserved.
+ *
+ */
 package org.pentaho.platform.web.http.security;
 
 import org.junit.Before;
@@ -63,9 +82,6 @@ public class PreventBruteForceUsernamePasswordAuthenticationFilterTest {
       spy( new PreventBruteForceUsernamePasswordAuthenticationFilter( mockLoginAttemptService ) );
 
     when( mockLoginAttemptService.isBlocked( ip ) ).thenReturn( true );
-
     authenticationFilter.attemptAuthentication( mockRequest, mockResponse );
-    verify( mockLoginAttemptService.isBlocked( ip ), times( 1 ) );
-
   }
 }
