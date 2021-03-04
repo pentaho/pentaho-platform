4114c271a459f1efb8ae922d61047d6a98300f9e
Bruno Costa <bruno.costa@hitachivantara.com>
2021-03-04 16:47:07 +0000

    [SP-5916] - Backport of BISERVER-14423 - Roles in lowercase/UPPERCASE are different for Creation/Deletion Event Order Affects Outcome (8.3 Suite)

--- user-console/src/main/java/org/pentaho/mantle/client/admin/UserRolesAdminPanelController.java
+++ user-console/src/main/java/org/pentaho/mantle/client/admin/UserRolesAdminPanelController.java
@@ -117,8 +117,18 @@ public class UserRolesAdminPanelController extends UserRolesAdminPanel implement
         }
 
         public void onResponseReceived( Request request, Response response ) {
-          initializeRoles( name, "api/userroledao/roles", rolesListBox );
-          initializeAvailableUsers( usersListBox.getValue( usersListBox.getSelectedIndex() ) );
+
+          if ( response.getStatusCode() != Response.SC_NO_CONTENT ) {
+            String errorMsg = Messages.getString( "newRoleErrorMessage" );
+            String errorValidationMessage = response.getHeader( PUC_VALIDATION_ERROR_MESSAGE );
+            if ( errorValidationMessage != null ) {
+              errorMsg = errorMsg + "\n" + errorValidationMessage;
+            }
+            showXulErrorMessage( Messages.getString( "newRole" ), errorMsg );
+          } else {
+            initializeRoles( name, "api/userroledao/roles", rolesListBox );
+            initializeAvailableUsers( usersListBox.getValue( usersListBox.getSelectedIndex() ) );
+          }
         }
       } );
     } catch ( RequestException e ) {
