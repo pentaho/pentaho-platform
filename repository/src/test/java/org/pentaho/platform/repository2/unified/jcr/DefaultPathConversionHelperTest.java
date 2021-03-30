ad52b3f60fdb131fe06e246597c6e24df4ee81f6
ssamora <sergiosamora@gmail.com>
2021-03-30 17:27:02 +0100

    [SP-5908] Backport of PDI-19084 - Backward slash ( \ ) is not resolved in the transformation path after upgrading from 8.1 to 8.3 (9.1 Suite)

---
+++ repository/src/test/java/org/pentaho/platform/repository2/unified/jcr/DefaultPathConversionHelperTest.java
@@ -0,0 +1,118 @@
+/*!
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright (c) 2002-2021 Hitachi Vantara. All rights reserved.
+ *
+ */
+package org.pentaho.platform.repository2.unified.jcr;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.pentaho.platform.api.repository2.unified.RepositoryFile;
+import org.pentaho.platform.repository2.unified.ServerRepositoryPaths;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+@RunWith( PowerMockRunner.class )
+@PrepareForTest( ServerRepositoryPaths.class )
+public class DefaultPathConversionHelperTest {
+  private DefaultPathConversionHelper converter;
+
+
+  @Before
+  public void setup() {
+    converter = new DefaultPathConversionHelper();
+  }
+
+
+  @Test( expected = IllegalArgumentException.class )
+  public void absToRelEmptyTest() {
+    converter.absToRel( "" );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void absToRelNullTest() {
+    converter.absToRel( null );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void absToRelStartsWithSeparatorTest() {
+    converter.absToRel( "somepath/somefile" );
+  }
+
+  @Test
+  public void absToRelNullTenantRootFolderTest() {
+    PowerMockito.mockStatic( ServerRepositoryPaths.class );
+    when( ServerRepositoryPaths.getTenantRootFolderPath() ).thenReturn( null );
+    assertNull( converter.absToRel( "/somepath/somefile" ) );
+  }
+
+  @Test
+  public void absToRelWrongTenantRootFolderTest() {
+    PowerMockito.mockStatic( ServerRepositoryPaths.class );
+    when( ServerRepositoryPaths.getTenantRootFolderPath() ).thenReturn( "/somepath" );
+    assertNull( converter.absToRel( "/otherpath/somefile" ) );
+  }
+
+  @Test
+  public void absToRelRootFolderTest() {
+    PowerMockito.mockStatic( ServerRepositoryPaths.class );
+    when( ServerRepositoryPaths.getTenantRootFolderPath() ).thenReturn( "/somepath" );
+    assertEquals( RepositoryFile.SEPARATOR, converter.absToRel( "/somepath" ) );
+  }
+
+  @Test
+  public void absToRelTest() {
+    PowerMockito.mockStatic( ServerRepositoryPaths.class );
+    when( ServerRepositoryPaths.getTenantRootFolderPath() ).thenReturn( "/somepath" );
+    assertEquals( "/other/thefile", converter.absToRel( "/somepath/other\\thefile" ) );
+    assertEquals( "/other/filename", converter.absToRel( "/somepath/other\\filename" ) );
+    assertEquals( "/other/filename", converter.absToRel( "/somepath\\other\\filename" ) );
+    assertEquals( "/other/else/filename", converter.absToRel( "/somepath\\other\\else\\filename" ) );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void relToAbsEmptyTest() {
+    converter.relToAbs( "" );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void relToAbsNullTest() {
+    converter.relToAbs( null );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void relToAbsStartsWithSeparatorTest() {
+    converter.relToAbs( "somepath/somefile" );
+  }
+
+  @Test
+  public void relToAbsTest() {
+    PowerMockito.mockStatic( ServerRepositoryPaths.class );
+    when( ServerRepositoryPaths.getTenantRootFolderPath() ).thenReturn( "/somepath" );
+    assertEquals( "/somepath/somefile", converter.relToAbs( "/somefile" ) );
+    assertEquals( "/somepath/somefolder/other/somefile/", converter.relToAbs( "/somefolder\\other/somefile/" ) );
+    assertEquals( "/somepath/somefolder/other/somefile/", converter.relToAbs( "/somefolder\\other\\somefile\\" ) );
+    assertEquals( "/somepath", converter.relToAbs( "/" ) );
+  }
+
+}
