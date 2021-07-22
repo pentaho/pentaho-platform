e08738c95b1aff292a4c62731d9ecd8b337b8cb5
Duarte Cunha Leão <duarte.leao@gmail.com>
2021-07-23 00:16:56 +0100

    [SP-6088] Backport of ANALYZER-3942 - Restoring ?locale query string parameter modifying session locale

--- user-console/src/main/java/org/pentaho/mantle/client/commands/SwitchLocaleCommand.java
+++ user-console/src/main/java/org/pentaho/mantle/client/commands/SwitchLocaleCommand.java
@@ -1,5 +1,4 @@
 /*!
- *
  * This program is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
  * Foundation.
@@ -13,23 +12,13 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- *
  * Copyright (c) 2002-2021 Hitachi Vantara. All rights reserved.
- *
  */
 
 package org.pentaho.mantle.client.commands;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.http.client.Request;
-import com.google.gwt.http.client.RequestBuilder;
-import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.RequestException;
-import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import org.pentaho.mantle.client.csrf.CsrfUtil;
-import org.pentaho.mantle.client.csrf.JsCsrfToken;
 
 public class SwitchLocaleCommand extends AbstractCommand {
 
@@ -47,45 +36,16 @@ public class SwitchLocaleCommand extends AbstractCommand {
   }
 
   protected void performOperation( boolean feedback ) {
-    // stuff the locale in the server's session so we can use it
-    // to override the browser setting, as needed
+    String newLocalePath = "Home?locale=" + locale;
 
-    final String url = GWT.getHostPageBaseURL() + "api/mantle/locale"; //$NON-NLS-1$
+    String baseUrl = GWT.getModuleBaseURL();
+    int index = baseUrl.indexOf( "/mantle/" );
+    if ( index >= 0 ) {
+      String basePath = baseUrl.substring( 0, index );
+      newLocalePath = basePath + "/" + newLocalePath;
+    }
 
-    CsrfUtil.getCsrfToken( url, new AsyncCallback<JsCsrfToken>() {
-
-      public void onFailure( Throwable caught ) {
-      }
-
-      public void onSuccess( JsCsrfToken token ) {
-        RequestBuilder builder = new RequestBuilder( RequestBuilder.POST, url );
-        try {
-          builder.setHeader( "If-Modified-Since", "01 Jan 1970 00:00:00 GMT" );
-          if ( token != null ) {
-            builder.setHeader( token.getHeader(), token.getToken() );
-          }
-
-          builder.sendRequest( locale, new RequestCallback() {
-
-            public void onError( Request request, Throwable exception ) {
-              // showError(exception);
-            }
-
-            public void onResponseReceived( Request request, Response response ) {
-              String newLocalePath = "Home";
-              String baseUrl = GWT.getModuleBaseURL();
-              int index = baseUrl.indexOf( "/mantle/" );
-              if ( index >= 0 ) {
-                newLocalePath = baseUrl.substring( 0, index ) + "/Home";
-              }
-              Window.Location.replace( newLocalePath );
-            }
-          } );
-        } catch ( RequestException e ) {
-          // showError(e);
-        }
-      }
-    } );
+    Window.Location.replace( newLocalePath );
   }
 
   public String getLocale() {
