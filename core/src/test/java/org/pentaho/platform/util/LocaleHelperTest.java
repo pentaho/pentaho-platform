13bb611e3c65054c75e5ff8c83281f40ad44e34d
Duarte Cunha Leão <duarte.leao@gmail.com>
2021-07-27 16:33:00 +0100

    [ANALYZER-3942] LocaleHelper tests

--- core/src/test/java/org/pentaho/platform/util/LocaleHelperTest.java
+++ core/src/test/java/org/pentaho/platform/util/LocaleHelperTest.java
@@ -14,14 +14,19 @@
  * See the GNU General Public License for more details.
  *
  *
- * Copyright (c) 2002-2018 Hitachi Vantara. All rights reserved.
+ * Copyright (c) 2002-2021 Hitachi Vantara. All rights reserved.
  *
  */
 
 package org.pentaho.platform.util;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.text.DateFormat;
 import java.text.NumberFormat;
@@ -30,6 +35,8 @@ import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.pentaho.platform.api.engine.IPentahoSession;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.util.messages.LocaleHelper;
 
 public class LocaleHelperTest {
@@ -37,7 +44,7 @@ public class LocaleHelperTest {
   @Test
   public void testLocaleHelper() {
     LocaleHelper helper = new LocaleHelper();
-    Assert.assertNotNull( helper );
+    assertNotNull( helper );
 
     Locale myLocale = Locale.US;
     Locale newLocale = Locale.FRANCE;
@@ -60,59 +67,58 @@ public class LocaleHelperTest {
 
     DateFormat dateFormat = LocaleHelper.getDateFormat( LocaleHelper.FORMAT_MEDIUM, LocaleHelper.FORMAT_MEDIUM );
     String format = dateFormat.format( new Date() );
-    Assert.assertNotNull( format );
+    assertNotNull( format );
 
     DateFormat fullDateFormat0 = LocaleHelper.getFullDateFormat( false, false );
-    Assert.assertNull( fullDateFormat0 );
+    assertNull( fullDateFormat0 );
 
     DateFormat fullDateFormat = LocaleHelper.getFullDateFormat( true, true );
     String format1 = fullDateFormat.format( new Date() );
-    Assert.assertNotNull( format1 );
+    assertNotNull( format1 );
 
     DateFormat fullDateFormat1 = LocaleHelper.getFullDateFormat( true, false );
     String format2 = fullDateFormat1.format( new Date() );
-    Assert.assertNotNull( format2 );
+    assertNotNull( format2 );
 
     DateFormat fullDateFormat2 = LocaleHelper.getFullDateFormat( false, true );
     String format3 = fullDateFormat2.format( new Date() );
-    Assert.assertNotNull( format3 );
+    assertNotNull( format3 );
 
     DateFormat longDateFormat = LocaleHelper.getLongDateFormat( true, true );
     String format4 = longDateFormat.format( new Date() );
-    Assert.assertNotNull( format4 );
+    assertNotNull( format4 );
 
     DateFormat longDateFormat1 = LocaleHelper.getLongDateFormat( true, false );
     String format5 = longDateFormat1.format( new Date() );
-    Assert.assertNotNull( format5 );
+    assertNotNull( format5 );
 
     DateFormat longDateFormat2 = LocaleHelper.getLongDateFormat( false, true );
     String format6 = longDateFormat2.format( new Date() );
-    Assert.assertNotNull( format6 );
+    assertNotNull( format6 );
 
     DateFormat mediumDateFormat = LocaleHelper.getMediumDateFormat( true, true );
     String format7 = mediumDateFormat.format( new Date() );
-    Assert.assertNotNull( format7 );
+    assertNotNull( format7 );
 
     DateFormat mediumDateFormat1 = LocaleHelper.getMediumDateFormat( true, false );
     String format8 = mediumDateFormat1.format( new Date() );
-    Assert.assertNotNull( format8 );
+    assertNotNull( format8 );
 
     DateFormat mediumDateFormat2 = LocaleHelper.getMediumDateFormat( false, true );
     String format9 = mediumDateFormat2.format( new Date() );
-    Assert.assertNotNull( format9 );
+    assertNotNull( format9 );
 
     DateFormat shortDateFormat = LocaleHelper.getShortDateFormat( true, true );
     String format10 = shortDateFormat.format( new Date() );
-    Assert.assertNotNull( format10 );
+    assertNotNull( format10 );
 
     DateFormat shortDateFormat1 = LocaleHelper.getMediumDateFormat( true, false );
     String format11 = shortDateFormat1.format( new Date() );
-    Assert.assertNotNull( format11 );
+    assertNotNull( format11 );
 
     DateFormat shortDateFormat2 = LocaleHelper.getMediumDateFormat( false, true );
     String format12 = shortDateFormat2.format( new Date() );
-    Assert.assertNotNull( format12 );
-
+    assertNotNull( format12 );
   }
 
   @Test
@@ -149,28 +155,338 @@ public class LocaleHelperTest {
     LocaleHelper.setSystemEncoding( "Shift_JIS" );
   }
 
+  // region parseLocale
   @Test
   public void testParseAndSetLocaleOverride() {
+    Locale initiaLocaleOverride = LocaleHelper.getThreadLocaleOverride();
     final String TEST_LOCALE_LANG = "en";
     final String TEST_LOCALE_COUNTRY = "US";
-    LocaleHelper.parseAndSetLocaleOverride( TEST_LOCALE_LANG + "_" + TEST_LOCALE_COUNTRY );
+    try {
+      LocaleHelper.parseAndSetLocaleOverride( TEST_LOCALE_LANG + "_" + TEST_LOCALE_COUNTRY );
 
-    assertTrue( LocaleHelper.getLocale().equals( new Locale( TEST_LOCALE_LANG, TEST_LOCALE_COUNTRY ) ) );
-
-    // reset override to not break other tests
-    LocaleHelper.setThreadLocaleOverride( null );
+      assertEquals( LocaleHelper.getLocale(), new Locale( TEST_LOCALE_LANG, TEST_LOCALE_COUNTRY ) );
+    } finally {
+      LocaleHelper.setThreadLocaleOverride( initiaLocaleOverride );
+    }
   }
+
+  @Test
+  public void testParseLocaleReturnsNullWhenGivenNull() {
+
+    Locale locale = LocaleHelper.parseLocale( null );
+
+    assertNull( locale );
+  }
+
+  @Test
+  public void testParseLocaleReturnsNullWhenGivenEmpty() {
+
+    Locale locale = LocaleHelper.parseLocale( "" );
+
+    assertNull( locale );
+  }
+
+  @Test
+  public void testParseLocaleHandlesLocalesWithOneSection() {
+    final String TEST_LOCALE_LANG = "en";
+
+    Locale locale = LocaleHelper.parseLocale( TEST_LOCALE_LANG );
+
+    assertEquals( locale, new Locale( TEST_LOCALE_LANG ) );
+  }
+
+  @Test
+  public void testParseLocaleHandlesLocalesWithTwoSections() {
+    final String TEST_LOCALE_LANG = "en";
+    final String TEST_LOCALE_COUNTRY = "US";
+
+    Locale locale = LocaleHelper.parseLocale( TEST_LOCALE_LANG + "_" + TEST_LOCALE_COUNTRY );
+
+    assertEquals( locale, new Locale( TEST_LOCALE_LANG, TEST_LOCALE_COUNTRY ) );
+  }
+
+  // NOTE: this test was written to match the existing implementation,
+  // but it is not clear if this behavior was intentional or a bug.
+  @Test
+  public void testParseLocaleSimplifiesLocalesWithMoreThanTwoSections() {
+    final String TEST_LOCALE_LANG = "en";
+    final String TEST_LOCALE_COUNTRY = "US";
+    final String TEST_LOCALE_VARIANT = "Foo";
+
+    Locale locale = LocaleHelper.parseLocale(
+      TEST_LOCALE_LANG +
+        "_" +
+        TEST_LOCALE_COUNTRY +
+        "_" +
+        TEST_LOCALE_VARIANT);
+
+    assertEquals( locale, new Locale( TEST_LOCALE_LANG, TEST_LOCALE_COUNTRY ) );
+    assertNotEquals( locale, new Locale( TEST_LOCALE_LANG, TEST_LOCALE_COUNTRY, TEST_LOCALE_VARIANT ) );
+  }
+
+  @Test
+  public void testParseLocaleHandlesAndNormalizesLocalesWithMixedCasingSections() {
+
+    Locale locale = LocaleHelper.parseLocale( "eN_Us" );
+
+    assertEquals( locale, new Locale( "en", "US" ) );
+
+    locale = LocaleHelper.parseLocale( "En_us" );
+
+    assertEquals( locale, new Locale( "en", "US" ) );
+  }
+
+  // NOTE: again, not a requirement per se, afaict, but it's how it's working.
+  @Test
+  public void testParseLocaleAcceptsSectionsOfAnyLength() {
+
+    Locale locale = LocaleHelper.parseLocale( "foo_guru" );
+
+    assertEquals( locale, new Locale( "foo", "GURU" ) );
+  }
+
+  // NOTE: again, not a requirement per se, afaict, but it's how it's working.
+  // Notably, see SystemResource#setLocaleOverride( . ) which also supports sections separated
+  // by "-" or "|". Additionally, it then uses org.apache.commons.lang3.LocaleUtils.toLocale
+  // to parse the locale, instead of LocaleHelper.parseLocale.
+  @Test
+  public void testParseLocaleDoesNotSeparateSectionsByHyphen() {
+
+    Locale locale = LocaleHelper.parseLocale( "foo-guru" );
+
+    assertEquals( locale, new Locale( "foo-guru" ) );
+  }
+  // endregion
+
+  // region Default Locale
+
+  // Cannot test the case where Locale.getDefault() would be null with this kind of test,
+  // because that would possibly only happen if `java` were passed a non-available locale parameter...
+
+  @Test
+  public void testDefaultLocaleIsJVMLocale() {
+    assertEquals( Locale.getDefault(), LocaleHelper.getDefaultLocale() );
+  }
+
+  @Test
+  public void testDefaultLocaleFallsbackToJVMLocaleWhenSetToNull() {
+    Locale initialLocale = Locale.getDefault();
+    Locale customLocale = Locale.forLanguageTag( "de-POSIX-x-URP-lvariant-Abc-Def" );
+    Locale.setDefault( customLocale );
+    try {
+      LocaleHelper.setDefaultLocale( null );
+      assertEquals( customLocale, LocaleHelper.getDefaultLocale() );
+    } finally {
+      Locale.setDefault( initialLocale );
+    }
+  }
+
+  @Test
+  public void testDefaultLocaleIsRespectedWhenSetToNonNull() {
+    Locale initialDefaultLocale = LocaleHelper.getDefaultLocale();
+    Locale customLocale = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+    try {
+      LocaleHelper.setDefaultLocale( customLocale );
+      assertEquals( customLocale, LocaleHelper.getDefaultLocale() );
+    } finally {
+      LocaleHelper.setDefaultLocale( initialDefaultLocale );
+    }
+  }
+  // endregion
+
+  // region Thread Locale Base
+  @Test
+  public void testThreadLocaleBaseDefaultsToNull() {
+    assertNull( LocaleHelper.getThreadLocaleBase() );
+  }
+
+  @Test
+  public void testThreadLocaleBaseRemembersBeingSetToNonNullValue() {
+    Locale initialLocale = LocaleHelper.getThreadLocaleBase();
+    Locale customLocale = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+
+    try {
+      LocaleHelper.setThreadLocaleBase( customLocale );
+
+      assertEquals( customLocale, LocaleHelper.getThreadLocaleBase() );
+    } finally {
+      LocaleHelper.setThreadLocaleBase( initialLocale );
+    }
+  }
+
+  @Test
+  public void testThreadLocaleBaseCanBeSetToNullValue() {
+    Locale initialLocale = LocaleHelper.getThreadLocaleBase();
+
+    try {
+      LocaleHelper.setThreadLocaleBase( Locale.ENGLISH );
+      assertNotNull( LocaleHelper.getThreadLocaleBase() );
+
+      LocaleHelper.setThreadLocaleBase( null );
+      assertNull( LocaleHelper.getThreadLocaleBase() );
+    } finally {
+      LocaleHelper.setThreadLocaleBase( initialLocale );
+    }
+  }
+  // endregion
+
+  // region Thread Locale Override
+  @Test
+  public void testThreadLocaleOverrideDefaultsToNull() {
+    assertNull( LocaleHelper.getThreadLocaleOverride() );
+  }
+
+  @Test
+  public void testThreadLocaleOverrideRemembersBeingSetToNonNullValue() {
+    Locale initialLocale = LocaleHelper.getThreadLocaleOverride();
+    Locale customLocale = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+
+    try {
+      LocaleHelper.setThreadLocaleOverride( customLocale );
+
+      assertEquals( customLocale, LocaleHelper.getThreadLocaleOverride() );
+    } finally {
+      LocaleHelper.setThreadLocaleOverride( initialLocale );
+    }
+  }
+
+  @Test
+  public void testThreadLocaleOverrideCanBeSetToNullValue() {
+    Locale initialLocale = LocaleHelper.getThreadLocaleOverride();
+
+    try {
+      LocaleHelper.setThreadLocaleOverride( Locale.ENGLISH );
+      assertNotNull( LocaleHelper.getThreadLocaleOverride() );
+
+      LocaleHelper.setThreadLocaleOverride( null );
+      assertNull( LocaleHelper.getThreadLocaleOverride() );
+    } finally {
+      LocaleHelper.setThreadLocaleOverride( initialLocale );
+    }
+  }
+  // endregion
+
+  // region (Effective) Locale
+  @Test
+  public void testEffectiveLocaleIsTheDefaultLocaleIfNoThreadBaseOrOverride() {
+    Locale initialDefaultLocale = LocaleHelper.getDefaultLocale();
+    Locale initialThreadLocaleBase = LocaleHelper.getThreadLocaleBase();
+    Locale initialThreadLocaleOverride = LocaleHelper.getThreadLocaleOverride();
+
+    Locale customLocale = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+    try {
+      LocaleHelper.setDefaultLocale( customLocale );
+      LocaleHelper.setThreadLocaleBase( null );
+      LocaleHelper.setThreadLocaleOverride( null );
+
+      assertEquals( customLocale, LocaleHelper.getLocale() );
+    } finally {
+      LocaleHelper.setDefaultLocale( initialDefaultLocale );
+      LocaleHelper.setThreadLocaleBase( initialThreadLocaleBase );
+      LocaleHelper.setThreadLocaleOverride( initialThreadLocaleOverride );
+    }
+  }
+
+  @Test
+  public void testEffectiveLocaleIsTheThreadLocaleBaseIfSetAndNoThreadOverride() {
+    Locale initialDefaultLocale = LocaleHelper.getDefaultLocale();
+    Locale initialThreadLocaleBase = LocaleHelper.getThreadLocaleBase();
+    Locale initialThreadLocaleOverride = LocaleHelper.getThreadLocaleOverride();
+
+    Locale customLocale1 = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+    Locale customLocale2 = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Ghi");
+    try {
+      LocaleHelper.setDefaultLocale( customLocale1 );
+      LocaleHelper.setThreadLocaleBase( customLocale2 );
+      LocaleHelper.setThreadLocaleOverride( null );
+
+      assertEquals( customLocale2, LocaleHelper.getLocale() );
+    } finally {
+      LocaleHelper.setDefaultLocale( initialDefaultLocale );
+      LocaleHelper.setThreadLocaleBase( initialThreadLocaleBase );
+      LocaleHelper.setThreadLocaleOverride( initialThreadLocaleOverride );
+    }
+  }
+
+  @Test
+  public void testEffectiveLocaleIsTheThreadLocaleOverrideIfSet() {
+    Locale initialDefaultLocale = LocaleHelper.getDefaultLocale();
+    Locale initialThreadLocaleBase = LocaleHelper.getThreadLocaleBase();
+    Locale initialThreadLocaleOverride = LocaleHelper.getThreadLocaleOverride();
+
+    Locale customLocale1 = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+    Locale customLocale2 = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Ghi");
+    Locale customLocale3 = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Jkl");
+    try {
+      LocaleHelper.setDefaultLocale( customLocale1 );
+      LocaleHelper.setThreadLocaleBase( customLocale2 );
+      LocaleHelper.setThreadLocaleOverride( customLocale3 );
+
+      assertEquals( customLocale3, LocaleHelper.getLocale() );
+    } finally {
+      LocaleHelper.setDefaultLocale( initialDefaultLocale );
+      LocaleHelper.setThreadLocaleBase( initialThreadLocaleBase );
+      LocaleHelper.setThreadLocaleOverride( initialThreadLocaleOverride );
+    }
+  }
+  // endregion
+
+  // region Session Locale Override
+
+  @Test
+  public void testSessionLocaleOverrideIsNoOpIfNoSession() {
+    IPentahoSession initialSession = PentahoSessionHolder.getSession();
+
+    Locale customLocale = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+    try {
+      LocaleHelper.setSessionLocaleOverride( customLocale );
+    } finally {
+      PentahoSessionHolder.setSession( initialSession );
+    }
+  }
+
+  @Test
+  public void testSessionLocaleOverrideSetsCurrentSessionLocaleOverrideWithLocaleToString() {
+    IPentahoSession initialSession = PentahoSessionHolder.getSession();
+    IPentahoSession customSession = mock( IPentahoSession.class );
+    Locale customLocale = Locale.forLanguageTag("de-POSIX-x-URP-lvariant-Abc-Def");
+    try {
+      PentahoSessionHolder.setSession( customSession );
+
+      LocaleHelper.setSessionLocaleOverride( customLocale );
+
+      verify( customSession, times( 1 )).setAttributeLocaleOverride( customLocale.toString() );
+    } finally {
+      PentahoSessionHolder.setSession( initialSession );
+    }
+  }
+
+  @Test
+  public void testSessionLocaleOverrideSetsCurrentSessionLocaleOverrideWithNullLocale() {
+    IPentahoSession initialSession = PentahoSessionHolder.getSession();
+    IPentahoSession customSession = mock( IPentahoSession.class );
+    try {
+      PentahoSessionHolder.setSession( customSession );
+
+      LocaleHelper.setSessionLocaleOverride( null );
+
+      verify( customSession, times( 1 )).setAttributeLocaleOverride( null );
+    } finally {
+      PentahoSessionHolder.setSession( initialSession );
+    }
+  }
+  // endregion
 
   @Test
   public void testGetNumberFormat() {
     NumberFormat nfmt = LocaleHelper.getNumberFormat();
-    Assert.assertNotNull( nfmt );
+    assertNotNull( nfmt );
   }
 
   @Test
   public void testGetCurrencyFormat() {
     NumberFormat cfmt = LocaleHelper.getCurrencyFormat();
-    Assert.assertNotNull( cfmt );
+    assertNotNull( cfmt );
   }
 
   @Test
