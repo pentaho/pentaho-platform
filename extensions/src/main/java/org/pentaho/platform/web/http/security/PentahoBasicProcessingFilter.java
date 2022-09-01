1d34d54bf61e1a01a895df6a2f13f037a4f75ac2
Duarte Cunha Leão <duarte.leao@gmail.com>
2022-09-01 18:13:18 +0100

    [BISERVER-14840] Cannot perform Basic HTTP Authentication with Cookie's SameSite=None and HTTPS after log-off (#5127)
    - removed impossible to use constructor
    - added always missing unit tests

--- extensions/src/main/java/org/pentaho/platform/web/http/security/PentahoBasicProcessingFilter.java
+++ extensions/src/main/java/org/pentaho/platform/web/http/security/PentahoBasicProcessingFilter.java
@@ -14,19 +14,21 @@
  * See the GNU Lesser General Public License for more details.
  *
  *
- * Copyright (c) 2002-2021 Hitachi Vantara. All rights reserved.
+ * Copyright (c) 2002-2022 Hitachi Vantara. All rights reserved.
  *
  */
 
 package org.pentaho.platform.web.http.security;
 
+import com.google.common.annotations.VisibleForTesting;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -37,13 +39,14 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
- * This class's sole purpose is to defeat the persistence of Basic-Auth credentials in the browser. The mechanism used
- * to accomplish this is to detect an expired (invalid) HttpSession from the client.
+ * This class's sole purpose is to defeat the persistence of Basic-Auth credentials in the browser.
+ * The mechanism used to accomplish this is to detect an expired (invalid) HttpSession from the client.
  * <p/>
  * If the first request after a session becomes invalid is a Basic-Auth request, we automatically deny, forcing
- * re-authentication.
+ * reauthentication.
  * <p/>
  * The second path is if the first request after session invalidation is not a basic-auth (user manually logged out and
  * was presented with the login page), we drop a cookie in the response noting the event. The next request with
@@ -54,13 +57,17 @@ import java.io.IOException;
  */
 public class PentahoBasicProcessingFilter extends BasicAuthenticationFilter implements ApplicationEventPublisherAware {
 
+  @VisibleForTesting
+  static final String SESSION_FLUSHED_COOKIE_NAME = "session-flushed";
+
+  @VisibleForTesting
+  static final String SESSION_ID_COOKIE_NAME = "JSESSIONID";
+
+
   private ApplicationEventPublisher applicationEventPublisher;
 
-  public PentahoBasicProcessingFilter( AuthenticationManager authenticationManager ) {
-    super( authenticationManager );
-  }
-
-  public PentahoBasicProcessingFilter( AuthenticationManager authenticationManager, AuthenticationEntryPoint authenticationEntryPoint ) {
+  public PentahoBasicProcessingFilter( AuthenticationManager authenticationManager,
+                                       AuthenticationEntryPoint authenticationEntryPoint ) {
     super( authenticationManager, authenticationEntryPoint );
   }
 
@@ -71,64 +78,48 @@ public class PentahoBasicProcessingFilter extends BasicAuthenticationFilter impl
   @Override
   public void doFilterInternal( HttpServletRequest request, HttpServletResponse response, FilterChain chain )
     throws IOException, ServletException {
-    if ( request.getRequestedSessionId() != null && !request.isRequestedSessionIdValid() ) {
-      // expired session detected.
-      Cookie expiredCookie = null;
-      for ( Cookie c : request.getCookies() ) {
-        if ( "JSESSIONID".equals( c.getName() ) ) {
-          expiredCookie = c; // cache that this definitely is a browser with an expired session.
-          break;
-        }
-      }
-      String header = request.getHeader( "Authorization" );
-      if ( header != null && header.indexOf( "Basic" ) == 0 ) {
-        // Session is expired and a Basic-Auth request is coming in. We'll drop a cookie to note this and force
-        // re-authentication
-        for ( Cookie c : request.getCookies() ) {
-          if ( "session-flushed".equals( c.getName() ) ) {
-            c.setMaxAge( 0 );
-            response.addCookie( c );
-            break;
-          }
-        }
-        // force the prompt for credentials
-        getAuthenticationEntryPoint()
-            .commence( request, response, new BadCredentialsException( "Clearing Basic-Auth" ) );
-        return;
-      } else if ( expiredCookie != null ) {
-        SessionCookieConfig sessionCookieConfig = request.getServletContext().getSessionCookieConfig();
-        // Session is expired but this request does not include basic-auth, drop a cookie to keep track of this event.
-        Cookie c = new Cookie( "session-flushed", "true" );
-        c.setPath( request.getContextPath() != null ? request.getContextPath() : "/" );
-        c.setMaxAge( -1 );
-        c.setHttpOnly( sessionCookieConfig.isHttpOnly() );
-        c.setSecure( sessionCookieConfig.isSecure() );
-        response.addCookie( c );
-      }
-    } else {
-      String header = request.getHeader( "Authorization" );
-      if ( header != null && header.indexOf( "Basic" ) == 0
-          && SecurityContextHolder.getContext().getAuthentication() == null ) {
-        // Session is valid, but Basic-auth is supplied. Check to see if the session end cookie we created is present,
-        // if so, force reauthentication.
 
-        Cookie[] cookies;
-        cookies = request.getCookies();
-        if ( cookies != null ) {
-          for ( Cookie c : cookies ) {
-            if ( "session-flushed".equals( c.getName() ) ) {
-              c.setMaxAge( 0 );
-              c.setPath( request.getContextPath() != null ? request.getContextPath() : "/" );
-              response.addCookie( c );
-              getAuthenticationEntryPoint().commence( request, response,
-                  new BadCredentialsException( "Clearing Basic-Auth" ) );
-              return;
-            }
-          }
-        }
+    if ( request.getRequestedSessionId() != null && !request.isRequestedSessionIdValid() ) {
+      // Expired session detected.
+
+      if ( isBasicAuthRequest( request ) ) {
+        // Session is expired and a Basic-Auth request is coming in.
+
+        // Clear any session-flushed cookie.
+        clearSessionFlushedCookie( request, response );
+
+        // Ignore provided credentials, and force the prompt for (new) credentials.
+        getAuthenticationEntryPoint()
+          .commence( request, response, new BadCredentialsException( "Clearing Basic-Auth" ) );
+        return;
+      }
+
+      // Check that this definitely is a browser with an expired session.
+      boolean hasExpiredSessionCookie = Arrays.stream( request.getCookies() )
+        .anyMatch( cookie -> SESSION_ID_COOKIE_NAME.equals( cookie.getName() ) );
+
+      if ( hasExpiredSessionCookie ) {
+        // Session is expired but this request does not include basic-auth,
+        // drop a cookie to keep track that session had expired.
+        response.addCookie( createSessionFlushedCookie( request ) );
+      }
+    } else if ( isBasicAuthRequest( request ) && SecurityContextHolder.getContext().getAuthentication() == null ) {
+      // Session is valid, not authenticated, and Basic-auth is supplied.
+      // Check to see if the session-flushed cookie we created is present, and, if so, force reauthentication for
+      // the Pentaho realm.
+      if ( clearSessionFlushedCookie( request, response ) ) {
+        getAuthenticationEntryPoint()
+          .commence( request, response, new BadCredentialsException( "Clearing Basic-Auth" ) );
+        return;
       }
     }
 
+    doFilterInternalSuper( request, response, chain );
+  }
+
+  @VisibleForTesting
+  void doFilterInternalSuper( HttpServletRequest request, HttpServletResponse response, FilterChain chain )
+    throws IOException, ServletException {
     super.doFilterInternal( request, response, chain );
   }
 
@@ -140,5 +131,46 @@ public class PentahoBasicProcessingFilter extends BasicAuthenticationFilter impl
     if ( applicationEventPublisher != null ) {
       applicationEventPublisher.publishEvent( new AuthenticationSuccessEvent( authResult ) );
     }
+  }
+
+  protected boolean isBasicAuthRequest( @NonNull HttpServletRequest request ) {
+    String header = request.getHeader( "Authorization" );
+    return header != null && header.indexOf( "Basic" ) == 0;
+  }
+
+  @NonNull
+  protected Cookie createSessionFlushedCookie( @NonNull HttpServletRequest request ) {
+    Cookie cookie = new Cookie( SESSION_FLUSHED_COOKIE_NAME, "true" );
+    // maxAge: -1 means the cookie is cleared when the web-browser session ends.
+    configureSessionFlushedCookie( cookie, request, -1 );
+    return cookie;
+  }
+
+  protected static void configureSessionFlushedCookie( @NonNull Cookie cookie,
+                                                     @NonNull HttpServletRequest request,
+                                                     int maxAge ) {
+    SessionCookieConfig sessionCookieConfig = request.getServletContext().getSessionCookieConfig();
+    cookie.setPath( request.getContextPath() != null ? request.getContextPath() : "/" );
+    cookie.setHttpOnly( sessionCookieConfig.isHttpOnly() );
+    cookie.setSecure( sessionCookieConfig.isSecure() );
+    cookie.setMaxAge( maxAge );
+  }
+
+  // Used by PentahoBasicAuthenticationEntryPoint
+  static boolean clearSessionFlushedCookie( @NonNull HttpServletRequest request,
+                                            @NonNull HttpServletResponse response ) {
+    Cookie[] cookies = request.getCookies();
+    if ( cookies != null ) {
+      for ( Cookie cookie : cookies ) {
+        if ( SESSION_FLUSHED_COOKIE_NAME.equals( cookie.getName() ) ) {
+          // maxAge: 0 causes the browser to remove the cookie.
+          configureSessionFlushedCookie( cookie, request, 0 );
+          response.addCookie( cookie );
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 }
