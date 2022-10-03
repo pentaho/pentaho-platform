52a39b5e4b4e26ddc28290afb85929501704f866
Nicholas Jordan <nicholas.jordan@hitachivantara.com>
2022-10-03 17:31:44 -0400

    [POC] add unit test get over 80% code coverage

--- core/src/test/java/org/pentaho/example/CalculatorTest.java
+++ core/src/test/java/org/pentaho/example/CalculatorTest.java
@@ -4,9 +4,15 @@ import junit.framework.TestCase;
 
 public class CalculatorTest extends TestCase {
 
-    public void testAdd() {
+    public void testAdd() { // should get to less than 80 % code coverage
 
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(0,5) );
+        assertEquals( 5, calculator.add( 0, 5 ) );
+    }
+
+    public void testMultiply() { // should get to 100 % code coverage
+
+        Calculator calculator = new Calculator();
+        assertEquals( 14, calculator.multiply( 2, 7 ) );
     }
 }
