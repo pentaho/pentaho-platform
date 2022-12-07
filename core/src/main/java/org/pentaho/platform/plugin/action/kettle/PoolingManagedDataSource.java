fa479ff5e4a3707cbd6c080103ef57637de7b5a9
renato-s <renato.santos@hitachivantara.com>
2022-12-07 16:54:43 +0000

    [CUST-273] - Requesting CUST for PDI-19604 on top of 9.3.0.2
    (cherry picked from commit e4433baad515baf31c964a0e7d63544feb0bea8d)
    
    [PDI-19604] - Connection related issues when Connection Pooling is enabled in transformations.
    
    (cherry picked from commit c73dfa8d9e70549a419c6a5760f598e93e52e243)
    
    [PDI-19604] - PR Review
    
    (cherry picked from commit cd003794596373c9cc0642c75f319de77e02de43)
    
    [PDI-19604] - PR Review
    
    (cherry picked from commit c294dbd6f9282fba3f67d20e70e9139a9d5474d5)
    
    [PDI-19604] - Connection related issues when Connection Pooling is enabled in transformations.
    
    (cherry picked from commit 87ef615c046cbfbdeb69c9b2d0253e02b2be877a)
    
    [PDI-19604] - Connection related issues when Connection Pooling is enabled in transformations.
    
    (cherry picked from commit 576042379bafb57aed4df6ef40551684a98197fd)

---
+++ core/src/main/java/org/pentaho/platform/plugin/action/kettle/PoolingManagedDataSource.java
@@ -0,0 +1,119 @@
+/*!
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright (c) 2022 Hitachi Vantara. All rights reserved.
+ *
+ */
+
+package org.pentaho.platform.plugin.action.kettle;
+
+
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
+import org.pentaho.database.IDatabaseDialect;
+import org.pentaho.database.model.IDatabaseConnection;
+import org.pentaho.di.core.database.CachedManagedDataSourceInterface;
+import org.pentaho.platform.api.data.IDBDatasourceService;
+import org.pentaho.platform.engine.services.connection.datasource.dbcp.PooledDatasourceHelper;
+
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class PoolingManagedDataSource extends PoolingDataSource implements CachedManagedDataSourceInterface {
+
+    private boolean isExpired;
+    private String poolConfigHash;
+    private List<String> usedBy;
+
+    public PoolingManagedDataSource( IDatabaseConnection databaseConnection, IDatabaseDialect dialect ) throws Exception {
+        isExpired = false;
+        poolConfigHash = "";
+        usedBy = new ArrayList<>();
+
+        Map<String, String> attributes = databaseConnection.getConnectionPoolingProperties();
+        GenericObjectPool pool = PooledDatasourceHelper.createGenericPool( databaseConnection, dialect, attributes );
+        /*
+         * All of this is wrapped in a DataSource, which client code should already know how to handle (since it's the
+         * same class of object they'd fetch via the container's JNDI tree
+         */
+        setPool( pool );
+        setHash( databaseConnection.calculateHash() );
+        if ( attributes.containsKey( IDBDatasourceService.ACCESS_TO_UNDERLYING_CONNECTION_ALLOWED ) ) {
+            setAccessToUnderlyingConnectionAllowed( Boolean.parseBoolean( attributes
+                    .get( IDBDatasourceService.ACCESS_TO_UNDERLYING_CONNECTION_ALLOWED ) ) );
+        }
+    }
+
+    @Override
+    public boolean isExpired() {
+        return isExpired;
+    }
+
+    @Override
+    public void tryInvalidateDataSource( String invalidatedBy ) {
+        expire();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public void addInUseBy( String ownerName ) {
+        if ( !usedBy.contains( ownerName ) ){
+            usedBy.add( ownerName );
+        }
+    }
+
+    @Override
+    public void removeInUseBy( String invalidatedBy ) {
+        usedBy.remove( invalidatedBy );
+    }
+
+    @Override
+    public void setHash( String poolConfig ) {
+        poolConfigHash = poolConfig;
+    }
+
+    @Override
+    public String getHash() {
+        return poolConfigHash;
+    }
+
+    public boolean hasSameConfig( String poolConfigHash ) {
+        return this.poolConfigHash.equals( poolConfigHash );
+    }
+
+    @Override
+    public boolean isInUse() {
+        return !usedBy.isEmpty();
+    }
+
+    @Override
+    public void setInUseBy( List<String> ownerList ) {
+        usedBy = ownerList;
+    }
+
+    @Override
+    public void expire() {
+        isExpired = true;
+    }
+}
