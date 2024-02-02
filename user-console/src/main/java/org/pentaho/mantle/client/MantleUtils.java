1f464f985bb4dd36d21b03e9f2c9de18a1dc0a04
Ezequiel Cuellar <ezequiel.cuellar-ojeda@hitachivantara.com>
2024-02-02 12:12:34 -0500

    [BACKLOG-39531] RunInBackground functionality still existing in the Platform

--- user-console/src/main/java/org/pentaho/mantle/client/MantleUtils.java
+++ user-console/src/main/java/org/pentaho/mantle/client/MantleUtils.java
@@ -14,7 +14,7 @@
  * See the GNU Lesser General Public License for more details.
  *
  *
- * Copyright (c) 2002-2023 Hitachi Vantara. All rights reserved.
+ * Copyright (c) 2002-2024 Hitachi Vantara. All rights reserved.
  *
  */
 
@@ -22,9 +22,12 @@ package org.pentaho.mantle.client;
 
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestException;
+import org.pentaho.gwt.widgets.client.filechooser.RepositoryFile;
 import org.pentaho.mantle.client.events.EventBusUtil;
 import org.pentaho.mantle.client.events.GenericEvent;
+import org.pentaho.mantle.client.events.SolutionFileHandler;
 import org.pentaho.mantle.client.solutionbrowser.SolutionBrowserPanel;
+import org.pentaho.mantle.client.solutionbrowser.filelist.FileItem;
 import org.pentaho.mantle.client.ui.PerspectiveManager;
 
 public class MantleUtils {
@@ -32,7 +35,6 @@ public class MantleUtils {
   static {
     setupNativeHooks( new MantleUtils() );
   }
-
 
   public void setSchedulesPerspective() {
     PerspectiveManager.getInstance().setPerspective( PerspectiveManager.SCHEDULES_PERSPECTIVE );
@@ -59,6 +61,34 @@ public class MantleUtils {
     event.setEventSubType( "RefreshFolderEvent" );
     event.setStringParam( outputLocation );
     EventBusUtil.EVENT_BUS.fireEvent( event );
+  }
+
+  public void handleRepositoryFileSelection( String solutionPath, Boolean isAdhoc) {
+    final SolutionBrowserPanel sbp = SolutionBrowserPanel.getInstance();
+    sbp.getFile( solutionPath, new SolutionFileHandler() {
+      @Override
+      public void handle( RepositoryFile repositoryFile ) {
+        if( isAdhoc ) {
+          openAdhocDialog( true );
+        } else {
+          FileItem fileItem = new FileItem( repositoryFile, null, null, false, null );
+          checkSchedulePermissionAndDialog( fileItem.getRepositoryFile().getId(), fileItem.getPath() );
+        }
+      }
+    } );
+  }
+
+  private native void openAdhocDialog( boolean feedback ) /*-{
+    $wnd.pho.showDialog(feedback);
+  }-*/;
+
+  private native void checkSchedulePermissionAndDialog( String repositoryFileId, String repositoryFilePath ) /*-{
+    $wnd.pho.checkSchedulePermissionAndDialog(repositoryFileId, repositoryFilePath);
+  }-*/;
+
+  public boolean checkSelectedPerspective() {
+    return !PerspectiveManager.getInstance().getActivePerspective().getId().equals(
+      PerspectiveManager.SCHEDULES_PERSPECTIVE );
   }
 
   public static native String getSchedulerPluginContextURL()/*-{
@@ -92,6 +122,16 @@ public class MantleUtils {
     $wnd.mantle.fireRefreshFolderEvent = function(outputLocation) {
       //CHECKSTYLE IGNORE LineLength FOR NEXT 1 LINES
       utils.@org.pentaho.mantle.client.MantleUtils::fireRefreshFolderEvent(Ljava/lang/String;)(outputLocation);
+    }
+
+    $wnd.mantle.handleRepositoryFileSelection = function(solutionPath, isAdhoc) {
+      //CHECKSTYLE IGNORE LineLength FOR NEXT 1 LINES
+      utils.@org.pentaho.mantle.client.MantleUtils::handleRepositoryFileSelection(Ljava/lang/String;Ljava/lang/Boolean;)(solutionPath, isAdhoc);
+    }
+
+    $wnd.mantle.checkSelectedPerspective = function() {
+      //CHECKSTYLE IGNORE LineLength FOR NEXT 1 LINES
+      return utils.@org.pentaho.mantle.client.MantleUtils::checkSelectedPerspective()();
     }
    }-*/;
 }
