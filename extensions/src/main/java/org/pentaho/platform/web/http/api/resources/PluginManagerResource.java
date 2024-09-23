50ca9465c9b769a0ffbcd09bcd7f5366053faee1
SahithiKommagani6 <sahithi.kommagani@hitachivantara.com>
2024-09-23 15:10:20 +0530

    [BACKLOG-41601]-Upgrade sun jersey from 1.x to glassfish jersey 2.x

--- extensions/src/main/java/org/pentaho/platform/web/http/api/resources/PluginManagerResource.java
+++ extensions/src/main/java/org/pentaho/platform/web/http/api/resources/PluginManagerResource.java
@@ -58,7 +58,7 @@ public class PluginManagerResource {
   @Path( "/overlays" )
   @Produces( { APPLICATION_JSON } )
   @Facet( name = "Unsupported" )
-  public List<Overlay> getOverlays( @QueryParam( "id" ) @DefaultValue( "" ) String id ) {
+  public OverlayWrapper getOverlays( @QueryParam( "id" ) @DefaultValue( "" ) String id ) {
     IPluginManager pluginManager = PentahoSystem.get( IPluginManager.class, PentahoSessionHolder.getSession() ); //$NON-NLS-1$
     List<XulOverlay> overlays = pluginManager.getOverlays();
     ArrayList<Overlay> result = new ArrayList<Overlay>();
@@ -71,7 +71,7 @@ public class PluginManagerResource {
               overlay.getPriority() );
       result.add( tempOverlay );
     }
-    return result;
+    return new OverlayWrapper( result );
   }
 
   /**
@@ -83,7 +83,7 @@ public class PluginManagerResource {
   @Path( "/perspectives" )
   @Produces( { APPLICATION_JSON } )
   @Facet ( name = "Unsupported" )
-  public ArrayList<PluginPerspective> getPluginPerpectives() {
+  public PluginPerspectiveWrapper getPluginPerpectives() {
     IPluginPerspectiveManager manager =
         PentahoSystem.get( IPluginPerspectiveManager.class, PentahoSessionHolder.getSession() ); //$NON-NLS-1$
 
@@ -110,7 +110,7 @@ public class PluginManagerResource {
       perspectives.add( pp );
     }
 
-    return perspectives;
+    return new PluginPerspectiveWrapper( perspectives );
   }
 
   /**
