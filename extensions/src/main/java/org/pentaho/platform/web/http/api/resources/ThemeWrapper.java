d94fc5c8d29ba3cb7cd072db404f11dd5d9b8cec
SahithiKommagani6 <sahithi.kommagani@hitachivantara.com>
2024-09-23 15:10:20 +0530

    [BACKLOG-41601]-Upgrade sun jersey from 1.x to glassfish jersey 2.x

---
+++ extensions/src/main/java/org/pentaho/platform/web/http/api/resources/ThemeWrapper.java
@@ -0,0 +1,51 @@
+/*!
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ *
+ * Copyright (c) 2024 Hitachi Vantara. All rights reserved.
+ *
+ */
+
+package org.pentaho.platform.web.http.api.resources;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement( name = "themes" )
+@XmlAccessorType( XmlAccessType.FIELD )
+public class ThemeWrapper {
+
+    @XmlElement( name = "theme" )
+    private List<Theme> themes;
+
+    public ThemeWrapper() {
+
+    }
+
+    public ThemeWrapper( List<Theme> themes ) {
+        this.themes = themes;
+    }
+
+    public List<Theme> getThemes() {
+        return themes;
+    }
+
+    public void setThemes( List<Theme> themes ) {
+        this.themes = themes;
+    }
+}
