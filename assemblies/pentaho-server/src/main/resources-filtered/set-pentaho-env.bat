0a9117611cc3977309bf23cb100b80ad54c5cb12
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-29 12:35:33 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit 3e6491adb90812e9453767a9d49d84178cd0f836.

--- assemblies/pentaho-server/src/main/resources-filtered/set-pentaho-env.bat
+++ assemblies/pentaho-server/src/main/resources-filtered/set-pentaho-env.bat
@@ -1,14 +1,55 @@
-REM ******************************************************************************
+
+REM *******************************************************************************************
+REM This program is free software; you can redistribute it and/or modify it under the
+REM terms of the GNU General Public License, version 2 as published by the Free Software
+REM Foundation.
 REM
-REM Pentaho
+REM You should have received a copy of the GNU General Public License along with this
+REM program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+REM or from the Free Software Foundation, Inc.,
+REM 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 REM
-REM Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+REM This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+REM without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+REM See the GNU General Public License for more details.
 REM
-REM Use of this software is governed by the Business Source License included
-REM in the LICENSE.TXT file.
 REM
-REM Change Date: 2028-08-13
-REM ******************************************************************************
+REM Copyright 2011 - ${copyright.year} Hitachi Vantara. All rights reserved.
+REM *******************************************************************************************
+
+rem ---------------------------------------------------------------------------
+rem Finds a suitable Java
+rem
+rem Looks in well-known locations to find a suitable Java then sets two 
+rem environment variables for use in other bat files. The two environment
+rem variables are:
+rem 
+rem * _PENTAHO_JAVA_HOME - absolute path to Java home
+rem * _PENTAHO_JAVA - absolute path to Java launcher (e.g. java.exe)
+rem 
+rem The order of the search is as follows:
+rem 
+rem 1. argument #1 - path to Java home
+rem 2. environment variable PENTAHO_JAVA_HOME - path to Java home
+rem 3. jre folder at current folder level
+rem 4. java folder at current folder level
+rem 5. jre folder one level up
+rem 6 java folder one level up
+rem 7. jre folder two levels up
+rem 8. java folder two levels up
+rem 9. environment variable JAVA_HOME - path to Java home
+rem 10. environment variable JRE_HOME - path to Java home
+rem 
+rem If a suitable Java is found at one of these locations, then 
+rem _PENTAHO_JAVA_HOME is set to that location and _PENTAHO_JAVA is set to the 
+rem absolute path of the Java launcher at that location. If none of these 
+rem locations are suitable, then _PENTAHO_JAVA_HOME is set to empty string and 
+rem _PENTAHO_JAVA is set to java.exe.
+rem 
+rem Finally, there is one final optional environment variable: PENTAHO_JAVA.
+rem If set, this value is used in the construction of _PENTAHO_JAVA. If not 
+rem set, then the value java.exe is used. 
+rem ---------------------------------------------------------------------------
 
 if not "%PENTAHO_JAVA%" == "" goto gotPentahoJava
 set __LAUNCHER=java.exe
