39b4f0d7aac4feff369de9271df882825fda732f
Ramaiz Mansoor <rmansoor@pentaho.com>
2024-12-03 14:45:27 +0000

    [PPP-5411] [SP-6708] Backport of PPP-5420: LICENSE.TXT and Splash Page
    Merge pull request #5794 from singletonc/PPP-5420
    
    [PPP-5411] [PPP-5420] Update Pentaho User Console community release h…
    
    (cherry picked from commit 90632bf1b5dd763d862bafbd1423129d2e945b8c)

--- user-console/src/main/java/org/pentaho/mantle/client/commands/AboutCommand.java
+++ user-console/src/main/java/org/pentaho/mantle/client/commands/AboutCommand.java
@@ -27,18 +27,17 @@ import org.pentaho.gwt.widgets.client.dialogs.PromptDialogBox;
 import org.pentaho.gwt.widgets.client.utils.string.StringUtils;
 import org.pentaho.mantle.client.MantleApplication;
 import org.pentaho.mantle.client.messages.Messages;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.TextResource;
 
 import java.util.Date;
 
 public class AboutCommand extends AbstractCommand {
+  private static final String LICENSE_FILE_URL = GWT.getHostPageBaseURL() + "mantle/LICENSE.TXT";
 
-  interface MyTextResource extends ClientBundle {
-    @Source("org/pentaho/mantle/public/license.txt")
-    TextResource myText();
-  }
+  private static final String LICENSE_READ_ERROR =
+    "Error reading license file from server URL: \"" + LICENSE_FILE_URL + "\"";
+
+  private String versionText;
+  private String licenseText;
 
   public AboutCommand() {
   }
@@ -47,9 +46,33 @@ public class AboutCommand extends AbstractCommand {
     performOperation( true );
   }
 
+  /**
+   * Begins callback chain for About window values then opens the window:
+   * -> retrieve version (if not set)
+   * -> retrieve license text (if not set nor error)
+   * -> show the window
+   */
   protected void performOperation( boolean feedback ) {
-    if ( StringUtils.isEmpty( MantleApplication.mantleRevisionOverride ) == false ) {
-      showAboutDialog( MantleApplication.mantleRevisionOverride );
+    retrieveVersionValue();
+  }
+
+
+  //Setters are here to be accessible in RequestCallbacks
+  private void setVersionText( String text ) {
+    versionText = text;
+  }
+
+  private void setLicenseFileText( String text ) {
+    licenseText = text.replace( "\t", "&emsp;" );
+    licenseText = "<pre>" + licenseText + "</pre>";
+  }
+
+  private void retrieveVersionValue() {
+    if ( versionText != null && !versionText.isEmpty() ) {
+      retrieveLicenseFileText();
+    } else if ( StringUtils.isEmpty( MantleApplication.mantleRevisionOverride ) == false ) {
+      setVersionText( MantleApplication.mantleRevisionOverride );
+      retrieveLicenseFileText();
     } else {
       final String url = GWT.getHostPageBaseURL() + "api/version/show"; //$NON-NLS-1$
       RequestBuilder requestBuilder = new RequestBuilder( RequestBuilder.GET, url );
@@ -59,43 +82,58 @@ public class AboutCommand extends AbstractCommand {
         requestBuilder.sendRequest( null, new RequestCallback() {
 
           public void onError( Request request, Throwable exception ) {
-            // showError(exception);
           }
 
           public void onResponseReceived( Request request, Response response ) {
-            showAboutDialog( response.getText() );
+            setVersionText( response.getText() );
+            retrieveLicenseFileText();
           }
         } );
       } catch ( RequestException e ) {
         Window.alert( e.getMessage() );
-        // showError(e);
       }
     }
   }
 
-  public String readTextFile() {
-    MyTextResource resource = GWT.create(MyTextResource.class);
-    String text = resource.myText().getText();
-    text = text.replace("\t", "&emsp;");
-    text = "<pre>" + text + "</pre>";
-    return text;
+  private void retrieveLicenseFileText() {
+    //if we already have a license text value that isn't the ERROR value, don't bother getting the value again.
+    if ( licenseText != null && !licenseText.isEmpty() && licenseText != LICENSE_READ_ERROR ) {
+      showAboutDialog();
+    } else {
+      RequestBuilder licenseFileRequest = new RequestBuilder( RequestBuilder.GET, LICENSE_FILE_URL );
+      licenseFileRequest.setHeader( "If-Modified-Since", "01 Jan 1970 00:00:00 GMT" );
+      licenseFileRequest.setHeader( "accept", "text/plain" );
+
+      try {
+        licenseFileRequest.sendRequest( null, new RequestCallback() {
+
+          public void onError( Request request, Throwable exception ) {
+            setLicenseFileText( LICENSE_READ_ERROR );
+            showAboutDialog();
+          }
+
+          public void onResponseReceived( Request request, Response response ) {
+            setLicenseFileText( response.getText() );
+            showAboutDialog();
+          }
+        } );
+      } catch ( RequestException e ) {
+        Window.alert( e.getMessage() );
+      }
+    }
   }
 
-  private void showAboutDialog( String version ) {
-    @SuppressWarnings( "deprecation" )
-    String licenseInfo = Messages.getString( "licenseInfo", "" + ( ( new Date() ).getYear() + 1900 ) );
+  private void showAboutDialog() {
     String releaseLabel = Messages.getString( "release" );
     PromptDialogBox dialogBox =
-        new PromptDialogBox( null, Messages.getString( "ok" ), null, false, true ); //$NON-NLS-1$
-
+      new PromptDialogBox( null, Messages.getString( "ok" ), null, false, true ); //$NON-NLS-1$
     VerticalPanel aboutContent = new VerticalPanel();
-    aboutContent.setBorderWidth(0);
-    String licenseText = readTextFile();
+    aboutContent.setBorderWidth( 0 );
     aboutContent.setStyleName( "about-splash" );
-    aboutContent.add( new Label( releaseLabel + " " + version ) );
+    aboutContent.add( new Label( releaseLabel + " " + versionText ) );
     aboutContent.add( new HTML( licenseText ) );
     dialogBox.setContent( aboutContent );
-    dialogBox.setPixelSize(700, 400);
+    dialogBox.setPixelSize( 700, 400 );
     dialogBox.center();
   }
 }
