b796e1892900c76b3585686ac5443c7b4ad0ec81
Silvia Fernandes <silvia.fernandes1@hitachivantara.com>
2025-05-15 16:44:11 +0100

    feat: add method to get catalog annotations as string

--- repository/src/main/java/org/pentaho/platform/repository/solution/filebased/MondrianFileObject.java
+++ repository/src/main/java/org/pentaho/platform/repository/solution/filebased/MondrianFileObject.java
@@ -24,7 +24,7 @@ import java.io.InputStream;
 /**
  * FileObject that applies Mondrian schema annotations to the InputStream
  */
-class MondrianFileObject extends DecoratedFileObject {
+public class MondrianFileObject extends DecoratedFileObject {
 
   private final FileObject annotationsFile;
   private final MondrianSchemaAnnotator annotator;
@@ -43,5 +43,9 @@ class MondrianFileObject extends DecoratedFileObject {
         return annotator.getInputStream( super.getInputStream(), annotationsFile.getContent().getInputStream() );
       }
     };
+  }
+
+  public FileObject getAnnotationsFile() {
+    return this.annotationsFile;
   }
 }
