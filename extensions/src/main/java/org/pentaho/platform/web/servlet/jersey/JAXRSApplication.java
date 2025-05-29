466a10b62d42b425b3c40661c5fc0edbfcf250ee
vrajidi <vamshi.rajidi@hitachivantara.com>
2025-05-29 10:28:34 +0530

    [BACKLOG-42168] - Upgrade sun jersey from 1.x to glassfish jersey 3.x

--- extensions/src/main/java/org/pentaho/platform/web/servlet/jersey/JAXRSApplication.java
+++ extensions/src/main/java/org/pentaho/platform/web/servlet/jersey/JAXRSApplication.java
@@ -1,22 +1,14 @@
-/*!
+/*! ******************************************************************************
  *
- * This program is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- * Foundation.
+ * Pentaho
  *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- * or from the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
  *
- *
- * Copyright (c) 2024 Hitachi Vantara. All rights reserved.
- *
- */
+ * Change Date: 2029-07-20
+ ******************************************************************************/
 
 package org.pentaho.platform.web.servlet.jersey;
 
@@ -49,9 +41,11 @@ public class JAXRSApplication extends ResourceConfig {
    */
 
   public JAXRSApplication() {
-    packages( "com.pentaho", "org.pentaho","com.hitachivantara","org.hitachivantara" );
+    packages( "com.pentaho", "org.pentaho", "com.hitachivantara", "org.hitachivantara" );
     register( MultiPartFeature.class );
     register( JacksonFeature.class );
+
+    property( "jersey.config.server.response.setStatusOverSendError", true );
 
     if ( logger.isDebugEnabled() ) {
       property( "jersey.config.server.tracing", "ALL" );
