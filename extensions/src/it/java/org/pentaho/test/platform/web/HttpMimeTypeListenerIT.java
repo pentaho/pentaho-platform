4dbb986ab1757f8d39b92b43343f479a96b6b901
SahithiKommagani6 <sahithi.kommagani@hitachivantara.com>
2025-06-11 17:04:44 +0530

    [BACKLOG-44195]-Fixing Integration tests as part of Tomcat Upgradation in Pentaho-platform

--- extensions/src/it/java/org/pentaho/test/platform/web/HttpMimeTypeListenerIT.java
+++ extensions/src/it/java/org/pentaho/test/platform/web/HttpMimeTypeListenerIT.java
@@ -13,9 +13,9 @@
 
 package org.pentaho.test.platform.web;
 
-import com.mockrunner.mock.web.MockHttpServletRequest;
-import com.mockrunner.mock.web.MockHttpServletResponse;
-import com.mockrunner.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.core.system.StandaloneApplicationContext;
 import org.pentaho.platform.web.servlet.HttpMimeTypeListener;
@@ -55,10 +55,10 @@ public class HttpMimeTypeListenerIT extends BaseTestCase {
     MockHttpServletRequest request = new MockHttpServletRequest();
     MockHttpSession session = new MockHttpSession();
     request.setSession( session );
-    request.setupAddParameter( "solution", "samples" ); //$NON-NLS-1$//$NON-NLS-2$
-    request.setupAddParameter( "path", "steel-wheels/reports" ); //$NON-NLS-1$ //$NON-NLS-2$
-    request.setupAddParameter( "action", "Inventory List.xaction" ); //$NON-NLS-1$ //$NON-NLS-2$
-    request.setupAddParameter( "details", "all" ); //$NON-NLS-1$ //$NON-NLS-2$
+    request.addParameter( "solution", "samples" ); //$NON-NLS-1$//$NON-NLS-2$
+    request.addParameter( "path", "steel-wheels/reports" ); //$NON-NLS-1$ //$NON-NLS-2$
+    request.addParameter( "action", "Inventory List.xaction" ); //$NON-NLS-1$ //$NON-NLS-2$
+    request.addParameter( "details", "all" ); //$NON-NLS-1$ //$NON-NLS-2$
 
     MockHttpServletResponse response = new MockHttpServletResponse();
     HttpMimeTypeListener listener = new HttpMimeTypeListener( request, response, "MyOwnReport" ); //$NON-NLS-1$
