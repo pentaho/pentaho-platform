a559737d6a781b75a6334ce3c107a86ae510f461
Duarte Cunha Leão <duarte.leao@gmail.com>
2025-09-23 19:22:12 +0100

    fix: many broken integration tests due to systemic issues [BACKLOG-45385]
    - missing dependencies
    - restore `ApplicationContextPentahoSystemRegisterer` bean in `repository.spring.xml` test files so that it is possible to use `pen:bean` and `pen:list`
    - Extensions:
      - Before: Tests run: 542, Failures: 24, Errors: 41, Skipped: 2
      - After: Tests run: 1774, Failures: 4, Errors: 4, Skipped: 1
    - Repository:
      - Before: Tests run: 24, Failures: 0, Errors: 0, Skipped: 0
      - After: Tests run: 151, Failures: 1, Errors: 0, Skipped: 0

--- repository/src/test/java/org/pentaho/platform/repository2/unified/DefaultUnifiedRepositoryBase.java
+++ repository/src/test/java/org/pentaho/platform/repository2/unified/DefaultUnifiedRepositoryBase.java
@@ -13,24 +13,6 @@
 
 package org.pentaho.platform.repository2.unified;
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.*;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.List;
-
-import javax.jcr.Repository;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-import javax.jcr.Workspace;
-import javax.jcr.security.AccessControlException;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.api.JackrabbitWorkspace;
 import org.apache.jackrabbit.api.security.authorization.PrivilegeManager;
@@ -81,11 +63,11 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.extensions.jcr.JcrCallback;
 import org.springframework.extensions.jcr.JcrTemplate;
 import org.springframework.extensions.jcr.SessionFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ContextConfiguration;
@@ -93,6 +75,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.Workspace;
+import javax.jcr.security.AccessControlException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Integration test. Tests {@link DefaultUnifiedRepository} and
@@ -297,7 +299,7 @@ public class DefaultUnifiedRepositoryBase implements ApplicationContextAware {
     mp.defineInstance( IUnifiedRepository.class, repo );
     mp.defineInstance( IRepositoryFileAclDao.class, repositoryFileAclDao );
     IUserRoleListService userRoleListService = mock( IUserRoleListService.class );
-    when( userRoleListService.getRolesForUser( any( ITenant.class ), anyString() ) ).thenReturn( Arrays.asList(
+    when( userRoleListService.getRolesForUser( nullable( ITenant.class ), anyString() ) ).thenReturn( Arrays.asList(
         tenantAdminRoleName, AUTHENTICATED_ROLE_NAME ) );
     mp.defineInstance( IUserRoleListService.class, userRoleListService );
     mp.defineInstance( "singleTenantAdminUserName", singleTenantAdminUserName );
@@ -576,6 +578,7 @@ public class DefaultUnifiedRepositoryBase implements ApplicationContextAware {
     repositoryFileAclDao = (IRepositoryFileAclDao) applicationContext.getBean( "repositoryFileAclDao" );
     testUserRoleDao = userRoleDao;
     txnTemplate = (TransactionTemplate) applicationContext.getBean( "jcrTransactionTemplate" );
+
     TestPrincipalProvider.userRoleDao = testUserRoleDao;
     TestPrincipalProvider.adminCredentialsStrategy =
         (CredentialsStrategy) applicationContext.getBean( "jcrAdminCredentialsStrategy" );
