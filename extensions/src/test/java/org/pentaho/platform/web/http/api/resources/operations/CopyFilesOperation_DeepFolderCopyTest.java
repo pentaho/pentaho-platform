2507576c385e1cd3610bc94e04888f6a9adbf8a4
André Jorge <andre.jorge@hitachivantara.com>
2025-12-02 16:37:00 +0000

    fix: nested self-copy into subfolder infinite loop [PPUC-198]

--- extensions/src/test/java/org/pentaho/platform/web/http/api/resources/operations/CopyFilesOperation_DeepFolderCopyTest.java
+++ extensions/src/test/java/org/pentaho/platform/web/http/api/resources/operations/CopyFilesOperation_DeepFolderCopyTest.java
@@ -26,14 +26,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.nullable;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -60,8 +61,6 @@ import static org.mockito.Mockito.verify;
 public class CopyFilesOperation_DeepFolderCopyTest {
   private static IUnifiedRepository repo;
   private static CopyFilesOperation operation;
-  private RepositoryFile from;
-  private RepositoryFile to;
 
   @Before
   public void init() {
@@ -69,30 +68,81 @@ public class CopyFilesOperation_DeepFolderCopyTest {
     operation = mock( CopyFilesOperation.class );
     doReturn( repo ).when( operation ).getRepository();
 
-    from = mockFolder();
-    to = mockFolder();
-
     doReturn( mockFolder() ).when( repo )
       .createFolder( any( Serializable.class ), any( RepositoryFile.class ), nullable( RepositoryFileAcl.class ),
         nullable( String.class ) );
 
-    doCallRealMethod().when( operation ).performFolderDeepCopy( any( RepositoryFile.class ),
-      any( RepositoryFile.class ), anyInt() );
+    doCallRealMethod().when( operation ).performFolderDeepCopy( nullable( RepositoryFile.class ),
+      nullable( RepositoryFile.class ), nullable( Integer.class) );
+
+    doCallRealMethod().when( operation ).performFolderDeepCopy( nullable( RepositoryFile.class ),
+      nullable( RepositoryFile.class ), nullable( Integer.class ), nullable( RepositoryFile.class ) );
   }
 
   @Test( expected = IllegalArgumentException.class )
-  public void deepCopyOfFile_Fails() {
-    RepositoryFile file1 = mockFile();
-    RepositoryFile file2 = mockFile();
-    doCallRealMethod().when( operation ).performFolderDeepCopy( file1, file2, null );
+  public void deepCopyFromFile_Fails() {
+    operation.performFolderDeepCopy( mockFile(), mockFolder(), CopyFilesOperation.DEFAULT_DEEPNESS );
+  }
 
-    operation.performFolderDeepCopy( file1, file2, null );
+  @Test( expected = IllegalArgumentException.class )
+  public void deepCopyToFile_Fails() {
+    operation.performFolderDeepCopy( mockFolder(), mockFile(), CopyFilesOperation.DEFAULT_DEEPNESS );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void deepCopyFromNull_Fails() {
+    operation.performFolderDeepCopy( null, mockFolder(), CopyFilesOperation.DEFAULT_DEEPNESS );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void deepCopyToNull_Fails() {
+    operation.performFolderDeepCopy( mockFolder(), null, CopyFilesOperation.DEFAULT_DEEPNESS );
+  }
+
+  @Test
+  public void deepCopyDeepnessNull() {
+    int numberOfFolders = 3;
+    int filesInEachFolder = 3;
+    List<RepositoryFile> folders = listOfMockedFolders( numberOfFolders );
+    RepositoryFile from = mockFolder();
+    mockRequest( from, folders );
+    RepositoryFile to = mockFolder();
+
+    for ( RepositoryFile folder : folders ) {
+      mockRequest( folder, listOfMockedFiles( filesInEachFolder ) );
+    }
+
+    operation.performFolderDeepCopy( from, to, null );
+
+    // one stands for root folder.
+    performVerification( numberOfFolders, numberOfFolders * filesInEachFolder );
+  }
+
+  @Test
+  public void deepCopyDeepnessNegative() {
+    int numberOfFolders = 3;
+    int filesInEachFolder = 3;
+    List<RepositoryFile> folders = listOfMockedFolders( numberOfFolders );
+    RepositoryFile from = mockFolder();
+    mockRequest( from, folders );
+    RepositoryFile to = mockFolder();
+
+    for ( RepositoryFile folder : folders ) {
+      mockRequest( folder, listOfMockedFiles( filesInEachFolder ) );
+    }
+
+    operation.performFolderDeepCopy( from, to, -1 );
+
+    // one stands for root folder.
+    performVerification( numberOfFolders, numberOfFolders * filesInEachFolder );
   }
 
   @Test
   public void one_LevelDeepness() {
     List<RepositoryFile> files = listOfMockedFiles( 3 );
+    RepositoryFile from = mockFolder();
     mockRequest( from, files );
+    RepositoryFile to = mockFolder();
 
     operation.performFolderDeepCopy( from, to, CopyFilesOperation.DEFAULT_DEEPNESS );
 
@@ -101,27 +151,157 @@ public class CopyFilesOperation_DeepFolderCopyTest {
 
   @Test
   public void nestedSelfCopy() {
-    List<RepositoryFile> children = listOfMockedFiles( 3 );
+    // Simulate copying /a into /a
+    // Source folder /a has one subfolder: /a/b
+    // When copying /a into /a, the new /a/a subfolder should be skipped to avoid infinite recursion
+    // /a/b should be copied into /a/a/b
+    // all files should be copied
 
-    // folders `from` and `to` are the same, using `folder` avoid further confusion
-    RepositoryFile folder = mockFolder();
-
-    // the nested root folder was previously created, to contain the content of `folder`
+    // mock "from" source folder structure
+    RepositoryFile from = mockFolderWithPath( "/a" );
+    RepositoryFile subfolder = mockFolderWithPath( "/a/b" );
+    // mock "to" destination folder created apriori, as per actual use case
     // for use case details see `CopyFileOperation` functions: `copyRenameMode` and `copyNoOverrideMode`
-    children.add( folder );
+    RepositoryFile to = mockFolderWithPath( "/a/a" );
 
-    mockRequest( folder, children );
+    // mock "from" source folder children, includes new "to" folder /a/a created before performFolderDeepCopy call
+    mockRequest( from, List.of( to, subfolder, mockFile() ) );
 
-    operation.performFolderDeepCopy( folder, folder, CopyFilesOperation.DEFAULT_DEEPNESS );
+    // mock subfolder /a/b children
+    mockRequest( subfolder, listOfMockedFiles( 2 ) );
 
-    // both the `selfCopyFolder` and the nested `selfCopy` folders
-    verify( operation, times( 1 ) )
-      .performFolderDeepCopy( any( RepositoryFile.class ), any( RepositoryFile.class ), anyInt() );
-
-    verify( repo, never() )
+    // mock /a/a/b child creation via repo.createFolder() call
+    RepositoryFile createdSubfolder = mockFolderWithPath( "/a/a/b" );
+    doAnswer( invocation -> {
+      RepositoryFile createdFolder = invocation.getArgument( 1 );
+      if ( createdFolder.getPath().equals( "/a/b" ) ) {
+        // add newly created /a/a/b folder as child of /a/a to simulate actual structure
+        mockRequest( to, List.of( createdSubfolder ) );
+        return createdSubfolder;
+      }
+      return mockFolder();
+    } ).when( repo )
       .createFolder( any( Serializable.class ), any( RepositoryFile.class ), nullable( RepositoryFileAcl.class ),
         nullable( String.class ) );
 
+    operation.performFolderDeepCopy( from, to, CopyFilesOperation.DEFAULT_DEEPNESS );
+
+    // Executes performFolderDeepCopy() for:
+    // - Root call (1): /a -> /a/a
+    // - subfolder (1): /a/b -> /a/a/b
+    // - to (0): /a/a folder is the new child folder that does not belong to the original /a folder structure,
+    //            skip to avoid infinite recursion
+    // Total: 2 calls
+    verify( operation, times( 2 ) )
+      .performFolderDeepCopy( any( RepositoryFile.class ), any( RepositoryFile.class ), anyInt(),
+        any( RepositoryFile.class ) );
+    verify( operation, times( 1 ) ).performFolderDeepCopy(
+      argThat( arg -> arg.getPath().equals( "/a" ) ),
+      argThat( arg -> arg.getPath().equals( "/a/a" ) ),
+      anyInt(), argThat( arg -> arg.getPath().equals( "/a/a" ) ) );
+    verify( operation, times( 1 ) ).performFolderDeepCopy(
+      argThat( arg -> arg.getPath().equals( "/a/b" ) ),
+      any( RepositoryFile.class ), // /a/b/a/b
+      anyInt(), argThat( a -> a.getPath().equals( "/a/a" ) ) );
+
+    // Should create one folder:
+    // - ancestorSubfolder (1): /a/b -> /a/a/b
+    // Total: 1 folder
+    verify( repo, times( 1 ) )
+      .createFolder( any( Serializable.class ), any( RepositoryFile.class ), nullable( RepositoryFileAcl.class ),
+        nullable( String.class ) );
+    verify( repo, times( 1 ) ).createFolder(
+      eq( to.getId() ),
+      argThat( arg -> arg.getPath().equals( "/a/b" ) ),
+      nullable( RepositoryFileAcl.class ), nullable( String.class ) );
+
+    // Should create files:
+    // - 1 file from /a folder: /a -> /a/a
+    // - 2 files from /a/b folder: /a/b -> /a/a/b
+    // Total: 3 files
+    verify( repo, times( 3 ) ).createFile( anyString(), any( RepositoryFile.class ), nullable(
+      IRepositoryFileData.class ), nullable( String.class ) );
+    verify( repo, times( 1 ) ).createFile(
+      eq( to.getId() ), // /a/a
+      any( RepositoryFile.class ),
+      nullable( IRepositoryFileData.class ), nullable( String.class ) );
+    verify( repo, times( 2 ) ).createFile(
+      eq( createdSubfolder.getId() ), // /a/a/b
+      any( RepositoryFile.class ),
+      nullable( IRepositoryFileData.class ), nullable( String.class ) );
+  }
+
+  @Test
+  public void nestedSelfCopyIntoSubfolder() {
+    // Simulate copying /a into /a/b
+    // Source folder /a has one subfolder: /a/b
+    // When copying /a into /a/b, the new /a/b/a subfolder should be skipped to avoid infinite recursion
+    // /a/b should be copied into /a/b/a/b
+    // all files should be copied
+
+    // mock "from" source folder structure
+    RepositoryFile from = mockFolderWithPath( "/a" );
+    RepositoryFile ancestorSubfolder = mockFolderWithPath( "/a/b" );
+
+    // mock "from" source folder children
+    mockRequest( from, List.of( ancestorSubfolder, mockFile() ) );
+
+    // mock "to" destination folder created apriori, as per actual use case
+    // for use case details see `CopyFileOperation` functions: `copyRenameMode` and `copyNoOverrideMode`
+    RepositoryFile to = mockFolderWithPath( "/a/b/a" );
+
+    // mock ancestorSubfolder children, includes new "to" folder /a/b/a created before performFolderDeepCopy call
+    mockRequest( ancestorSubfolder, List.of( to, mockFile(), mockFile() ) );
+
+    // mock /a/b/a/b child creation via repo.createFolder() call
+    RepositoryFile createdSubfolderB = mockFolderWithPath( "/a/b/a/b" );
+    doAnswer( invocation -> {
+      RepositoryFile createdFolder = invocation.getArgument( 1 );
+      if ( createdFolder.getPath().equals( "/a/b" ) ) {
+        // add newly created /a/b/a/b folder as child of /a/b/a to simulate actual structure
+        mockRequest( to, List.of( createdSubfolderB ) );
+        return createdSubfolderB;
+      }
+      return mockFolder();
+    } ).when( repo )
+      .createFolder( any( Serializable.class ), any( RepositoryFile.class ), nullable( RepositoryFileAcl.class ),
+        nullable( String.class ) );
+
+    operation.performFolderDeepCopy( from, to, CopyFilesOperation.DEFAULT_DEEPNESS );
+
+    // Should process children of:
+    // - Root call (1): /a -> /a/b/a
+    // - ancestorSubFolder (1): /a/b -> /a/b/a/b
+    // - to (0) - /a/b/a folder is the new child folder that does not belong to the original /a folder structure,
+    //            skip to avoid infinite recursion
+    // Total: 2 calls
+    verify( operation, times( 2 ) )
+      .performFolderDeepCopy( any( RepositoryFile.class ), any( RepositoryFile.class ), anyInt(),
+        any( RepositoryFile.class ) );
+    verify( operation, times( 1 ) ).performFolderDeepCopy(
+      argThat( arg -> arg.getPath().equals( "/a" ) ),
+      argThat( arg -> arg.getPath().equals( "/a/b/a" ) ),
+      anyInt(), argThat( arg -> arg.getPath().equals( "/a/b/a" ) ) );
+    verify( operation, times( 1 ) ).performFolderDeepCopy(
+      argThat( arg -> arg.getPath().equals( "/a/b" ) ),
+      any( RepositoryFile.class ), // /a/b/a/b
+      anyInt(), argThat( a -> a.getPath().equals( "/a/b/a" ) ) );
+
+    // Should create folder:
+    // - ancestorSubfolder (1): /a/b -> /a/b/a/b
+    // Total: 1 folder
+    verify( repo, times( 1 ) )
+      .createFolder( any( Serializable.class ), any( RepositoryFile.class ), nullable( RepositoryFileAcl.class ),
+        nullable( String.class ) );
+    verify( repo, times( 1 ) ).createFolder(
+      eq( to.getId() ),
+      argThat( arg -> arg.getPath().equals( "/a/b" ) ),
+      nullable( RepositoryFileAcl.class ), nullable( String.class ) );
+
+    // Should create files:
+    // - 1 file from sourceChildren (1) - from /a to /a/b/a
+    // - 2 files from ancestorSubfolder (2) - from /a/b to /a/b/a/b
+    // Total: 3 files
     verify( repo, times( 3 ) ).createFile( anyString(), any( RepositoryFile.class ), nullable(
       IRepositoryFileData.class ), nullable( String.class ) );
   }
@@ -131,7 +311,9 @@ public class CopyFilesOperation_DeepFolderCopyTest {
     int numberOfFolders = 3;
     int filesInEachFolder = 3;
     List<RepositoryFile> folders = listOfMockedFolders( numberOfFolders );
+    RepositoryFile from = mockFolder();
     mockRequest( from, folders );
+    RepositoryFile to = mockFolder();
 
     for ( RepositoryFile folder : folders ) {
       mockRequest( folder, listOfMockedFiles( filesInEachFolder ) );
@@ -154,7 +336,9 @@ public class CopyFilesOperation_DeepFolderCopyTest {
     List<RepositoryFile> foldersFiveLevel = listOfMockedFolders( foldersInEachFolderNotLastLevel );
     List<RepositoryFile> foldersSixLevel = listOfMockedFolders( foldersInEachFolderNotLastLevel );
 
+    RepositoryFile from = mockFolder();
     mockRequest( from, foldersSecondLevel );
+    RepositoryFile to = mockFolder();
 
     for ( RepositoryFile folder : foldersSecondLevel ) {
       mockRequest( folder, foldersThirdLevel );
@@ -187,7 +371,8 @@ public class CopyFilesOperation_DeepFolderCopyTest {
   private void performVerification( int newFolders, int newFiles ) {
     // 1 stands for first method-call
     verify( operation, times( newFolders + 1 ) )
-      .performFolderDeepCopy( any( RepositoryFile.class ), any( RepositoryFile.class ), anyInt() );
+      .performFolderDeepCopy( nullable( RepositoryFile.class ), nullable( RepositoryFile.class ),
+        nullable( Integer.class ), nullable( RepositoryFile.class ) );
 
     verify( repo, times( newFolders ) )
       .createFolder( any( Serializable.class ), any( RepositoryFile.class ), nullable( RepositoryFileAcl.class ),
@@ -250,6 +435,13 @@ public class CopyFilesOperation_DeepFolderCopyTest {
 
   public static RepositoryFile mockFolder() {
     return mockFolder( generateID() );
+  }
+
+  public static RepositoryFile mockFolderWithPath( String path ) {
+    RepositoryFile folder = mockFolder( generateID() );
+    doReturn( path ).when( folder ).getPath();
+
+    return folder;
   }
 
   private static String generateID() {
