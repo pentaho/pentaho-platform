4eb7f075b179a6bf006bd9eb5d380a150bbbf0bc
Duarte Cunha Leão <duarte.leao@gmail.com>
2026-02-05 18:06:33 +0000

    feat(auth): simulation action role binding rule [PPUC-183]

--- repository/src/main/java/org/pentaho/platform/security/policy/rolebased/ActionRoleBindingAuthorizationRule.java
+++ repository/src/main/java/org/pentaho/platform/security/policy/rolebased/ActionRoleBindingAuthorizationRule.java
@@ -13,21 +13,16 @@
 package org.pentaho.platform.security.policy.rolebased;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
-import org.pentaho.platform.api.engine.security.authorization.AuthorizationDecisionReportingMode;
-import org.pentaho.platform.api.engine.security.authorization.IAuthorizationContext;
-import org.pentaho.platform.api.engine.security.authorization.IAuthorizationRequest;
 import org.pentaho.platform.api.engine.security.authorization.IAuthorizationRole;
-import org.pentaho.platform.api.engine.security.authorization.decisions.IAuthorizationDecision;
-import org.pentaho.platform.api.engine.security.authorization.resources.IResourceAuthorizationRequest;
-import org.pentaho.platform.engine.security.authorization.core.rules.AbstractAuthorizationRule;
 import org.springframework.util.Assert;
 
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
-public class ActionRoleBindingAuthorizationRule extends AbstractAuthorizationRule<IAuthorizationRequest> {
+/***
+ * An implementation of the {@link AbstractActionRoleBindingAuthorizationRule} that retrieves role bindings from a
+ * provided {@link IRoleAuthorizationPolicyRoleBindingDao}.
+ */
+public class ActionRoleBindingAuthorizationRule extends AbstractActionRoleBindingAuthorizationRule {
 
   @NonNull
   private final IRoleAuthorizationPolicyRoleBindingDao roleBindingDao;
@@ -39,61 +34,8 @@ public class ActionRoleBindingAuthorizationRule extends AbstractAuthorizationRul
     this.roleBindingDao = roleBindingDao;
   }
 
-  @NonNull
   @Override
-  public Class<IAuthorizationRequest> getRequestType() {
-    return IAuthorizationRequest.class;
-  }
-
-  /**
-   * Authorizes the given request by checking if the principal has the necessary role-action bindings
-   * for the requested action.
-   * <p>
-   * If the principal is a user, this rule checks if any of the user's roles are bound to the action being requested.
-   * If at least one role is bound to the action, the request is granted; otherwise, it is denied.
-   * <p>
-   * If the principal is a role, this rule checks if that specific role is bound to the action being requested.
-   * If it is, the request is granted; otherwise, it is denied.
-   * <p>
-   * If the request is a resource-specific authorization request, this rule abstains from making a decision.
-   *
-   * @param request The authorization request to evaluate.
-   * @param context The authorization context providing additional information for decision-making.
-   * @return An {@code Optional} containing the authorization decision, or empty if the rule abstains.
-   * @throws IllegalArgumentException if the request or context is {@code null}.
-   */
-  @NonNull
-  @Override
-  public Optional<IAuthorizationDecision> authorize( @NonNull IAuthorizationRequest request,
-                                                     @NonNull IAuthorizationContext context ) {
-    // Rule only applies to self or resource-general requests
-    if ( request instanceof IResourceAuthorizationRequest ) {
-      return abstain();
-    }
-
-    // <Role, Action>
-    // Which role(s) actually are bound to the action?
-    String actionName = request.getAction().getName();
-    AuthorizationDecisionReportingMode decisionReportingMode = context.getOptions().getDecisionReportingMode();
-
-    Set<IAuthorizationRole> rolesWithBinding = new LinkedHashSet<>();
-
-    for ( IAuthorizationRole role : request.getAllRoles() ) {
-      if ( hasRoleActionBinding( role, actionName ) ) {
-        rolesWithBinding.add( role );
-
-        if ( decisionReportingMode == AuthorizationDecisionReportingMode.SETTLED ) {
-          // If we are in first-match mode, we can stop checking further roles.
-          break;
-        }
-      }
-    }
-
-    // Grant or deny (denies when empty set).
-    return Optional.of( new ActionRoleBindingAuthorizationDecision( request, rolesWithBinding ) );
-  }
-
-  private boolean hasRoleActionBinding( @NonNull IAuthorizationRole role, @NonNull String actionName ) {
+  protected boolean hasRoleActionBinding( @NonNull IAuthorizationRole role, @NonNull String actionName ) {
     return roleBindingDao.getBoundLogicalRoleNames( List.of( role.getName() ) ).contains( actionName );
   }
 }
