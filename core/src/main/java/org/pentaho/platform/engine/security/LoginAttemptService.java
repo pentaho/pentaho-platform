b226da507c0c37a1ce53214868a1d1cd50d864e0
Sergio Ribeiro <sergio.ribeiro@hitachivantara.com>
2026-03-12 21:13:53 +0000

    chore(BISERVER-14985): some optimizations and improvements
    Also improved unit testing for the class (functionalities and coverage)

--- core/src/main/java/org/pentaho/platform/engine/security/LoginAttemptService.java
+++ core/src/main/java/org/pentaho/platform/engine/security/LoginAttemptService.java
@@ -12,51 +12,76 @@
 
 package org.pentaho.platform.engine.security;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.pentaho.platform.api.security.ILoginAttemptService;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
 
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.Map;
 
 public class LoginAttemptService implements ILoginAttemptService {
 
-  @VisibleForTesting
   protected LoadingCache<String, Integer> attemptsCache;
-  @VisibleForTesting
   protected final int maxAttempt;
 
   public LoginAttemptService( int maxAttempt, int cacheMinutes ) {
+    if ( maxAttempt < 0L ) {
+      throw new IllegalArgumentException( "Maximum attempts cannot be negative" );
+    }
+
+    if ( cacheMinutes < 0L ) {
+      throw new IllegalArgumentException( "Cache minutes cannot be negative" );
+    }
+
     this.maxAttempt = maxAttempt;
     attemptsCache = CacheBuilder.newBuilder().
-      expireAfterWrite( cacheMinutes, TimeUnit.MINUTES ).build( new CacheLoader<String, Integer>() {
-        public Integer load( String key ) {
+      expireAfterWrite( cacheMinutes, TimeUnit.MINUTES ).build( new CacheLoader<>() {
+        @Override
+        public @Nonnull Integer load( @Nonnull String key ) {
           return 0;
         }
       } );
   }
 
+  /**
+   * Called when a login attempt succeeds.
+   * It clears the failed attempts counter for the key.
+   *
+   * @param key the key (IP address or username) that successfully logged in.
+   */
   @Override
   public void loginSucceeded( String key ) {
     attemptsCache.invalidate( key );
   }
 
+  /**
+   * Called when a login attempt fails.
+   * It increments the failed attempt counter for the key.
+   *
+   * @param key the key (IP address or username) that failed to log in.
+   */
   @Override
   public void loginFailed( String key ) {
-    int attempts = 0;
+    int attempts;
     try {
       attempts = attemptsCache.get( key );
     } catch ( ExecutionException e ) {
       attempts = 0;
     }
-    attempts++;
-    attemptsCache.put( key, attempts );
+
+    attemptsCache.put( key, ++attempts );
   }
 
+  /**
+   * Checks if the given key is currently blocked due to too many failed login attempts.
+   *
+   * @param key the key (IP address or username) to check.
+   * @return true if the key is blocked, false otherwise.
+   */
   @Override
   public boolean isBlocked( String key ) {
     try {
@@ -67,32 +92,42 @@ public class LoginAttemptService implements ILoginAttemptService {
   }
 
   /**
-   * Get all entries in the attempts cache
-   * @return Map of all cache entries
+   * Get all entries in the attempts cache as an unmodifiable Map.
+   * Each Map entry represents a key and its corresponding number of failed login attempts.
+   *
+   * @return an unmodifiable Map of all cache entries
    */
+  @Override
   public Map<String, Integer> getAllAttempts() {
-    return attemptsCache.asMap();
+    return Collections.unmodifiableMap( attemptsCache.asMap() );
   }
 
   /**
-   * Remove a specific key from the cache
-   * @param key the key to remove
+   * Remove a specific key from the cache.
+   * The key will be unblocked whatever its former state.
+   *
+   * @param key the key to remove.
    */
+  @Override
   public void removeFromCache( String key ) {
     attemptsCache.invalidate( key );
   }
 
   /**
-   * Clear all entries from the cache
+   * Clear all entries from the cache.
+   * All keys will be unblocked whatever their former state.
    */
+  @Override
   public void clearCache() {
     attemptsCache.invalidateAll();
   }
 
   /**
-   * Get the maximum number of allowed attempts
-   * @return maximum attempts
+   * Get the maximum number of failed login attempts allowed before blocking a key.
+   *
+   * @return maximum number of failed login attempts allowed before blocking a key.
    */
+  @Override
   public int getMaxAttempt() {
     return maxAttempt;
   }
--- core/src/test/java/org/pentaho/platform/engine/security/LoginAttemptServiceTest.java
+++ core/src/test/java/org/pentaho/platform/engine/security/LoginAttemptServiceTest.java
@@ -11,10 +11,8 @@
  ******************************************************************************/
 
 
-
 package org.pentaho.platform.engine.security;
 
-import com.google.common.cache.LoadingCache;
 import org.junit.Before;
 import org.junit.Test;
 import org.pentaho.platform.api.security.ILoginAttemptService;
@@ -23,53 +21,179 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class LoginAttemptServiceTest {
 
   private ILoginAttemptService loginAttemptService;
   private String key;
 
+  /**
+   * The following values are the defaults to be used under this unit test class.
+   */
+  private static final int MAX_ATTEMPTS = 10;
+  private static final int CACHE_MINUTES = 5;
+  /**
+   * Number of attempts that should trigger a block.
+   * This value is greater than {@link #MAX_ATTEMPTS} so that the blocking behaviour is exercised.
+   */
+  private static final int ATTEMPTS_TO_LOCK = MAX_ATTEMPTS + 5;
+
   @Before
   public void setUp() throws Exception {
-    int maxAttempt = 10;
-    int cacheMinutes = 24 * 60;
-    loginAttemptService = new LoginAttemptService( maxAttempt, cacheMinutes );
+    loginAttemptService = new LoginAttemptService( MAX_ATTEMPTS, CACHE_MINUTES );
     key = UUID.randomUUID().toString();
   }
 
   @Test
   public void testLoginAttemptServiceConstructorParameters() {
-    int maxAttempt = 3;
-    int cacheMinutes = 60;
-    ILoginAttemptService loginAttemptService = new LoginAttemptService( maxAttempt, cacheMinutes );
-    assertEquals( maxAttempt, ((LoginAttemptService)loginAttemptService).maxAttempt );
+    // Test something different from the test class default values, to ensure parameters are being set correctly
+    int maxAttempts = MAX_ATTEMPTS + 1;
+    LoginAttemptService las = new LoginAttemptService( maxAttempts, CACHE_MINUTES );
+    assertEquals( maxAttempts, las.getMaxAttempt() );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void testCacheMinutesShouldNotBeNegative() {
+    LoginAttemptService las = new LoginAttemptService( MAX_ATTEMPTS, -1 );
+    fail( "An IllegalArgumentException should have been thrown for negative cache minutes" );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void testMaxAttemptsShouldNotBeNegative() {
+    LoginAttemptService las = new LoginAttemptService( -1, CACHE_MINUTES );
+    fail( "An IllegalArgumentException should have been thrown for negative maximum attempts" );
   }
 
   @Test
-  public void testLoginFailed() throws ExecutionException {
-    Integer attempts = 30;
-    testLoginFailed( attempts );
+  public void testLoginFailedAttempts() throws ExecutionException {
+    // Fail login once
+    simulateFailedLoginAttempts( key, 1 );
+    // The fail count should be 1
+    assertEquals( Integer.valueOf( 1 ), loginAttemptService.getAllAttempts().get( key ) );
+
+    // Fail two more times
+    simulateFailedLoginAttempts( key, 2 );
+    // The fail count should be 3 (1+2)
+    assertEquals( Integer.valueOf( 3 ), loginAttemptService.getAllAttempts().get( key ) );
+
+    // Fail three more times
+    simulateFailedLoginAttempts( key, 3 );
+    // The fail count should be 6 (3+3)
+    assertEquals( Integer.valueOf( 6 ), loginAttemptService.getAllAttempts().get( key ) );
   }
 
-  private void testLoginFailed( Integer attempts ) throws ExecutionException {
-
+  /**
+   * Helper method to simulate failed login attempts with a specified number of attempts.
+   *
+   * @param key      the key to use for the login attempts
+   * @param attempts number of failed login attempts to simulate
+   */
+  private void simulateFailedLoginAttempts( String key, Integer attempts ) {
     for ( int i = 1; i <= attempts; i++ ) {
       loginAttemptService.loginFailed( key );
-      assertEquals( Integer.valueOf( i ), ( ( (LoginAttemptService) loginAttemptService ).attemptsCache ).get( key ) );
     }
   }
 
   @Test
-  public void testLoginSucceeded() throws ExecutionException {
-    testLoginFailed();
-    loginAttemptService.loginSucceeded( key );
-    assertEquals( Integer.valueOf( 0 ), ( ( (LoginAttemptService) loginAttemptService ).attemptsCache ).get( key ) );
+  public void testIsBlocked() throws ExecutionException {
+    // Initially, the key should not be blocked
+    assertFalse( loginAttemptService.isBlocked( key ) );
+
+    // Block the key with failed attempts
+    simulateFailedLoginAttempts( key, ATTEMPTS_TO_LOCK );
+
+    // The key should, now, be blocked
+    assertTrue( loginAttemptService.isBlocked( key ) );
   }
 
   @Test
-  public void testIsBlocked() throws ExecutionException {
-    assertEquals( false, loginAttemptService.isBlocked( key ) );
-    testLoginFailed( 30 );
-    assertEquals( true, loginAttemptService.isBlocked( key ) );
+  public void testLoginSucceeded() throws ExecutionException {
+    // Block the key with failed attempts
+    simulateFailedLoginAttempts( key, ATTEMPTS_TO_LOCK );
+    // The key should, now, be blocked
+    assertTrue( loginAttemptService.isBlocked( key ) );
+
+    // Successful login should reset the attempts count for the key
+    loginAttemptService.loginSucceeded( key );
+    assertFalse( loginAttemptService.isBlocked( key ) );
+  }
+
+  @Test
+  public void testRemoveFromCache() throws ExecutionException {
+    // Initially, the key should not be blocked
+    assertFalse( loginAttemptService.isBlocked( key ) );
+
+    // Block the key with failed attempts
+    simulateFailedLoginAttempts( key, ATTEMPTS_TO_LOCK );
+
+    // Have some other keys in the cache to ensure only the specified key is removed
+    int numberOfOtherKeys = 6;
+    for ( int i = 1; i <= numberOfOtherKeys; i++ ) {
+      simulateFailedLoginAttempts( key + i, i );
+    }
+
+    // The key should, now, be blocked
+    assertTrue( loginAttemptService.isBlocked( key ) );
+
+
+    // Remove the key from the cache
+    loginAttemptService.removeFromCache( key );
+
+
+    // The key should not exist in the cache
+    assertNull( loginAttemptService.getAllAttempts().get( key ) );
+    // The key should not be blocked
+    assertFalse( loginAttemptService.isBlocked( key ) );
+    // The key should, now, exist but been zeroed out in the cache
+    assertEquals( Integer.valueOf( 0 ), loginAttemptService.getAllAttempts().get( key ) );
+
+    // All other existing keys should maintain their former values
+    for ( int i = 1; i <= numberOfOtherKeys; i++ ) {
+      assertEquals( Integer.valueOf( i ), loginAttemptService.getAllAttempts().get( key + i ) );
+    }
+  }
+
+  @Test
+  public void testClearCache() throws ExecutionException {
+    // Initially, the key should not be blocked
+    assertFalse( loginAttemptService.isBlocked( key ) );
+
+    // Block the key with failed attempts
+    simulateFailedLoginAttempts( key, ATTEMPTS_TO_LOCK );
+
+    // Have some other keys in the cache to ensure only the specified key is removed
+    int numberOfOtherKeys = 6;
+    for ( int i = 1; i <= numberOfOtherKeys; i++ ) {
+      simulateFailedLoginAttempts( key + i, i );
+    }
+
+    // The key should, now, be blocked
+    assertTrue( loginAttemptService.isBlocked( key ) );
+
+
+    // Clear the cache
+    loginAttemptService.clearCache();
+
+
+    // The key should not exist in the cache
+    assertNull( loginAttemptService.getAllAttempts().get( key ) );
+    // The key should not be blocked
+    assertFalse( loginAttemptService.isBlocked( key ) );
+    // The key should, now, exist but been zeroed out in the cache
+    assertEquals( Integer.valueOf( 0 ), loginAttemptService.getAllAttempts().get( key ) );
+
+    // All other existing keys should have been reset also
+    for ( int i = 1; i <= numberOfOtherKeys; i++ ) {
+      // The key should not exist in the cache
+      assertNull( loginAttemptService.getAllAttempts().get( key + i ) );
+      // The key should not be blocked
+      assertFalse( loginAttemptService.isBlocked( key + i ) );
+      // The key should, now, exist but been zeroed out in the cache
+      assertEquals( Integer.valueOf( 0 ), loginAttemptService.getAllAttempts().get( key + i ) );
+    }
   }
 }
