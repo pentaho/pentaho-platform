ead60314ac2ff1f8bdfaf2007a38b45bfecd3423
joana-fb <132680682+joana-fb@users.noreply.github.com>
2026-05-11 18:09:59 +0100

    feat(BISERVER-14985): new features for brute force administration (#6200) (#6233)
    Backported for: SP-7263
    
    Co-authored-by: Sergio Ribeiro <sergio.ribeiro@hitachivantara.com>

--- core/src/main/java/org/pentaho/platform/engine/security/LoginAttemptService.java
+++ core/src/main/java/org/pentaho/platform/engine/security/LoginAttemptService.java
@@ -12,55 +12,120 @@
 
 package org.pentaho.platform.engine.security;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.pentaho.platform.api.security.ILoginAttemptService;
 
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class LoginAttemptService implements ILoginAttemptService {
 
-  @VisibleForTesting
   protected LoadingCache<String, Integer> attemptsCache;
-  @VisibleForTesting
   protected final int maxAttempt;
 
   public LoginAttemptService( int maxAttempt, int cacheMinutes ) {
+    if ( maxAttempt < 0L ) {
+      throw new IllegalArgumentException( "Maximum attempts cannot be negative" );
+    }
+
+    if ( cacheMinutes < 0L ) {
+      throw new IllegalArgumentException( "Cache minutes cannot be negative" );
+    }
+
     this.maxAttempt = maxAttempt;
     attemptsCache = CacheBuilder.newBuilder().
-      expireAfterWrite( cacheMinutes, TimeUnit.MINUTES ).build( new CacheLoader<String, Integer>() {
-        public Integer load( String key ) {
+      expireAfterWrite( cacheMinutes, TimeUnit.MINUTES ).build( new CacheLoader<>() {
+        @Override
+        public @Nonnull Integer load( @Nonnull String key ) {
           return 0;
         }
       } );
   }
 
+  /**
+   * Called when a login attempt succeeds.
+   * It clears the failed attempts counter for the key.
+   *
+   * @param key the key (IP address or username) that successfully logged in.
+   */
   @Override
   public void loginSucceeded( String key ) {
     attemptsCache.invalidate( key );
   }
 
+  /**
+   * Called when a login attempt fails.
+   * It increments the failed attempt counter for the key.
+   *
+   * @param key the key (IP address or username) that failed to log in.
+   */
   @Override
   public void loginFailed( String key ) {
-    int attempts = 0;
+    int attempts;
     try {
       attempts = attemptsCache.get( key );
     } catch ( ExecutionException e ) {
       attempts = 0;
     }
-    attempts++;
-    attemptsCache.put( key, attempts );
+
+    attemptsCache.put( key, ++attempts );
   }
 
+  /**
+   * Checks if the given key is currently blocked due to too many failed login attempts.
+   *
+   * @param key the key (IP address or username) to check.
+   * @return true if the key is blocked, false otherwise.
+   */
   @Override
   public boolean isBlocked( String key ) {
-    try {
-      return attemptsCache.get( key ) >= maxAttempt;
-    } catch ( ExecutionException e ) {
-      return false;
-    }
+    Integer cacheValue = attemptsCache.getIfPresent( key );
+    return ( cacheValue != null ) && ( cacheValue >= maxAttempt );
+  }
+
+  /**
+   * Get all entries in the attempts cache as an unmodifiable Map.
+   * Each Map entry represents a key and its corresponding number of failed login attempts.
+   *
+   * @return an unmodifiable Map of all cache entries
+   */
+  @Override
+  public Map<String, Integer> getAllAttempts() {
+    return Collections.unmodifiableMap( attemptsCache.asMap() );
+  }
+
+  /**
+   * Remove a specific key from the cache.
+   * The key will be unblocked whatever its former state.
+   *
+   * @param key the key to remove.
+   */
+  @Override
+  public void removeFromCache( String key ) {
+    attemptsCache.invalidate( key );
+  }
+
+  /**
+   * Clear all entries from the cache.
+   * All keys will be unblocked whatever their former state.
+   */
+  @Override
+  public void clearCache() {
+    attemptsCache.invalidateAll();
+  }
+
+  /**
+   * Get the maximum number of failed login attempts allowed before blocking a key.
+   *
+   * @return maximum number of failed login attempts allowed before blocking a key.
+   */
+  @Override
+  public int getMaxAttempt() {
+    return maxAttempt;
   }
 }
