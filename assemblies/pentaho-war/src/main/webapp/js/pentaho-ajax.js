a8d3ad48199e1004ba822feb5a2f61190cb9963c
Joao Franca <joao.franca@pentaho.com>
2026-07-18 11:54:21 +0100

    DEVO-14154: updated license and copyright headers

--- assemblies/pentaho-war/src/main/webapp/js/pentaho-ajax.js
+++ assemblies/pentaho-war/src/main/webapp/js/pentaho-ajax.js
@@ -2,13 +2,14 @@
  *
  * Pentaho
  *
- * Copyright (C) 2024 by Pentaho, LLC : http://www.pentaho.com
+ * Copyright (C) 2024 - 2026 by Pentaho Canada Inc. : http://www.pentaho.com
  *
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file.
  *
- * Change Date: 2029-07-20
+ * Change Date: 2030-06-15
  ******************************************************************************/
+
 
 document.write("<!-- webcontext.js is written by PentahoWebContextFilter. Content of this file contains values of CONTEXT_PATH and FULL_QUALIFIED_URL --> \n");
 document.write("<script type='text/javascript' src='webcontext.js'></script>");
